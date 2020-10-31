77985a5829bebec5c2537ef54441223d0938c862
Tomas Turecek <tturecek@utb.cz>
2020-10-31 05:26:02 +0100

    doctest added

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+"""
+This is the "example" module.
+
+The example module supplies one function, factorial().  For example,
+
+>>> compute(3)
+6
+"""
+
 def compute(x):
     """
     Functon compute returns evaluation of expression
