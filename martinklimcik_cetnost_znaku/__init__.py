3a32e463bb5b34fe3611a09c60f1825e1f0fea43
Martin Klimcik <martin.klimcik@gmail.com>
2020-10-31 21:25:26 +0100

    adjusted for flake8 standards

---
+++ martinklimcik_cetnost_znaku/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package for DV project.
+
+Method char_frequency takes an input text
+and prints out stats regarding character frequency
+"""
+from .char_freq import char_frequency
+
+__all__ = ['char_frequency']
