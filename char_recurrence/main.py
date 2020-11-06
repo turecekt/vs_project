e6a0b8912874ead6a5d8d7fa6a6cbf68e4d6b3b0
MatusJ96 <73904041+MatusJ96@users.noreply.github.com>
2020-11-06 18:49:06 +0100

    Created method for reading characters

--- char_recurrence/main.py
+++ char_recurrence/main.py
@@ -1,7 +1,8 @@
 import sys
+import re
 from argparse import ArgumentParser
 
-def arguments(arg_len):
+def arguments():
     # Method for parsing argument from stdin or as parameters
     # Use argparse library
     # Without parameter it read from stdin else from input file
@@ -9,16 +10,29 @@ def arguments(arg_len):
     parser = ArgumentParser()
     parser.add_argument("-f", "--file", dest = "filename", action = "store",
                         help = "Type input FILE", metavar = "FILE")
+
+    arg_len = len(sys.argv)
     if arg_len == 1:
         result = sys.stdin.read()
-        return result
+        return read_file(result, arg_len)
     else:
         args = parser.parse_args().filename
-        return args
+        return read_file(args, arg_len)
+
+def read_file(input_file, arg_len):
+
+    if arg_len != 1:
+        with open(input_file, "rt") as f:
+            text = f.read()
+            data = text.split('#', 1)[0]
+    else:
+        data = input_file.split('#', 1)[0]
+
+    return data
+
 
 def main():
-    arg_length = len(sys.argv)
-    input_file = arguments(arg_length)
+    file_data  = arguments()
 
 if __name__ == '__main__':
     main()
