1ca7ba1c7da5f3b60bd3e47374321b2324d90b11
grasski <jirkadaberger@seznam.cz>
2020-11-07 15:04:56 +0100

    Added math functions

---
+++ rychlostReakce/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# import radom library for random numbers used for examples
+import random
+
+# @author Daberger Jiri
+# @description Counting response of user on easy math examples
+
+def addition(a, b):
+    # function return sum of two numbers
+    return a + b
+
+def subtraction(a, b):
+    # function return difference of two numbers
+    return a - b
+
+def multiply(a, b):
+    # function return multiple of two numbers
+    return a * b
+
+def division(a, b):
+    # function return division of two numbers
+    return a / b
+
+
+
+
