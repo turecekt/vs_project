72d72546b6211b9550e90ac8fdfde2ced2bab2e8
petrzamecnik <Petazamecek@gmail.com>
2020-11-09 16:38:12 +0100

    created project file with main.py file

---
+++ Project/main.py
@@ -0,0 +1,32 @@
+"""
+Main file
+
+"""
+
+"""
+function to calculate binary number from decimal number
+input -> decimal number
+output -> binary number
+"""
+def decimalToBinary():
+    pass
+
+"""
+function to calculate octal number from decimal number
+input -> decimal number
+output -> octal number
+"""
+def decimalToOctal():
+    pass
+
+"""
+function to calculate hexadecimal number from decimal number
+input -> decimal number
+output -> hexadecimal number
+"""
+def decimalToHexadecimal():
+    pass
+
+
+# Main Program Here
+print("test")
