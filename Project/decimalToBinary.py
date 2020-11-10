f8c3d5016c751282c1b86cc005c07606fc3f0ce8
petrzamecnik <Petazamecek@gmail.com>
2020-11-10 13:30:48 +0100

    refactored file structure

---
+++ Project/decimalToBinary.py
@@ -0,0 +1,9 @@
+"""
+function to calculate binary number from decimal number
+input -> decimal number
+output -> binary number
+"""
+def decimal_to_binary():
+    #TODO Add code to convert decimal number to binary number
+
+    pass
