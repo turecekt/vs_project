be4af5be24ecfae39c9193871f4635a514af1ea3
petrzamecnik <Petazamecek@gmail.com>
2020-11-10 16:31:08 +0100

    added function that converts decimal number to hexadecimal

--- Project/decimalToHexadecimal.py
+++ Project/decimalToHexadecimal.py
@@ -5,8 +5,13 @@ output -> hexadecimal number
 """
 
 
-def decimal_to_hexadecimal():
-    # TODO Add code to convert decimal number to hexadecimal number
+def convert(input_number):
+    x = (input_number % 16)  # modulo
+    chars = "0123456789ABCDEF"
+    rest = input_number / 16  # rest after division
 
-
-    pass
+    # if rest equals zero, return characters where index is x
+    if rest == 0:
+        return chars[x]
+    # if rest is not zero, use method "convert" recursively
+    return convert(rest) + chars[x]
