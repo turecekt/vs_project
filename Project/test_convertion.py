e346bfc58d3c10590c2d50a8040dbaced185075f
petrzamecnik <Petazamecek@gmail.com>
2020-11-10 14:09:06 +0100

    added test module

---
+++ Project/test_convertion.py
@@ -0,0 +1,23 @@
+# library imports
+import unittest
+
+# module imports
+import decimalToBinary
+import decimalToOctal
+import decimalToHexadecimal
+
+
+class TestConvertion(unittest.TestCase):
+
+    def test_decimal_to_binary(self):
+        pass
+
+    def test_decimal_to_octal(self):
+        pass
+
+    def test_decimal_to_hexadecimal(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
