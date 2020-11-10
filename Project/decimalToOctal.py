2ecad91744840cf08907d2854dce6962c740a9ec
petrzamecnik <Petazamecek@gmail.com>
2020-11-10 14:48:59 +0100

    refactored structure

---
+++ Project/decimalToOctal.py
@@ -0,0 +1,11 @@
+"""
+function to calculate octal number from decimal number
+input -> decimal number
+output -> octal number
+"""
+
+
+def decimal_to_octal():
+    # TODO Add code to convert decimal number to octal number
+
+    pass
