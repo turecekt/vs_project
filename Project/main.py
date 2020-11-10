764d37a68af434238b790b18d4ca8eaf01ab7053
petrzamecnik <Petazamecek@gmail.com>
2020-11-10 13:31:43 +0100

    refactored main function

--- Project/main.py
+++ Project/main.py
@@ -1,36 +1,12 @@
-#Imports here
+# Imports here
+import decimalToBinary
+import decimalToOctal
+import decimalToHexadecimal
 
 
-"""
-function to calculate binary number from decimal number
-input -> decimal number
-output -> binary number
-"""
-def decimal_to_binary():
-    #TODO Add code to convert decimal number to binary number
-
-    pass
-
-"""
-function to calculate octal number from decimal number
-input -> decimal number
-output -> octal number
-"""
-def decimal_to_octal():
-    #TODO Add code to convert decimal number to octal number
-
-    pass
-
-"""
-function to calculate hexadecimal number from decimal number
-input -> decimal number
-output -> hexadecimal number
-"""
-def decimal_to_hexadecimal():
-    #TODO Add code to convert decimal number to hexadecimal number
-
+def main():
     pass
 
 
-# Main Program Here
-print("test")
+if __name__ == '__main__':
+    main()
