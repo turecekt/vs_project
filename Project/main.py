462847478e83e020edd73c75b478e71f968a2363
petrzamecnik <Petazamecek@gmail.com>
2020-11-10 16:31:49 +0100

    updated code

--- Project/main.py
+++ Project/main.py
@@ -7,9 +7,29 @@ import decimalToOctal
 import decimalToHexadecimal
 
 
+def pass_args(numeral_system, input_number):
+    return numeral_system, input_number
+
+
 def main():
-    number = 1
-    print(number)
+    # Variables
+
+    if len(sys.argv) == 3:
+        numeral_system_type_arg = sys.argv[1]
+        input_number = int(sys.argv[2])
+    else:
+        # raise TypeError
+        numeral_system_type_arg = "not valid"
+        input_number = 0
+
+    if numeral_system_type_arg == "-b":
+        print(decimalToBinary.convert(input_number))
+    if numeral_system_type_arg == "-o":
+        print(decimalToOctal.convert(input_number))
+    if numeral_system_type_arg == "-h":
+        print(decimalToHexadecimal.convert(input_number))
+    else:
+        print("Unable to convert")
 
 
 if __name__ == '__main__':
