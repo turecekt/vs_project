926b4b6d0320041a9eab28ad674990f3c47cf30b
maetiskou <maetiskou115@gmail.com>
2020-11-13 12:35:30 +0100

    added conversion to hexadecimal

--- numsys/numsys.py
+++ numsys/numsys.py
@@ -2,13 +2,56 @@ def convertToBinary(number):
 
     convertedNumber = ""
 	remainder = number
-	isNumberConverted = false
 
 	while remainder != 0
 		if(remainder % 2 == 0)
 			convertedNumber += "0"
 		else
 			convertedNumber += "1"
+
 		remainder = remainder / 2
+  
+    return convertedNumber
+
+def convertToHexadecimal(number): 
+
+    convertedNumber = ""
+	remainder = number
+
+	while remainder != 0
+		if(remainder % 16 == 0)
+			convertedNumber += "0"
+		if(remainder % 16 == 1)
+			convertedNumber += "1"
+		if(remainder % 16 == 2)
+			convertedNumber += "2"
+		if(remainder % 16 == 3)
+			convertedNumber += "3"
+		if(remainder % 16 == 4)
+			convertedNumber += "4"
+		if(remainder % 16 == 5)
+			convertedNumber += "5"
+		if(remainder % 16 == 6)
+			convertedNumber += "6"
+		if(remainder % 16 == 7)
+			convertedNumber += "7"
+		if(remainder % 16 == 8)
+			convertedNumber += "8"
+		if(remainder % 16 == 9)
+			convertedNumber += "9"
+		if(remainder % 16 == 10)
+			convertedNumber += "A"
+		if(remainder % 16 == 11)
+			convertedNumber += "B"
+		if(remainder % 16 == 12)
+			convertedNumber += "C"
+		if(remainder % 16 == 13)
+			convertedNumber += "D"
+		if(remainder % 16 == 14)
+			convertedNumber += "E"
+		if(remainder % 16 == 15)
+			convertedNumber += "F"
+
+		remainder = remainder / 16
   
     return convertedNumber
