eb7cf1c64b78fa9a0262f124a7f79510244f0d26
cernoma3 <73735470+cernoma3@users.noreply.github.com>
2020-11-14 12:15:00 +0100

    Update trojuholnik.py

--- trojuholnik.py
+++ trojuholnik.py
@@ -11,7 +11,7 @@ def vypocetStrany(x1, y1, x2, y2):
     """Vypocita vzdialenost dvoch bodov.
 
     >>> vypocetStrany(0, 0, 0, 2)
-    2,0
+    2.0
     """
     strana = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     return strana
