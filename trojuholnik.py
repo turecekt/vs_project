c82b7b9d5e6903b65b05d1e486d39095b2cbefb1
cernoma3 <73735470+cernoma3@users.noreply.github.com>
2020-11-14 11:36:33 +0100

    Update trojuholnik.py

--- trojuholnik.py
+++ trojuholnik.py
@@ -5,27 +5,25 @@ Program si vyziada suradnice 3 bodov,
 nasledne zisti dlzky stran, zostrojitelnost, pravouhlost, obvod a obsah
 """
 import math
-'''
+
+
+"""
 Vypocita vzdialenost dvoch bodov
 
 >>> vypocetStrany(0, 0, 0, 2)
 2,0
-'''
-
-
+"""
 def vypocetStrany(x1, y1, x2, y2):
     strana = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     return strana
 
 
-'''
+"""
 Zisti zostrojitelnost trojuholnika zo zadanych 3 stran
 
 >>> zostrojitelnost(5, 6, 7)
 True
-'''
-
-
+"""
 def zostrojitelnost(a, b, c):
     if((a+b > c) and (a+c) > b and (b+c) > a):
         return True
@@ -33,11 +31,11 @@ def zostrojitelnost(a, b, c):
         return False
 
 
-'''
+"""
 Zisti pravouhlost trojuholnika zo zadanych troch stran
 >>> pravouhlost(5, 8, 9)
 Nie je pravouhly
-'''
+"""
 def pravouhlost(a, b, c):
     if((a > 0 and b > 0 and c > 0) and (a**2 == b**2+c**2 or b**2 == c**2+a**2 or c**2 == a**2+b**2)):
         print("Je pravouhly")
@@ -45,11 +43,11 @@ def pravouhlost(a, b, c):
         print("Nie je pravouhly")
 
 
-'''
+"""
 Vypocita obsah trojuholnika pomocou Heronovho vzorca
 >>> obsah(4,9,8)
 15.998046755776157
-'''
+"""
 def obsah(a, b, c):
     if(zostrojitelnost(a, b, c)):
         p=(a+b+c) / 2
@@ -71,7 +69,7 @@ if __name__ == '__main__':
     print("{:.2f}".format(a))
     print("{:.2f}".format(b))
     print("{:.2f}".format(c))
-    if(zostrojitelnost(a, b, c)):
+    if(zostrojitelnost(a, b, c) is True):
         print("Trojuholnik sa da zostrojit")
         obvod = a + b + c
         print("Obvod: ")
