f23c20d27922e73720989d239fd38a6bde4e6c25
cernoma3 <73735470+cernoma3@users.noreply.github.com>
2020-11-14 11:39:28 +0100

    Update trojuholnik.py

--- trojuholnik.py
+++ trojuholnik.py
@@ -13,6 +13,8 @@ Vypocita vzdialenost dvoch bodov
 >>> vypocetStrany(0, 0, 0, 2)
 2,0
 """
+
+
 def vypocetStrany(x1, y1, x2, y2):
     strana = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     return strana
@@ -24,6 +26,8 @@ Zisti zostrojitelnost trojuholnika zo zadanych 3 stran
 >>> zostrojitelnost(5, 6, 7)
 True
 """
+
+
 def zostrojitelnost(a, b, c):
     if((a+b > c) and (a+c) > b and (b+c) > a):
         return True
@@ -36,6 +40,8 @@ Zisti pravouhlost trojuholnika zo zadanych troch stran
 >>> pravouhlost(5, 8, 9)
 Nie je pravouhly
 """
+
+
 def pravouhlost(a, b, c):
     if((a > 0 and b > 0 and c > 0) and (a**2 == b**2+c**2 or b**2 == c**2+a**2 or c**2 == a**2+b**2)):
         print("Je pravouhly")
@@ -48,6 +54,8 @@ Vypocita obsah trojuholnika pomocou Heronovho vzorca
 >>> obsah(4,9,8)
 15.998046755776157
 """
+
+
 def obsah(a, b, c):
     if(zostrojitelnost(a, b, c)):
         p=(a+b+c) / 2
@@ -79,6 +87,7 @@ if __name__ == '__main__':
         pravouhlost(a, b, c)
     else:
         print("Trojuholnik sa neda zostrojit")
+
 
 def test_zostrojitelnost():
     assert zostrojitelnost(0, 0, 0) == False
