b34118807f4df3104751bcd9a2a2e9c61fc14cae
veron <veron@LAPTOP-D4P43O39.local>
2020-11-16 23:38:08 +0100

    Funkce pro zjištění počtu určitého znaku. Převod retezce na malé písmena.

--- example/__init__.py
+++ example/__init__.py
@@ -17,3 +17,13 @@ def compute(x):
         - output - Output of the function
     """
     return x * x - 2 * x
+x=2
+print(x**2)
+print (compute(1))
+print("___")
+
+retezec = "Abeceda"
+print(retezec.lower())
+retezecMalym = retezec.lower()
+print(retezec.count("a"))
+print(retezecMalym.count("a"))
