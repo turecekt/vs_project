186aa3e9b8175bb0e7209d2e5d5a261a4ebf791f
petrzamecnik <Petazamecek@gmail.com>
2020-11-17 18:31:36 +0100

    refactored file

--- Project/test_convertion.py
+++ Project/test_convertion.py
@@ -10,13 +10,9 @@ import decimalToHexadecimal
 class TestConvertion(unittest.TestCase):
 
     def test_decimal_to_binary(self):
-        # self.assertEqual(decimalToBinary.convert("100"), "1100100")
-        # self.assertEqual(decimalToBinary.convert("15119"), "11101100001111")
         pass
 
     def test_decimal_to_octal(self):
-        # self.assertEqual(decimalToOctal.convert("100"), "144")
-        # self.assertEqual(decimalToOctal.convert("15119"), "35417")
         pass
 
     def test_decimal_to_hexadecimal(self):
