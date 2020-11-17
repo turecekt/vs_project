5fe457e061c122aa63951d15c49f5524ae4e5ba1
Filip Slezák <filip_slezak@outlook.com>
2020-11-17 15:19:07 +0100

    Uvodni commit, ziskani dat od uzivatele

---
+++ trojuhelnik.py
@@ -0,0 +1,14 @@
+"""
+Projekt AK1VS - Trojúhelník
+"""
+
+"""
+Program začíná v tomto místě.
+Uživatel je vyzván, aby zadal souřadnice bodů.
+"""
+ax = int(input("Zadej souřadnici x bodu A: "))
+ay = int(input("Zadej souřadnici y bodu A: "))
+bx = int(input("Zadej souřadnici x bodu B: "))
+by = int(input("Zadej souřadnici y bodu B: "))
+cx = int(input("Zadej souřadnici x bodu C: "))
+cy = int(input("Zadej souřadnici y bodu C: "))
