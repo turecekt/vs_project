3167cff6d1a43b20890ba5fec42ab11c8ccf9f23
Filip Slezák <filip_slezak@outlook.com>
2020-11-18 11:13:47 +0100

    Přidán výpočet délek stran

--- trojuhelnik.py
+++ trojuhelnik.py
@@ -1,6 +1,16 @@
+from math import sqrt
 """
 Projekt AK1VS - Trojúhelník
 """
+"""
+Fuknce pro výpočet délky stran.
+Délka je počítána přesně, nicméně se v programu zobrazují pouze dvě desetinná místa.
+"""
+
+
+def delka_strany(a1, a2, b1, b2):
+    return sqrt(((b1-a1)**2)+((b2-a2)**2))
+
 
 """
 Program začíná v tomto místě.
@@ -12,3 +22,15 @@ bx = int(input("Zadej souřadnici x bodu B: "))
 by = int(input("Zadej souřadnici y bodu B: "))
 cx = int(input("Zadej souřadnici x bodu C: "))
 cy = int(input("Zadej souřadnici y bodu C: "))
+
+""""
+Výpočet délek stran
+"""
+a = delka_strany(bx, by, cx, cy)
+b = delka_strany(ax, ay, cx, cy)
+c = delka_strany(ax, ay, bx, by)
+print()
+print("Délky stran:")
+print("Délka strany a je ", "{:.2f}".format(a))
+print("Délka strany b je ", "{:.2f}".format(b))
+print("Délka strany c je ", "{:.2f}".format(c))
