f041a4af4c8a0d2efc37c1cb8956b42720c80936
Filip Slezák <filip_slezak@outlook.com>
2020-11-18 19:53:14 +0100

    Upraveno kódování

--- trojuhelnik.py
+++ trojuhelnik.py
@@ -5,19 +5,19 @@ from math import sqrt
 
 
 """
-Projekt AK1VS - Trojuhelnik
+Projekt AK1VS - Trojúhelník
 """
 
 
-# Fuknce pro vypocet delky stran.
-# Delka je pocitana presne, nicmene se v programu zobrazuji pouze dve desetinna mista.
-# a1, a2 jsou souradnice bodu A; b1, b2 souradnice bodu B
+# Fuknce pro výpočet délky stran.
+# Delka je počítána přesně, nicméně se v programu zobrazují pouze dve desetinná místa.
+# a1, a2 jsou souřadnice bodu A; b1, b2 souřadnice bodu B
 def delka_strany(a1, a2, b1, b2):
     return sqrt(((b1 - a1) ** 2) + ((b2 - a2) ** 2))
 
 
-# Funkce urcuje, jestli lze dany trojuhelnik sestrojit.
-# Pokud ano, vraci 1, jinak 0.
+# Funkce určuje, jestli lze dany trojúhelník sestrojit.
+# Pokud ano, vrací 1, jinak 0.
 def setrojitelnost(a1, b1, c1):
     if a1 + b1 > c1 and a1 + c1 > b1 and b1 + c1 > a1:
         return 1
@@ -25,19 +25,19 @@ def setrojitelnost(a1, b1, c1):
         return 0
 
 
-# Funkce pocita obvod trojuhelniku.
-# Pocita presne, zobrazeny budou ale jen dve desetinna cisla.
+# Funkce počítá obvod trojúhelníku.
+# Počítá přesně, zobrazeny budou ale jen dve desetinná čísla.
 def obvod(a1, b1, c1):
     return a1 + b1 + c1
 
 
-# Funkce pocita obsah pomoci Heronova vzorce, tudiz neni potreba znat vysku v trojuhelniku.
+# Funkce počítá obsah pomocí Heronova vzorce, tudíž není potřeba znát výšku v trojúhelníku.
 def obsah(a1, b1, c1):
     s = (a1 + b1 + c1) / 2
     return sqrt(s * (s - a1) * (s - b1) * (s - c1))
 
 
-# Funkce pomoci Pythagorovy vety zjisti pravouhlost trojuhelniku.
+# Funkce pomocí Pythagorovy věty zjistí pravouhlost trojúhelníku.
 def pravouhlost(a1, b1, c1):
     if c1 ** 2 == (a1 ** 2 + b1 ** 2) and (a != 0 and b != 0 and c != 0):
         return 1
@@ -45,12 +45,10 @@ def pravouhlost(a1, b1, c1):
         return 0
 
 
-# Program zacina v tomto miste.
-# Uzivatel je vyzvan, aby zadal souradnice bodu.
+# Program začíná v tomto místě.
+# Uživatel je vyzván, aby zadal souřadnice bodů.
 
-
-# Hodonoty bodů jsou zadány kvůli testování. 
-
+# Hodnoty bodů jsou zadány "natvrdo" kvůli testování. 
 """
 ax = int(input("Zadej souřadnici x bodu A: "))
 ay = int(input("Zadej souřadnici y bodu A: "))
@@ -58,7 +56,7 @@ bx = int(input("Zadej souřadnici x bodu B: "))
 by = int(input("Zadej souřadnici y bodu B: "))
 cx = int(input("Zadej souřadnici x bodu C: "))
 cy = int(input("Zadej souřadnici y bodu C: "))
-""" 
+"""
 
 ax = 1
 ay = 1
@@ -67,7 +65,7 @@ by = 5
 cx = 7
 cy = 3
 
-# Vypocet delek stran. Promenne budou pouzity pro dalsi vypocty.
+# Výpočet délek stran. Proměnné budou použity pro další výpočty.
 a = delka_strany(bx, by, cx, cy)
 b = delka_strany(ax, ay, cx, cy)
 c = delka_strany(ax, ay, bx, by)
@@ -91,4 +89,3 @@ if pravouhlost(a, b, c):
 
 def test_obvod():
     assert obvod(1, 2, 3) == 6
-    
