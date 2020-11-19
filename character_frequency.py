6a3d6b33ca4884a2583d4eea26adc9552a9a878b
Martin Klimcik <martin.klimcik@gmail.com>
2020-11-19 18:40:38 +0100

    improve test coverage

--- character_frequency.py
+++ character_frequency.py
@@ -24,10 +24,11 @@ VÝSTUP
 
 Tests:
 >>> test_open_file()
+>>> test_input()
 >>> test_count_char_freq()
 >>> test_calculate_stats()
 """
-from string import ascii_lowercase
+from string import ascii_uppercase
 from typing import List, AnyStr
 import sys
 import pytest
@@ -50,7 +51,7 @@ def get_file_content(filename: str) -> List[AnyStr]:
         return None
 
 
-def get_input() -> str:
+def get_input(filename: str = None) -> str:
     """
     Get input text.
 
@@ -58,15 +59,25 @@ def get_input() -> str:
     to a file with text, all other parameters are ignored.
     If no argument was given,
     ask user for input text cyclically until # symbol is given.
+    Priority order for input:
+        1. filename given as parameter to this method
+        2. filename given as script parameter
+        3. user input
+    Args:
+        filename - optional, name of file to open.
+
     Returns:
         List of string, each string representing one line of input
     """
-    if len(sys.argv) > 1:
-        # parameter given, try to open file path from first one
+    _file = None
+    if filename is not None:
+        # method parameter given, try to open file
+        result = get_file_content(filename)
+        _file = filename
+    elif len(sys.argv) > 1:
+        # script parameter given, try to open file path from first one
         result = get_file_content(sys.argv[1])
-        if result is None:
-            print(f'File "{sys.argv[1]}" was not found!')
-            sys.exit(1)
+        _file = sys.argv[1]
     else:
         # No parameter given, wait for user input
         print('Write down text to parse. May be multiple lines. '
@@ -78,6 +89,9 @@ def get_input() -> str:
             if '#' in user_input:
                 result = values
                 break
+    if result is None:
+        print(f'File "{_file}" was not found!')
+        sys.exit(1)
     return ''.join(result)
 
 
@@ -113,11 +127,11 @@ def count_char_freq(text: str) -> {}:
     alphabet = {}
     for _c in text:
         # Upper case letters are cast to lower case for simplicity
-        char = _c.lower()
+        char = _c.upper()
         if char in alphabet:
             # increase character count if it is already present in dictionary
             alphabet[char] += 1
-        elif char in ascii_lowercase:
+        elif char in ascii_uppercase:
             # add to dictionary if it is part of english alphabet
             alphabet[char] = 1
     # get list of all letter counts and calculate relevant stats
@@ -171,7 +185,7 @@ class CharStats:
                       f"occurrence{_grammar(self.max_count)}: " \
                       f"{', '.join(self.max_chars)}\n" \
                       "Frequency of each appearing character:\n"
-            for char in ascii_lowercase:
+            for char in ascii_uppercase:
                 if char in self.all_chars:
                     result += f' {char} : {self.all_chars[char]: >4}\n'
             result += "Alphabet characters that do not appear at all: " \
@@ -204,7 +218,7 @@ def calculate_stats(alphabet: dict) -> CharStats:
     s.min_chars = [c for c, cnt in alphabet.items() if cnt == s.min_count]
     s.max_chars = [c for c, cnt in alphabet.items() if cnt == s.max_count]
     # find which character are missing
-    for char in ascii_lowercase:
+    for char in ascii_uppercase:
         if char not in alphabet:
             s.missing_chars.append(char)
     return s
@@ -216,49 +230,72 @@ TEST_FILE_2 = 'test_file_2.txt'
 TEST_FILE_INVALID = 'test_file_99.txt'
 
 
+def _eq(actual, expected) -> None:
+    """
+    Assert equal with more reasonable output on failure.
+
+    Args:
+        actual: value to compare
+        expected: expected value
+
+    Returns:
+        Raises AssertionError if actual does not equal expected
+    """
+    assert actual == expected, f'\nActual:   "{actual}" ({type(actual)})\n' \
+                               f'Expected: "{expected}" {type(expected)})'
+
+
 def test_open_file():
     """Test open_file method."""
     # Open file
     content = get_file_content(TEST_FILE_2)
-    assert len(content) == 2
-    assert content == ['aaa\n', 'zzz'], f"Content: {content}"
+    _eq(len(content), 2)
+    _eq(content, ['aaa\n', 'zzz'])
     # Try open non-existent file
     content = get_file_content(TEST_FILE_INVALID)
     assert content is None
 
 
+def test_input():
+    """Test get_input method."""
+    content = get_input(TEST_FILE_2)
+    _eq(content, 'aaa\nzzz')
+
+
 def test_count_char_freq():
     """Test count_char_freq method."""
     # count character frequency of test file
-    s = count_char_freq(''.join(get_file_content(TEST_FILE_2)))
-    assert s['a'] == 3
-    assert s['z'] == 3
+    s = count_char_freq(get_input(TEST_FILE_2))
+    _eq(s['A'], 3)
+    _eq(s['Z'], 3)
     # count char freq of directly given text
     s = count_char_freq('The Test\n 123456 t')
-    assert s['t'] == 4
-    assert s['h'] == 1
-    assert s['e'] == 2
-    assert s['s'] == 1
+    _eq(s['T'], 4)
+    _eq(s['H'], 1)
+    _eq(s['E'], 2)
+    _eq(s['S'], 1)
 
 
 def test_calculate_stats():
     """Test calculate_stats method."""
     # Calculate stats of input from test file
-    s = calculate_stats(count_char_freq(
-        ''.join(get_file_content(TEST_FILE_1))))
-    assert s.total_count == 2231
-    assert s.unique_chars_count == 20
-    assert s.average == pytest.approx(111.55, 0.01)
-    assert s.min_count == 22
-    assert s.min_chars == ['q', 'f', 'h']
-    assert s.max_count == 244
-    assert s.max_chars == ['e']
-    assert s.all_chars['t'] == 175
-    assert s.all_chars['c'] == 92
-    assert s.missing_chars == ['j', 'k', 'w', 'x', 'y', 'z'], \
-        f"{s.missing_chars}"
+    s = calculate_stats(count_char_freq(get_input(TEST_FILE_1)))
+    _eq(s.total_count, 2231), f"{s.total_count}"
+    _eq(s.unique_chars_count, 20)
+    _eq(s.average, pytest.approx(111.55, 0.01))
+    _eq(s.min_count, 22)
+    _eq(s.min_chars, ['Q', 'F', 'H'])
+    _eq(s.max_count, 244)
+    _eq(s.max_chars, ['E'])
+    _eq(s.all_chars['T'], 175)
+    _eq(s.all_chars['C'], 92)
+    _eq(s.missing_chars, ['J', 'K', 'W', 'X', 'Y', 'Z'])
 
 
+test_open_file()
+test_input()
+test_count_char_freq()
+test_calculate_stats()
 # === Run script - only when not imported ===
 if __name__ == "__main__":
     INPUT_VALUES = get_input()
