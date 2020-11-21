d0ad7ba8c47cf3b2cfd234933349d67411353708
Karel <Karel@Karels-MacBook-Pro.local>
2020-11-21 17:27:03 +0100

    fix init project flake8 errors

--- Triangle.py
+++ Triangle.py
@@ -1,4 +1,5 @@
-""" This is a project with four functions to calculate details about triangle.
+"""This is a project with four functions to calculate details about triangle.
+
 To determinate if we can get a triangle according to inputs and if so,
 to calcul lenght of the sides,
 to calcul perimetr and area of the triangle.
