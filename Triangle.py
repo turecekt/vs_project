7b157632a064ade83268e46a4e2839603e6401f2
Karel <Karel@Karels-MacBook-Pro.local>
2020-11-21 15:42:01 +0100

    init project Triangle

---
+++ Triangle.py
@@ -0,0 +1,9 @@
+""" This is a project with four functions to calculate details about triangle.
+To determinate if we can get a triangle according to inputs and if so,
+to calcul lenght of the sides,
+to calcul perimetr and area of the triangle.
+"""
+
+
+if __name__ == '__main__':
+    print("Triangle summary")
