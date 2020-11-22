7d0adc90c93178434e0f5beac1c2200a1786b822
petrzamecnik <Petazamecek@gmail.com>
2020-11-22 18:17:07 +0100

    fixed flake8 warnings

--- Project/test_convertion.py
+++ Project/test_convertion.py
@@ -1,3 +1,4 @@
+"""Test convert functions."""
 # library imports
 import unittest
 
@@ -7,17 +8,25 @@ import decimalToOctal
 import decimalToHexadecimal
 
 
+"""Unittest class for convert functions."""
+
+
 class TestConvertion(unittest.TestCase):
+    """Class testing convert functions."""
 
     def test_decimal_to_binary(self):
+        """Function testing binary conversion."""
         self.assertEqual(decimalToBinary.convert(78), "1001110")
-        self.assertEqual(decimalToBinary.convert(1234567890), "1001001100101100000001011010010")
+        self.assertEqual(decimalToBinary.convert(1234567890),
+                         "1001001100101100000001011010010")
 
     def test_decimal_to_octal(self):
+        """Function testing octal conversion."""
         self.assertEqual(decimalToOctal.convert(234), "352")
         self.assertEqual(decimalToOctal.convert(2781), "5335")
 
     def test_decimal_to_hexadecimal(self):
+        """Function testing hexadecimal conversion."""
         self.assertEqual(decimalToHexadecimal.convert(100), "64")
         self.assertEqual(decimalToHexadecimal.convert("15119"), "3B0F")
 
