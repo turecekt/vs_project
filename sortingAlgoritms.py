3db0959f1c879e34144a1fe1e99eb0fe4a6275d8
AlexandrCizek <alexandr.cizek1@gmail.com>
2020-11-23 11:12:54 +0100

    pridani souboru sortingAlgoritms.py a test tridicich algoritmu

---
+++ sortingAlgoritms.py
@@ -0,0 +1,40 @@
+def bubblesort(pole):
+    for _ in pole:
+        for j in range(len(pole) - 1):
+            if pole[j] > pole[j + 1]:
+                temp = pole[j]
+                pole[j] = pole[j + 1]
+                pole[j + 1] = temp
+
+    print("Serazene pole:", pole)
+
+
+def insertionsort(pole):
+    for i in range(1, len(pole)):
+        prvek = pole[i]
+        j = i - 1
+
+        while j >= 0 and pole[j] > prvek:
+            pole[j + 1] = pole[j]
+            j -= 1
+
+        pole[j + 1] = prvek
+
+    print("Serazene pole:", pole)
+
+
+def selectionsort(pole):
+    for i in range(len(pole)):
+        minIndex = i
+        for j in range(i + 1, len(pole)):
+            if pole[j] < pole[minIndex]:
+                minIndex = j
+        pole[i], pole[minIndex] = pole[minIndex], pole[i]
+
+    print("Serazene pole:", pole)
+
+pole = [5,5,4,4,8,9,11,25,1,52,225]
+print(pole)
+bubblesort(pole)
+insertionsort(pole)
+selectionsort(pole)
