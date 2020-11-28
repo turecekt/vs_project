8b707ba6411e58d6b0632ab4bc6178db9edb53e8
Greemen <j_andrysek@utb.cz>
2020-11-28 15:05:42 +0100

    numbers support added

--- morsecode/test_MorseCode.py
+++ morsecode/test_MorseCode.py
@@ -9,7 +9,9 @@ from MorseCode import encode
                          ("morseova abeceda je skupina symbolu",
                          "--|---|.-.|...|.|---|...-|.-||.-|-...|.|-.-.|.|"
                           "-..|.-||.---|.||...|-.-|..-|.--.|..|-.|.-||...|-."
-                          "--|--|-...|---|.-..|..-")])
+                          "--|--|-...|---|.-..|..-"),
+                         ("0123456789", "-----|.----|..---|...--|....-|.....|"
+                          "-....|--...|---..|----.")])
 def test_encrypt(input, expected):
     """Tests encode function."""
     assert(encode(input) == expected)
