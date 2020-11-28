ccda03b29de4986d6db71a860b7b9955891d4219
Martin Novák <novakmartin52@gmail.com>
2020-11-28 11:03:08 +0100

    Added main loop. Added language selection logic. Added comments.

--- src/main.py
+++ src/main.py
@@ -57,3 +57,41 @@ morse_alphabet = {
     "-.--": "y",
     "--..": "z"
 }
+
+# Mezera
+space = " ";
+
+# Podporované jazyky
+languages = ["English", "Czech"]
+
+# Funkce, která vypíše do konzole podporované jazyky
+def printSupportLanguages():
+    index = 1
+    for language in languages:
+        print(str(index) + " - " + language)
+        index += 1
+
+# Funkce, která zajišťuje správný výběr podporovaného jazyka
+def language():
+    print("Select languages:")
+    printSupportLanguages()
+    # Smyčka se opakuje, tak douho, dokud není vybrán správný jazyk
+    while True:
+        indexLanguageStr = input("Enter number of language: ")
+        # Kontrola, jestli je uživatelský vstup číslo
+        try:
+            indexLanguageInt = int(indexLanguageStr) - 1
+        except ValueError:
+            print("Not an integer!")
+            continue
+        if (indexLanguageInt >= 0 and indexLanguageInt < len(languages)):
+            return indexLanguageInt
+
+# Funkce s logikou hlavní smyčky programu    
+def mainLoop():
+    # Menu (základní nastavení pro kódování a dekódování)
+    print("Welcome to the translator!\n")
+    lang = language()
+    
+# Spuštění hlavní smyčky
+mainLoop()
