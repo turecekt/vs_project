b85e908009c0e205749a563f0736af94122e2746
petrzamecnik <Petazamecek@gmail.com>
2020-11-29 15:17:56 +0100

    added convert_hexadecimal to main python file

--- Project/main.py
+++ Project/main.py
@@ -9,6 +9,28 @@ import decimalToOctal
 import decimalToHexadecimal
 
 
+def convert_hexadecimal(input_number):
+    """Convert decimal number to hexadecimal."""
+    input_number = int(input_number)
+    x = (input_number % 16)  # modulo
+    chars = "0123456789ABCDEF"
+    rest = input_number / 16  # rest after division
+
+    # if rest equals zero, return characters where index is x
+    if rest == 0:
+        return chars[x]
+    # if rest is not zero, use method "convert" recursively
+    return convert_hexadecimal(rest) + chars[x]
+
+
+def test_convert_hexadecimal():
+    """Test function convert_hexadecimal."""
+    if sys.version_info > (3, 0):
+        assert convert_hexadecimal(100) == "064"
+    else:
+        assert convert_hexadecimal(100) == "64"
+
+
 def main():
     """Get arguments from console, then call functions to convert number."""
     if len(sys.argv) == 3:  # get arguments from terminal
@@ -29,7 +51,7 @@ def main():
     elif numeral_system_type_arg == "-o":
         print(decimalToOctal.convert(input_number))
     elif numeral_system_type_arg == "-h":
-        print(decimalToHexadecimal.convert(input_number))
+        print(convert_hexadecimal(input_number))
     else:
         print("Unable to convert")
 
