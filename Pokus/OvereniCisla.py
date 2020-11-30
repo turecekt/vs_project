943216ab4d6be8a5b20eb376f49ffa4cf777719c
MikulasMikeska <m2_mikeska@utb.cz>
2020-11-30 21:33:56 +0100

    skript overeni rimskeho cisla version 1

---
+++ Pokus/OvereniCisla.py
@@ -0,0 +1,61 @@
+"""Skript pro spravnou kontrolu zapsanehorim. c..
+
+Tento skript bude prilozen pozdeji do celeho projektu
+"""
+
+
+def test_checkcorrect1():
+    """Funkce pro kontrolu zadaneho cisla.
+
+    Testovaci funkce kontrolu
+    >>> checkcorrect("LL", ["I", "V", "X", "L", "C", "D", "M"])
+    False
+    >>> checkcorrect("IMI", ["I", "V", "X", "L", "C", "D", "M"])
+    False
+    >>> checkcorrect("ILM", ["I", "V", "X", "L", "C", "D", "M"])
+    False
+    >>> checkcorrect("CCCCC", ["I", "V", "X", "L", "C", "D", "M"])
+    False
+    >>> checkcorrect("LLL", ["I", "V", "X", "L", "C", "D", "M"])
+    False
+    """
+
+
+def checkcorrect(numb, cisla):
+    """Funkce pro zjištění jestli je  ř. č. v dobrem tvaru).
+
+    Vystup bool
+    """
+    dobre = True
+    coun = 0
+    pocitadlostejnychznaku = 1
+    maxy = "M"
+    if (len(numb) > 0):
+        while coun+1 < len(numb):
+            if cisla.index(numb[coun]) < cisla.index(numb[coun+1]):
+                maxy = (numb[coun])
+                if coun+2 < len(numb):
+                    if cisla.index(numb[coun+1]) <= cisla.index(numb[coun+2]):
+                        dobre = False
+            elif cisla.index(numb[coun]) > cisla.index(maxy):
+                dobre = False
+            else:
+                if numb[coun] != "M" and maxy == numb[coun]:
+                    pocitadlostejnychznaku = pocitadlostejnychznaku+1
+                    d = (maxy == "L" or maxy == "V" or maxy == "D")
+                    if pocitadlostejnychznaku > 3:
+                        dobre = False
+                    elif pocitadlostejnychznaku > 1 and d:
+                        dobre = False
+                else:
+                    maxy = numb[coun]
+                    pocitadlostejnychznaku = 1
+            coun = coun+1
+        if numb[len(numb)-1] != "M" and maxy == numb[len(numb)-1]:
+            pocitadlostejnychznaku = pocitadlostejnychznaku+1
+            d = (maxy == "L" or maxy == "V" or maxy == "D")
+            if pocitadlostejnychznaku > 3:
+                dobre = False
+            elif pocitadlostejnychznaku > 1 and d:
+                dobre = False
+        return dobre
