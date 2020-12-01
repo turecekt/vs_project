c094794c078d0a22639e0e0e8c3914206c1f7c86
Martin Němeček <m_nemecek@utb.cz>
2020-12-01 13:16:28 +0100

    added a way to run the program

--- projekt/main.py
+++ projekt/main.py
@@ -8,3 +8,23 @@ def lengthofside(jednaX, dveX, jednaY, dveY):
     sb = jednaY - dveY
     vysledek = math.sqrt(sa * sa + sb * sb)
     return vysledek
+
+
+if __name__ == '__main__':
+    try:
+        Ax = float(input("Napiš pozici x bodu A:"))
+        Ay = float(input("Napiš pozici y bodu A:"))
+        Bx = float(input("Napiš pozici x bodu B:"))
+        By = float(input("Napiš pozici y bodu B:"))
+        Cx = float(input("Napiš pozici x bodu C:"))
+        Cy = float(input("Napiš pozici y bodu C:"))
+    except ValueError:
+        print("Nebylo zadano číslo")
+
+a = lengthofside(Cx, Bx, Cy, By)
+b = lengthofside(Ax, Cx, Ay, Cy)
+c = lengthofside(Bx, Ax, By, Ay)
+
+print(a)
+print(b)
+print(c)
