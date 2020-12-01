20f7d23019a7b93dd0c10ea6705ab81fc5442145
Ondrej Marecek <o_marecek@utb.cz>
2020-12-01 23:49:47 +0100

    Base of comments using doxygen

--- project.py
+++ project.py
@@ -1,33 +1,58 @@
-'''Morse code translator'''
-library = {'A': '.-', 'B': '-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-', ' ':'.....'}
+"""Morse code translator.
 
-# Translating morse code to latin
-def morseEncrypt(txt):
+@file             project.py
+@author           Ondrej Marecek <o_marecek@utb.cz>
+@version          1.0
+@brief            Morse Code Translator
+
+@section LICENSE
+
+This program is free to use....
+
+@section DESCRIPTION
+
+Morse code encryptor to and from morse code
+
+"""
+
+library = {'A': '.-', 'B': '-...',
+           'C': '-.-.', 'D': '-..', 'E': '.',
+           'F': '..-.', 'G': '--.', 'H': '....',
+           'I': '..', 'J': '.---', 'K': '-.-',
+           'L': '.-..', 'M': '--', 'N': '-.',
+           'O': '---', 'P': '.--.', 'Q': '--.-',
+           'R': '.-.', 'S': '...', 'T': '-',
+           'U': '..-', 'V': '...-', 'W': '.--',
+           'X': '-..-', 'Y': '-.--', 'Z': '--..',
+           '1': '.----', '2': '..---', '3': '...--',
+           '4': '....-', '5': '.....', '6': '-....',
+           '7': '--...', '8': '---..', '9': '----.',
+           '0': '-----', ', ': '--..--', '.': '.-.-.-',
+           '?': '..--..', '/': '-..-.', '-': '-....-',
+           '(': '-.--.', ')': '-.--.-', ' ': '.....'}
+
+"""Translating morse code to latin from user inserted txt."""
+
+
+def morseencrypt(txt):
+    """Setting new string that will be used as output."""
     translation = ''
-    # Swaping keys from library:
+    """Swapping keys from library."""
     lib_encrypt = dict([(v, k) for k, v in library.items()])
-    # Morse code output will be splitted with space
-    txt = txt.split(' ')
+    """For cycle that will cycle trough user inserted morse code and finding the equivalent inside library."""
     for x in txt:
         translation += lib_encrypt.get(x)
+        """Return stripped translation."""
     return translation.strip()
-# Translating latin to morse code:
-def morseDecrypt(txt):
+
+
+"""Translating latin to morse code from user inserted txt."""
+
+
+def morsedecrypt(txt):
+    """Setting new string that will be used as output."""
     translation = ''
+    """"""
     txt = txt.upper()
     for x in txt:
         translation += library.get(x) + ' '
