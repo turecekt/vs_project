a7162488307ff76741fe266197ef22eca1f6ee61
David Papaj <d_papaj@utb.cz>
2020-12-01 19:05:52 +0100

    Added code, where you can chose to translate textx or morse

---
+++ Morse_code_translator/translator.py
@@ -0,0 +1,18 @@
+"""Input text or morse code .Output translated text or morse."""
+from morse import MorseCodeTranslator
+
+translator = MorseCodeTranslator()
+# Insert text for translation to morse
+text = "Dobry den"
+# Insert morse for translation to text 
+morse = "-.. --- -... .-. -.--   -.. . -."
+
+# Translate text to morse code
+translated_morse = translator.translate_text(text)
+
+# Translate morse code to text
+translated_text = translator.translate_morse(morse)
+
+print(translated_morse)
+print(translated_text)
+
