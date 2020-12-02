f1e81c90f627a250b9983b0c5cb2413d1513d60a
Toma <novotny.tom96@gmail.com>
2020-12-02 12:19:38 +0100

    execute successfully with flake8

--- trojuhelnik.py
+++ trojuhelnik.py
@@ -1,5 +1,5 @@
 """
-VS PROJECT
+VS PROJECT.
 
 This project contains methods for calculating triangle lengths,
 perimeter and area. It also tells you whether specified triangle
@@ -8,9 +8,10 @@ can be constructed
 All values are rounded to 3 decimal digits
 """
 
+
 def getUserInput(message):
     """
-    Method for getting input from user
+    Get input from user.
 
     Args:
         message: prompt message
@@ -23,9 +24,10 @@ def getUserInput(message):
         inp = input("Invalid input. Try it again. Example input: 1 2" + "\n")
     return inp.split()
 
+
 def sqrt(n):
     """
-    Square root calculation
+    Square root calculation.
 
     Args:
         n: Input number
@@ -35,9 +37,10 @@ def sqrt(n):
     """
     return round(n ** (1/2), 3)
 
+
 def sqr(n):
     """
-    Power of 2 calculation
+    Power of 2 calculation.
 
     Args:
         n: Input number
@@ -47,9 +50,10 @@ def sqr(n):
     """
     return n * n
 
+
 def calculatePerimeter(a, b, c):
     """
-    Calculates triangle perimeter
+    Calculate triangle perimeter.
 
     Args:
         a: a side length
@@ -61,9 +65,11 @@ def calculatePerimeter(a, b, c):
     """
     return round(a + b + c, 3)
 
+
 def calculateArea(a, b, c):
     """
-    Calculates triangle area using Heron's formula
+    Calculate triangle area using Heron's formula.
+
     Heron's formula: s = (a + b + c) / 2
 
     Args:
@@ -77,9 +83,10 @@ def calculateArea(a, b, c):
     s = (a + b + c) / 2
     return sqrt(s * (s - a) * (s - b) * (s - c))
 
+
 def isConstructable(a, b, c):
     """
-    Checks if triangle can be constructed
+    Check if triangle can be constructed.
 
     Args:
         a: a side length
@@ -91,9 +98,11 @@ def isConstructable(a, b, c):
     """
     return a + b > c and a + c > b and b + c > a
 
+
 def isRightAngled(a, b, c):
     """
-    Checks if triangle is right angled using Pythagora's theorem
+    Check if triangle is right angled using Pythagora's theorem.
+
     c^2 = a^2 + b^2
 
     Args:
@@ -109,60 +118,54 @@ def isRightAngled(a, b, c):
     sc = sqr(c)
     return sc == sa + sb or sb == sa + sc or sa == sb + sc
 
+
 def test_sqrt():
-    """
-    Test for sqrt method
-    """
+    """Test for sqrt method."""
     assert sqrt(81) == 9
     assert sqrt(9) == 3
     assert sqrt(16) == 4
     assert sqrt(144) == 12
 
+
 def test_sqr():
-    """
-    Test for sqr method
-    """
+    """Test for sqr method."""
     assert sqr(4) == 16
     assert sqr(8) == 64
     assert sqr(3) == 9
     assert sqr(16) == 256
 
+
 def test_calculatePerimeter():
-    """
-    Test for calculatePerimeter method
-    """
+    """Test for calculatePerimeter method."""
     assert calculatePerimeter(15, 20, 25) == 60
     assert calculatePerimeter(10.5, 18, 14.5) == 43
     assert calculatePerimeter(15, 15, 15) == 45
 
+
 def test_calculateArea():
-    """
-    Test for calculateArea method
-    """
+    """Test for calculateArea method."""
     assert calculateArea(15, 20, 25) == 150
     assert calculateArea(20, 20, 30) == 198.431
     assert calculateArea(15, 15, 15) == 97.428
 
+
 def test_isConstructable():
-    """
-    Test for isConstructable method
-    """
+    """Test for isConstructable method."""
     assert isConstructable(15, 20, 25)
     assert isConstructable(20, 20, 30)
     assert isConstructable(15, 15, 15)
 
+
 def test_isRightAngled():
-    """
-    Test for isRightAngled method
-    """
+    """Test for isRightAngled method."""
     assert isRightAngled(12, 9, 15)
 
+
 def test_round():
-    """
-    Test for round method
-    """
+    """Test for round method."""
     assert round(1.40123, 3) == 1.401
     assert round(1.04681, 3) == 1.047
+
 
 if __name__ == "__main__":
     # Main function
