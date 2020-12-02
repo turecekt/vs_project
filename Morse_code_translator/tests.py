4b65b9aeff0276b55298a1f5d29dca3d51fc751d
David Papaj <d_papaj@utb.cz>
2020-12-02 11:32:11 +0100

    added more unittests

--- Morse_code_translator/tests.py
+++ Morse_code_translator/tests.py
@@ -35,6 +35,13 @@ class TestMorseCodeTranslator(unittest.TestCase):
         x = "&@)(=!-+?"
         self.assertEqual(translator.translate_morse(morse), x)
 
+    def test_morse_to_punctuation1(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        morse = ".----. ---... --..-- .-.-.- .-..-. -..-."
+        x = "':,.\"/"
+        self.assertEqual(translator.translate_morse(morse), x)
+
     def test_morse_to_text_not_strict(self):
         """Unit tests."""
         translator = MorseCodeTranslator()
@@ -49,11 +56,32 @@ class TestMorseCodeTranslator(unittest.TestCase):
         translator = MorseCodeTranslator()
         morse = ".... . .-.. .-.. ---    .-- --- .-. .-.. -.."
         expected_output = (
-            "Unable to translate morse code(4spaces)."
+            "Unable to translate morse code."
         )
         self.assertEqual(
             translator.translate_morse(morse, strict=True), expected_output
         )
+
+    def test_morse_to_text1(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        morse = "!<testing>!?"
+        x = "-.-.-- - . ... - .. -. --. -.-.-- ..--.."
+        self.assertEqual(translator.translate_text(morse), x)
+
+    def test_morse_to_text2(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        morse = "12 & 4 56 7+8"
+        x = ".---- ..---   .-...   ....-   ..... -....   --... .-.-. ---.."
+        self.assertEqual(translator.translate_text(morse), x)
+
+    def test_morse_to_text3(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        morse = "(this) is?"
+        x = "-.--. - .... .. ... -.--.-   .. ... ..--.."
+        self.assertEqual(translator.translate_text(morse), x)
 
     # Text to morse code
     def test_text_to_morse_no_text(self):
@@ -82,6 +110,48 @@ class TestMorseCodeTranslator(unittest.TestCase):
         translator = MorseCodeTranslator()
         text = "&@)(=!-+?"
         x = ".-... .--.-. -.--.- -.--. -...- -.-.-- -....- .-.-. ..--.."
+        self.assertEqual(translator.translate_text(text), x)
+
+    def test_punctuation_to_morse1(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        text = "':,.\"/"
+        x = ".----. ---... --..-- .-.-.- .-..-. -..-."
+        self.assertEqual(translator.translate_text(text), x)
+
+    def test_all_to_morse(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        text = "Hello world.."
+        x = ".... . .-.. .-.. ---   .-- --- .-. .-.. -.. .-.-.- .-.-.-"
+        self.assertEqual(translator.translate_text(text), x)
+
+    def test_all_to_morse1(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        text = "12 & 4 56 7+8"
+        x = ".---- ..---   .-...   ....-   ..... -....   --... .-.-. ---.."
+        self.assertEqual(translator.translate_text(text), x)
+
+    def test_all_to_morse2(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        text = "(this) is?"
+        x = "-.--. - .... .. ... -.--.-   .. ... ..--.."
+        self.assertEqual(translator.translate_text(text), x)
+
+    def test_all_to_morse3(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        text = "'Just' @#,.:"
+        x = ".----. .--- ..- ... - .----.   .--.-. --..-- .-.-.- ---..."
+        self.assertEqual(translator.translate_text(text), x)
+
+    def test_all_to_morse4(self):
+        """Unit tests."""
+        translator = MorseCodeTranslator()
+        text = "!<testing>!?"
+        x = "-.-.-- - . ... - .. -. --. -.-.-- ..--.."
         self.assertEqual(translator.translate_text(text), x)
 
 
