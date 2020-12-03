b4037a69269b405cb62a6819a1b8ab6c67ce9f8e
2ez4me1 <75311956+2ez4me1@users.noreply.github.com>
2020-12-03 13:29:51 +0100

    "Update Unit testov"

--- kód/test_projekt.py
+++ kód/test_projekt.py
@@ -1,10 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec  3 12:51:26 2020
-
-@author: Jaqub
-"""
-import projekt.py
+import projekt
 
 def test_dlzkystran():
-    assert projekt.dlzkastrany(1,1,4,5)=="5.0"
+    assert projekt.dlzkastrany(1,1,4,5)==5.0
+    assert projekt.dlzkastrany(1,1,2,1)==1.0
+
+def test_obvod():
+    assert projekt.obvod(1,2,3)==6
+
+def test_obsah():
+    assert projekt.obsah(4,5,6)==9.9216
+
+def test_zostrojenost():
+    assert projekt.zostrojenost(3,4,5)==True
+
+def test_pravouhlost():
+    assert projekt.pravouhlost(3,4,5)==True
