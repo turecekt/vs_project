752a004c77c995c09b8d71318fb8b8274f0caf3c
gearszero <gearszero0@gmail.com>
2020-12-03 17:50:22 +0100

    Change code

--- FilipJadrnicek_MorseCode.py
+++ FilipJadrnicek_MorseCode.py
@@ -30,6 +30,7 @@ HOW TO USE:
 This document owns University of Thomas Bata in Zlin (UTB FAI).
 
 """
+
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
            "J", "K", "L", "M", "N", "O", "P", "Q", "R",
            "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1",
@@ -82,7 +83,7 @@ def convert_to_letter(code):
     return tmp
 
 
-def unit_test():
+def test_unit():
     """Use this function for unit testing the code."""
     checks = 0
     stringCheck = "abcdefghijklmnopqrstuvwxyz0123456789"
@@ -98,6 +99,7 @@ def unit_test():
     if stringCheck.upper() == morseCodeCheckFinal:
         checks += 1
 
+    assert stringCheck.upper() == morseCodeCheckFinal
     for i in range(len(stringCheck)):
         checkMorse += convert_to_code(stringCheck[i].upper())
 
@@ -106,42 +108,75 @@ def unit_test():
     if checkMorse == morseCodeCheck:
         checks += 1
 
+    assert checkMorse == morseCodeCheck
     if checks == 2:
         return True
     else:
         return False
 
 
-if unit_test():
-    # Checking if unit test return true.
-    code = input("Enter morse code or sentence: ")
-
-    # Checking what is first char in input, if it is . or -
-    # it will use convert to letter function
-    if is_code(code[0]):
-        final = convert_to_letter(code)
-        if final == -1 or final == '':
-            print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX "
-                  "exp: ...|---|...| for sos")
+def launch(zprava):
+    if test_unit():
+        if zprava == "":
+            # Checking if unit test return true.
+            code = input("Enter morse code or sentence: ")
         else:
-            final = ''.join(final)
-            print(final)
-    # Else it will use convert to code function
-    else:
-        finalLetter = []
-        for i in range(len(code)):
+            # Checking if unit test return true.
+            code = zprava
 
-            tmp = convert_to_code(code[i].upper())
-
-            if tmp == -1:
+        # Checking what is first char in input, if it is . or -
+        # it will use convert to letter function
+        if is_code(code[0]):
+            final = convert_to_letter(code)
+            if final == -1 or final == '':
                 print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX "
                       "exp: ...|---|...| for sos")
-                break
-
             else:
-                finalLetter += tmp
+                final = ''.join(final)
+                return final
+        # Else it will use convert to code function
+        else:
+            finalLetter = []
+            for i in range(len(code)):
 
-        finalLetter = ''.join(finalLetter)
-        print(finalLetter)
-else:
-    print("ERROR HAS OCCURRED WHILE UNIT TESTING")
+                tmp = convert_to_code(code[i].upper())
+
+                if tmp == -1:
+                    print("ERROR INVALID INPUT, PLEASE USE THIS SYNTAX "
+                          "exp: ...|---|...| for sos")
+                    break
+
+                else:
+                    finalLetter += tmp
+
+            finalLetter = ''.join(finalLetter)
+            return finalLetter
+    else:
+        print("ERROR HAS OCCURRED WHILE UNIT TESTING")
+
+
+def test_is_code():
+    """Use this function for testing code."""
+    assert is_code(".")
+
+
+def test_convert_to_letter():
+    """Use this function for testing covert to letter function."""
+    convert_to_letter_text = convert_to_letter(".-|")
+    assert convert_to_letter_text == ["A"]
+
+
+def test_convert_to_code():
+    """Use this function for testing convert to code function."""
+    convert_to_code_test = convert_to_code("A")
+    assert convert_to_code_test == ".-|"
+
+
+def test_launch():
+    """Use this for test launch."""
+    launch_test = launch("A")
+    assert launch_test == ".-|"
+
+
+if __name__ == '__main__':
+    print(launch(""))
