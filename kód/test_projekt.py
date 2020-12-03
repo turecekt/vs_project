311dec37da01ba2f698b2a2fcb1f4dadcbd3c120
JakubSvec1 <75311912+JakubSvec1@users.noreply.github.com>
2020-12-03 13:11:40 +0100

    "pridanie pytestu"

---
+++ kód/test_projekt.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 12:51:26 2020
+
+@author: Jaqub
+"""
+import projekt.py
+
+def test_dlzkystran():
+    assert projekt.dlzkastrany(1,1,4,5)=="5.0"
