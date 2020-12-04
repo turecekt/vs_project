26e388427e13dd87a50ce671118102423e2566eb
2ez4me1 <75311956+2ez4me1@users.noreply.github.com>
2020-12-05 00:07:00 +0100

    "Dekorativne zmeny"

--- kód/projekt.py
+++ kód/projekt.py
@@ -1,52 +1,60 @@
+"""Proste projekt."""
+
 import math
-xarray = [1,4,2]
-yarray = [1,5,8]
+xarray = [1, 2, 3]
+yarray = [1, 2, 3]
 '''for i in range(3):
     xarray.append(input('Zadaj x suradnicu bodu '))
     yarray.append(input('Zadaj x suradnicu bodu '))'''
 
-
 Ax = xarray[0]
-Ay = yarray[0] 
+Ay = yarray[0]
 Bx = xarray[1]
 By = yarray[1]
 Cx = xarray[2]
 Cy = yarray[2]
 
-def dlzkastrany(ax,ay,bx,by):
+
+def dlzkastrany(ax, ay, bx, by):
+    """Funkcia pre vypocet dlzky strany trojuholnika."""
     return math.sqrt(((int(bx)-int(ax))**2)+((int(by)-int(ay))**2))
 
 
+print('Strana AB ma dlzku:', round(dlzkastrany(Ax, Ay, Bx, By), 4))
+print('Strana AC ma dlzku:', round(dlzkastrany(Ax, Ay, Cx, Cy), 4))
+print('Strana BC ma dlzku:', round(dlzkastrany(Bx, By, Cx, Cy), 4))
 
 
-print('Strana AB ma dlzku:',round(dlzkastrany(Ax,Ay,Bx,By),4),'\nStrana AC ma dlzku:',round(dlzkastrany(Ax,Ay,Cx,Cy),4),'\nStrana BC ma dlzku:',round(dlzkastrany(Bx,By,Cx,Cy),4))
-#---------------------
-def zostrojenost(a,b,c):
+def zostrojenost(a, b, c):
+    """Funkcia ktora zisti ci sa da trojuholnik zostrojit."""
     if (a + b > c and a + c > b and b + c > a):
-        return True
+        return 1
     else:
-        return False
+        return 0
 
-#---------------------
-def obvod(a,b,c):
-    return round(a+b+c,4)
 
-def obsah(a,b,c):
-    return round(a*(b*math.sin(math.acos((a**2 + b**2 - c**2)/(2*a*b))))/2,4)
-#---------------------
-def pravouhlost(a,b,c):
+def obvod(a, b, c):
+    """Funkcia vypocita obvod."""
+    return round(a+b+c, 4)
+
+
+def obsah(a, b, c):
+    """Funkcia vypocita obsah."""
+    return round(a*(b*math.sin(math.acos((a**2 + b**2 - c**2)/(2*a*b))))/2, 4)
+
+
+def pravouhlost(a, b, c):
+    """Funkcia zisti ci je trojuholnik pravouhly."""
     if(a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2):
-        return True
+        return 1
     else:
-        return False
+        return 0
 
-print('Obvod trojuholnika ABC je',obvod(dlzkastrany(Ax,Ay,Bx,By),dlzkastrany(Bx,By,Cx,Cy),dlzkastrany(Ax,Ay,Cx,Cy)))
-print('Obsah trojuholnika ABC je',obsah(dlzkastrany(Ax,Ay,Bx,By),dlzkastrany(Bx,By,Cx,Cy),dlzkastrany(Ax,Ay,Cx,Cy)))
-if(zostrojenost(dlzkastrany(Ax, Ay, Bx, By),dlzkastrany(Ax, Ay, Cx, Cy),dlzkastrany(Bx, By, Cx, Cy))==True):
-    print('Trojuholník ABC sa dá zostrojiť')
-else:
-    print('Trojuholník sa nedá zostrojiť')
 
-#--------unit testy--------
-
-#-------------
+s1 = dlzkastrany((Ax, Ay, Bx, By), 4)
+s2 = dlzkastrany((Ax, Ay, Cx, Cy), 4)
+s3 = dlzkastrany((Bx, By, Cx, Cy), 4)
+o = obvod(s1, s2, s3)
+S = obsah(s1, s2, s3)
+print(o)
+print(S)
