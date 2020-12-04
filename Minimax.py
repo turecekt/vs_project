347897204391460f6f0be86fc6e60017184acfb4
machalamartin1 <m_machala@utb.cz>
2020-12-04 09:36:39 +0100

    test
    Signed-off-by: machalamartin1 <m_machala@utb.cz>

--- Minimax.py
+++ Minimax.py
@@ -1,5 +1,8 @@
-import random
-rand = []
-for i in range(0, 19):
-    rand.append(random.randint(0, 200))
+import sys
 
+input = []
+for x in sys.argv:
+    input.append(x)
+
+for y in input:
+    print(y)
