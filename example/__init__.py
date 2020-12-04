8adca2ad9f39c41a60fd17ebad783641772caa26
Sosolikova <veronika.sosolikova@gmail.com>
2020-12-05 00:31:00 +0100

    Funkce nacti_text

--- example/__init__.py
+++ example/__init__.py
@@ -19,26 +19,29 @@ def compute(x):
     return x * x - 2 * x
 
 
+def nacti_text(nazev_souboru):
 
+    soubor = open(nazev_souboru, encoding='utf-8')
+    obsah = soubor.read()
+    soubor.close()
+    print("kolikátý je to znak: ",obsah.find("#"))
+    ukoncovaciZnak = obsah.find("#")
+    if ukoncovaciZnak == 0:
+        obsah = ""
+    elif ukoncovaciZnak < 0:
+        obsah = obsah
+    else:
+        obsah = obsah[0:ukoncovaciZnak]
 
-soubor = open('basnicka.txt', encoding='utf-8')
-obsah = soubor.read()
-soubor.close()
-"""print("kolikátý je to znak: ",obsah.find("#"))"""
-ukoncovaciZnak = obsah.find("#")
-if ukoncovaciZnak < 0:
-   obsah = obsah
-else:
-   obsah = obsah[0:ukoncovaciZnak]
-
+    return obsah
     
     
 
-print ("ukončovací znak: ",ukoncovaciZnak)
-print(obsah)
+# print ("ukončovací znak: ",ukoncovaciZnak)
+print(nacti_text('basnicka.txt'))
 
 """str = input ("Enter a string")"""
-str = obsah
+str = nacti_text('basnicka.txt')
 print ("String is ",str)
 str = str.lower()
 str = str.replace(" ","")
@@ -61,39 +64,32 @@ print("Počet různých znaků: ",len(count))
 MaxDictVal = max(count, key=count.get)
 print("Nejčetnější znak:",MaxDictVal)
 
-"""MinDictVal = min(count, key=count.get)"""
-"""print("Nejméně četný znak:",MinDictVal)"""
-
+MinDictVal = min(count, key=count.get)
+print("Nejméně četný znak:",MinDictVal)
 
 
 def nejmeneCetnyZnak(poctyZnaku):
-
     MinDictVal = min(poctyZnaku, key=poctyZnaku.get)
     return MinDictVal
 
-def kolikZnakuJeVtextu(obsah,nejmeneCetnyZnak):
-    return obsah.count(nejmeneCetnyZnak(poctyZnaku)
+# def kolikZnakuJeVtextu(obsah,nejmeneCetnyZnak):
+#     return obsah.count(nejmeneCetnyZnak(poctyZnaku)
                        
 
     
-print("Nejméně četný znak FUNKCE:",nejmeneCetnyZnak(count))
+# print("Nejméně četný znak FUNKCE:",nejmeneCetnyZnak(count))
 
-for x in count.keys ():
-    if count[x] == 2:
-        print("jen písmena",x)
-    else:
-        print("nefunguje")
-
-
-
-"""Ještě pořešit:
-    - jak vypsat všechny nejméně četné znaky
-        ještě získat hodnotu nejméně četného znaku a potom vypsat
-        jen ty nejméně četné
-    - když text nebude obsahovat #, tak neodečítat -1    --- hotovo
-    - průměrná četnost"""
+#  for x in count.keys ():
+#     if count[x] == 2:
+#         print("jen písmena",x)
+#     else:
+#         print("nefunguje") 
 
 
 
-
-
+# Ještě pořešit:
+#     - jak vypsat všechny nejméně četné znaky
+#         ještě získat hodnotu nejméně četného znaku a potom vypsat
+#         jen ty nejméně četné
+#     - když text nebude obsahovat #, tak neodečítat -1    --- hotovo
+#     - průměrná četnost
