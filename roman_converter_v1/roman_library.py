ffbb9191712e623c480f575b2a081c80f86566c3
Ladislav Tomeček <l_tomecek@utb.cz>
2020-12-04 16:01:30 +0100

    Files loaded

---
+++ roman_converter_v1/roman_library.py
@@ -0,0 +1,50 @@
+"""This module works with Roman Numerals.
+
+The module supplies one function: toRoman(self, iArabicNum). For example,
+
+>>> toRoman(self, 10)
+X
+"""
+
+class roman_lib:
+    
+
+    def toRoman(self, iArabicNum):
+
+        """Function comverts Arabic Number to Roman Numerals.
+        
+        Args:
+        
+            iArabicNum (int): an integer
+
+        Returns:
+
+            str: A string of Roman Numerals
+        """
+
+        value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbol = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        sRomanNum = ''
+        i = 0
+        while  iArabicNum > 0:
+            for x in range(iArabicNum // value[i]):
+                sRomanNum += symbol[i]
+                iArabicNum -= value[i]
+            i += 1
+        return sRomanNum
+
+    def test_toRoman():
+        
+        """Function tests comvertion of Arabic Numbers to Roman Numerals.
+        
+        Args:
+        
+            None
+
+        Returns:
+
+            None
+        """
+        
+        # all basic symbols are used exactly once
+        assert(toRoman(0, 1444)=="MCDXLIV")
