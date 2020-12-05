af13c62018d7469eedc3dab3bccd066679346f84
Naith_cz <m.raska@aragnet.cz>
2020-12-05 19:30:16 +0100

    Add first test for method test_findAnchor() in  links module.

--- test_source.py
+++ test_source.py
@@ -4,3 +4,15 @@
 #           Roman Mareček <r_marecek@utb.cz>
 #
 # License:
+
+import links
+
+# *********** Modul links  ***********
+
+link = links.Links()
+
+
+def test_findAnchor():
+    """Test for search <a as beginning HTML anchor."""
+    testText = "Totoje proste test"
+    assert (link.findAnchor(testText + " <a href=...", 0) == 19)
