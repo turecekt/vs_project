02a75d978636cddab6438f9b64dc30d715c43193
HawKerino <d_papulak@utb.cz>
2020-12-05 15:08:19 +0100

    added is_rectangular, changed is_able

--- __init__.py
+++ __init__.py
@@ -13,14 +13,22 @@ def side_lenght(a, b):
     lenght = math.sqrt(pow(int(a[0]) - int(b[0]), 2) + pow(int(a[1]) - int(b[1]), 2))
     return lenght
 
+
 def is_able(A, B, C):
-    ab = side_lenght(A, B)
-    bc = side_lenght(B, C)
-    ac = side_lenght(A, C)
-    if ab + bc > ac or ab + ac > bc or bc + ac > ab:
-        print("Trojuholník je zostrojiteľný")
-    else:
+    if(A == B or B == C or C == A):
         print("Trojuholník nie je zostrojiteľný")
+    else:
+        print("Trojuholník je zostrojiteľný")
+
+
+
+def is_rectangular(A, B, C):
+    sides = [side_lenght(A, B), side_lenght(B, C), side_lenght(A, C)]
+    sides.sort(reverse=True)
+    if round(sides[0] * sides[0]) == sides[1] * sides[1] + sides[2] * sides[2]:
+        print("Je pravouhlý")
+    else:
+        print("Nie je pravouhlý")
 
 
 if __name__ == '__main__':
@@ -31,4 +39,5 @@ if __name__ == '__main__':
     valid_input(B[0], B[1])
     C = input("zadejte souřadnice bodu C: ").split(",")
     valid_input(C[0], C[1])
-
+    is_rectangular(A, B, C)
+    is_able(A, B, C)
