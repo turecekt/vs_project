02c8ce7bbc8876b3f808f9fa677c284fc3770448
Jiri Einspigl <j_einspigl@utb.cz>
2020-12-05 22:49:31 +0100

    Code

--- Morseovka/Morse.py
+++ Morseovka/Morse.py
@@ -1,3 +1,6 @@
+"""Importy."""
+import collections
+
 """Zdroj hodnot."""
 knihovnaZnaku = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
                  'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -7,8 +10,8 @@ knihovnaZnaku = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
                  'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
                  '4': '....-', '5': '.....', '6': '-....', '7': '--...',
                  '8': '---..', '9': '----.', '0': '-----', ',': '--..--',
-                 '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-',
-                 '(': '-.--.', ')': '-.--.-'}
+                 '.': '.-.-.-', '?': '..--..', '(': '-.--.', ')': '-.--.-',
+                 '/': '-..-.', '-': '-....-'}   # Knihovna znaků
 
 
 def zasifruj(zprava):
@@ -17,17 +20,22 @@ def zasifruj(zprava):
     >>> zasifruj('TEST')
     '- . ... - '
     """
+    # proměná pro zašifrovanou zprávu
     sifra = ''
+    # for postupně vybere každý znak ze zprávy
     for znak in zprava:
         if znak != ' ':
+            # pokud je znak z knihovny - přiřadí znak z morseovy abecedy
+            # a přidá za něj mezeru
             sifra += knihovnaZnaku[znak] + ' '
         else:
+            # pokud je znak mezera - zapíše mezeru
             sifra += ' '
-
+    # vrací zašifrovanou zprávu
     return sifra
 
 
-def test_sifovani():
+def test_sifovani():    # Unit test metody zasifruj()
     """Unit test pro šifrování."""
     assert zasifruj('TEST') == '- . ... - '
 
@@ -38,76 +46,111 @@ def desifruj(zprava):
     >>> desifruj('- . ... -')
     'TEST'
     """
+    # přidá za zprávu mezeru pro zpřístupnění posledního znaku
     zprava += ' '
+    # proměná pro dešifrovanou zprávu
     desifrovano = ''
+    # proměná kam se ukládá jeden znak morseovky
     mznak = ''
+    # for postupně vybere každý znak ze zprávy
     for znak in zprava:
+        # kontroluje zda je znak mezera
         if (znak != ' '):
+            # řeší mezery mezi znaky
             i = 0
+            # zapíše znak ze zprávy do znaku morseovky
             mznak += znak
         else:
+            # pokud je i = 1 - nový charakter morseovky
             i += 1
+            # pokud je i = 2 - nové slovo
             if i == 2:
+                # přidá mezeru mezi slova
                 desifrovano += ' '
             else:
+                # ošetřuje nepovolené znaky
                 try:
+                    # přiřadí hodnotu morseovky ke znaku abecedy
+                    # a zapíše do proměné desifrovano
                     desifrovano += list(knihovnaZnaku.keys())[list(
                         knihovnaZnaku.values()).index(mznak)]
+                    # vynuluje znak morseovky
                     mznak = ''
                 except ValueError:
-                    print('Chyba...')
+                    print('Chyba... nepovolený znak')
                     konec()
-
+    # vrací dešifrovanou zprávu
     return desifrovano
 
 
-def test_desifovani():
+def test_desifovani():  # Unit test metody desifruj()
     """Unit test pro dešifrování."""
     assert desifruj('- . ... -') == 'TEST'
 
 
-def konec():
-    """Konec."""
-    x = input('Chcete pokračovat? [Y]/[N] \n')
-    if (x == 'N' or x == 'n'):
-        exit(0)
-    elif(x == 'Y' or x == 'y'):
-        menu()
-    else:
-        print('Chyba...')
-        konec()
-
-
-def menu():
+def main():
     """Menu."""
-    otazka = input('Chcete zprávu: \n [1] Zašifrovat \n [2] Dešifrovat' +
-                   '\n [3] Automatický překlad (může chybovat) \n')
+    # dotaz na akci
+    dotaz = input('Chcete: \n [1] Zašifrovat zprávu \n [2] Dešifrovat zprávu' +
+                  '\n [3] Automatický překlad zprávy (může chybovat) \n ' +
+                  '[4] Vypsat knihovnu znaků \n')
 
-    if (otazka == '1' or otazka == '1.'):
+    # if ... pro vybrání akce
+    if (dotaz == '1' or dotaz == '1.'):
+        # spouští šifrování
         zprava = input('Zadejte zprávu, kterou chcete zašifrovat' +
                        '(bez diakritiky): \n')
         vysledek = zasifruj(zprava.upper())
         print('šifruji...\nVýsledek: ' + vysledek)
         konec()
-    elif (otazka == '2' or otazka == '2.'):
+    elif (dotaz == '2' or dotaz == '2.'):
+        # spouští dešifrování
         zprava = input('Zadejte zašifrovanou zprávu: \n')
         vysledek = desifruj(zprava)
         print('dešifruji...\nVýsledek: ' + vysledek.lower())
         konec()
-    elif (otazka == '3' or otazka == '3.'):
+    elif (dotaz == '3' or dotaz == '3.'):
         zprava = input('Zadejte zprávu: \n')
+        # rozhoduje zda je zpráva zašifrovaná
         if (zprava.startswith('.') or zprava.startswith('-')):
+            # spouští dešifrování
             vysledek = desifruj(zprava)
             print("dešifruji...")
-            print('Výsledek: ' + vysledek)
+            print('Výsledek: ' + vysledek.lower())
             konec()
         else:
+            # spouští šifrování
             vysledek = zasifruj(zprava.upper())
             print("šifruji...")
             print('Výsledek: ' + vysledek)
             konec()
-    elif (otazka == 'test'):
-        return 'OK'
+    elif (dotaz == '4' or dotaz == '4.'):
+        # vypíše knihovnu znaků
+        temp = collections.Counter(knihovnaZnaku)
+        for z in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.?()/-':
+            print(z, ' = ', temp[z], '\t',)
+        konec()
     else:
         print('Neplatná hodnota')
         konec()
+
+
+def konec():
+    """Konec.
+
+    Ptá se zda chtějí pokračovat
+    Y - spouší znovu main() / N - ukončuje program
+    """
+    x = input('Chcete pokračovat? [Y]/[N] \n')
+    if (x == 'N' or x == 'n'):
+        exit(0)
+    elif(x == 'Y' or x == 'y'):
+        main()
+    else:
+        print('Chyba...')
+        konec()
+
+
+# Spuštění programu
+if __name__ == '__main__':
+    main()
