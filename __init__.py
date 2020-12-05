6b6ac924a16d5d71304b10ae738371d0a52b9420
Adam Bubnik <halfrozen01@gmail.com>
2020-12-05 15:55:30 +0100

    code has been reformated. valid_input fix

--- __init__.py
+++ __init__.py
@@ -6,25 +6,29 @@ def valid_input(x, y):
         int(x)
         int(y)
     except ValueError:
-        return "Napište hodnoty ve správném formátu!"
+        exit("Napište hodnoty ve správném formátu!")
 
 
-def side_lenght(a, b):
-    lenght = math.sqrt(pow(int(a[0]) - int(b[0]), 2) + pow(int(a[1]) - int(b[1]), 2))
-    return lenght
+def side_lenght(point_1, point_2):
+    return math.sqrt(pow(int(point_1[0]) - int(point_2[0]), 2) + pow(int(point_1[1]) - int(point_2[1]), 2))
 
 
-def is_able(A, B, C):
-    if(A == B or B == C or C == A):
+def is_able(point_a, point_b, point_c):
+    if point_a == point_b or point_b == point_c or point_c == point_a:
+
         print("Trojuholník nie je zostrojiteľný")
     else:
         print("Trojuholník je zostrojiteľný")
 
 
-
-def is_rectangular(A, B, C):
-    sides = [side_lenght(A, B), side_lenght(B, C), side_lenght(A, C)]
+def is_rectangular(point_a, point_b, point_c):
+    sides = [
+        side_lenght(point_a, point_b),
+        side_lenght(point_b, point_c),
+        side_lenght(point_a, point_c)
+    ]
     sides.sort(reverse=True)
+
     if round(sides[0] * sides[0]) == sides[1] * sides[1] + sides[2] * sides[2]:
         print("Je pravouhlý")
     else:
@@ -33,11 +37,15 @@ def is_rectangular(A, B, C):
 
 if __name__ == '__main__':
     print("například: 1,1")
-    A = input("zadejte souřadnice bodu A: ").split(",")
-    valid_input(A[0], A[1])
-    B = input("zadejte souřadnice bodu B: ").split(",")
-    valid_input(B[0], B[1])
-    C = input("zadejte souřadnice bodu C: ").split(",")
-    valid_input(C[0], C[1])
-    is_rectangular(A, B, C)
-    is_able(A, B, C)
+
+    point_a = input("zadejte souřadnice bodu A: ").split(",")
+    valid_input(point_a[0], point_a[1])
+
+    point_b = input("zadejte souřadnice bodu B: ").split(",")
+    valid_input(point_b[0], point_b[1])
+
+    point_c = input("zadejte souřadnice bodu C: ").split(",")
+    valid_input(point_c[0], point_c[1])
+
+    is_rectangular(point_a, point_b, point_c)
+    is_able(point_a, point_b, point_c)
