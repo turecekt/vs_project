e2c38a1e47bd6153b47bef7104f55052a12366bf
Naith_cz <m.raska@aragnet.cz>
2020-12-05 19:37:08 +0100

    Add test for method test_findTargetURL() in  links module.

--- test_source.py
+++ test_source.py
@@ -16,3 +16,10 @@ def test_findAnchor():
     """Test for search <a as beginning HTML anchor."""
     testText = "Totoje proste test"
     assert (link.findAnchor(testText + " <a href=...", 0) == 19)
+
+
+def test_findTargetURL():
+    """Test to find the target URL."""
+    testText = "Totoje proste test"
+    assert (link.findTargetURL(0, testText))
+    assert (link.findTargetURL(-1, testText) == -1)
