181072c1aba9f9f91d22b0a3944fd70447b9c0fb
tomasprikasky <tom.prikasky@seznam.cz>
2020-12-06 19:45:05 +0100

    logic for evaluate if number is prime number

--- primeNumber.py
+++ primeNumber.py
@@ -2,6 +2,14 @@ number = input("Vlož číslo: ")
 if number.isdigit() and int(number) > 0:
     number = int(number)
     divisors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            divisors.append(i)
+    print(f"delitelia: {divisors}")
+    if len(divisors) == 2:
+        print(f"Číslo {number} je prvočíslo")
+    else:
+        print(f"Číslo {number} není prvočíslo")
 
 else:
     print("Nebylo zadáno kladné číslo")
