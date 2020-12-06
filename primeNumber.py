402dd8bfd3a3dbc063a35cc4c694551dbd880d4b
Kubaskova <kubaskova.lucia@gmail.com>
2020-12-06 19:53:03 +0100

    refactoring

--- primeNumber.py
+++ primeNumber.py
@@ -2,13 +2,19 @@ number = input("Vlož číslo: ")
 if number.isdigit() and int(number) > 0:
     number = int(number)
     divisors = []
+    count = 0
+
     for i in range(1, number + 1):
         if number % i == 0:
             divisors.append(i)
-    print(f"delitelia: {divisors}")
-    if len(divisors) == 2:
+            count += 1
+            if count > 2:
+                break
+    if count == 2:
+        print(f"delitelia: {divisors}")
         print(f"Číslo {number} je prvočíslo")
     else:
+        print(f"delitelia: {divisors}")
         print(f"Číslo {number} není prvočíslo")
 
 else:
