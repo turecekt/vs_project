f6e1ecf61aa7e9dec4bec068e7754dc0a5e01abc
HJosster <j_huml@utb.cz>
2020-12-07 14:34:02 +0100

    fixed no input - added randoms

--- projekt/minimax.py
+++ projekt/minimax.py
@@ -1,28 +1,32 @@
 import sys
 import os.path
 from os import path
+import random
 
 pole = []
-sys.argv.pop(0)
-if(path.exists(sys.argv[0])):
-    file = open(sys.argv[0], "r")
-    words = file.read().splitlines()
-    file.close()
-    data = []
-    for x in words:
-        data = x.split()
-        for k in data:
+if len(sys.argv) > 1:
+    sys.argv.pop(0)
+    if(path.exists(sys.argv[0])):
+        file = open(sys.argv[0], "r")
+        words = file.read().splitlines()
+        file.close()
+        data = []
+        for x in words:
+            data = x.split()
+            for k in data:
+                try:
+                    TryParse = int(k)
+                    pole.append(TryParse)
+                except ValueError:
+                    sys.exit("Všechny vstupy musí být typu int!")
+    else:
+        for x in sys.argv:
             try:
-                TryParse = int(k)
+                TryParse = int(x)
                 pole.append(TryParse)
             except ValueError:
                 sys.exit("Všechny vstupy musí být typu int!")
 else:
-    for x in sys.argv:
-        try:
-            TryParse = int(x)
-            pole.append(TryParse)
-        except ValueError:
-            sys.exit("Všechny vstupy musí být typu int!")
+    pole = [random.randint(1,100) for _ in range(10)]
 
 print(pole)
