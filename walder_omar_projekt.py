cb139811ff265d00fdd025b7c45fba009decadd4
Omar Walder <o_walder@utb.cz>
2020-12-07 15:12:10 +0100

    Zakladni metoda a třída

--- walder_omar_projekt.py
+++ walder_omar_projekt.py
@@ -1,19 +1,46 @@
-"""This is the "example" module.
+"""Number to Rome Numeral converter.
 
-The example module supplies one function: compute(). For example,
-
->>> compute(3)
-3
+This is a Python converter for numbers, it converts its input (form of a number)
+and returns an output in the form of a Roman Numeral
 """
 
 
-def compute(x):
-    """Functon compute returns evaluation of expression using argument x.
+class Converter():
+    """Convert number."""
+    
+    def convert(self, num):
+        """Function convert returns a Roman Numeral based on argument num, which is a full number.
 
-    Args:
-        - x - Input of the function
+        Args:
+            - num - Input of the function
 
-    Returns:
-        - output - Output of the function
-    """
-    return x * x - 2 * x
+        Returns:
+            - output - Output of the function
+        """
+        numbers = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        romannumbers = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman = ''
+        i = 0
+        while num > 0:
+            for x in range(num // numbers[i]):
+                roman += romannumbers[i]
+                num -= numbers[i]
+            i += 1
+        return roman
+
+if __name__ == '__main__':
+    print('Running')
+
+print('Zadejte hodnotu pro převod:')
+numIn = int(input())
+print(Converter().convert(numIn))
