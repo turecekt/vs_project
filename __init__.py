a504b07bfa1765a8339ef7215120e2951ab27a71
Adam Bubnik <halfrozen01@gmail.com>
2020-12-07 15:25:14 +0100

    docstring added

--- __init__.py
+++ __init__.py
@@ -2,19 +2,41 @@ import math
 
 
 class Rectangle:
+    """
+        Class of the rectangle
+
+        :ivar
+            point_a, point_b, point_c: used for storing the points inserted by user
+            a, b, c: used for storing the calculated sides
+    """
 
     def __init__(self):
         print("Souřadnice zadávejte ve formátu: x,y\nNapříklad: 1,1")
-        ex, self.point_a = input_values(point_name="A")
-        ex, self.point_b = input_values(point_name="B")
-        ex, self.point_c = input_values(point_name="C")
+        t, self.point_a = input_values(point_name="A")
+        t, self.point_b = input_values(point_name="B")
+        t, self.point_c = input_values(point_name="C")
         self.a, self.b, self.c = self.side_lenght()
 
-    def side_lenght(self):
-        sides = [(self.point_a, self.point_b), (self.point_a, self.point_c), (self.point_b, self.point_c)]
-        return (math.sqrt(pow(i[0] - j[0], 2) + pow(i[1] - j[1], 2)) for i, j in sides)
+    def side_lenght(self) -> list:
+        """
+        Take inserted points and calculate sides
 
-    def is_able(self):
+        :return: [float, float, float] -> list
+        """
+        sides = (self.point_a, self.point_b), \
+                (self.point_a, self.point_c), \
+                (self.point_b, self.point_c)
+
+        return [
+            math.sqrt(pow(i[0][0] - i[1][0], 2) + pow(i[0][1] - i[1][1], 2))
+            for i in sides]
+
+    def is_able(self) -> bool:
+        """
+        Check if the triangle is able to construct
+
+        :return: True | False
+        """
         a, b, c = self.a, self.b, self.c
 
         if a + b > c and a + c > b and c + b > a:
@@ -22,25 +44,49 @@ class Rectangle:
         else:
             return False
 
-    def is_rectangular(self):
+    def is_rectangular(self) -> bool:
+        """
+        Check if there is 90 degrees corner in the triangle
+
+        :return: True | False
+        """
         sides: list[float] = [self.a, self.b, self.c]
         sides.sort(reverse=True)
 
-        if round(sides[0] * sides[0]) == sides[1] * sides[1] + sides[2] * sides[2]:
+        if round(sides[0] * sides[0]) == sides[1] * sides[1] + sides[2] * \
+                sides[2]:
             return True
         else:
             return False
 
-    def perimeter(self):
+    def perimeter(self) -> float:
+        """
+        Calculate the other contour of the triangle
+
+        :return: perimeter of the triangle (float)
+        """
         return self.a + self.b + self.c
 
-    def area(self):
+    def area(self) -> float:
+        """
+        Calculate the inner area of the triangle
+
+        :return: Area of the triangle (float)
+        """
         s = self.perimeter() / 2
         return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
 
 
 def input_values(point_name):
-    point = str_list_to_int(input(f"Zadejte souřadnice bodu {point_name}: ").split(","))
+    """
+    User input method
+    Split user input into list
+
+    :param point_name:
+    :return: True, user input (list[int, int]) -> (tuple) | False
+    """
+    point = str_list_to_int(
+        input(f"Zadejte souřadnice bodu {point_name}: ").split(","))
     if point:
         return point
     else:
@@ -48,10 +94,16 @@ def input_values(point_name):
         return input_values(point_name)
 
 
-def str_list_to_int(list_to_convert):
+def str_list_to_int(list_to_convert) -> list or bool:
+    """
+    Check if user input is in the right format
+
+    :param list_to_convert: user input converted to list (list[str])
+    :return: True & User input converted to (int) -> (tuple) | False
+    """
     try:
-        x = list_to_convert[0]
-        x = list_to_convert[1]
+        t = list_to_convert[0]
+        t = list_to_convert[1]
         return True, [int(i) for i in list_to_convert]
     except ValueError or NameError:
         return False
@@ -68,6 +120,7 @@ if __name__ == '__main__':
         )
         print(f"Obvod trojúhelníku je: {round(rectangle.perimeter(), 2)}")
         print(f"Obsah trojúhelníku je: {round(rectangle.area(), 2)}")
-        print("Je pravouhlý" if rectangle.is_rectangular() else "Není pravouhlý")
+        print(
+            "Je pravouhlý" if rectangle.is_rectangular() else "Není pravouhlý")
     else:
         print("Trojuholník nie je zostrojiteľný")
