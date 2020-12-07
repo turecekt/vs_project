53e8dfdd614ad994a86181d5cab9a00b6aeba6c2
Kubaskova <kubaskova.lucia@gmail.com>
2020-12-07 23:29:52 +0100

    create non-prime number test

--- test_primeNumber.py
+++ test_primeNumber.py
@@ -27,5 +27,23 @@ class TestPrimeNumber(unittest.TestCase):
         self.assertEqual(vypis, 'Číslo 5 je prvočíslo')
 
 
+    def test_tenIsNotPrimeNumber(self, ):
+        number = 10
+        number_is_prime_number = True
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                number_is_prime_number = False
+                break
+        if number_is_prime_number and number > 1:
+            vypis = "Číslo 10 je prvočíslo"
+            print(f"{vypis}")
+            print("\n")
+        else:
+            vypis = "Číslo 10 není prvočíslo"
+            print(f"{vypis} ")
+            print("\n")
+        self.assertEqual(vypis, 'Číslo 10 není prvočíslo')
+
+
 if __name__ == '__main__':
     unittest.main()
