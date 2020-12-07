b92a0479a70e3404206c4489251cdac7e7651674
Denisa Bartova <d_bartova@utb.cz>
2020-12-07 19:33:24 +0100

    Uprava kodu pro flake8 test

--- soustava.py
+++ soustava.py
@@ -1,36 +1,36 @@
+"""Program pro převod čísla z desítkové soustavy.
+
+Program obsahuje 1 funkci: prevod()
+"""
 # -*- coding: utf-8 -*-
 import math as m
 
-"""Program pro převod čísla z desítkové soustavy
 
-Program obsahuje 1 funkci: Prevod()
+def prevod(c, s):
+    """Funkce prevod převádí zadané číslo z 10-ové soustavy na zvolenou soustavu.
 
-"""
-
-def Prevod(c, s):
-    """Funkce Prevod převádí zadané číslo z desítkové soustavy na zvolenou soustavu 
-    
     Args:
         - c - kladné celé číslo v desítkové soustavě
-        - s - 2, 8, 12 nebo 16 - cílová soustava, do které chceme převést číslo c
-        
-    Returns
+        - s - 2, 8, 12 nebo 16 - cílová soustava, do které chceme převést
+        číslo c
+
+    Returns:
         - output - řetězec vracející číslo v cílové číselné soustavě
     """
     # vytvoření prázdného řetězce "vysledek" pro uložení výsledné hodnoty
-    vysledek =""
+    vysledek = ""
     # cyklus, ve kterém číslo dělíme soustavou, dokud se nerovná 0
     while(c/s != 0):
-        #ukládání zbytku po dělení do proměnné "zbytek"
+        # ukládání zbytku po dělení do proměnné "zbytek"
         zbytek = c % s
         vysledek = vysledek + str(zbytek)
-        #přepsání hodnoty do proměnné "c" po vydělení a zaokrouhlení
+        # přepsání hodnoty do proměnné "c" po vydělení a zaokrouhlení
         c = m.floor(c/s)
-        
-    #Výsledek otočen a vypsán    
+
+    # Výsledek otočen a vypsán
     return vysledek[::-1]
 
-# pytest
+
 def test_soustava():
-    assert Prevod(405, 8) == 625
-    
+    """Funkce pro otestování programu."""
+    assert prevod(405, 8) == '625'
