ad3830f41aa12e251e84336d2ccc805a3abd2d4d
Omar Walder <o_walder@utb.cz>
2020-12-08 14:32:21 +0100

    Projekt, bez kontroly flake8

--- walder_omar_projekt.py
+++ walder_omar_projekt.py
@@ -1,15 +1,16 @@
-"""Number to Rome Numeral converter.
+"""Number converter.
 
-This is a Python converter for numbers, it converts it's input (form of a number)
+This is a Python converter for normal numbers
+it converts it's input (in the form of a number)
 and returns an output in the form of a Roman Numeral
 """
 
 
 class Converter():
     """Convert number."""
-    
+
     def convert(self, num):
-        """Function convert returns a Roman Numeral based on argument num, which is a full number.
+        """Function returns a Roman Numeral based on the input, which is a full number.
 
         Args:
             - num - Input of the function
@@ -17,36 +18,36 @@ class Converter():
         Returns:
             - roman - The converter number
         """
-        #Vytvoříme pole pro obyčejné čísla
+        # Vytvoříme pole pro obyčejné čísla
         numbers = [
             1000, 900, 500, 400,
             100, 90, 50, 40,
             10, 9, 5, 4,
             1
             ]
-        #Vytvoříme pole pro Římské číslice
+        # Vytvoříme pole pro Římské číslice
         romannumbers = [
             "M", "CM", "D", "CD",
             "C", "XC", "L", "XL",
             "X", "IX", "V", "IV",
             "I"
             ]
-        #Vytvoříme string, který bude return a bude mít převedené římské číslice
+        # Vytvoříme string, který bude return a bude mít převedené římské číslice
         roman = ''
-        #Vytvoříme číslo pro pozice v polích
+        # Vytvoříme číslo pro pozice v polích
         i = 0
-        #Zadáme podmínku, že když bude input číslo 0, tak je metoda hotová a vrátí Římskou číslici
+        # Zadáme podmínku, že když bude input číslo 0, tak je metoda hotová a vrátí Římskou číslici
         while num > 0:
-            #For cyclus, který nám vídělí input číslo číslem z pole čísel se zbytkem, projede to celé pole
-            #dokud mu nezbyde žádný zbytek
+            # For cyclus, který nám vídělí input číslo číslem z pole čísel se zbytkem, projede to celé pole
+            # dokud mu nezbyde žádný zbytek
             for x in range(num // numbers[i]):
-                #Přídá číslo z pole s Římskými číslicemi do outputu
+                # Přídá číslo z pole s Římskými číslicemi do outputu
                 roman += romannumbers[i]
-                #Odečte od input čísla hodnotu, která již byla převedena
+                # Odečte od input čísla hodnotu, která již byla převedena
                 num -= numbers[i]
-            #Přidá číslu i 1, aby se pole dále projeli pro další převod
+            # Přidá číslu i 1, aby se pole dále projeli pro další převod
             i += 1
-        #Vrátí Římskou číslici
+        # Vrátí Římskou číslici
         return roman
 
 
@@ -55,32 +56,44 @@ class Converter():
 
         Prints a message depending on the result of the test
         """
+        # Nastavíme si dvě testovací čísla
         num1 = 1
         num2 = 586
+        # Nastavíme si jaký by měl vyjít výsledek
         result1 = 'I'
         result2 = 'DLXXXVI'
+        # Otestujeme, jestli se hodnoty shodují s tím, jak mají vyjít
         num1_test = Converter().convert(num1)
         num2_test = Converter().convert(num2)
+        # Nastavíme if, pro obě situace
         if result1 == num1_test and result2 == num2_test:
+            # Jestli se hodnoty rovnají, napíše na konzoli, že metoda pracuje
             print("Metoda convert pracuje.")
         else:
+            # Jestli se hodnoty nerovanjí, napíše na konzoli, že metoda nepracuje
             print("Metoda convert nepracuje!")
+            exit()
 
-
-    def test_convertInput()
-        """Function test_convertInput tests, if the input for the method convert is a number and isn't blank
-
-        Prints a message depending on the result of the test
-        """
 
 if __name__ == '__main__':
     print('Running')
 
+# Zavoláme metodu pro testování funkčnosti metody convert
 Converter().test_convert()
-#Vložíme do konzole otázku pro uživatele, aby zadal číslici pro převod
-print('Zadejte hodnotu pro převod:')
-#Nastaví výsledek (vloženou hodnotu) jako promměnou, aby jsme s ní mohli pracovat
-#numIn = int(input())
-numIn = input()
-#Vloženou hodnotu vložíme do metody convert, aby ji převedla
-print(Converter().convert(numIn))
+print('')
+# Vytvoříme promměnou opakovani, která bude 0, dokud uživatel nezadá správnou hodnotu
+opakovani = 0
+while opakovani == 0:
+    try:
+        # Vložíme do konzole otázku pro uživatele, aby zadal číslici pro převod
+        numIn = int(input('Zadejte hodnotu pro převod: '))
+        # Nastaví výsledek (vloženou hodnotu) jako argument pro metodu convert a výsledek vložíme na konzoli
+        print(Converter().convert(numIn))
+        # Opakování ukončíme
+        opakovani += 1
+    except:
+        print('')
+        # Když uživatel zadá špatně (string nebo prázdné) input, konzole vypíše že je to napsané špatně
+        # a celý proces opakuje
+        print('Hodnota byla zadána ve špatném tvaru')
+        print('')
