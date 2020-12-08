9fe82ba094c453d53f7f53a5c96a7585ee1cc6db
Nikola Jurenova <nikola1507@seznam.cz>
2020-12-08 19:11:27 +0100

    Fix comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#funkce pro prevod arabskeho cisla na rimska
+# funkce pro prevod arabskeho cisla na rimska
 def prevod(cislo):
-    #rimska cisla
+    # rimska cisla
     rimska = [
         "M", "CM", "D", "CD",
         "C", "XC", "L", "XL",
         "X", "IX", "V", "IV",
         "I"
     ]
-    #arabska cisla
+    # arabska cisla
     arabska = [
         1000, 900, 500, 400,
         100, 90, 50, 40,
@@ -17,19 +17,19 @@ def prevod(cislo):
 
     rimska_cislice = ''
     i = 0
-    #zacneme loop dokud neni cislo 0
+    # zacneme loop dokud neni cislo 0
     while cislo > 0:
-        #do stringu zapiseme ekvivalent arabskeho cisla rimskym
-        #a ubereme z puvodniho cisla
+        # do stringu zapiseme ekvivalent arabskeho cisla rimskym
+        # a ubereme z puvodniho cisla
         for _ in range(cislo // arabska[i]):
             rimska_cislice += rimska[i]
             cislo -= arabska[i]
         i += 1
-    #vracime hotovy string
+    # vracime hotovy string
     return rimska_cislice
 
 
-#try/except je zde kvuli moznosti nezadani cisla
+# try/except je zde kvuli moznosti nezadani cisla
 try:
     print("Rimska cislice: ", prevod(int(input("Zadej cislo: "))))
 except Exception:
