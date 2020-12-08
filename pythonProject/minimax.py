4da0a548dd416c4e9193512d0c0ba945eead05b4
tomas-uhrik <t_uhrik@utb.cz>
2020-12-08 14:30:36 +0100

    merge sort algorithm added

--- pythonProject/minimax.py
+++ pythonProject/minimax.py
@@ -92,3 +92,42 @@ def array_min_value(array):
             min = array[x + 1]
             index = x+1
     return min, index
+
+
+def merge_sort(inp_arr):
+    """Merge sort algorithm."""
+    size = len(inp_arr)
+    if size > 1:
+        middle = size // 2
+        left_arr = inp_arr[:middle]
+        right_arr = inp_arr[middle:]
+
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        p = 0
+        q = 0
+        r = 0
+
+        left_size = len(left_arr)
+        right_size = len(right_arr)
+        while p < left_size and q < right_size:
+            if left_arr[p] < right_arr[q]:
+                inp_arr[r] = left_arr[p]
+                p += 1
+            else:
+                inp_arr[r] = right_arr[q]
+                q += 1
+
+            r += 1
+
+        while p < left_size:
+            inp_arr[r] = left_arr[p]
+            p += 1
+            r += 1
+
+        while q < right_size:
+            inp_arr[r] = right_arr[q]
+            q += 1
+            r += 1
+    return inp_arr
