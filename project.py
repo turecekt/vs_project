90e0ff3956f483af8188c983e005c94e81b86213
KordacVojtech <v_kordac@utb.cz>
2020-12-08 01:19:10 +0100

    Pridani pulky commentu

--- project.py
+++ project.py
@@ -1,16 +1,19 @@
-userInp = input('Zadejte cele cislo na preklad :')
-userInp = int(userInp)
+userInp = input('Zadejte cele cislo na preklad :')  #Tohle vezme input od uzivatele
+userInp = int(userInp) #Tohle prevede input od uzivatele na int aby to program mohl zpracovat
 
 class romanNum:
 
+#Vytvorime funcki ktera vezme userInp jako vstupni parametr
     def intToRom(self, userInp):
 
+#Nadefinujeme si cisla na ktere pak budem odkazovat
         val = [
             1000, 900, 500, 400,
             100, 90, 50, 40,
             10, 9, 5, 4,
             1
             ]
+#Nadefinujeme si symboly na ktere pak budem odkazovat
         syb = [
             "M", "CM", "D", "CD",
             "C", "XC", "L", "XL",
