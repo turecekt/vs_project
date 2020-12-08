e566b68bbe30c29952db3082b6040a743fffb62c
KordacVojtech <v_kordac@utb.cz>
2020-12-08 01:35:22 +0100

    Pouziti linteru

--- project.py
+++ project.py
@@ -1,38 +1,40 @@
-userInp = input('Zadejte cele cislo na preklad :')  #Tohle vezme input od uzivatele
-userInp = int(userInp) #Tohle prevede input od uzivatele na int aby to program mohl zpracovat
+userInp = input('Zadejte cele cislo na preklad :')  # Tohle vezme input od uzivatele
+userInp = int(userInp)  # Tohle prevede input od uzivatele na int aby to program mohl zpracovat
+
 
 class romanNum:
 
-#Vytvorime funcki ktera vezme userInp jako vstupni parametr
+    # Vytvorime funcki ktera vezme userInp jako vstupni parametr
     def intToRom(self, userInp):
 
-#Nadefinujeme si cisla na ktere pak budem odkazovat
+        # Nadefinujeme si cisla na ktere pak budem odkazovat
         val = [
             1000, 900, 500, 400,
             100, 90, 50, 40,
             10, 9, 5, 4,
             1
-            ]
-#Nadefinujeme si symboly na ktere pak budem odkazovat
+        ]
+        # Nadefinujeme si symboly na ktere pak budem odkazovat
         syb = [
             "M", "CM", "D", "CD",
             "C", "XC", "L", "XL",
             "X", "IX", "V", "IV",
             "I"
-            ]
-#Vytvorim transNum ktery zatim bude mit value prazdneho stringu
+        ]
+        # Vytvorim transNum ktery zatim bude mit value prazdneho stringu
         transNum = ''
-#While loopem loopnu syb a val, priradim hodnotu k transNum ktera odpovida danemu user inputu (userInp)
+        # While loopem loopnu syb a val, priradim hodnotu k transNum ktera odpovida danemu user inputu (userInp)
         i = 0
-        while  userInp > 0:
+        while userInp > 0:
             for _ in range(userInp // val[i]):
                 transNum += syb[i]
                 userInp -= val[i]
             i += 1
 
-#Vratim transNum
+        # Vratim transNum
         return transNum
 
-#Do konzole vypisu prevedenou hodnotu userInp. V kozoli se ukaze hodnota cisla ktere zadal user v rimskych cislicih
+
+# Do konzole vypisu prevedenou hodnotu userInp. V kozoli se ukaze hodnota cisla ktere zadal user v rimskych cislicih
 
 print(romanNum().intToRom(userInp))
