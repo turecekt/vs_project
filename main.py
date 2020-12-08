1e1f979a66f09c5a34a97293cafdfc7345ab1f61
Marsiso <m_olsak@utb.cz>
2020-12-08 12:32:26 +0100

    Testing files.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Minimax."""
+
+
+from library import choose_sorting_method, inicializer
+
+
+"""Find minimum and maximum of array\
+with sorting methods"""
+
+
+# choose_sorting_method(inicializer())
