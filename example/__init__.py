d18d21d1efc4cd7fd66a620d68419b3a98453fc5
Sosolikova <veronika.sosolikova@gmail.com>
2020-12-08 15:28:45 +0100

    Opravený kód Flake8

--- example/__init__.py
+++ example/__init__.py
@@ -18,21 +18,23 @@ def compute(x):
     """
     return x * x - 2 * x
 
+
 nazev_souboru = 'basnicka.txt'
 
+
 def nacti_text(nazev_souboru):
-    """Funkce načte text ze souboru. Pokud je přítomen znak "#", načte text jen po tento znak. Soubor zavře.
+    """Funkce načítá znak ze souboru po znak "#". Soubor zavře.
 
     Args:
         - nazev_souboru - Soubor formátu .txt, který chceme načítst
 
     Returns:
         - obsah - Vrací string """
-    
+
     soubor = open(nazev_souboru, encoding='utf-8')
     obsah = soubor.read()
     soubor.close()
-    print("kolikátý je to znak: ",obsah.find("#"))
+    # print("kolikátý je to znak: ", obsah.find("#"))
     ukoncovaciZnak = obsah.find("#")
     if ukoncovaciZnak < 0:
         obsah = obsah
@@ -40,60 +42,101 @@ def nacti_text(nazev_souboru):
     elif ukoncovaciZnak > 0:
         obsah = obsah[0:ukoncovaciZnak]
         return obsah
-    else: 
-        print ("v souboru je ukončovací znak hned na začátku")
+    else:
+        print("v souboru je ukončovací znak hned na začátku")
 
 
-print("základní text: ",nacti_text(nazev_souboru))
+print("Text: ", nacti_text(nazev_souboru))
+
 
 def uprav_text(text):
-    """ Funkce upravuje text: převede všechna písmena na málá a odstraní mezery mezi slovy.
+    """ Funkce převádí písmena na málá, odstraní mezery.
 
         Args:
             - text - Vstup je string, který chceme upravit
-        
-        Returns
+
+        Returns:
             - text - Vrací upravený string """
 
     str = text.lower()
-    str = str.replace(" ","")
+    str = str.replace(" ", "")
     return str
 
-print("upravený text: ",uprav_text(nacti_text(nazev_souboru)))
+
+# print("Upravený text: ", uprav_text(nacti_text(nazev_souboru)))
+print("Celkový počet znaků (bez mezer): ", len(uprav_text(nacti_text(nazev_souboru))))
 
 
-def zjisti_cetnosti (text):
+def zjisti_cetnosti(text):
+    """ Funkce z textu zjistí četnosti jednotlivých znaků.
+
+        Args:
+            - text - String ve kterém zjišťujeme četnosti znaků
+
+        Returns:
+            - count - Vrací četnosti (dictionary)"""
+
     pripraveny_text = uprav_text(nacti_text(text))
     count = {}
     for x in pripraveny_text:
         if x in count.keys():
-            count[x] +=1
-            
+            count[x] += 1
+
         else:
-            count[x] = 1    
+            count[x] = 1
     return(count)
+
 
 print(zjisti_cetnosti(nazev_souboru))
 
-# for x in count.keys ():
-#     print("znak",x ,"se v textu vyskytuje: ",count[x])
-
 
 def pocet_ruznych_znaku(slovnik):
+    """ Funkce vrátí počet různých znaků.
+
+        Args:
+            - slovnik - četnosti (dictionary)
+
+        Returns:
+            - pocty - počet znaků"""
+
     pocty = len(slovnik)
     return pocty
 
-print ("z metody počet různých znaků: ",pocet_ruznych_znaku(zjisti_cetnosti(nazev_souboru)))
-    
+
+print("Počet různých znaků: ", pocet_ruznych_znaku(zjisti_cetnosti(nazev_souboru)))
+
+
 def nejcetnejsi_znaky(slovnik):
+    """ Funkce vrátí nejčetnější znak.
+
+        Args:
+            - slovnik - četnosti (dictionary)
+
+        Returns:
+            - MaxDictVal - nejčetnější znak (string)"""
+
     MaxDictVal = max(zjisti_cetnosti(nazev_souboru), key=zjisti_cetnosti(nazev_souboru).get)
     return MaxDictVal
-print("Nejčetnější znaky z metody: ", nejcetnejsi_znaky(zjisti_cetnosti(nazev_souboru)))
+
+
+print("Nejčetnější znak: ", nejcetnejsi_znaky(zjisti_cetnosti(nazev_souboru)))
+
 
 def nejmenecetne_znaky(slovnik):
+    """ Funkce vrátí nejméně četný znak.
+
+        Args:
+            - slovnik - četnosti (dictionary)
+
+        Returns:
+            - MinDictVal - nejméně četný znak (string)"""
+
     MinDictVal = min(zjisti_cetnosti(nazev_souboru), key=zjisti_cetnosti(nazev_souboru).get)
     return MinDictVal
-print("Nejméně četné znaky z metody: ", nejmenecetne_znaky(zjisti_cetnosti(nazev_souboru)))
+
+
+print("Nejméně četný znak: ", nejmenecetne_znaky(zjisti_cetnosti(nazev_souboru)))
+
 
 # MaxDictVal = max(count, key=count.get)
 # print("Nejčetnější znak:",MaxDictVal)
@@ -116,8 +159,6 @@ print("Nejméně četné znaky z metody: ", nejmenecetne_znaky(zjisti_cetnosti(n
 #         print("jen písmena",x)
 #     else:
 #         print("nefunguje") 
-
-
 
 # Ještě pořešit:
 #     - jak vypsat všechny nejméně četné znaky
