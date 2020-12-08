bc77ec80aec1488aaaff5452171d8585fc8c4ac5
Omar Walder <o_walder@utb.cz>
2020-12-08 14:43:24 +0100

    Kompletní projekt

--- walder_omar_projekt.py
+++ walder_omar_projekt.py
@@ -1,4 +1,4 @@
-"""Number converter.
+"""Convert normal number to Roman numeral.
 
 This is a Python converter for normal numbers
 it converts it's input (in the form of a number)
@@ -10,7 +10,10 @@ class Converter():
     """Convert number."""
 
     def convert(self, num):
-        """Function returns a Roman Numeral based on the input, which is a full number.
+        """Convert a normal number into a Roman numeral.
+
+        This method convert its input (a number) and based on it
+        returns it converted into Roman numerals
 
         Args:
             - num - Input of the function
@@ -32,14 +35,14 @@ class Converter():
             "X", "IX", "V", "IV",
             "I"
             ]
-        # Vytvoříme string, který bude return a bude mít převedené římské číslice
+        # Vytvoříme string, který bude mít převedené římské číslice
         roman = ''
         # Vytvoříme číslo pro pozice v polích
         i = 0
-        # Zadáme podmínku, že když bude input číslo 0, tak je metoda hotová a vrátí Římskou číslici
+        # Když bude input číslo 0, tak je metoda hotová a vrátí Římskou číslici
         while num > 0:
-            # For cyclus, který nám vídělí input číslo číslem z pole čísel se zbytkem, projede to celé pole
-            # dokud mu nezbyde žádný zbytek
+            # For cyclus, který vídělí input číslem, z pole numbers, se zbytkem
+            # projede to celé pole dokud mu nezbyde žádný zbytek
             for x in range(num // numbers[i]):
                 # Přídá číslo z pole s Římskými číslicemi do outputu
                 roman += romannumbers[i]
@@ -50,9 +53,8 @@ class Converter():
         # Vrátí Římskou číslici
         return roman
 
-
     def test_convert(self):
-        """Function test_convert tests, if the method converts correctly.
+        """Function tests, if the method converts correctly.
 
         Prints a message depending on the result of the test
         """
@@ -70,7 +72,8 @@ class Converter():
             # Jestli se hodnoty rovnají, napíše na konzoli, že metoda pracuje
             print("Metoda convert pracuje.")
         else:
-            # Jestli se hodnoty nerovanjí, napíše na konzoli, že metoda nepracuje
+            # Jestli se hodnoty nerovanjí, napíše na konzoli
+            # že metoda nepracuje
             print("Metoda convert nepracuje!")
             exit()
 
@@ -81,19 +84,27 @@ if __name__ == '__main__':
 # Zavoláme metodu pro testování funkčnosti metody convert
 Converter().test_convert()
 print('')
-# Vytvoříme promměnou opakovani, která bude 0, dokud uživatel nezadá správnou hodnotu
+# Vytvoříme promměnou opakovani, která bude 0
+# dokud uživatel nezadá správnou hodnotu
 opakovani = 0
 while opakovani == 0:
     try:
         # Vložíme do konzole otázku pro uživatele, aby zadal číslici pro převod
         numIn = int(input('Zadejte hodnotu pro převod: '))
-        # Nastaví výsledek (vloženou hodnotu) jako argument pro metodu convert a výsledek vložíme na konzoli
+        # Nastaví výsledek (vloženou hodnotu) jako argument pro metodu convert
+        # výsledek vložíme na konzoli
         print(Converter().convert(numIn))
         # Opakování ukončíme
         opakovani += 1
-    except:
+    except NameError:
         print('')
-        # Když uživatel zadá špatně (string nebo prázdné) input, konzole vypíše že je to napsané špatně
+        # Když uživatel zadá špatně (string nebo prázdné) input
+        # konzole vypíše že je to napsané špatně
         # a celý proces opakuje
+        print('Hodnota byla zadána ve špatném tvaru')
+        print('')
+    except ValueError:
+        # Stejné jako NameError expect
+        print('')
         print('Hodnota byla zadána ve špatném tvaru')
         print('')
