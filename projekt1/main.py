62d4464be8775262168ccfc6754e02bd8993cf5f
PospisilJ <j1_pospisil@utb.cz>
2020-12-09 18:52:56 +0100

    Uprava codu 01

--- projekt1/main.py
+++ projekt1/main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
 
 
-# Press the green button in the gutter to run the script.
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+
+            cipher += ' '
+
+    return cipher
+
+
+
+def decrypt(message):
+
+    message += ' '
+
+    decipher = ''
+    citext = ''
+    for letter in message:
+
+
+        if (letter != ' '):
+
+
+            i = 0
+
+
+            citext += letter
+
+        # in case of space
+        else:
+
+            i += 1
+
+
+            if i == 2:
+
+
+                decipher += ' '
+            else:
+
+
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                                                              .values()).index(citext)]
+                citext = ''
+
+    return decipher
+
+
+
+def main():
+    message = "sos"
+    result = encrypt(message.upper())
+    print(result)
+
+    message = "--. . . -.- ... -....- ..-. --- .-. -....- --. . . -.- ... "
+    result = decrypt(message)
+    print(result)
+
+
+# Executes the main function
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
