c128811fb62545197066c8d36fbebb2da4de30ca
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-09 22:51:39 +0100

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -1,34 +1,29 @@
+docstring = str
 # unit testy pro morseovku a jeji stringove fce
 
 import unittest
 
-#vytvoření testovací třídy class
+# vytvoření testovací třídy class
 
 
 class TestStringMethods(unittest.TestCase):
 
-
 # test pro spravnou funkci na zvětšení písmena
+# druhý test na zvětšení písmena
+# třetí test na zjištění správnosti funkce split
 
     def test_upper(self):
         self.assertEqual("abc".upper(), "ABC")
 
-
-# druhý test na zvětšení písmena
-
     def test_isupper(self):
         self.assertTrue("ABC".isupper())
         self.assertFalse("Abc".isupper())
-
-
-# test na zjištění správnosti funkce split
 
     def test_split(self):
         s = "miroslav olivik"
         self.assertEqual(s.split(), ["miroslav", "olivik"])
         with self.assertRaises(TypeError):
             s.split(2)
-
 
 if __name__ == "__main__":
     unittest.main()
