7810d5a36cc4d8e6e4c2dff9db0fcb6ad1a1994f
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-09 21:58:06 +0100

    Update morseovka_Miroslav_Olivik.py

--- morseovka_Miroslav_Olivik.py
+++ morseovka_Miroslav_Olivik.py
@@ -32,23 +32,23 @@ SLOVNIK_MORSEOVKA = {' ': '/', 'A': '.-',
 # A string necháme přečíst
 
 def Text_To_Code():
-       text2 = input("Napište slova, která chcete kodovat: ")
-       code2 = [SLOVNIK_MORSEOVKA
-                [i.upper()] + " "
-                for i in text2 if i.upper() in SLOVNIK_MORSEOVKA.keys()]
-       morseovka = "".join(code2)
-       print(morseovka)
+  text2 = input("Napište slova, která chcete kodovat: ")
+  code2 = [SLOVNIK_MORSEOVKA
+           [i.upper()] + " "
+           for i in text2 if i.upper() in SLOVNIK_MORSEOVKA.keys()]
+  morseovka = "".join(code2)
+  print(morseovka)
 
 # Stejný způsob použijeme i zde u dekodovani námi zadaného textu
 # Vytvoříme zde to, že námi zadaný kod program rozloží na jednotky
 # Opet nechame udělat string z code a necháme ho vypsat
 
 def  Code_To_Text():
-       text1 = input("Napište kod, který chcete dekodovat: ")
-       code1 = [k for i in text1.split() for k,
-                v in SLOVNIK_MORSEOVKA.items() if i == v]
-       novytext = "".join(code1)
-       print(novytext)
+  text1 = input("Napište kod, který chcete dekodovat: ")
+  code1 = [k for i in text1.split() for k,
+           v in SLOVNIK_MORSEOVKA.items() if i == v]
+  novytext = "".join(code1)
+  print(novytext)
 
 
 # Vypiseme menu pro vyber, co hceme aby program delal
@@ -57,24 +57,32 @@ def  Code_To_Text():
 
 print("""\n1 - TEXT KE KODOVÁNÍ \n2 - KÓD K ROZKÓDOVÁNÍ\n3 - KONEC\n""")
 
+
 while True:
 
-                vyber = int(input("TVŮJ VÝBĚR: "))
-                if vyber == 1:
-                        print(Text_To_Code())
-                        break
+  vyber = int(input("TVŮJ VÝBĚR: "))
 
-                elif vyber == 2:
-                        print(Code_To_Text())
-                        break
+  if vyber == 1:
+    print(Text_To_Code())
+    break
 
-                elif vyber == 3:
-                        print("UKONČUJI")
-                        exit()
 
-                else:
-                        print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
-       exception:
+  elif vyber == 2:
+    print(Code_To_Text())
+    break
+
+
+  elif vyber == 3:
+    print("UKONČUJI")
+    exit()
+
+
+
+  else:
+
+    print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
+
+    exception:
                print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
 
 
