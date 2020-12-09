3591b18d452bd6ccc554fabee4549b885a905b07
machalamartin1 <m_machala@utb.cz>
2020-12-09 19:12:37 +0100

    pridan filtr cisel, mensi upravy a opravy

--- Minimax.py
+++ Minimax.py
@@ -1,18 +1,25 @@
 import sys
 import random
 
+
 def main():
     input = []  # vytvori list input
 
     for x in sys.argv:  # nacte vsechny arumenty do listu input
         input.append(x)
     input.pop(0)
+
     if len(input) == 0:  # tady se prida funkce na nahodne generovain cisel
         input = randomnumbers()
     elif input[0] == "soubor-s-cisly.txt":  # funkce pro cteni ze souboru
         input = readfile()
 
-    MoznostiZoradenia(input)
+    numbers = []  # list pro filtrovani vstupu
+    for x in range(0, len(input)):
+        if input[x].isnumeric():  # zjisti jestli je polozka cislo
+            numbers.append(int(input[x]))  # preda polozku jako cislo do promenne
+
+    MoznostiZoradenia(numbers)
     # for y in input:  # docasne, vypise cisla
     #    print(y)
 
@@ -44,39 +51,44 @@ def randomnumbers():  # funkce vygenereuje 20 nahodnich cisel
 def bubble_sort(pomocna):  # funkce bubble sort
     for i in range(0, len(pomocna)-1):  # Cyklus ktorý sa bude opakovat pre každý prvek
         for j in range(0, len(pomocna)-1):  # Cyklus ktorý sa bude opakovat pre každý prvek
-            if int(pomocna[j]) > int(pomocna[j+1]):  # Porovnani sučasného čisla s nasledujúcim čislem
+            if pomocna[j] > pomocna[j+1]:  # Porovnani sučasného čisla s nasledujúcim čislem
                 a = pomocna[j]  # Priradenie sučasneho čisla do pomocnej
                 pomocna[j] = pomocna[j+1]  # Priradenie sučasneho čísla na nasledujúce čislo
                 pomocna[j+1] = a  # Priradenie nasledujúceho čisla na promenu
     for y in pomocna:  # docasne, vypise cisla
         print(y)
 
-def selection_sort(pole): # funkce selection sort
-    for i in range (0, len(pole)-1): # Cyklus, který se bude opakovat pro každý prvek.
-        mezivypocet = i # Přiřazení prvního nezařazeného prvku jako mezivýpočet
-        for j in range (i, len(pole)-1): # Cyklus, který se bude opakovat pro každý nezařazený prvek.
+
+def selection_sort(pole):  # funkce selection sort
+    for i in range(0, len(pole)):  # Cyklus, který se bude opakovat pro každý prvek.
+        mezivypocet = i  # Přiřazení prvního nezařazeného prvku jako mezivýpočet
+        for j in range(i, len(pole)):  # Cyklus, který se bude opakovat pro každý nezařazený prvek.
             if pole[mezivypocet] > pole[j]:
-                mezivypocet = j # Pokud je současný prvek vyšší než mezivýpočet, tak se prvek stává mezivýpočtem
+                mezivypocet = j  # Pokud je současný prvek vyšší než mezivýpočet, tak se prvek stává mezivýpočtem
         mezivypocet2 = pole[i]
         pole[i] = pole[mezivypocet]
-        pole[mezivypocet] = mezivypocet2 # Zařazení prvku
+        pole[mezivypocet] = mezivypocet2  # Zařazení prvku
 
     for y in pole:  # Dočasně vypíše čísla
         print(y)
     return pole
 
+
 def MoznostiZoradenia(cislo): # funkce moznosti zoradenia
-    print('Moznosti zoradenia:\n 1 - Zoradenie pomocou bubble sort\n 2 - Zoradenie pomocou selection sort\n 3 - Zoradenie pomocou ') # Vypis moznosti zoradenia
-    klavesa = input('Prosim zadaj moznost zoradenia :\n ') # Žadosť o zadanie možnosti zoradenia
-    klavesa = int(klavesa) # Načítanie klávesy
-    if klavesa == 1: # Načitanie čisla
-        bubble_sort(cislo) # Odkazovanie na funkciu bubble sort
-    elif klavesa == 2: # Načitanie čisla
-        selection_sort(cislo) # Odkazovanie na funkciu selection sort
-    elif klavesa == 3: # Načitanie čisla
+    print('Moznosti zoradenia:\n '
+          '1 - Zoradenie pomocou bubble sort\n '
+          '2 - Zoradenie pomocou selection sort\n '
+          '3 - Zoradenie pomocou ')  # Vypis moznosti zoradenia
+    klavesa = input('Prosim zadaj moznost zoradenia :\n ')  # Žadosť o zadanie možnosti zoradenia
+    klavesa = int(klavesa)  # Načítanie klávesy
+    if klavesa == 1:  # Načitanie čisla
+        bubble_sort(cislo)  # Odkazovanie na funkciu bubble sort
+    elif klavesa == 2:  # Načitanie čisla
+        selection_sort(cislo)  # Odkazovanie na funkciu selection sort
+    elif klavesa == 3:  # Načitanie čisla
         print('')
-    else: # Ak zadané čislo sa nenachádza v možnostiach
-        print('Zadali ste spatnu hodnotu') # Vypis chybného hlášenia
+    else:  # Ak zadané čislo sa nenachádza v možnostiach
+        print('Zadali ste spatnu hodnotu')  # Vypis chybného hlášenia
 
 
 if __name__ == '__main__':  # umoznuje psani funkci pod main
