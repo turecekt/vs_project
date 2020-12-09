e6c456eb15bd0a29702b8a0f00945b69e86c6fc6
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-09 22:17:13 +0100

    Update morseovka_Miroslav_Olivik.py

--- morseovka_Miroslav_Olivik.py
+++ morseovka_Miroslav_Olivik.py
@@ -35,7 +35,7 @@ def Text_To_Code():
     text2 = input("Napište slova, která chcete kodovat: ")
     code2 = [SLOVNIK_MORSEOVKA
              [i.upper()] + " "
-              for i in text2 if i.upper() in SLOVNIK_MORSEOVKA.keys()]
+             for i in text2 if i.upper() in SLOVNIK_MORSEOVKA.keys()]
     morseovka = "".join(code2)
     print(morseovka)
 
@@ -60,24 +60,25 @@ print("""\n1 - TEXT KE KODOVÁNÍ \n2 - KÓD K ROZKÓDOVÁNÍ\n3 - KONEC\n""")
 
 
 while True:
-   try:
+
+    try:
 
     vyber = int(input("TVŮJ VÝBĚR: "))
 
     if vyber == 1:
-       print(Text_To_Code())
-       break
+        print(Text_To_Code())
+        break
 
     elif vyber == 2:
-       print(Code_To_Text())
-       break
+        print(Code_To_Text())
+        break
 
     elif vyber == 3:
-       print("UKONČUJI")
-       exit()
+        print("UKONČUJI")
+        exit()
 
     else:
-       print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
+        print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
 
-    exception:
-       print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
+    except:
+        print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
