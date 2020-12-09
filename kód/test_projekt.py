1290812a1d22f11ba5768aabedf9f087c81ef5c6
KristianZednikovic <75309747+KristianZednikovic@users.noreply.github.com>
2020-12-09 12:00:54 +0100

    test

--- kód/test_projekt.py
+++ kód/test_projekt.py
@@ -6,16 +6,13 @@ import projekt
 def test_dlzkystran():
     """Test pre funkciu dlzkastrany."""
     assert projekt.dlzkastrany(1, 1, 4, 5) == 5.0
-    assert projekt.dlzkastrany(1, 1, 2, 1) == 1.0
-    assert projekt.dlzkastrany(-3, 1, 4, 1) == 7.0
+
 
 
 def test_obvod():
     """Test pre funkciu obvod."""
     assert projekt.obvod(1, 2, 3) == 6
-    assert projekt.obvod(5, 4, 2) == 11
-    assert projekt.obvod(12, 23, 30) == 65
-    assert projekt.obvod(8, 8, 8) == 24
+
 
 
 def test_obsah():
@@ -27,12 +24,10 @@ def test_obsah():
 def test_zostrojenost():
     """Test pre funkciu zostrojenost."""
     assert projekt.zostrojenost(3, 4, 5) == 1
-    assert projekt.zostrojenost(1, 5, 10) == 0
-    assert projekt.zostrojenost(1, 2, 3) == 1
+
 
 
 def test_pravouhlost():
     """Test pre funkciu pravouhlost."""
     assert projekt.pravouhlost(3, 4, 5) == 1
-    assert projekt.pravouhlost(4, 4, 4) == 0
-    assert projekt.pravouhlost(7, 9, 16) == 0
+
