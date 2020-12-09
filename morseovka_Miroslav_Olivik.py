cab560428ae6b93b00d3ac538961e7b8b83bafdd
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-09 20:02:48 +0100

    Update morseovka_Miroslav_Olivik.py

--- morseovka_Miroslav_Olivik.py
+++ morseovka_Miroslav_Olivik.py
@@ -2,15 +2,15 @@
 
 
 # Jako první věc si msíme definovat abecedu, ke každému písmenu deklarujeme morseovo písmeno
-SLOVNIK_MORSEOVKA = { ' ': '/', 'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-                    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.','O': '---', 'P': '.--.', 'Q': '--.-',
-                    'R': '.-.', 'S': '...', 'T': '-','U': '..-', 'V': '...-', 'W': '.--','X': '-..-', 'Y': '-.--', 'Z': '--..',
-                    '1': '.----', '2': '..---', '3': '...--','4': '....-', '5': '.....', '6': '-....','7': '--...', '8': '---..', '9': '----.',
+SLOVNIK_MORSEOVKA = {' ': '/', 'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 
+                    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.','O': '---', 'P': '.--.', 'Q': '--.-', 
+                    'R': '.-.', 'S': '...', 'T': '-','U': '..-', 'V': '...-', 'W': '.--','X': '-..-', 'Y': '-.--', 'Z': '--..', 
+                    '1': '.----', '2': '..---', '3': '...--','4': '....-', '5': '.....', '6': '-....','7': '--...', '8': '---..', '9': '----.', 
 
                     '0': '-----', ', ': '--..--', '.': '.-.-.-','?': '..--..', '/': '-..-.', '-': '-....-','(': '-.--.', ')': '-.--.-'
                     }
 # Dále definujeme první funkci - a to tedy funkci pro zakodovani textu díky prikazu def
-def text_to_code():
+def text_to_code(): 
 # Jako první věc definujeme trext, ke kterému máme doplnit text pro kodovani
         text = input("Napište text, který hcete kodovat: ")
 # Dále vytvoříme code, který nám říká, že text námi zadaný se automaticky převde na velké písmeno a to má ekvivalent i v morseove abecede a jejich znacich
@@ -21,10 +21,10 @@ def text_to_code():
         print(morseovka)
                 
 # Stejný způsob použijeme i zde u dekodovani námi zadaného textu
-def code_to_text():
+def code_to_text(): 
         text = input("Napište kod, který chcete dekodovat: ")
 # Vytvoříme zde to, že námi zadaný kod rozloží program na jednotlivé znaky morseovy abecedy a ty rozdělí a potom k těmto znakum přiřadi i písmena
-        code = [k for i in text.split() for k,v in SLOVNIK_MORSEOVKA.items() if i==v]
+        code = [k for i in text.split() for k, v in SLOVNIK_MORSEOVKA.items() if i==v]
         novytext = "".join(code)
 # Opet nechame udělat string z code a necháme ho vypsat 
         print(novytext)
@@ -34,26 +34,26 @@ def code_to_text():
 print("""\n1 - TEXT KE KODOVÁNÍ \n2 - KÓD K ROZKÓDOVÁNÍ\n3 - KONEC\n""")
 
 # Na zaver vypisime cyklus, ktery nám k menu ve stringu priradi i funkce
-while True:
-        try:
+while True: 
+        try: 
                 vyber = int(input("TVŮJ VÝBĚR: "))
-                if vyber == 1:
+                if vyber == 1: 
                         print(text_to_code())
                         break
                 
-                elif vyber == 2:
+                elif vyber == 2: 
                         print (code_to_text())
                         break
                         
-                elif vyber == 3:
+                elif vyber == 3: 
                         print("UKONČUJI")
                         exit()
                      
 
 # Zvolili jsme i fci else, která nám pokryje jakoukoliv jinou možnost než 1-3
-                else:
+                else: 
                         print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
-        except:
+        except: 
                 print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
 
                 
