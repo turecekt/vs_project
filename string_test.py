c5e7384e9657a83859b8a4c3034478e27842f234
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-09 23:18:29 +0100

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -7,25 +7,23 @@ import unittest
 
 
 class TestStringMethods(unittest.TestCase):
-
-
     """vytvoření třídy pro testování"""
- 
+
+"""testování fce zvětšení písmena"""
     def test_upper(self):
         self.assertEqual("abc".upper(), "ABC")
-        """testování fce zvětšení písmena"""
 
+"""testování fce zvětšení písmena"""
     def test_isupper(self):
         self.assertTrue("ABC".isupper())
         self.assertFalse("Abc".isupper())
-        """testování fce zvětšení písmena"""
 
+"""testování fce split"""
     def test_split(self):
         s = "miroslav olivik"
         self.assertEqual(s.split(), ["miroslav", "olivik"])
         with self.assertRaises(TypeError):
             s.split(2)
-            """testování fce split"""
 
 
 if __name__ == "__main__":
