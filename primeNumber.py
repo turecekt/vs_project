07aee3285e3bf3933fdec39fe1002de9c3f4e2e5
BaskaKlim <baska.klimekova@gmail.com>
2020-12-09 09:38:56 +0100

    test

---
+++ primeNumber.py
@@ -0,0 +1,105 @@
+""" Module prime number checker.
+This module is use for checking the input
+"""
+
+
+# Author:   Barbana Klimekova <b_klimekova@utb.cz>
+#           Lucia Kubaskova <l_kubaskova@utb.cz>
+#           Tomas_Prikasky <t_prikasky@utb.cz>
+#
+# Description:  Create a program to evaluate whether the entered number
+#               is a prime number.
+#               Prime numbers have exactly two divisors, are divisible
+#               by only 1 and by themselves.
+
+
+class PrimeNumber:
+    """
+           Class PrimeNumber for evaluating whether the entered number
+           is a prime number.
+           A class to represent logic how to validate any number and
+           print the results.
+           ...
+           Attributes
+           ----------
+           number : int
+               input from console
+           counter : int
+               counter of dividors
+           dividers : int[]
+               list of dividors
+           Methods
+           -------
+           isPrimeNumber():
+               Function validates the entered number is prime number
+           printDivisors():
+                Function prints number of dividers and their values
+           """
+
+
+# define input from console and global variables
+number = input("Vlož číslo: ")
+counter = int
+
+# validation of console inputs
+if number.isdigit() and int(number) > 0:
+    number = int(number)
+    # if input is valid, create variable dividors - list of int
+    divisors = []
+else:
+    print("Nebylo zadáno kladné číslo")
+
+
+def isPrimeNumber():
+    """
+        Function validates the entered number is prime number
+           according to divisors and modulos
+            Parameters:
+                number (int): Number is input from console
+            Returns:
+                output (str): String printed into console,
+                statement if number is prime number
+            """
+    # logic of prime number evaluation
+    isPrimeNumber = True
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            isPrimeNumber = False
+            break
+    if isPrimeNumber and number > 1:
+        print(f"Číslo {number} je prvočíslo")
+    else:
+        print(f"Číslo {number} není prvočíslo")
+
+
+def printDivisors():
+    """Function counts number of dividers and print them
+                Parameters:
+                    dividers (int[]): List of integers - dividers
+                    counter (int): number of dividers
+                Returns:
+                    output (int): number of dividors (counter) and
+                    printed list of dividors
+                """
+    # inicialize and define number of divisers as 0 at the beginning
+    counter = 0
+    print('Delitele:', end=' ')
+    for divisor in range(1, number + 1):
+        if number % divisor == 0:
+            counter += 1
+            print(divisor, end=' ')
+    print()
+    print('Počet deliteľov:', counter)
+
+
+isPrimeNumber()
+printDivisors()
+
+# print documentation using pydoc.
+
+# print documentation for class
+# print(PrimeNumber.__doc__)
+# print documentation for function isPrimeNumber
+# print(isPrimeNumber.__doc__)
+# print documentation for function printDivisors
+# print(printDivisors.__doc__)
---
+++ test_primeNumber.py
@@ -0,0 +1,161 @@
+""" Module unit tests .
+This module is use for do unit test to each function from primeNumber.py
+"""
+# Author:   Barbana Klimekova <b_klimekova@utb.cz>
+#           Lucia Kubaskova <l_kubaskova@utb.cz>
+#           Tomas_Prikasky <t_prikasky@utb.cz>
+#
+# Description:  Create unit tests to validate units from primeNumber.py
+#               Units: functions isPrimeNumber(), printDivisors()
+#               TestCases: 1. prime number validation
+#                          2. non-prime number validation
+#                          3. dividors validation
+#                          4. printed outputs validation
+import unittest
+from contextlib import redirect_stdout
+from io import StringIO
+
+
+class TestPrimeNumber(unittest.TestCase):
+    """
+                Class TestPrimeNumber for testing function primeNumber()
+               ...
+               Attributes
+               ----------
+               number : int
+                   we are testing numbers 5 and 10, 5 as prime number,
+                   10 as non-prime number
+               vypis: str
+                   the string value that is printed as a result into console
+               Methods
+               -------
+               test_fiveIsPrimeNumber(self, ):
+                   Function validates that entered number 5 is prime number
+               test_tenIsNotPrimeNumber(self, ):
+                   Function validates that number 10 is non-prime number
+               """
+
+    def test_fiveIsPrimeNumber(self, ):
+        """Function test function isPrimeNumber
+                        Parameters:
+                            number (int): tested number 5
+                        Returns:
+                            output (str): String printed into console,
+                            statement if number is
+                            prime number
+                        """
+        number = 5
+        number_is_prime_number = True
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                number_is_prime_number = False
+                break
+        if number_is_prime_number and number > 1:
+            vypis = "Číslo 5 je prvočíslo"
+            print(f"\n{vypis}")
+        else:
+            vypis = "Číslo 5 není prvočíslo"
+            print(f"\n{vypis} ")
+
+        self.assertEqual(vypis, 'Číslo 5 je prvočíslo')
+
+    def test_tenIsNotPrimeNumber(self, ):
+        """Function test function isPrimeNumber
+                       Parameters:
+                           number (int): tested number 10
+                       Returns:
+                           output (str): String printed into console,
+                           statement if number is
+                           prime number
+                       """
+        number = 10
+        number_is_prime_number = True
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                number_is_prime_number = False
+                break
+        if number_is_prime_number and number > 1:
+            vypis = "Číslo 10 je prvočíslo"
+            print(f"\n{vypis}")
+
+        else:
+            vypis = "Číslo 10 není prvočíslo"
+            print(f"\n{vypis} ")
+        self.assertEqual(vypis, 'Číslo 10 není prvočíslo')
+
+
+class TestPrintedValues(unittest.TestCase):
+    """
+                Class TestPrintedValues for testing function printDivisors()
+               ...
+               Attributes
+               ----------
+               number : int
+                   We are testing numbers 5 and 44, 5 for evaluating
+                   number of divisors and 44 for evaluating values of divisors
+               dividers: int[]
+                    List of integers - dividers
+               counter: int
+                    Number of dividers
+
+               Methods
+               -------
+               test_numberOfDivisors(self, ):
+                   Function validates the number of dividers
+               test_printedValues(self, ):
+                   Function validates if after evaluation the
+                   divisors are printed into console
+               """
+
+    def test_numberOfDivisors(self, ):
+        """Function validates the number of dividers
+                        Parameters:
+                            number (int): tested number 5
+                            counter (int): number of dividers
+                        Returns:
+                            output (int): number of dividers, printed
+                            statement into console
+                        """
+        number = 5
+        counter = 0
+
+        for divisor in range(1, number + 1):
+            if number % divisor == 0:
+                counter += 1
+
+        print()
+        print('počet deliteľov:', counter)
+        self.assertEqual(counter, 2)
+
+    def test_printedValues(self, ):
+        """Function validates if after evaluation the divisors are printed into console
+                        Parameters:
+                            out: output from function catch into method scope
+                            number (int): tested number 5
+                            counter (int): number of dividers
+                        Returns:
+                            output ():  none, function just asserts values
+                        """
+        out = StringIO()
+        with redirect_stdout(out):
+            # any calls to print (either here or in a called method)
+            # get caught while in this scope
+            number = 44
+            counter = 0
+            print('delitele:', end=' ')
+            for divisor in range(1, number + 1):
+                if number % divisor == 0:
+                    counter += 1
+                    print(divisor, end=' ')
+            print()
+            print('počet deliteľov:', counter)
+            print("\n")
+        self.assertEqual('delitele: 1 2 4 11 22 44 \npočet deliteľov: 6\n\n\n',
+                         out.getvalue())
+
+
+if __name__ == '__main__':
+    unittest.main()
+# print documentation using pydoc.
+# print documentation for class
+"""print(TestPrimeNumber.__doc__)"""
