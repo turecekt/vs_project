8b5f6395ab7eea9c769caa22f6047f24d7748042
Michael Semerád <73735319+michaelsemerad@users.noreply.github.com>
2020-12-10 13:39:58 +0100

    Formatted the docstring

--- calculator.py
+++ calculator.py
@@ -1,40 +1,55 @@
-'''
+"""
 This is a program for simple mathematical operations with two numbers.
+
 The operations are addition, subtraction, multiplication and division.
+
 The input method is through the console.
-'''
+"""
+
 
 def add(x, y):
-    '''
-    Returns addition of two numbers
-    Return is:
-    int
-    '''
+    """ Function adds two numbers.
+    
+    Args:
+        - input - Input of the funcion are two numbers.
+        
+    Returs:
+        - output - Output of the function is a addition of two numbers.
+    """
     return x + y
 
 
 def subtract(x, y):
-    '''
-    Returns subtraction of two numbers
-    Return is:
-    int
-    '''
+    """ Function subtracts two numbers.
+    
+    Args:
+        - input - Input of the funcion are two numbers.
+        
+    Returs:
+        - output - Output of the function is a subtraction of two numbers.
+    """
     return x - y
 
 
 def multiply(x, y):
-    '''
-    Returns multiplication of two numbers
-    Return is:
-    int
-    '''
+    """ Function multiplies two numbers.
+    
+    Args:
+        - input - Input of the funcion are two numbers.
+        
+    Returs:
+        - output - Output of the function is a multiplication of two numbers.
+    """
     return x * y
 
 
 def divide(x, y):
-    '''
-    Returns division of two numbers
-    Return is:
-    int
-    '''
+    """ Function divides two numbers.
+    
+    Args:
+        - input - Input of the funcion are two numbers.
+        
+    Returs:
+        - output - Output of the function is a division of two numbers.
+    """
     return x / y
