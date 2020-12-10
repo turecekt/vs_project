ead4e9528fa599cde177fa4b9ec598c4feb1c32d
Petr Marak <p_marak@utb.cz>
2020-12-10 15:02:04 +0100

    Pridani popisu a prikladu pouziti

---
+++ source.py
@@ -0,0 +1,14 @@
+"""
+Program pro preklad textu na morseovku a naopak.
+
+1. priklad - input:
+"sos"
+1. ocekavany output:
+"...|---|..."
+
+2. priklad - input:
+"Wikipedia is a free online encyclopedia"
+2. ocekavany output:
+".--|..|-.-|..|.--.|.|-..|..|.-||..|...||.-||..-.|.-.|.|.||---|-.|.-..|..|-.|.||.|-.|-.-.|-.--|-.-.|.-..|---|.--.|.|-..|..|.-"
+
+"""
