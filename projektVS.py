89664d4557e9ed7f96e5a6d6ebdab9852a7f51f1
Dominik Lahoda <xlahoda@fi.muni.cz>
2020-12-10 13:48:28 +0100

    rozvrzeni projektu

--- projektVS.py
+++ projektVS.py
@@ -1,5 +1,26 @@
 #zde budeme delat nas projekt
-blabalab
+#TODO: potreba udelat komentare
+#TODO: udelat funkci toMorse, ktera prevede text do morseovky
+#TODO: udelat funkci toWord, která prevede morseovku do slov
+#TODO: udelat nacitani vstupu vyber zda chce uzivatel kodovat nebo dekodovat
+#TODO: udelat testy
 
-46465465
-K
+def toMorse(message):
+    pass
+
+def toWord(morseMessage):
+    pass
+
+def testToMorse():
+    pass
+
+def testToWord():
+    pass
+
+def main():
+    #zde bude nacitani vstupu a vyber zda se bude kodovat nebo dekodovat
+    pass
+
+if __name__ == '__main__':
+    main()
+
