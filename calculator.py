f77b876776cf18a80326af17d48904bd5a09531d
Michael Semerád <73735319+michaelsemerad@users.noreply.github.com>
2020-12-10 17:12:23 +0100

    Removed the input and  output.
    Relocated input and output to app.py.

--- calculator.py
+++ calculator.py
@@ -1,8 +1,6 @@
-"""This is a program for simple mathematical operations with two numbers.
+"""Functions for simple mathematical operations with two numbers.
 
 The operations are addition, subtraction, multiplication and division.
-
-The input method is through the console.
 """
 
 
@@ -48,39 +46,3 @@ def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero.")
     return x / y
-
-
-print("""
-Select operation:
-1.Add
-2.Subtract
-3.Multiply
-4.Divide
-""")
-"""List the choices for the user.
-"""
-
-while True:
-    choice = input("Insert your choice |1|2|3|4| > ")
-    """Take input from the user.
-
-    Input are four number, representing a single mathematical operation.
-    """
-    if choice in ("1", "2", "3", "4"):
-        number1 = float(input("Insert your first number > "))
-        number2 = float(input("Insert your second number > "))
-
-        if choice == "1":
-            print(number1, "+", number2, "=", add(number1, number2))
-
-        elif choice == "2":
-            print(number1, "-", number2, "=", subtract(number1, number2))
-
-        elif choice == "3":
-            print(number1, "*", number2, "=", multiply(number1, number2))
-
-        elif choice == "4":
-            print(number1, "/", number2, "=", divide(number1, number2))
-        break
-    else:
-        print("Invalid Input! Input must be one of these numbers: 1,2,3,4.")
