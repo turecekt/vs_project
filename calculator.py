51ce34a128cc940ab0160ca0e408b59fb7989dbc
Michael Semerád <73735319+michaelsemerad@users.noreply.github.com>
2020-12-10 14:10:03 +0100

    Updated docstring.
    Removed extra whitespaces in blank lines. Rephrased first lines.

--- calculator.py
+++ calculator.py
@@ -1,50 +1,46 @@
-"""
-This is a program for simple mathematical operations with two numbers.
+"""This is a program for simple mathematical operations with two numbers.
 
 The operations are addition, subtraction, multiplication and division.
 
-The input method is through the console."""
+The input method is through the console.
+"""
 
 
 def add(x, y):
-    """Function add two numbers.
-    
-    Args:
-        - input - Input of the funcion are two numbers.
-        
-    Returs:
-        - output - Output of the function is a addition of two numbers."""
+    """Find the addition of x,y and return the result
+
+    Input of the funcion are two numbers.
+
+    Output of the function is a addition of two numbers.
+    """
     return x + y
 
 
 def subtract(x, y):
-    """Function subtract two numbers.
-    
-    Args:
-        - input - Input of the funcion are two numbers.
-        
-    Returs:
-        - output - Output of the function is a subtraction of two numbers."""
+    """Find the subtraction of x,y and return the result.
+
+    Input of the funcion are two numbers.
+
+    Output of the function is a subtraction of two numbers.
+    """
     return x - y
 
 
 def multiply(x, y):
-    """Function multiplie two numbers.
-    
-    Args:
-        - input - Input of the funcion are two numbers.
-        
-    Returs:
-        - output - Output of the function is a multiplication of two numbers."""
+    """Find the multiplication of x,y and return the result.
+
+    Input of the funcion are two numbers.
+
+    Output of the function is a multiplication of two numbers.
+    """
     return x * y
 
 
 def divide(x, y):
-    """Function divide two numbers.
-    
-    Args:
-        - input - Input of the funcion are two numbers.
-        
-    Returs:
-        - output - Output of the function is a division of two numbers."""
+    """Find the division of x,y and return the result.
+
+    Input of the funcion are two numbers.
+
+    Output of the function is a division of two numbers.
+    """
     return x / y
