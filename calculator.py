d6a5330254ae2cd935fde4e3ca6e42db3ba06397
Michael Semerád <73735319+michaelsemerad@users.noreply.github.com>
2020-12-10 12:45:05 +0100

    Defined the core functions
    Added functions for addition, subtraction, multiplication and division of two numbers.

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Program for simple mathematical operations with two numbers
+
+# Function for addition of two numbers
+def add(x, y):
+    return x + y
+
+
+# Function for subtraction of two numbers
+def subtract(x, y):
+    return x - y
+
+
+# Function for multiplication of two numbers
+def multiply(x, y):
+    return x * y
+
+
+# Function for division of two numbers
+def divide(x, y):
+    return x / y
+
