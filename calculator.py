26de9383b71f208ecd8dd9669aa2284992ead8ee
Michael Semerád <73735319+michaelsemerad@users.noreply.github.com>
2020-12-10 15:27:18 +0100

    Added input, output, calculations functionality.
    Corrected typos in docstring.
    Input and output from the console.

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ The input method is through the console.
 def add(x, y):
     """Find the addition of x,y and return the result.
 
-    Input of the funcion are two numbers.
+    Input of the function are two numbers.
 
     Output of the function is a addition of two numbers.
     """
@@ -19,7 +19,7 @@ def add(x, y):
 def subtract(x, y):
     """Find the subtraction of x,y and return the result.
 
-    Input of the funcion are two numbers.
+    Input of the function are two numbers.
 
     Output of the function is a subtraction of two numbers.
     """
@@ -29,7 +29,7 @@ def subtract(x, y):
 def multiply(x, y):
     """Find the multiplication of x,y and return the result.
 
-    Input of the funcion are two numbers.
+    Input of the function are two numbers.
 
     Output of the function is a multiplication of two numbers.
     """
@@ -39,8 +39,48 @@ def multiply(x, y):
 def divide(x, y):
     """Find the division of x,y and return the result.
 
-    Input of the funcion are two numbers.
+    Input of the function are two numbers.
 
     Output of the function is a division of two numbers.
+
+    Raise Value error when second number is 0.
     """
+    if y == 0:
+        raise ValueError("Cannot divide by zero.")
     return x / y
+
+
+print("""
+Select operation:
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+""")
+"""List the choices for the user.
+"""
+
+while True:
+    choice = int(input("Insert your choice |1|2|3|4| > "))
+    """Take input from the user.
+
+    Input are four numbers: 1,2,3,4. Each representing a single mathematical operation.
+    """
+    if choice in (1, 2, 3, 4):
+        first_number = float(input("Insert your first number > "))
+        second_number = float(input("Insert your second number > "))
+
+        if choice == 1:
+            print(first_number, "+", second_number, "=", add(first_number, second_number))
+
+        elif choice == 2:
+            print(first_number, "-",second_number, "=", subtract(first_number, second_number))
+
+        elif choice == 3:
+            print(first_number, "*", second_number, "=", multiply(first_number, second_number))
+
+        elif choice == 4:
+            print(first_number, "/", second_number, "=", divide(first_number,second_number))
+        break
+    else:
+        print("Invalid Input! Input must be one of these numbers: 1,2,3,4.")
