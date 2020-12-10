f19193f8b7ec080201f0356074885892316d7269
Lukáš Venhoda <43666193+LuK3zz@users.noreply.github.com>
2020-12-10 18:24:40 +0100

    Fix linting

--- main.py
+++ main.py
@@ -1,61 +1,68 @@
 def main():
-  """
-  Get user input and check if the input is prime number
+    """
+    Get user input and check if the input is prime number
 
-  Return None
-  """
+    Return None
+    """
 
-  print_input()
-  text = input()
-
-  while not check_number(text):
+    print_input()
     text = input()
-    pass
 
-  if(check_prime_number(int(text))):
-    print("Je prvočíslo")
-  else:
-    print("Není prvočíslo")
+    while not check_number(text):
+        text = input()
+        pass
+
+    if(check_prime_number(int(text))):
+        print("Je prvočíslo")
+    else:
+        print("Není prvočíslo")
+
 
 def print_input():
-  """
-  Print sentece to user
-  """
-  print('Zadejte číslo k zjíštění, zda je číslo prvočíslo')
+    """
+    Print sentece to user
+
+    Return None
+    """
+
+    print('Zadejte číslo k zjíštění, zda je číslo prvočíslo')
+
 
 def check_number(text: str):
-  """
-  This function is checking, if the input is Integer return True if is not
-  return False
-
-  Return Boolean
-  """
-  try:
-    int(text)
-    return True
-  except ValueError:
-    print('Zadaný vstup není celé číslo.')
-    print_input()
+    """
+    This function is checking, if the input is Integer return True if is not
     return False
+
+    Return Boolean
+    """
+    try:
+        int(text)
+        return True
+    except ValueError:
+        print('Zadaný vstup není celé číslo.')
+        print_input()
+        return False
+
 
 def check_prime_number(number: int):
-  """
-  Checking if number is prime number
+    """
+    Checking if number is prime number
 
-  Return Boolean
-  """
+    Return Boolean
+    """
 
-  if(number <= 1):
-    return False
+    if(number <= 1):
+        return False
 
-  if(number == 2):
+    if(number == 2):
+        return True
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
     return True
 
-  for i in range(2, number):
-    if number % i == 0:
-      return False
-
-  return True
 
 if __name__ == '__main__':
-  main()
+    main()
