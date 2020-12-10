22414f2b448af74fb8a1ee6e59b78446c28553be
Radim Halfar <r_halfar@utb.cz>
2020-12-10 22:13:51 +0100

    Upraveno pro flake8

--- example/__init__.py
+++ example/__init__.py
@@ -1,71 +1,74 @@
-import os.path
-import math
-from os import path
+"""Program určí, zda je vstupní číslo prvočíslo či nikoliv.
 
-"""Ukázka souboru: """
-"""This is the "example" module.
+Pro vyšší čísla program použije některou z heuristických či statistických
+metod, o použití alternativní metody program bude informovat uživatele.
 
-The example module supplies one function: compute(). For example,
-
->>> compute(3)
-3
-"
-
-
-def compute(x):
-    "Functon compute returns evaluation of expression using argument x.
-
-    Args:
-        - x - Input of the function
-
-    Returns:
-        - output - Output of the function
-    "
-    return x * x - 2 * x
-
-"""
-
-"""
-Zadání úkolu: Prvočíslo
-------------------------------------------------------------------------------
-
-Program určí, zda je vstupní číslo prvočíslo či nikoliv. Pro vyšší čísla program použije
-některou z heuristických či statistických metod, o použití alternativní metody program bude
-informovat uživatele.
 VSTUP
     - Libovolné celé číslo zadané uživatelem
     - Program neakceptuje jiný/neplatný vstup
 VÝSTUP
     - Informace o tom zda je číslo prvočíslo či nikoliv
-    - Informace jakým způsobem byl výsledek zjištěn (deterministickou/heuristickou metodou)
+    - Informace jakým způsobem byl výsledek zjištěn
+        (deterministickou/heuristickou metodou)
 
-- Klasický kód -
+Program obsahuje 7 funkcí, například:
+funkce Vstup()
+>>> Vstup()
+0
+
+funkce Metody()
+>>> Metody(2)
+3
+
+funkce MetodaPravidelDelitelnosti()
+>>> MetodaPravidelDelitelnosti(22)
+True
+
+funkce DeterministickaMetoda()
+>>> DeterministickaMetoda(56545121)
+False
+
+funkce HeurestickaMetoda()
+>>> HeurestickaMetoda(4)
+False
+
 """
+
+import math
+from os import path
+
+
 def Vstup():
-    # Funkce na ošetření vstupu, vrátí hodnotu čísla, popřípadě vrátí hodnotu 0 pokud
-    # vstup neodpovídá číslu na ověření
-    
+    """Funkce získá vstup od uživatele a ošetří jej proti chybám.
+
+    Returns:
+        - output - Vrátí číslo uživatele nebo 0 jako chybu
+    """
     NapsanaRadka = input()
- 
+
     for znak in NapsanaRadka:
         if not(znak in "0123456789"):
             return 0
- 
+
     vstupniCislo = int(NapsanaRadka)
- 
+
     return vstupniCislo
 
-def Vystup(pouzitaMetoda = 0):
-    # Funkce vypíše výstup, 
-    # Obsahuje parametr int (pro označení použité metody a zda výsledek vyšel či ne)
-    # Zobrazí i informace o použité metodě, ne jen její název
 
+def Vystup(pouzitaMetoda=0):
+    """Funkce vypíše výstup programu (chybu, je či není prvočíslo; použitá metoda).
+
+    Args:
+        - pouzitaMetoda - Metoda, kterou program použil pro zjištění výsledku
+    """
     if(pouzitaMetoda == 0):
-        print("-------------------------------------------------------------------------------")
+        print("------------------------------------------------------------")
         print("Nastala CHYBA způsobená uživatelem.")
-        print("-------------------------------------------------------------------------------")
-        print("Zadaná hodnota neopodívá formátu, který se dá přečíst pro rozpoznání prvočísla.")
-        print("Zkuste program restartovat a zadat přirozené číslo. (1 až nekonečno)")
+        print("------------------------------------------------------------")
+        print("Zadaná hodnota neopodívá formátu, který se dá přečíst pro " +
+              "rozpoznání prvočísla.")
+        print("Zkuste program restartovat a zadat přirozené číslo. (1 až " +
+              "nekonečno)")
         return
     elif(pouzitaMetoda > 0):
         print("Zadané číslo PATŘÍ mezi prvočísla.")
@@ -73,49 +76,59 @@ def Vystup(pouzitaMetoda = 0):
         print("Zadané číslo NEPATŘÍ mezi prvočísla.")
         pouzitaMetoda *= (-1)
 
-    print("-------------------------------------------------------------------------------")
+    print("------------------------------------------------------------")
 
-    #Popis postupů
+    # Popis postupů
     if(pouzitaMetoda == 1):
         print("Byla použita HEURISTICKÁ metoda - 1")
-        print("-------------------------------------------------------------------------------")
-        print("Metoda rozpozná číslici 1 a vyřadí ji z pátrání, protože toto číslo nesplňuje")
-        print("definici prvočísel. (číslo musí být dělitelné DVĚMA čísly, samosebou a jedničkou)")
+        print("------------------------------------------------------------")
+        print("Metoda rozpozná číslici 1 a vyřadí ji z pátrání, protože " +
+              "toto číslo nesplňuje definici prvočísel. (číslo musí být ")
+        print("dělitelné DVĚMA čísly, samosebou a jedničkou)")
     elif(pouzitaMetoda == 2):
         print("Byla použita HEURISTICKÁ metoda - pravidla dělitelnosti")
-        print("-------------------------------------------------------------------------------")
-        print("Metoda určí některá neprvočísla, pomocí toho, že je číslo končí určitým číslem, ")
-        print("které podle pravidel dělitelnosti, lze vydělit již prvočíslem. (např. čísla končící")
-        print("na číslo 5 a 0 jdou vždy vydělit 5)")
+        print("------------------------------------------------------------")
+        print("Metoda určí některá neprvočísla, pomocí toho, že je číslo "
+              "končí určitým číslem, které podle pravidel dělitelnosti, lze ")
+        print("vydělit již prvočíslem. (např. čísla končící na číslo 5 a 0 ")
+        print("jdou vždy vydělit 5)")
     elif(pouzitaMetoda == 3):
         print("Byla použita HEURISTICKÁ metoda - Wilsonova věta")
-        print("-------------------------------------------------------------------------------")
-        print("Metoda používá matematický vztah '((n - 1)! + 1) % n' pro zjištění některých ")
-        print("prvočísel.")
+        print("------------------------------------------------------------")
+        print("Metoda používá matematický vztah '((n - 1)! + 1) % n' pro " +
+              "zjištění některých prvočísel.")
     elif(pouzitaMetoda == 4):
         print("Byla použita DETERMINISTICKÁ metoda")
-        print("-------------------------------------------------------------------------------")
-        print("Metoda zkouší pokud je číslo dělitelné některým z již předtím nalezených")
-        print("prvočísel do maximální hodnoty odmocniny ze zadaného čísla. Pokud číslo není")
-        print("dělitelné, je to další prvočíslo.")
-        print("Např. u čísla 7 otestuje dělitelnost prvočísly do čísla 4, tudíž číslem 2 a 3,")
-        print("nevznikne zbytek 0, tudíž číslo 7 je prvočíslo.")
-        print("(metoda používá paměť s vypočítanými prvočísly k zrychlení procesu)")
+        print("------------------------------------------------------------")
+        print("Metoda zkouší pokud je číslo dělitelné některým z již " +
+              "předtím nalezených prvočísel do maximální hodnoty odmocniny ")
+        print("ze zadaného čísla. Pokud číslo není dělitelné, je to ")
+        print("další prvočíslo.")
+        print("Např. u čísla 7 otestuje dělitelnost prvočísly do čísla 4, " +
+              "tudíž číslem 2 a 3, nevznikne zbytek 0 a tak je číslo 7 ")
+        print("prvočíslo. (metoda používá paměť s vypočítanými prvočísly " +
+              "k zrychlení procesu)")
     else:
-        print("Pravděpodobně jde o nějakou metodu, kterou si vymyslela umělá inteligence")
-        print("vašeho počítače. Tuhle zprávu byste totiž neměli nikdy vidět.")
+        print("Pravděpodobně jde o nějakou metodu, kterou si vymyslela " +
+              "umělá inteligence vašeho počítače. Tuhle zprávu byste ")
+        print("totiž neměli nikdy vidět.")
+
 
 def Metody(cislo):
-    # Funkce, kde se budou zkoušet jednotlivé metody a podle toho se bude informovat uzivatel
-    # Vrátí int, s mínusem pokud číslo není prvočíslo, s plusem pokud je
-    # Číslo navíc ozančuje číslo metody, která byla použita k dostání výsledku
+    """Funkce zkouší zjistit výsledek různými metodami podle typu čísla.
 
-    # Označení metod:
-    # 1 - Metoda 0 a 1
-    # 2 - MetodaPravidelDelitelnosti
-    # 3 - HeurestickaMetoda
-    # 4 - DeterministickaMetoda
+    Označení metod:
+        1 - Metoda 0 a 1
+        2 - MetodaPravidelDelitelnosti
+        3 - HeurestickaMetoda
+        4 - DeterministickaMetoda
 
+    Args:
+        - cislo - Číslo, u kterého se má zjistit, zda-li je prvočíslo
+
+    Returns:
+        - output - Číslo použité metody a znaménko určující je/není prvočíslo
+    """
     pouzitaMetoda = 1
     if(cislo <= 1):
         return -pouzitaMetoda
@@ -123,56 +136,87 @@ def Metody(cislo):
     pouzitaMetoda = 2
     if(MetodaPravidelDelitelnosti(cislo)):
         return -pouzitaMetoda
-    
+
     pouzitaMetoda = 3
     if(cislo < 40000):
         if(HeurestickaMetoda(cislo)):
             return pouzitaMetoda
         else:
             return -pouzitaMetoda
-    
+
     pouzitaMetoda = 4
     if(DeterministickaMetoda(cislo)):
         return pouzitaMetoda
     else:
         return -pouzitaMetoda
 
+
 def MetodaPravidelDelitelnosti(cislo):
-    # Funkce zkouší, zda-li je číslo větší jak 9 a zároveň končí čílicí 0, 2, 4, 5, 6 či 8
-    # Pokud funkce splňuje podmínky, je jasné, že zadané číslo není prvočíslo
-    # Vrací True pokud číslo není prvočíslo
+    """Funkce zkouší, zda číslo není prvočíslo.
+
+    Je-li je číslo větší jak 9 a zároveň končí čílicí 0, 2, 4, 5, 6 či 8,
+    tak toto číslo provčíslo není.
+
+    Args:
+        - cislo - Číslo, u kterého se má zjistit, zda-li je prvočíslo
+
+    Returns:
+        - output - Boolean pokud je/není prvočíslo.
+    """
     return (cislo > 9 and str(cislo)[-1] in "024568")
 
+
 def DeterministickaMetoda(moznePrvocislo):
-    #Funkce zkouší zjistit prvočíslo Deterministickou metodou
-    
+    """Funkce zkouší zjistit prvočíslo deterministickou metodou.
+
+    Pokud číslo není dělitelné prvočísly do odmocnina z čísla,
+    pak je prvočíslo. Funkce vytváří cache soubor.
+
+    Args:
+        - moznePrvocislo - Číslo, u kterého se má zjistit, zda-li je prvočíslo
+
+    Returns:
+        - output - Boolean pokud je/není prvočíslo.
+    """
     if(not path.exists("nalezenaPrvocisla.txt")):
         cachePrvociselSoubor = open("nalezenaPrvocisla.txt", "w")
         cachePrvociselSoubor.write("2\n")
         cachePrvociselSoubor.close()
-    
+
     cachePrvociselSoubor = open("nalezenaPrvocisla.txt", "r")
     driveNalezenaPrvocisla = cachePrvociselSoubor.read().strip().split("\n")
-    
+
     cachePrvociselSoubor.close()
 
     if(str(moznePrvocislo) in driveNalezenaPrvocisla):
         return True
     else:
         odmocninaMoznehoPrvocisla = int(math.ceil(math.sqrt(moznePrvocislo)))
-        
-        if(odmocninaMoznehoPrvocisla > int(driveNalezenaPrvocisla[len(driveNalezenaPrvocisla) - 1])):
-            driveNalezenaPrvocisla = ZjistiVicePrvocisel(driveNalezenaPrvocisla, odmocninaMoznehoPrvocisla)
+
+        if(odmocninaMoznehoPrvocisla >
+                int(driveNalezenaPrvocisla[len(driveNalezenaPrvocisla)-1])):
+            driveNalezenaPrvocisla = ZjistiVicePrvocisel(
+                driveNalezenaPrvocisla, odmocninaMoznehoPrvocisla)
 
         for prvocislo in driveNalezenaPrvocisla:
             if(int(prvocislo) > odmocninaMoznehoPrvocisla):
                 break
             if(moznePrvocislo % int(prvocislo) == 0):
                 return False
-                
+
     return True
 
+
 def ZjistiVicePrvocisel(cachePrvocisel, doCisla):
+    """Funkce zjišťuje a vytváří nová prvočísla. Vytváří cache soubor.
+
+    Args:
+        - cachePrvocisel - Pole dříve nalezených prvočísel
+        - doCisla - Do jakého čísla hledat prvočísla
+
+    Returns:
+        - output - Pole všech dosud nalezených prvočísel
+    """
     if(int(cachePrvocisel[len(cachePrvocisel) - 1]) == 2):
         cislo = 3
     else:
@@ -196,7 +240,7 @@ def ZjistiVicePrvocisel(cachePrvocisel, doCisla):
             cachePrvocisel.append(str(cislo))
 
         cislo += step
-    #END WHILE
+    # END OF WHILE
 
     cachePrvociselSoubor = open("nalezenaPrvocisla.txt", "w")
     cachePrvociselSoubor.write("\n".join(cachePrvocisel))
@@ -204,8 +248,16 @@ def ZjistiVicePrvocisel(cachePrvocisel, doCisla):
 
     return cachePrvocisel
 
+
 def HeurestickaMetoda(o):
-    """Funkce zkouší zjistit prvočíslo Heurestickou metodou ((n-1)!+1)%n."""
+    """Funkce zkouší zjistit prvočíslo Heurestickou metodou ((n-1)!+1)%n.
+
+    Args:
+        - o - Číslo, u kterého se má zjistit, zda-li je prvočíslo
+
+    Returns:
+        - output - Boolean pokud je/není prvočíslo.
+    """
     k = 1
     factorial = 1
     for k in range(1, int(o)):
@@ -216,6 +268,7 @@ def HeurestickaMetoda(o):
     else:
         return False
     pass
+
 
 # FUNKCE MAIN >>
 moznePrvocislo = Vstup()            # Získá vstup od uživatele
