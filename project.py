c7a0e587ab3460c96f1c42184042d9bf7242022e
Jiri Kantor <j_kantor@utb.cz>
2020-12-11 17:04:35 +0100

    project.py updated

--- project.py
+++ project.py
@@ -1,24 +1,102 @@
+""".
+
+SEMESTRALNI PROJEKT DO AP1VS
+
+project.py
+
+version:    1.0
+
+Author: Jiri Kantor https://github.com/kantorj
+
+---------------------------------------------------
+
+This algorithm is used to define if a
+value entered by the user is a prime number
+
+---------------------------------------------------
+
+This project was made for educational purpose only.
+
+"""
+
+
+def prvocislo(val):  # program pro urceni prvocisla
+    """Chceck the val."""
+    if isinstance(val, int):         # checks if 'val' is of type 'int'
+
+        if val > 1:  # pro cislo > 1
+            for i in range(2, val):
+                if (val % i) == 0:  # cislo nedává zbytek po dělení
+                    print(val, ' neni prvocislo.')
+                    print(i, ' * ', val // i, ' = ', val)  # důkaz dělitelnosti
+                    print('(Pro vypocet byla pouzita deterministicka metoda.)')
+                    return 2
+            else:  # cislo dává zbytek po dělení
+                print(val, ' je prvocislo.')
+                print('(Pro vypocet byla pouzita deterministicka metoda.)')
+                return 99
+        elif val < 0:  # pro cislo <= 1
+            print('Zadane cislo musi byt > 0.')
+            return 0
+        elif val == 1:
+            print('Cislo 1 neni prvocislo dle definice.')
+            return 1
+        else:
+            print(val, ' neni prvocislo.')  # cislo mezi 0 a 1
+            print('(Pro vypocet byla pouzita deterministicka metoda.)')
+            return 10
+
+
+# print('(Pro vypocet byla pouzita deterministicka metoda.)')
+# deterministicka metoda =
+# vysledek je pokazde stejny a je urcen dle definice prvocisel
+# algoritmus je predvitatelny
+
+
+# unit testy:
+
+def test_is_one():
+    """Use this function to check the entered value 1."""
+    assert prvocislo(1) == 1        # from return
+
+
+def test_is_not_val():
+    """Check if the entered value is not of the type 'int'."""
+    assert prvocislo('a') == 22
+
+
+def test_small_not_primes():
+    """Check if the entered low values aren't a prime number."""
+    for i in range(4, 100, 2):
+        assert prvocislo(i) == 2
+
+
+def test_big_not_primes():
+    """Check if the entered high values aren't a prime number."""
+    for i in range(100, 1000000, 2):
+        assert prvocislo(i) == 2
+
+
+def test_small_primes():
+    """Use this function to check the small prime values."""
+    numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    for i in range(0, (len(numbers)-1)):
+        assert prvocislo(numbers[i]) == 99
+
+
+def test_big_primes():
+    """Use this function to check the big prime values."""
+    numbers = [104677, 104681, 104683, 104693, 104701,
+               104707, 104711, 104717, 104723, 104729]
+    for i in range(0, (len(numbers)-1)):
+        assert prvocislo(numbers[i]) == 99
+
+
+def test_is_between():
+    """Check the entered value is between 0 and 1."""
+    assert prvocislo(0) == 10
+
+
 print('===Program pro posouzeni cisla / prvocisla===')
 cislo = int(input('Zadejte libovolne cele cislo k posouzeni: '))
-    # program pro urceni prvocisla                          # pro cislo > 1
-if cislo > 1:
-    for i in range(2,cislo):
-        if (cislo % i) == 0:                    # cislo nedává zbytek po dělení
-            print(cislo ,' neni prvocislo.')
-            print(i ,' * ', cislo//i, ' = ', cislo)    # důkaz dělitelnosti
-            print('(Pro vypocet byla pouzita deterministicka metoda.)')
-            break
-    else:                                       # cislo dává zbytek po dělení
-            print(cislo ,' je prvocislo.')
-            print('(Pro vypocet byla pouzita deterministicka metoda.)')
-elif cislo < 0:                                       # pro cislo <= 1
-    print('Zadane cislo musi byt > 0.')
-elif cislo == 1:
-    print('Cislo 1 neni prvocislo dle definice.')
-else:
-    print(cislo ,' neni prvocislo.')
-    print('(Pro vypocet byla pouzita deterministicka metoda.)')
-        
-# print('(Pro vypocet byla pouzita deterministicka metoda.)')
-# deterministicka metoda = vysledek je pokazde stejny a je urcen dle definice prvocisel
-# algoritmus je predvitatelny
+prvocislo(cislo)
