ece6786b484ee3baac42abbc5406890eaaebde93
machalamartin1 <m_machala@utb.cz>
2020-12-11 10:23:53 +0100

    pridani quick sortu, uprava vyberu tridiciho algoritmu a mensi opravy
    Signed-off-by: machalamartin1 <m_machala@utb.cz>

--- Minimax.py
+++ Minimax.py
@@ -3,24 +3,25 @@ import random
 
 
 def main():
-    input = []  # vytvori list input
+    vstup = []  # vytvori list vstup
+    numbers = []  # vytvori list filtrovany vstup
 
-    for x in sys.argv:  # nacte vsechny arumenty do listu input
-        input.append(x)
-    input.pop(0)
+    for x in sys.argv:  # nacte vsechny arumenty do listu vstup
+        vstup.append(x)
+    vstup.pop(0)  # ondstrani prvni argument (cestu k souboru)
 
-    if len(input) == 0:  # tady se prida funkce na nahodne generovain cisel
-        input = randomnumbers()
-    elif input[0] == "soubor-s-cisly.txt":  # funkce pro cteni ze souboru
-        input = readfile()
+    if len(vstup) == 0:  # nahodne generovani cisel
+        numbers = randomnumbers()
+    elif vstup[0] == "soubor-s-cisly.txt":  # funkce pro cteni ze souboru
+        vstup = readfile()
 
-    numbers = []  # list pro filtrovani vstupu
-    for x in range(0, len(input)):
-        if input[x].isnumeric():  # zjisti jestli je polozka cislo
-            numbers.append(int(input[x]))  # preda polozku jako cislo do promenne
+    if len(vstup) > 0:
+        for x in range(0, len(vstup)):
+            if vstup[x].isnumeric():  # zjisti jestli je polozka cislo
+                numbers.append(int(vstup[x]))  # preda polozku jako cislo do promenne
 
-    MoznostiZoradenia(numbers)
-    # for y in input:  # docasne, vypise cisla
+    moznostizoradenia(numbers)
+    # for y in vstup:  # docasne, vypise cisla
     #    print(y)
 
 
@@ -55,8 +56,8 @@ def bubble_sort(pomocna):  # funkce bubble sort
                 a = pomocna[j]  # Priradenie sučasneho čisla do pomocnej
                 pomocna[j] = pomocna[j+1]  # Priradenie sučasneho čísla na nasledujúce čislo
                 pomocna[j+1] = a  # Priradenie nasledujúceho čisla na promenu
-    for y in pomocna:  # docasne, vypise cisla
-        print(y)
+
+    return pomocna
 
 
 def selection_sort(pole):  # funkce selection sort
@@ -69,26 +70,37 @@ def selection_sort(pole):  # funkce selection sort
         pole[i] = pole[mezivypocet]
         pole[mezivypocet] = mezivypocet2  # Zařazení prvku
 
-    for y in pole:  # Dočasně vypíše čísla
-        print(y)
     return pole
 
 
-def MoznostiZoradenia(cislo): # funkce moznosti zoradenia
+def quick_sort(pole):
+    for i in range(1, len(pole)):
+        val = pole[i]
+        j = i - 1
+        while j >= 0 and val < pole[j]:
+            pole[j+1] = pole[j]
+            j = j - 1
+        pole[j+1] = val
+    return pole
+
+
+def moznostizoradenia(cislo):  # funkce moznosti zoradenia
     print('Moznosti zoradenia:\n '
           '1 - Zoradenie pomocou bubble sort\n '
           '2 - Zoradenie pomocou selection sort\n '
           '3 - Zoradenie pomocou ')  # Vypis moznosti zoradenia
     klavesa = input('Prosim zadaj moznost zoradenia :\n ')  # Žadosť o zadanie možnosti zoradenia
-    klavesa = int(klavesa)  # Načítanie klávesy
-    if klavesa == 1:  # Načitanie čisla
-        bubble_sort(cislo)  # Odkazovanie na funkciu bubble sort
-    elif klavesa == 2:  # Načitanie čisla
-        selection_sort(cislo)  # Odkazovanie na funkciu selection sort
-    elif klavesa == 3:  # Načitanie čisla
-        print('')
+    if klavesa == '1':  # Načitanie čisla
+        serazeno = bubble_sort(cislo)  # Odkazovanie na funkciu bubble sort
+    elif klavesa == '2':  # Načitanie čisla
+        serazeno = selection_sort(cislo)  # Odkazovanie na funkciu selection sort
+    elif klavesa == '3':  # Načitanie čisla
+        serazeno = quick_sort(cislo)  # Odkaz na funkci quick sort
     else:  # Ak zadané čislo sa nenachádza v možnostiach
         print('Zadali ste spatnu hodnotu')  # Vypis chybného hlášenia
+        return
+    for i in serazeno:
+        print(i)
 
 
 if __name__ == '__main__':  # umoznuje psani funkci pod main
