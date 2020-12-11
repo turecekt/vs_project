3d3515ce0a235adb2ce90d7f8c3250e4aec8d396
Tomas Adamek <ad.tomik@seznam.cz>
2020-12-11 05:21:29 -0800

    vyresení trojuhelniku sss

--- main.py
+++ main.py
@@ -49,22 +49,53 @@ class tris():
             obsah = math.sqrt(s * (s - sA) * (s - sB) * (s - sC))
             cosA = (sB ** 2 + sC ** 2 - sA ** 2) / (2 * sB * sC)
             cosB = (sA ** 2 + sC ** 2 - sB ** 2) / (2 * sA * sC)
-            cosC = (sA ** 2 + sB ** 2 - sC ** 2) / (2 * sA * sC)
+            cosC = (sA ** 2 + sB ** 2 - sC ** 2) / (2 * sA * sB)
             uA = round(math.degrees(math.acos(cosA)), 2)
             uB = round(math.degrees(math.acos(cosB)), 2)
             uC = round(math.degrees(math.acos(cosC)), 2)
             obsah = round(obsah, 2)
-
-            return {"obsah" : str(obsah) + "cm\u00B2",
-                    "obvod" : str(obvod) + "cm",
-                    "úhel \u03B1" : str(uA) + "°",
-                    "úhel \u03B2" : str(uB) + "°",
-                    "úhel \u03B3" : str(uC) + "°"}
+            if uA == uB and uB == uC:
+                return {"obsah" : str(obsah) + "cm\u00B2",
+                        "obvod" : str(obvod) + "cm",
+                        "úhel \u03B1, \u03B2, \u03B3" : str(uA) + "°",
+                        "typ trojúhelníku" : "rovnostranný"}
+            elif uA == uB:
+                return {"obsah" : str(obsah) + "cm\u00B2",
+                        "obvod" : str(obvod) + "cm",
+                        "úhel \u03B1, \u03B2" : str(uA) + "°",
+                        "úhel \u03B3" : str(uC) + "°",
+                        "typ trojúhelníku" : "rovnoramenný"}
+            elif uA == uC:
+                return {"obsah" : str(obsah) + "cm\u00B2",
+                        "obvod" : str(obvod) + "cm",
+                        "úhel \u03B1, \u03B3" : str(uA) + "°",
+                        "úhel \u03B2" : str(uB) + "°",
+                        "typ trojúhelníku" : "rovnoramenný"}
+            elif uB == uC:
+                return {"obsah" : str(obsah) + "cm\u00B2",
+                        "obvod" : str(obvod) + "cm",
+                        "úhel \u03B1" : str(uA) + "°",
+                        "úhel \u03B2, \u03B3" : str(uB) + "°",
+                        "typ trojúhelníku" : "rovnoramenný"}
+            elif uA == 90 or uB == 90 or uC == 90:
+                return {"obsah" : str(obsah) + "cm\u00B2",
+                        "obvod" : str(obvod) + "cm",
+                        "úhel \u03B1" : str(uA) + "°",
+                        "úhel \u03B2" : str(uB) + "°",
+                        "úhel \u03B3" : str(uC) + "°",
+                        "typ trojúhelníku" : "pravoúhlý"}
+            else:
+                return {"obsah" : str(obsah) + "cm\u00B2",
+                        "obvod" : str(obvod) + "cm",
+                        "úhel \u03B1" : str(uA) + "°",
+                        "úhel \u03B2" : str(uB) + "°",
+                        "úhel \u03B3" : str(uC) + "°",
+                        "typ trojúhelníku" : "obecný"}
 
         else:
             return "Nejedná se o trojúhelník. Součet dvou stran musí být větší než stranatřetí."
 
 
 
-sss =tris(3, 3, 3)
+sss =tris(3, 4, 5)
 print(sss)
