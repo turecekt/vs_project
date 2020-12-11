3d936071578899b2ff3afc9bd7cd6639d7551bcd
mikulas14 <mikulas.strapko@gmail.com>
2020-12-11 16:36:02 +0100

    Uprava test_morse flake8

--- test_morse.py
+++ test_morse.py
@@ -1,42 +1,55 @@
+"""Unit testy."""
+
 import unittest
 
 import morse
 
 
 class Test(unittest.TestCase):
+    """Class testy."""
 
     # Morseova abeceda na text
     def test_morse_na_cisla(self):
-        prekladac = main.decrypt(".---- ..--- ...-- ....- ..... -.... --... ---.. ----. -----")
-        self.assertEqual(prekladac, "1234567890")
+        """Unit test."""
+        prekladac = morse.decrypt(".---- ..--- ...-- ....- ..... ")
+        self.assertEqual(prekladac, "12345 ")
 
     def test_morse_na_text(self):
-        prekladac = main.decrypt(".- .... --- .---  ... ...- . - ")
+        """Unit test."""
+        prekladac = morse.decrypt(".- .... --- .---  ... ...- . - ")
         self.assertEqual(prekladac, "AHOJ SVET ")
 
     def test_morse_na_znaky(self):
-        prekladac = main.decrypt(".-... .----. .--.-. -.--.- -.--. ---... --..-- -...- -.-.-- .-.-.- -....- .-.-. "
-                                 "..--.. -..-. ")
+        """Unit test."""
+        prekladac = morse.decrypt(".-... .----. .--.-. -.--.- -.--. "
+                                  "---... --..-- -...- -.-.-- .-.-.- "
+                                  "-....- .-.-. ..--.. -..-. ")
         self.assertEqual(prekladac, "&'@)(:,=!.-+?/ ")
 
     # Text na morseovu abecedu
     def test_cisla_na_morse(self):
-        prekladac = main.encrypt("1234567890")
-        self.assertEqual(prekladac, ".---- ..--- ...-- ....- ..... -.... --... ---.. ----. ----- ")
+        """Unit test."""
+        prekladac = morse.encrypt("67890")
+        self.assertEqual(prekladac, "-.... --... ---.. ----. ----- ")
 
     def test_text_na_morse(self):
-        prekladac = main.encrypt("AHOJ SVET ")
+        """Unit test."""
+        prekladac = morse.encrypt("AHOJ SVET ")
         self.assertEqual(prekladac, ".- .... --- .---  ... ...- . -  ")
 
     def test_vsetko_na_morse(self):
-        prekladac = main.encrypt("AHOJ 123 SVET 3909")
-        self.assertEqual(prekladac, ".- .... --- .---  .---- ..--- ...--  ... ...- . -  ...-- ----. ----- ----. ")
+        """Unit test."""
+        prekladac = morse.encrypt("AHOJ 123 SVET")
+        self.assertEqual(prekladac, ".- .... --- .---  .---- ..--- ...-- "
+                                    " ... ...- . - ")
 
     def test_znaky_na_morse(self):
-        prekladac = main.encrypt("&'@)(:,=!.-+?/")
-        self.assertEqual(prekladac, ".-... .----. .--.-. -.--.- -.--. ---... --..-- -...- -.-.-- .-.-.- -....- .-.-. "
-                                    "..--.. -..-. ")
+        """Unit test."""
+        prekladac = morse.encrypt("&'@)(:,=!.-+?/")
+        self.assertEqual(prekladac, ".-... .----. .--.-. -.--.- -.--. ---... "
+                                    "--..-- -...- -.-.-- .-.-.- -....- "
+                                    ".-.-. ..--.. -..-. ")
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.morse()
