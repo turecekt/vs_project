84f215a232bab29ac403c9a71dcb9885183e6dc0
JakubSladky4 <jakub.sladky96@gmail.com>
2020-12-12 13:37:09 +0100

    Vytvoření kodu souboru program.py zajištující vlastní průběh programu

--- program.py
+++ program.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec 12 13:25:27 2020
+Tento soubor obsahuje vlastní průběh programu.
+
+Created on Tue Dec  1 17:32:47 2020
 
 @author: jakub
 """
 
+import classes
+
+"""Vlastní program"""
+
+zadanyTrojuhelnik = classes.vstup()
+if zadanyTrojuhelnik.jeSestrojitelny() == "NE":
+    a = zadanyTrojuhelnik.delkaStrany("a")
+    b = zadanyTrojuhelnik.delkaStrany("b")
+    c = zadanyTrojuhelnik.delkaStrany("c")
+    result = """
+    Trojúhelník z vámi zadaných bodů
+    není sestrojitelný, další parametry
+    nebudou dopočítávány.
+    """
+    print(result)
+    print()
+    input()
+else:
+    a = zadanyTrojuhelnik.delkaStrany("a")
+    b = zadanyTrojuhelnik.delkaStrany("b")
+    c = zadanyTrojuhelnik.delkaStrany("c")
+    print("Trojúhelník je sestrojitelný, má následující parametry:")
+    print("Delka strany a:")
+    print(a)
+    print("Delka strany b:")
+    print(b)
+    print("Delka strany c:")
+    print(c)
+    print("Obvod:")
+    print(zadanyTrojuhelnik.obvod())
+    print("Obsah:")
+    print(zadanyTrojuhelnik.obsah())
+    print("Pravoúhlý:")
+    print(zadanyTrojuhelnik.jePravouhly())
+    input()
