5acaf7428a5fdc5533a4f09b8a0d797f3821f3b9
j_potociar <j_potociar@utb.cz>
2020-12-13 00:42:25 +0100

    Unittesty na délku strany AB

--- test_functions.py
+++ test_functions.py
@@ -12,3 +12,10 @@ if __name__ == '__main__':
 # Třída pro unittesty na délky stran trojúhelníku
 class TestDelkaStrany(unittest.TestCase):
     """Unittest na délku stran AB, BC, AC."""
+
+    """Unittest na délku strany AB"""
+    def test_delkaStranyAB(self):
+        """Unittesty pro stranu AB."""
+        self.assertEqual(functions.delkaStranyAB(2, 5, 5, 9), 5)
+        self.assertEqual(functions.delkaStranyAB(2, -6, -13, -19), 10)
+        self.assertEqual(functions.delkaStranyAB(-2.6, -2.6, 119, 130), 11)
