5e8f43456be187dc47950aeeaab93e8c332d6c43
MatusJ96 <73904041+MatusJ96@users.noreply.github.com>
2020-12-12 14:49:36 +0100

    Update code-style for main code.

--- char_recurrence/main.py
+++ char_recurrence/main.py
@@ -5,49 +5,45 @@ from operator import itemgetter
 
 
 def arguments():
-    """
-    Method for parsing argument from std-in or as parameters.
+    """Method for parsing argument from std-in or as parameters.
     Use argparse library.
     Without parameter it read from std-in else from input files
     return all arguments.
     """
-    parser=ArgumentParser()
-    parser.add_argument("-f", "--file", dest = "filename", action = "store",
-                        help = "Type input FILE", metavar = "FILE")
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename", action="store",
+                        help="Type input FILE", metavar="FILE")
 
     arg_len = len(sys.argv)
     if arg_len == 1:
-        result=sys.stdin.read()
+        result = sys.stdin.read()
         return read_file(result, arg_len)
     else:
-        args=parser.parse_args().filename
+        args = parser.parse_args().filename
         return read_file(args, arg_len)
 
 def read_file(input_file, arg_len):
-    """
-    Method for reading char to specific char #.
+    """Method for reading char to specific char #.
     Method return @data (string which will be working next).
     """
     if arg_len != 1:
         with open(input_file, "rt") as f:
-            text=f.read()
-            data=text.split('#', 1)[0]
+            text = f.read()
+            data = text.split('#', 1)[0]
     else:
-        data=input_file.split('#', 1)[0]
+        data = input_file.split('#', 1)[0]
 
     return data
 
 def number_char(file_data):
-    """
-    Method return lenght of passing string.
+    """Method return lenght of passing string.
     """
     num_char=len(file_data)
 
     return num_char
 
 def number_occurence(file_data):
-    """
-    Method create list of occurrence and eleminate unwanted chars from string.
+    """Method create list of occurrence and eleminate unwanted chars from string.
     """
     my_list=[]
     file_data=file_data.lower()
@@ -60,14 +56,16 @@ def number_occurence(file_data):
     return my_list
 
 def print_number_occurence(list):
-    """
-    Method for printing results collected and sorted in list.
+    """Method for printing results collected and sorted in list.
     """
     for x in list:
         print('Znak \"', x[0], "\" sa v texte nachadza : ", x[1], "krat")
-    print("Najcastejsie sa vyskutuje znak: \"", max(list, key = itemgetter(1))[0], "\" a pocet vyskytov je: ", max(list, key = itemgetter(1))[1])
-    print("Najmenej sa vyskutuje znak: \"", min(list, key = itemgetter(1))[0], "\" a pocet vyskytov je: ", min(list, key = itemgetter(1))[1])
-
+    print("Najcastejsie sa vyskutuje znak: \"",
+          max(list, key = itemgetter(1))[0], "\" a pocet vyskytov je: ",
+          max(list, key = itemgetter(1))[1])
+    print("Najmenej sa vyskutuje znak: \"",
+          min(list, key = itemgetter(1))[0], "\" a pocet vyskytov je: ",
+          min(list, key = itemgetter(1))[1])
 
 def main():
     file_data=arguments()
