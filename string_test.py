eb5955712c0da3d186a341ae46bee7ab417fe6e8
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-12 18:47:39 +0100

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -8,24 +8,14 @@ import unittest
 
 class Tests(unittest.TestCase):
     """Vytvoření třídy."""
-
-
     def test_upper(self): """Test na fce zvětšení písmena."""
-
-
     self.assertEqual("abc".upper(), "ABC")
 
-
     def test_isupper(self): """Testování fce zvětšení písmena."""
-
-
     self.assertTrue("ABC".isupper())
     self.assertFalse("Abc".isupper())
 
-
     def test_split(self): """Testování fce split."""
-
-
     s = "miroslav olivik"
     self.assertEqual(s.split(), ["miroslav", "olivik"])
     with self.assertRaises(TypeError):
