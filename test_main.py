507406130ca2c7fb7364db9cf2ca25ada80ee71d
Dominik Vanduch <dominikvanduch@post.cz>
2020-12-12 22:59:41 +0100

    Updates to test_main.py for cleaner code.

--- test_main.py
+++ test_main.py
@@ -5,8 +5,8 @@ import main
 def test_int_to_roman():
     """This function tests output of int_to_roman."""
     assert main.int_to_roman(10) == 'X'
-
-
-if __name__ == '__main__':
     assert main.int_to_roman(326) == 'CCCXXVI'
-    test_int_to_roman()
+    assert main.int_to_roman(111) == 'CXI'
+    assert main.int_to_roman(38) == 'XXXVIII'
+    assert main.int_to_roman(987) == 'CMLXXXVII'
+    assert main.int_to_roman(326) == 'CCCXXVI'
