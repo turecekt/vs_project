3a9f2c2bfa333abc5b54087266b60f7b0097331e
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-12 19:44:09 +0100

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -8,18 +8,18 @@ import unittest
 
 class Tests(unittest.TestCase):
     """Vytvoření třídy."""
-    
-    def test_upper(vlastni): """Test na fce zvětšení písmena."""
-    vlastni.assertEqual("abc".upper(), "ABC")
 
-    def test_isupper(vlastni): """Testování fce zvětšení písmena."""
-    vlastni.assertTrue("ABC".isupper())
-    vlastni.assertFalse("Abc".isupper())
+    def test_upper(self): """Test na fce zvětšení písmena."""
+    self.assertEqual("abc".upper(), "ABC")
 
-    def test_split(vlastni): """Testování fce split."""
+    def test_isupper(self): """Testování fce zvětšení písmena."""
+    self.assertTrue("ABC".isupper())
+    self.assertFalse("Abc".isupper())
+
+    def test_split(self): """Testování fce split."""
     s = "miroslav olivik"
-    vlastni.assertEqual(s.split(), ["miroslav", "olivik"])
-    with vlastni.assertRaises(TypeError):
+    self.assertEqual(s.split(), ["miroslav", "olivik"])
+    with self.assertRaises(TypeError):
         s.split(2)
 
 
