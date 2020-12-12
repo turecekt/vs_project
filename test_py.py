287756f0805650efcd08eaded0620b1783f76aee
Katerina Chytilova <kate.chytilova@gmail.com>
2020-12-12 23:11:08 +0100

    Flake8 fixes

--- test_py.py
+++ test_py.py
@@ -5,17 +5,19 @@ import morsecode
 
 @pytest.mark.parametrize("input,expected", [("e", "."),
                          ("My name is Kate", "-- -.--  -. .- -- .  "
-                          ".. ...  -.- .- - ."), ("?,.;/=-'():_+@",
-                          "..--.. --..-- .-.-.- -.-.-. -..-. -...- "
-                          "-....- .----. -.--. -.--.- ---... "
-                          "..--.- .-.-. .--.-."), ("čáp!", "Č Á .--. !")])
+                          ".. ...  -.- .- - ."),
+                         ("?,.;/=-'():_+@", "..--.. --..-- .-.-.- "
+                          "-.-.-. -..-. -...- -....- .----. -.--. "
+                          "-.--.- ---... ..--.- .-.-. .--.-."),
+                         ("čáp!", "Č Á .--. !")])
 def test_encode(input, expected):
     """Tests encode function."""
     assert(morsecode.encode_morse(input) == expected)
 
 
 @pytest.mark.parametrize("input,expected", [("....", "H"),
-                         ("?", ""), ("-- --- .-. ... . --- "
+                         ("?", ""),
+                         ("-- --- .-. ... . --- "
                           "...- -.- .-", "MORSEOVKA")])
 def test_decode(input, expected):
     """Tests decode_morse function."""
