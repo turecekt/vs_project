905c01aefe0b8d4288b3e59bf6ca8dfbf77b5d1b
Danil Tkachenko <75219332+danilt2000@users.noreply.github.com>
2020-12-12 19:19:57 +0100

    Provedeni flake8 pro test_AP1.py

--- Audioplayer/test_AP1.py
+++ Audioplayer/test_AP1.py
@@ -1,3 +1,8 @@
+"""
+Unit Test Music Player.
+
+Data:12/12/2020
+"""
 import unittest
 from pygame import mixer
 from pygame.compat import unicode_
@@ -6,26 +11,30 @@ import os
 
 
 class TestMusicPlayer(unittest.TestCase):
+    """Vytvoreni classu TestMusicPlayer."""
+
     def test_sound_unicode(self):
-        """test non-ASCII unicode path"""
-        #Zacatek inicalizaci
+        """Test non-ASCII unicode path."""
+        # Zacatek inicalizaci
         mixer.init()
 
         import shutil
-        # Modul shutil nabizi radu operaci na vysoke urovni se soubory a sbirkami souboru.
-        # Konkretne jsou k dispozici funkce podporujici kopirovini a mazini souboru.
+        # Modul shutil nabizi radu operaci na vysoke urovni
+        # Se soubory a sbirkami souboru
+        # Konkretne jsou k dispozici funkce podporujici kopirovini
+        # A mazini souboru
 
         ep = unicode_(example_path('data'))
 
         temp_file = os.path.join(ep, u'你好.wav')
         org_file = os.path.join(ep, u'house_lo.wav')
         # metoda path.join sdruzuje ruzne komponenty cesty s presne jednim
-        # oddelovacem adresaru ( ' / ' ) po kazde neprazdne casti s vyjimkou komponenty posledni cesty.
+        # oddelovacem adresaru ( ' / ' )
+        # po kazde neprazdne casti s vyjimkou komponenty posledni cesty
 
         shutil.copy(org_file, temp_file)
         # kopiruje soubory org_file, temp_file
         try:
-        # Pokus otevreni soubpru
             with open(temp_file, 'rb'):
                 # vynechani
                 pass
@@ -40,13 +49,16 @@ class TestMusicPlayer(unittest.TestCase):
             os.remove(temp_file)
 
     def test_init__zero_values(self):
-        # Kontroluje, zda vstupni podminky jsou ekvivalentni.
+        """Kontrola vstupnich podminek zvukovych charakteristik."""
+        # Kontroluje, zda vstupni podminky jsou ekvivalentni
         mixer.pre_init(44100, 8, 1)
         mixer.init(0, 0, 0)
-        self.assertNotEqual(mixer.get_init(), (44100, 8, 1))        
+        self.assertNotEqual(mixer.get_init(), (44100, 8, 1))
 
     def test_get_num_channels__defaults_eight_after_init(self):
-        # Porovnani poctu zvukovych kanalu v mixer.get_num_channels() s ocekavanymi 8 kanaly.
+        """Kontrola poctu vstupnych konalu."""
+        # Porovnani poctu zvukovych kanalu
+        # v mixer.get_num_channels() s ocekavanymi 8 kanaly.
         mixer.init()
         self.assertEqual(mixer.get_num_channels(), 8)
 
