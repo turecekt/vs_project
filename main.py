712c0536019b8948c3151300106ab0b4bc914a17
pannexy <m_kocenda@utb.cz>
2020-12-12 20:34:33 +0100

    text edit

--- main.py
+++ main.py
@@ -13,3 +13,28 @@ morse_code = {'A': '.-', 'B': '-...',
               '3': '...--', '4': '....-', '5': '.....',
               '6': '-....', '7': '--...', '8': '---..',
               '9': '----.'}
+"""
+text -> promenna do ktere si ulozime
+originalni text k prelozeni
+cyklus for -> preklada pismeno ve zprave
+podminka if    -> pokud pismeno ve zprave neni mezera
+najdu si v poli morse_code
+odpovidajici znak k pismenu a pridam mezeru
+        else   -> pokud pismeno ve
+        zprave je mezera pridam mezeru
+"""
+
+
+def zasifrovat(zprava):
+    """Funkce zasifrovat -> slouzi k zasifrovani zpravy pomoci.
+    
+    morseovy abecedy funkce vraci zasifrovany text.
+    """
+    text = ''
+    for pismeno in zprava:
+
+        if pismeno != ' ':
+            text += morse_code[pismeno] + ' '
+        else:
+            text += ' '
+    return text
