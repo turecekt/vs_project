900397dcccd98b1466e5e70dcea3be8f980d0cf2
Petr Nagy <p_nagy@utb.cz>
2020-12-13 00:43:57 +0100

    Podmínka pro trojúhelník + výpis hodnot

--- mainProgram.py
+++ mainProgram.py
@@ -20,3 +20,18 @@ if functions.souradniceJeCislo(xA, xB, xC, yA, yB, yC) == 1:
     yB = float(yB.replace(',', '.'))
     xC = float(xC.replace(',', '.'))
     yC = float(yC.replace(',', '.'))
+
+    # Podmínka pro zkontolování jestli je daný objekt trojúhelník
+    if functions.obsahTrojuhelniku(xA, xB, xC, yA, yB, yC) != 0:
+        "Výpis výsledných hodnot volaných ze souboru functions.py"
+        print('\nPodle zadaných souřadnic bodů se jedná o trojúhelník.\n')
+        print('Souřadnice jednotlivých bodů trojúhelníku:\n')
+        print(f'A[{xA},{yA}]\nB[{xB},{yB}]\nC[{xC},{yC}]\n')
+        print('Délky jednotlivých stran:\n')
+        print(f'c=|AB|={functions.delkaStranyAB(xA, xB, yA, yB)}\n')
+        print(f'a=|BC|={functions.delkaStranyBC(xB, xC, yB, yC)}\n')
+        print(f'b=|AC|={functions.delkaStranyAC(xA, xC, yA, yC)}\n')
+        print('Obvod a obsah zadaného trojúhelníku:\n')
+        print(f'Obvod = {functions.obvodTroj(xA, xB, xC, yA, yB, yC)}\n')
+        print(f'Obsah = {functions.obsahTroj(xA, xB, xC, yA, yB, yC)}\n')
+        print('Informace o pravoúhlosti:')
