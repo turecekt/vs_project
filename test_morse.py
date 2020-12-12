080d857e53e27ba332048e7e2d9cc0ec32cabacc
Mikulecky_Pavel <p_mikulecky@utb.cz>
2020-12-12 19:08:24 +0100

    Oprava docstring v test_morse.py

--- test_morse.py
+++ test_morse.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec 12 16:40:00 2020
+Created on Sat Dec 12 16:40:00 2020.
 
 @author: pavel
 """
@@ -10,36 +10,43 @@ import morse
 
 
 class TestMorse(unittest.TestCase):
+    """Trida unit-testu."""
 
     def test_morse_na_text(self):
+        """Test z morseovky na text."""
         result = morse.decryption(".- -... -.-. -.. . ..-. --. .... .. .--- "
                                   "-.- .-.. -- -. --- .--. --.- .-. ... - ..- "
                                   "...- .-- -..- -.-- --..")
         self.assertEqual(result, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     def test_morse_na_cisla(self):
+        """Test z morseovky na cisla."""
         result = morse.decryption(".---- ..--- ...-- ....- ..... -.... --... "
                                   "---.. ----. -----")
         self.assertEqual(result, "1234567890")
 
     def test_morse_na_znaky(self):
+        """Test z morseovky na znaky."""
         result = morse.decryption(".-... .----. .--.-. -.--.- -.--. ---... "
                                   "--..-- -...- -.-.-- .-.-.- -....- .-.-. "
                                   "..--.. -..-.")
         self.assertEqual(result, "&'@)(:,=!.-+?/")
 
     def test_text_na_morse(self):
+        """Test z textu na morseovku."""
         result = morse.encryption("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
         self.assertEqual(result, ".- -... -.-. -.. . ..-. --. .... .. .--- "
                          "-.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- "
                          ".-- -..- -.-- --.. ")
 
     def test_cisla_na_morse(self):
+        """Test z cisel na morseovku."""
         result = morse.encryption("1234567890")
         self.assertEqual(result, ".---- ..--- ...-- ....- ..... -.... --... "
                          "---.. ----. ----- ")
 
     def test_znaky_na_morse(self):
+        """Test ze znaku na morseovku."""
         result = morse.encryption("&'@)(:,=!.-+?/")
         self.assertEqual(result, ".-... .----. .--.-. -.--.- -.--. ---... "
                          "--..-- -...- -.-.-- .-.-.- -....- .-.-. "
