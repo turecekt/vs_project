bcb7ac9764fa8f7fd6e318dd4065745334af6ced
Marek Dolanský <m1_dolansky@utb.cz>
2020-12-12 21:37:45 +0100

    Přidání komentářů podle flake8

--- project.py
+++ project.py
@@ -1,42 +1,66 @@
+"""Číselné soustavy."""
+
+# Imports
 import sys
-import math
+
+__author__ = 'Marek Dolanský'
+__version__ = '1.0.0'
+__email__ = 'm1_dolansky@utb.cz'
+__status__ = 'Work in progress'
+
 
 def numberSelection():
+    """Zadání čísla.
+
+    Uživatel zadá číslo, které chce
+    převést do jíné číselné soustavy
+    """
     global n
-    print('Zadejte kladné celé číslo v desítkové soustavě (Pro ukončení zadejte \"exit\"):')
+    print('Zadejte kladné celé číslo v desítkové soustavě '
+          '(Pro ukončení zadejte \"exit\"):')
     n = input()
 
     if n == 'exit':
-            sys.exit('Program byl ukončen')
+        sys.exit('Program byl ukončen')
 
     while not n.isnumeric():
         print('Zadali jste neplatnou hodnotu:', n)
-        print('Musíte zadat kladné celé číslo (Pro ukončení zadejte \"exit\"):')
+        print('Musíte zadat kladné celé číslo '
+              '(Pro ukončení zadejte \"exit\"):')
         n = input()
         if n == 'exit':
             sys.exit('Program byl ukončen')
 
     while int(n) < 0:
         print('Zadali jste neplatnou hodnotu:', n)
-        print('Musíte zadat kladné celé číslo (Pro ukončení zadejte \"exit\"):')
+        print('Musíte zadat kladné celé číslo '
+              '(Pro ukončení zadejte \"exit\"):')
         n = input()
         if n == 'exit':
             sys.exit('Program byl ukončen')
 
+
 def systemSelection():
+    """Výběr cílové soustavy.
+
+    Uživatel zadá číslo soustavy,
+    do které chce převést dříve zadanou hodnotu
+    """
     global s
-    print('Vyberte soustavu, do které chcete číšlo přenést (Pro ukončení zadejte \"exit\"):')
+    print('Vyberte soustavu, do které chcete číšlo převést '
+          '(Pro ukončení zadejte \"exit\"):')
     print('1 - Dvojková')
     print('2 - Osmičková')
     print('3 - Šestnáctková')
     s = input()
 
     if s == 'exit':
-            sys.exit('Program byl ukončen')
+        sys.exit('Program byl ukončen')
 
     while not s.isnumeric():
         print('Zadali jste neplatnou hodnotu:', s)
-        print('Vyberte soustavu, do které chcete číšlo přenést (Pro ukončení zadejte \"exit\"):')
+        print('Vyberte soustavu, do které chcete číšlo převést '
+              '(Pro ukončení zadejte \"exit\"):')
         print('1 - Dvojková')
         print('2 - Osmičková')
         print('3 - Šestnáctková')
@@ -46,7 +70,8 @@ def systemSelection():
 
     while not 1 <= int(s) <= 3:
         print('Zadali jste neplatnou hodnotu:', s)
-        print('Vyberte soustavu, do které chcete číšlo přenést (Pro ukončení zadejte \"exit\"):')
+        print('Vyberte soustavu, do které chcete číšlo převést '
+              '(Pro ukončení zadejte \"exit\"):')
         print('1 - Dvojková')
         print('2 - Osmičková')
         print('3 - Šestnáctková')
@@ -54,16 +79,24 @@ def systemSelection():
         if s == 'exit':
             sys.exit('Program byl ukončen')
 
+
 def decimalToBinary(decimal):
+    """Převod do dvojkové soustavy."""
     print(decimal, 'je ve dvojkové soustavě:', bin(decimal)[2:])
 
+
 def decimalToOctal(decimal):
+    """Převod do osmičkové soustavy."""
     print(decimal, 'je v osmičkové soustavě:', oct(decimal)[2:])
 
+
 def decimalToHexadecimal(decimal):
+    """Převod do šestnáctkové soustavy."""
     print(decimal, 'je v šestnáctkové soustavě:', hex(decimal)[2:])
 
+
 def switch(selection, n):
+    """Metoda pro výběr soustavy."""
     if selection == 1:
         decimalToBinary(n)
     elif selection == 2:
@@ -71,10 +104,13 @@ def switch(selection, n):
     elif selection == 3:
         decimalToHexadecimal(n)
 
+
 def main():
+    """Spuštění programu."""
     numberSelection()
     systemSelection()
     switch(int(s), int(n))
+
 
 if __name__ == "__main__":
     main()
