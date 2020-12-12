e379baab9e3cd1fed6982458e73134026553d1d9
Petr Nagy <p_nagy@utb.cz>
2020-12-13 00:42:04 +0100

    Zadání vstupních hodnot

--- mainProgram.py
+++ mainProgram.py
@@ -2,3 +2,11 @@
 
 # Import souboru functions.py
 import functions
+
+# Zadání vstupních hodnot uživatelem a uložení je do přoměnných
+xA = input("Zadej x-ovou souřadnici bodu A: ")
+yA = input("Zadej y-ovou souřadnici bodu A: ")
+xB = input("Zadej x-ovou souřadnici bodu B: ")
+yB = input("Zadej y-ovou souřadnici bodu B: ")
+xC = input("Zadej x-ovou souřadnici bodu C: ")
+yC = input("Zadej y-ovou souřadnici bodu C: ")
