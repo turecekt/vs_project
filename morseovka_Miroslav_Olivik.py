2654d2bd7f5409581c0f67082fc6be0cb72621f3
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-12 17:21:48 +0100

    Update morseovka_Miroslav_Olivik.py

--- morseovka_Miroslav_Olivik.py
+++ morseovka_Miroslav_Olivik.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python 3
 
-"""Jako první věc si musíme definovat abecedu"""
+"""Jako první věc si musíme definovat abecedu."""
 
 
 SLOVNIK_MORSEOVKA = {' ': '/', 'A': '.-',
@@ -25,14 +25,14 @@ SLOVNIK_MORSEOVKA = {' ': '/', 'A': '.-',
                      '?': '..--..', '/': '-..-.', '-': '-....-',
                      '(': '-.--.', ')': '-.--.-'}
 
-"""Dále definujeme první funkci"""
-"""Jako první věc definujeme text, kterému máme doplnit text pro kodovani"""
-"""Dále vytvoříme převodový text na kod"""
-"""Vytvoříme string k danemu code"""
-"""A string necháme přečíst"""
+"""Dále definujeme první funkci."""
+"""Jako první věc definujeme text, kterému máme doplnit text pro kodovani."""
+"""Dále vytvoříme převodový text na kod."""
+"""Vytvoříme string k danemu code."""
+"""A string necháme přečíst."""
 
 
-def Text_To_Code(): """Funkce - překlad slov do morseovky"""
+def Text_To_Code(): """Funkce - překlad slov do morseovky."""
 text2 = input("Napište slova, která chcete kodovat: ")
 code2 = [SLOVNIK_MORSEOVKA
         [i.upper()] + " "
@@ -41,24 +41,24 @@ morseovka = "".join(code2)
 print(morseovka)
 
 
-"""Stejný způsob použijeme i zde u dekodovani námi zadaného textu"""
-"""Vytvoříme zde to, že námi zadaný kod program rozloží na jednotky"""
-"""Opet nechame udělat string z code a necháme ho vypsat"""
+"""Stejný způsob použijeme i zde u dekodovani námi zadaného textu."""
+"""Vytvoříme zde to, že námi zadaný kod program rozloží na jednotky."""
+"""Opet nechame udělat string z code a necháme ho vypsat."""
 
 
-def Code_To_Text(): """Vytvořené funkce - přeložení morseovky"""
+def Code_To_Text(): """Vytvořené funkce - přeložení morseovk."""
 
 
 text1 = input("Napište kod, který chcete dekodovat: ")
 code1 = [k for i in text1.split() for k,
-             v in SLOVNIK_MORSEOVKA.items() if i == v]
+         v in SLOVNIK_MORSEOVKA.items() if i == v]
 novytext = "".join(code1)
 print(novytext)
 
 
-"""Vypiseme menu pro vyber, co hceme aby program delal"""
-"""Na zaver vypisime cyklus, ktery nám k menu ve stringu priradi i funkce"""
-"""Zvolili jsme fci else, která nám pokryje jinou možnost než-li 1-3"""
+"""Vypiseme menu pro vyber, co hceme aby program delal."""
+"""Na zaver vypisime cyklus, ktery nám k menu ve stringu priradi i funkce."""
+"""Zvolili jsme fci else, která nám pokryje jinou možnost než-li 1-3."""
 
 
 print("""\n1 - TEXT KE KODOVÁNÍ \n2 - KÓD K ROZKÓDOVÁNÍ\n3 - KONEC\n""")
