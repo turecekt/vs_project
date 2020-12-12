e3a54efbfcdd90f03d53dba9d0a895a2bce93b6c
MatusJ96 <73904041+MatusJ96@users.noreply.github.com>
2020-12-12 14:59:25 +0100

    Update code-style and comments

--- char_recurrence/main.py
+++ char_recurrence/main.py
@@ -6,9 +6,13 @@ from operator import itemgetter
 
 def arguments():
     """Method for parsing argument from std-in or as parameters.
+
     Use argparse library.
+    Args:
+        - input - Input file
     Without parameter it read from std-in else from input files
-    return all arguments.
+    Returns:
+        all parsed arguments.
     """
     parser = ArgumentParser()
     parser.add_argument("-f", "--file", dest="filename", action="store",
@@ -22,9 +26,12 @@ def arguments():
         args = parser.parse_args().filename
         return read_file(args, arg_len)
 
+
 def read_file(input_file, arg_len):
-    """Method for reading char to specific char #.
-    Method return @data (string which will be working next).
+    """Method for reading char to specific char.
+
+    Returns:
+         data = string which will be working next.
     """
     if arg_len != 1:
         with open(input_file, "rt") as f:
@@ -35,37 +42,50 @@ def read_file(input_file, arg_len):
 
     return data
 
+
 def number_char(file_data):
-    """Method return lenght of passing string.
+    """Method return length of passing string.
+
+    Returns:
+        number_char = number of characters in string
     """
     num_char=len(file_data)
 
     return num_char
 
+
 def number_occurence(file_data):
-    """Method create list of occurrence and eleminate unwanted chars from string.
+    """Method create list of occurrence without unwanted chars.
+
+    Returns:
+        my_list = list of all characters
     """
-    my_list=[]
-    file_data=file_data.lower()
+    my_list = []
+    file_data = file_data.lower()
     for char in file_data:
         if re.match('^[a-z]+$', char):
-            count=len(re.findall(char, file_data))
+            count = len(re.findall(char, file_data))
             if not tuple((char, count)) in my_list:
                 my_list.append(tuple((char, count)))
 
     return my_list
 
+
 def print_number_occurence(list):
     """Method for printing results collected and sorted in list.
+
+    Returns:
+        Print output to std-out
     """
     for x in list:
         print('Znak \"', x[0], "\" sa v texte nachadza : ", x[1], "krat")
     print("Najcastejsie sa vyskutuje znak: \"",
-          max(list, key = itemgetter(1))[0], "\" a pocet vyskytov je: ",
-          max(list, key = itemgetter(1))[1])
+          max(list, key=itemgetter(1))[0], "\" a pocet vyskytov je: ",
+          max(list, key=itemgetter(1))[1])
     print("Najmenej sa vyskutuje znak: \"",
-          min(list, key = itemgetter(1))[0], "\" a pocet vyskytov je: ",
-          min(list, key = itemgetter(1))[1])
+          min(list, key=itemgetter(1))[0], "\" a pocet vyskytov je: ",
+          min(list, key=itemgetter(1))[1])
+
 
 def main():
     file_data=arguments()
