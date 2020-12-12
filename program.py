e2fc7133552f6c0a39510ef1173af44f8a0d8647
JakubSladky4 <jakub.sladky96@gmail.com>
2020-12-12 13:29:28 +0100

    vytvoření souboru program a souboru pro jednotkové testy

---
+++ program.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 13:25:27 2020
+
+@author: jakub
+"""
+
---
+++ test_.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 13:27:27 2020
+
+@author: jakub
+"""
+
