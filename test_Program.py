b4c8ac2f728c1315dd4ff49759950d4404e47980
Filip Spacek <f_spacek@utb.cz>
2020-12-12 15:35:41 +0100

    Doplneni dalsich unit testu

--- test_Program.py
+++ test_Program.py
@@ -7,9 +7,24 @@ def test_formateError():
     assert p.formateError('test') == "!!! test !!!"
 
 
-def test_parseAction():
+def test_parseAction_Encode():
     """Test parsovani akce."""
     assert p.parseAction('Z') == 1
+
+
+def test_parseAction_Decode():
+    """Test parsovani invalidni akce."""
+    assert p.parseAction('D') == 2
+
+
+def test_parseAction_End():
+    """Test parsovani akce konec."""
+    assert p.parseAction('K') == 3
+
+
+def test_parseAction_Invalid():
+    """Test parsovani invalidni akce."""
+    assert p.parseAction('X') == 4
 
 
 def test_checkUserInputText():
@@ -18,6 +33,14 @@ def test_checkUserInputText():
     if (p.checkUserInputText('"test"')):
         res = 'True'
     assert res == 'True'
+
+
+def test_checkUserInputText_Invalid():
+    """Test nevalidniho vstupu uzivatele."""
+    res = 'True'
+    if (not p.checkUserInputText('test')):
+        res = 'False'
+    assert res == 'False'
 
 
 def test_encodeText():
