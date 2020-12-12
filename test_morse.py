5293695070c86d5efc61a8befdba0186b4763b37
Mikulecky_Pavel <p_mikulecky@utb.cz>
2020-12-12 18:26:51 +0100

    provedeni flake8 na comit-testu

--- test_morse.py
+++ test_morse.py
@@ -10,30 +10,41 @@ import morse
 
 
 class TestMorse(unittest.TestCase):
-    
+
     def test_morse_na_text(self):
-        result = morse.decryption(".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..")
+        result = morse.decryption(".- -... -.-. -.. . ..-. --. .... .. .--- "
+                                  "-.- .-.. -- -. --- .--. --.- .-. ... - ..- "
+                                  "...- .-- -..- -.-- --..")
         self.assertEqual(result, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     def test_morse_na_cisla(self):
-        result = morse.decryption(".---- ..--- ...-- ....- ..... -.... --... ---.. ----. -----")
+        result = morse.decryption(".---- ..--- ...-- ....- ..... -.... --... "
+                                  "---.. ----. -----")
         self.assertEqual(result, "1234567890")
 
     def test_morse_na_znaky(self):
-        result = morse.decryption(".-... .----. .--.-. -.--.- -.--. ---... --..-- -...- -.-.-- .-.-.- -....- .-.-. ..--.. -..-.")
+        result = morse.decryption(".-... .----. .--.-. -.--.- -.--. ---... "
+                                  "--..-- -...- -.-.-- .-.-.- -....- .-.-. "
+                                  "..--.. -..-.")
         self.assertEqual(result, "&'@)(:,=!.-+?/")
 
     def test_text_na_morse(self):
         result = morse.encryption("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-        self.assertEqual(result, ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. ")
+        self.assertEqual(result, ".- -... -.-. -.. . ..-. --. .... .. .--- "
+                         "-.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- "
+                         ".-- -..- -.-- --.. ")
 
     def test_cisla_na_morse(self):
         result = morse.encryption("1234567890")
-        self.assertEqual(result, ".---- ..--- ...-- ....- ..... -.... --... ---.. ----. ----- ")
+        self.assertEqual(result, ".---- ..--- ...-- ....- ..... -.... --... "
+                         "---.. ----. ----- ")
 
     def test_znaky_na_morse(self):
         result = morse.encryption("&'@)(:,=!.-+?/")
-        self.assertEqual(result, ".-... .----. .--.-. -.--.- -.--. ---... --..-- -...- -.-.-- .-.-.- -....- .-.-. ..--.. -..-. ")
+        self.assertEqual(result, ".-... .----. .--.-. -.--.- -.--. ---... "
+                         "--..-- -...- -.-.-- .-.-.- -....- .-.-. "
+                         "..--.. -..-. ")
+
 
 if __name__ == "__main__":
     unittest.main()
