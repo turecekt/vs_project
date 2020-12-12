a6fd0dc2e935181ca902d2d56fc38a9d8570dc1b
Dominik Vanduch <dominikvanduch@post.cz>
2020-12-12 21:32:55 +0100

    Rewritten for flake8 and docstring documentation.

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
+"""
+Integer to Roman numerals converter by Dominik Vanduch.
+
+This console app was written to take integer and convert it into a Roman
+numeral system as a term project for AP1VS.
+"""
+
+
 def int_to_roman(number):
+    """
+    Convert integer to Roman numeral system.
+
+    Tests for this function:
+    >>> int_to_roman(10)
+    'X'
+    >>> int_to_roman(468)
+    'CDLXVIII'
+    >>> int_to_roman(967)
+    'CMLXVII'
+    """
     values = [
         1000, 900, 500, 400,
         100, 90, 50, 40,
         10, 9, 5, 4,
         1
         ]
+
     dictionary = [
         "M", "CM", "D", "CD",
         "C", "XC", "L", "XL",
         "X", "IX", "V", "IV",
         "I"
         ]
+
     roman_num = ''
     i = 0
-    while  number > 0:
+
+    while number > 0:
         for _ in range(number // values[i]):
             roman_num += dictionary[i]
             number -= values[i]
@@ -21,4 +43,6 @@ def int_to_roman(number):
     return roman_num
 
 
-print(int_to_roman(int(input("Enter a number to be converted to Roman numeral system: "))))
+unconverted = int(input("Enter a number to be converted: "))
+
+print(int_to_roman(unconverted))
---
+++ test_main.py
@@ -0,0 +1,12 @@
+"""File for unit testing."""
+import main
+
+
+def test_int_to_roman():
+    """This function test output of int_to_roman."""
+    assert main.int_to_roman(10) == 'X'
+
+
+if __name__ == '__main__':
+    assert main.int_to_roman("326") == 'CCCXXVI'
+    test_int_to_roman()
