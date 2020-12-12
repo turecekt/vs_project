2b32b4d82d745733a29de92dd4894a7539b53716
Katerina Chytilova <kate.chytilova@gmail.com>
2020-12-12 22:39:27 +0100

    Added pytest

---
+++ test_py.py
@@ -0,0 +1,26 @@
+"""This module tests morsecode."""
+import pytest
+import morsecode
+
+
+@pytest.mark.parametrize("input,expected", 
+                         [("e", "."), 
+                          ("My name is Kate", "-- -.--  -. .- -- .  "
+                           ".. ...  -.- .- - ."), ("?,.;/=-'():_+@",
+                           "..--.. --..-- .-.-.- -.-.-. -..-. -...- "
+                           "-....- .----. -.--. -.--.- ---... "
+                           "..--.- .-.-. .--.-."), ("čáp!", "Č Á .--. !")])
+
+def test_encode(input, expected):
+    """Tests encode function."""
+    assert(morsecode.encode_morse(input) == expected)
+
+
+@pytest.mark.parametrize("input,expected", 
+                         [("....", "H"), ("?", ""),
+                         ("-- --- .-. ... . --- ...- -.- .-", "MORSEOVKA")])
+
+
+def test_decode(input, expected):
+    """Tests decode_morse function."""
+    assert(morsecode.decode_morse(input) == expected)
--- testpy.py
+++
@@ -1,7 +0,0 @@
-"""This module tests morsecode."""
-import morsecode
-
-
-def test_simple_input():
-    """Tests simple input."""
-    assert (morsecode.encode_morse("e") == ".")
