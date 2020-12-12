8c92608119dff3044854f02dc843932bc3b3a6bc
Dominik Vanduch <dominikvanduch@post.cz>
2020-12-12 22:50:09 +0100

    Optimizing code and readability.

--- main.py
+++ main.py
@@ -32,24 +32,26 @@ def int_to_roman(number):
         "I"
         ]
 
-    roman_num = ''
+    roman_number = ''
     i = 0
 
     while number > 0:
         for _ in range(number // values[i]):
-            roman_num += dictionary[i]
+            roman_number += dictionary[i]
             number -= values[i]
         i += 1
-    return roman_num
+    return roman_number
 
 
 def main():
     """Ask for user input, convert it and print it to console."""
-    unconverted = int(input("Enter a number to be converted: "))
-    converted = int_to_roman(unconverted)
-    print(converted)
+    print("This script is for converting number to Roman numeral system.")
+    print("Enter a number to be converted and press enter:")
+    unconverted = int(input())
+    print("Converted number is:")
+    print(int_to_roman(unconverted))
 
 
-# This if runs the main function.
+# This if statement runs the main function.
 if __name__ == '__main__':
     main()
