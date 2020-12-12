7edc8f1714dd2825d85ac8b80276961c2e1d3ae5
lukaszaruba168 <l_zaruba@utb.cz>
2020-12-12 20:38:32 +0100

    function decipher added

--- main.py
+++ main.py
@@ -38,3 +38,53 @@ def zasifrovat(zprava):
         else:
             text += ' '
     return text
+    
+    
+"""
+zprava -> pridani mezery na konec zpravy k pristupu
+poslednimu znaku morseovy abecedy
+ ve zprave protoze index zacina na 0
+prelozeny_text -> promenna
+ve ktere je ulozeny finalni text z morseovy abecedy
+i -> promenna
+ktera nam kontroluje kolik je mezer ve zprave
+podminka if     -> pokud ve zprave neni mezera
+do promenne prelozit_text
+pridam pismena z morseovy abecedy
+         else   -> pokud ve zprave je mezera,
+         pokud je i = 1 tak to znamena ze je ve zprave novy znak
+         if     -> pokud i = 2 znamena to ze je ve
+         zprave nove slovo a pridam do promenne prelozeny_text mezeru
+"""
+
+
+def odsifrovat(zprava):
+    """Funkce odsifrovat -> slouzi k odsifrovani zpravy.
+
+    pomoci morseovy abecedy funkce vraci odsifrovany text.
+    """
+    zprava += ' '
+
+    prelozit_text = ''
+    prelozeny_text = ''
+
+    for pismeno in zprava:
+
+        if (pismeno != ' '):
+            i = 0
+
+            prelozit_text += pismeno
+
+        else:
+            i += 1
+
+            if i == 2:
+                prelozeny_text += ' '
+
+            else:
+
+                prelozeny_text += list(morse_code.keys())[
+                    list(morse_code.values()).index(prelozit_text)]
+                prelozit_text = ''
+
+    return prelozeny_text
