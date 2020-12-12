88fce986bda997009da6b71efce4f92004aa7617
Petr Nagy <p_nagy@utb.cz>
2020-12-13 00:42:48 +0100

    Zkontrolování vstupních hodnot

--- mainProgram.py
+++ mainProgram.py
@@ -10,3 +10,13 @@ xB = input("Zadej x-ovou souřadnici bodu B: ")
 yB = input("Zadej y-ovou souřadnici bodu B: ")
 xC = input("Zadej x-ovou souřadnici bodu C: ")
 yC = input("Zadej y-ovou souřadnici bodu C: ")
+
+# Podmínka pro zkontrolování vstupních hodnot
+if functions.souradniceJeCislo(xA, xB, xC, yA, yB, yC) == 1:
+    "Předělání vstupních hodnot na čísla typu float"
+    xA = float(xA.replace(',', '.'))
+    yA = float(yA.replace(',', '.'))
+    xB = float(xB.replace(',', '.'))
+    yB = float(yB.replace(',', '.'))
+    xC = float(xC.replace(',', '.'))
+    yC = float(yC.replace(',', '.'))
