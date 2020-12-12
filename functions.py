9147fb8de5cab595dcc301d42f78281f0091449f
Petr Nagy <p_nagy@utb.cz>
2020-12-13 00:35:29 +0100

    Funkce pro výpočet délky strany AB

--- functions.py
+++ functions.py
@@ -2,3 +2,9 @@
 
 # Import knihovny math pro matematické funkce
 import math
+
+
+# Funkce pro výpočet délky strany AB trojúhelníku
+def delkaStranyAB(xa, xb, ya, yb):
+    """Funkce vrátí velikost délky strany AB."""
+    return math.sqrt(((xb-xa)**2)+(yb-ya)**2)
