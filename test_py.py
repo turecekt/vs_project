2c4df7aefab3d3a7cf93ee301f50d3207115285a
Katerina Chytilova <kate.chytilova@gmail.com>
2020-12-12 22:51:39 +0100

    More Flake8 fixes

--- test_py.py
+++ test_py.py
@@ -3,21 +3,20 @@ import pytest
 import morsecode
 
 
-@pytest.mark.parametrize("input,expected",
-                         [("e", "."),
-                          ("My name is Kate", "-- -.--  -. .- -- .  "
-                            ".. ...  -.- .- - ."), ("?,.;/=-'():_+@",
-                            "..--.. --..-- .-.-.- -.-.-. -..-. -...- "
-                            "-....- .----. -.--. -.--.- ---... "
-                            "..--.- .-.-. .--.-."), ("čáp!", "Č Á .--. !")])
+@pytest.mark.parametrize("input,expected",[("e", "."),
+                         ("My name is Kate", "-- -.--  -. .- -- .  "
+                          ".. ...  -.- .- - ."), ("?,.;/=-'():_+@",
+                          "..--.. --..-- .-.-.- -.-.-. -..-. -...- "
+                          "-....- .----. -.--. -.--.- ---... "
+                          "..--.- .-.-. .--.-."), ("čáp!", "Č Á .--. !")])
 def test_encode(input, expected):
     """Tests encode function."""
     assert(morsecode.encode_morse(input) == expected)
 
 
-@pytest.mark.parametrize("input,expected",
-                         [("....", "H"), ("?", ""),
-                            ("-- --- .-. ... . --- ...- -.- .-", "MORSEOVKA")])
+@pytest.mark.parametrize("input,expected", [("....", "H"),
+                         ("?", ""),("-- --- .-. ... . --- "
+                          "...- -.- .-", "MORSEOVKA")])
 def test_decode(input, expected):
     """Tests decode_morse function."""
     assert(morsecode.decode_morse(input) == expected)
