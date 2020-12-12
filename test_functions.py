2006292f4e20fd06ff7b0500f9d977ef06d218df
j_potociar <j_potociar@utb.cz>
2020-12-13 00:39:37 +0100

    Rychly zpusob napsani modulu

--- test_functions.py
+++ test_functions.py
@@ -3,3 +3,8 @@
 # Inport frameworku unittest a souboru functions
 import unittest
 import functions
+
+# Rychlý způsob, jak napsat modul, který jde jak importovat, tak spustit
+if __name__ == '__main__':
+    unittest.main()
+
