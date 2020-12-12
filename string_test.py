ae6dcf875d1437eb9ce715deef95f571aa10449f
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-12 20:51:11 +0100

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -10,17 +10,17 @@ class TestMorse(unittest.TestCase):
     """Vytvoření třídy."""
 
     def test_upper(self): """Test na fce zvětšení písmena."""
-        self.assertEqual("abc".upper(), "ABC")
+    self.assertEqual("abc".upper(), "ABC")
 
     def test_isupper(self): """Testování fce zvětšení písmena."""
-        self.assertTrue("ABC".isupper())
-        self.assertFalse("Abc".isupper())
+    self.assertTrue("ABC".isupper())
+    self.assertFalse("Abc".isupper())
 
     def test_split(self): """Testování fce split."""
-        s = "miroslav olivik"
-        self.assertEqual(s.split(), ["miroslav", "olivik"])
-        with self.assertRaises(TypeError):
-            s.split(2)
+    s = "miroslav olivik"
+    self.assertEqual(s.split(), ["miroslav", "olivik"])
+    with self.assertRaises(TypeError):
+        s.split(2)
 
 
 if __name__ == "__main__":
