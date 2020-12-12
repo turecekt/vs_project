4c0c415d6e28a9ca173bda31ceb00a42a99610bd
BuildTools <unconfigured@null.spigotmc.org>
2020-12-12 10:33:04 +0100

    change of internal structure regarding input, added unit tests

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ def morse(text):
     else:
         return 'Vstupni retezec neni napsan v uvozovkach'
 
+    for x in text:
+        x = x.upper()
+        key_check = x
+        if x == '-' or x == '.':
+            continue
+        if key_check in morseovka:
+            continue
+        else:
+            return 'Vstupni retezec obsahuje neocekavane znaky'
+
     if text.startswith('.') or text.startswith('−'):
         morseovka_preklad = dict([(v, k) for k, v in morseovka.items()])
         text = text.split(' ')
@@ -26,7 +36,45 @@ def morse(text):
     return preklad.strip()
 
 
-print("Zadejte retezec (musi byt v uvozovkach):")
-inputstr = str(input())
+# here is the input string
+inputstr = '"ahoj"'
 
 print(morse(inputstr))
+
+
+def test_morse_lower():
+    """test text-to-morse function with only lowercase letters"""
+    assert morse('"ahoj"') == '.- .... --- .---'
+
+
+def test_morse_upper():
+    """test text-to-morse function with only uppercase letters"""
+    assert morse('"AHOJ"') == '.- .... --- .---'
+
+
+def test_morse_mixed():
+    """test text-to-morse function with mixed lowercase and uppercase letters"""
+    assert morse('"Ahoj"') == '.- .... --- .---'
+
+
+def test_morse_unsupported():
+    """test text-to-morse function with unsupported chars"""
+    assert morse('"123ěšč?!"') == 'Vstupni retezec obsahuje neocekavane znaky'
+
+
+def test_morse_mixed_unsupported():
+    """test text-to-morse function with mixed unsupported chars and supported chars"""
+    assert morse('"a1"') == 'Vstupni retezec obsahuje neocekavane znaky'
+
+
+def test_text():
+    """test morse-to-text function"""
+    assert morse('".- .... --- .---"') == 'AHOJ'
+
+
+def test_text_unsupported():
+    assert morse('"123"') == 'Vstupni retezec obsahuje neocekavane znaky'
+
+
+def test_text_mixed_unsupported():
+    assert morse('".- .... 123"') == 'Vstupni retezec obsahuje neocekavane znaky'
