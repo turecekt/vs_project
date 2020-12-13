e14b76476b3055e25a44e81cd4bf162a2aa8c136
drtilp <swpv@email.cz>
2020-12-13 13:51:45 +0100

    Added comments and unittests to the code

--- Minimax/Heapsort.py
+++ Minimax/Heapsort.py
@@ -1,15 +1,23 @@
+import random
+
+#Konstrukce haldy, použití tzv. maximové haldy (bubble up - bublání nahoru) 
 def heapify(arr, n, i):
+    #inicializace bublání, n - velikost velikost haldy, kořen na indexu i
     largest = i
     l = 2 * i + 1
     r = 2 * i + 2
+    #Porovnání levých synů stromu, jestli jsou větší než kořen tak prohodíme
     if l < n and arr[i] < arr[l]:
         largest = l
+    #Porovnání pravých synů stromu, jestli jsou větší než kořen tak prohodíme
     if r < n and arr[largest] < arr[r]:
         largest = r
+    #Výměna kořene (opakované mazání maxima)
     if largest != i:
         arr[i],arr[largest] = arr[largest],arr[i]
         heapify(arr, n, largest)
 
+#Main funkce heap (halda) sortu
 def heap_sort(arr):
     n = len(arr)
     for i in range(n // 2 - 1, -1, -1):
@@ -17,3 +25,17 @@ def heap_sort(arr):
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
         heapify(arr, i, 0)
+
+#Unittest heapsortu
+def test_heapsort():
+    """Heapsort Unittest."""
+    test_arr = [37, 41, 73, 13, 7, 101]
+    heap_sort(test_arr)
+    assert(test_arr) == [7, 13, 37, 41, 73, 101]
+    #test nahodných vstupů
+    test_rArr = [random.sample(range(100), 10)]
+    test_rArrCopy = test_rArr.copy()
+    heap_sort(test_rArr)
+    test_rArrCopy.sort()
+    assert (test_rArr) == test_rArrCopy
+
--- Minimax/Minimax.py
+++ Minimax/Minimax.py
@@ -1,9 +1,11 @@
-import sys, getopt
+import sys
 import random
 from Quicksort import quick_sort
 from Heapsort import heap_sort
 from array import array
+import pytest
 
+#Funkce pro zjištění nejmenšího prvku v poli
 def min(nums):
     min_value = None
     for value in nums:
@@ -13,6 +15,7 @@ def min(nums):
             min_value = value
     return min_value
 
+#Funkce pro zjištění největšího prvku v poli
 def max(nums):
     max_value = nums[0]
     for value in nums:
@@ -20,67 +23,79 @@ def max(nums):
             max_value = value
     return max_value
 
+#Výpis nejmenšího a největšího prvku v poli s jeho indexem
 def min_max(mylist):
     print("Nejmenší prvek v seznamu má hodnotu: " + str(min(mylist)) + " a nachazi se na indexu: " + str(mylist.index(min(mylist))))
     print("Největší prvek v seznamu má hodnotu: " + str(max(mylist)) + " a nachazi se na indexu: " + str(mylist.index(max(mylist))))
 
-def print_array(arr):
-    for i in range(0, len(arr)):
-        print(arr[i], end = " ")
-    print()
-
+#Switch na zvolení typu řadícího algortimu
 def sort(mylist):
     print("Jaký řadící algortimus chcete použít?") 
     print("Quick sort[1], Heap sort[2], Python Builtin[3]")
     x = input()
     if x == '1':
-        for i in range(0, len(mylist)):
-            mylist[i] = int(mylist[i])
-        arr = array("i", mylist)
-        quick_sort(arr, 0, len(arr)-1)
-        print_array(arr)
+        quick_sort(mylist, 0, len(mylist)-1)
+        print(mylist)
     elif x == '2':
-        for i in range(0, len(mylist)):
-            mylist[i] = int(mylist[i])
-        arr = array("i", mylist)
-        heap_sort(arr)
-        print_array(arr)
+        heap_sort(mylist)
+        print(mylist)
     elif x == '3':
         mylist.sort()
         print(mylist)
 
+#Generovaní pole při zavolání funkce uživatele bez argumentů
 def generate_array():
     mylist = []
     while len(mylist) < 20:
         r = random.randint(1, 100)
         if r not in mylist:
             mylist.append(r)
-    print("Sorted list: ", mylist)
+    print("Generated list: ", mylist)
+    list2 = [rand = random.randint() if rand not in for _ in range(10)]
+    print("list comp rand:", list2)
+    sort(mylist)
     min_max(mylist)
 
-def read_input(argument=None):
-    mylist = []
-    if len(sys.argv) > 1:
-        argument = sys.argv[1]
-        if argument[-3:]=='txt':
-            row_data = []
-            file = open(sys.argv[1], "r")
-            lines = file.read().splitlines()
-            file.close()
-            for line in lines:
-                row_data = line.split()
-                for num in row_data:
-                    mylist.append(int(num))
-            print(mylist)
-            min_max(mylist)
-            sort(mylist)
-        else:
-            mylist = []
-            for i in sys.argv[1:]:
-                mylist.append(i) 
-            min_max(mylist)
-            sort(mylist)        
+#Vstup:
+mylist = []
+if len(sys.argv) > 1:
+    argument = sys.argv[1]
+    if argument[-3:]=='txt':
+        row_data = []
+        file = open(sys.argv[1], "r")
+        lines = file.read().splitlines()
+        file.close()
+        for line in lines:
+            row_data = line.split()
+            for num in row_data:
+                mylist.append(int(num))
+        print(mylist)
+        min_max(mylist)
+        sort(mylist)
     else:
+        mylist = []
+        for i in sys.argv[1:]:
+            mylist.append(i)
+        min_max(mylist)
+        sort(mylist)
+else:
         generate_array()
 
-read_input()
+#Unittesty
+def test_min():
+    """Min test."""
+    test_list = [1, 2, 3]
+    assert(min(test_list)) == 1
+
+def test_max():
+    """Max test."""
+    test_list = [1, 2, 3]
+    assert(max(test_list)) == 3 
+
+def testQuicksort():
+    """Quicksort test."""
+    test_arr = [7, 13, 5]
+    quick_sort(test_arr, 0, len(test_arr)-1)
+    assert test_arr == [5, 7, 13]
+
+
