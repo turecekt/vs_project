1c16185af0afeb53f2c868ee2b6ffc81ecbea03c
tesaro <o_tesar@utb.cz>
2020-12-13 17:01:56 +0100

    Drobne upravy

--- morseovka.py
+++ morseovka.py
@@ -3,85 +3,100 @@
 # This program encodes and decodes latin alphabet to and from MorseCode
 
 # Morsecode dictionary
-MORSE_CODE_DICT = {'A':'.-'
-                   'B':'-...'
-                   'C':'-.-.'
-                   'D':'-..'
-                   'E':'.'
-                   'F':'..-.'
-                   'G':'--.'
-                   'H':'-.-'
-                   'I':'..'
-                   'J':'.---'
-                   'K':'-.-'
-                   'L':'.-..'
-                   'M':'--'
-                   'N':'-.'
-                   'O':'---'
-                   'P':'.---'
-                   'Q':'--.-'
-                   'R':'.-.'
-                   'S':'...'
-                   'T':'-'
-                   'U':'..-'
-                   'V':'...-'
-                   'W':'.--'
-                   'X':'-..-'
-                   'Y':'-.--'
-                   'Z':'--..'
-                   '1':'.----'
-                   '2':'..---'
-                   '3':'...--'
-                   '4':'....-'
-                   '5':'.....'
-                   '6':'-....'
-                   '7':'--...'
-                   '8':'---..'
-                   '9':'----.'
-                   '0':'-----'
-                   ',':'--..--'
-                   '.':'.-.-.-'
-                   '?':'..--..'
-                   '/':'-..-.'
-                   '-':'-....-'
-                   '(':'-.--.'
-                   ')':'-.--.-'}
+MORSE_DICT = {'A':'.-',
+              'B':'-...',
+              'C':'-.-.',
+              'D':'-..',
+              'E':'.',
+              'F':'..-.',
+              'G':'--.',
+              'H':'-.-',
+              'I':'..',
+              'J':'.---',
+              'K':'-.-',
+              'L':'.-..',
+              'M':'--',
+              'N':'-.',
+              'O':'---',
+              'P':'.---',
+              'Q':'--.-',
+              'R':'.-.',
+              'S':'...',
+              'T':'-',
+              'U':'..-',
+              'V':'...-',
+              'W':'.--',
+              'X':'-..-',
+              'Y':'-.--',
+              'Z':'--..',
+              '1':'.----',
+              '2':'..---',
+              '3':'...--',
+              '4':'....-',
+              '5':'.....',
+              '6':'-....',
+              '7':'--...',
+              '8':'---..',
+              '9':'----.',
+              '0':'-----',
+              ',':'--..--',
+              '.':'.-.-.-',
+              '?':'..--..',
+              '/':'-..-.',
+              '-':'-....-',
+              '(':'-.--.',
+              ')':'-.--.-'}
 
 # Function to encrypt the string
 # according to the morse code dictionary
 def encrypt(message):
-    cipher = ''
+    cipher = ' '
     for letter in message:
-        if letter != '':
-            cipher += MORSE_CODE_DICT[letter] + ''
+        if letter != ' ':
+            
+            #Looking for corresponding morsecode in dictionary and adding space
+            cipher += MORSE_DICT[letter] + ' '
         else:
-            cipher += ''
+            # one space indicates different characters and two indicatates different words
+            cipher += ' '
             
     return cipher
 
+#Funcion to decrypt the string from Morsecode to latin
 def decrypt(message):
-    message +=''
     
-    decipher = ''
-    citext = ''
+    #Adding extra space at the end to acces the last morsecode
+    message +=' '
+    
+    decipher = ' '
+    citext = ' '
     for letter in message:
-        if (letter != ''):
+        
+        #Checks for space
+        if (letter != ' '):
             
+            #Counter to keep track of space
             i = 0
             
+            #Storing morse code of a single character
             citext += letter
-            
+           
+        #In case of space    
         else:
             
+            #If i=1 that indicates a new character
             i += 1
             
+            #If i=2 that indicates a new word
             if i == 2 :
                 
-                decipher +=
+                #Adding space to separate words
+                decipher +=' '
             else:
                 
-                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICTvalues()).index(citext)]
-                citext = ''
+                #accesing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_DICT.keys())[list(MORSE_DICT.values()).index(citext)]
+                citext = ' '
     return decipher
         
             
