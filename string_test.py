3525dfcf08aec1364347977d77c41249c1f40b8f
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-13 22:44:19 +0100

    Update string_test.py

--- string_test.py
+++ string_test.py
@@ -5,18 +5,20 @@ import unittest
 
 class TestMorseovka(unittest.TestCase):
     """Tato trida funguje jako testovaci trida."""
+    
     def test_1(self):
+        """Testovani zvetsovani pismen."""
         self.asserEqual("abc".upper(), "ABC")
         self.assertTrue("ABC".isupper())
         self.assertFalse("Abc".isupper())
 
     def test_2(self):
-        """Prevod na rozlozeni textu"""
-        s= "miroslav olivik"
-        self.assertEqual(s.split(), ["miroslav","olivik"])
-        with seld.assertRaises(TypeError):
+        """Prevod na rozlozeni textu."""
+        s = "miroslav olivik"
+        self.assertEqual(s.split(), ["miroslav", "olivik"])
+        with self.assertRaises(TypeError):
             s.split(2)
 
 
-if __name__=="main":
+if __name__ == "main":
     unittest.main()
