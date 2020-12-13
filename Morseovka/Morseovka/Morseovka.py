7a6ef9e078b9d084093bd6298ffd999e14ead1e2
Apofemo <alexei.gaas@yandex.ru>
2020-12-13 22:09:26 +0100

    Druha_Pulka_Kodu

--- Morseovka/Morseovka/Morseovka.py
+++ Morseovka/Morseovka/Morseovka.py
@@ -24,3 +24,34 @@ def TextToMorse(text):
           return('prazdne pole')
     else:
          return(stroka)
+
+def MorseToText(text):
+    sp = {'.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+           '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+           '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q',
+           '.-.': 'r', '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+           '-.--': 'y', '--..': 'z',
+           '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+           '.....': '5',
+           '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0', '/': ' '}
+    mozne = set('.- ') # vsechny mozne symboly, ktere se mohou zapsat do promenne
+    stroka = ''
+    for j in text: # kontrola zadaneho textu
+       if set(j).difference(mozne): # kontroluje, jestli symbol ze zadaneho textu se lisi od moznych symbolu
+                print('nespravne znaky')
+                stroka = 'nespravne znaky'
+                continue
+       else:
+           if set(text).difference(sp): # kontroluje, jestli zadany text je v sp
+                print('chyba pri zadavani znaku')
+                stroka = 'chyba pri zadavani znaku' 
+                continue
+           else:
+                print(sp[j], end=" ") 
+                stroka = stroka + sp[j] # zapisuje prelozeny text do promenne
+    print()
+    if stroka == '': # kontroluje, jestli do stroki je zapsana jenom mezera
+        print('prazdne pole')
+        return('prazdne pole')
+    else:
+        return(stroka)
