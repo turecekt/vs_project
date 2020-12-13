8e56819ec4470b2fe4391e4068f1573365cead29
MartinDanielcik <75914086+MartinDanielcik@users.noreply.github.com>
2020-12-13 17:47:19 +0100

    Update Minimax.py

--- Minimax.py
+++ Minimax.py
@@ -13,6 +13,7 @@ pole = []
 test = True 
 # ověřím zda nebyl zadán nějaký argument, pokud ne vygeneruji pole, pokud ano jdu dál
 if len(sys.argv) < 2:
+    # 20x vygeneruji číslo mezi 1 až 30 a rozčířím o něj pole potom napíšu co jsem udělal
     for i in range(0, 20): 
         pole.append(randint(1, 30))
     print ("Pole s 20 pseudonáhodně vygenerovanými prvky mezi čísly 1 a 30:")
@@ -31,7 +32,7 @@ elif sys.argv[1].endswith(".txt") == True:
             pole.append(tmp)
     # pokud byly všechny řádky v pořádku vypíšu tuto větu
     if test == True:
-        print ("Pole s hodnotami obsažené ve vašem soubotu", sys.argv[1],":")
+        print ("Pole s hodnotami obsažené ve vašem souboru", sys.argv[1],":")
     file.close()
 # Pokud zde byly argumenty a první nebyl název textového souboru tak museli být zadané čísla
 else:
@@ -46,9 +47,60 @@ else:
     # pokud byly všechny argumenty v pořádku vypíšu tuto větu
     if test == True:
         print ("Pole s vámi zadanými hodnotami:")
-# pokud byly všechny požadavky splněny vypíšu pole, pokud ne napíšu chybovou hlášku
+# pokud byly všechny požadavky splněny vypíšu pole a pokračuju dál, pokud ne napíšu chybovou hlášku
 if test == True:
     print(pole)
+    # najdu minimální a maximální prvek
+    mini = min(pole)
+    maxi = max(pole)
+    # vypíšu hodnotu minimálního prvku a projdu celé pole vždy když se číslo v poli shoduje vypíšu jeho pozici
+    print("Nejmenší hodnota v poli je:", mini, "a nachází se na pozici/pozicích:")
+    for i in range(0, len(pole)):
+        if pole[i] == mini:
+            print(i)
+    # vypíšu hodnotu největšího prvku a projdu celé pole vždy když se číslo v poli shoduje vypíšu jeho pozici
+    print("Nevětší hodnota v poli je:", maxi, "a nachází se na pozici/pozicích:")
+    for i in range(0, len(pole)):
+        if pole[i] == maxi:
+            print(i)
+    # budu se ptát na to jaký třídící algoritmus chce uživatel použí dokud nezadá celé číslo mezi 1 a 3
+    al = 24
+    while al > 3 or al < 1: 
+        al = input("Chcete pole seřadit pomocí algoritmu 1)Bubble Sort 2)Insertion Sort 3)Selection Sort (napiš 1 - 3):")
+        test = True
+        try:
+            int(al)
+        except ValueError:
+            al = 24
+            test = False
+        if test == True:
+            al = int(al)
+    # podle toho které číslo mezi 1 a 3 uživatel napsal provedu třídící algoritmus
+    # Bubble sort
+    if al == 1:
+        for i in range(len(pole)):
+            for j in range(len(pole) - 1):
+                if pole[j] > pole[j+1]:
+                    pole[j], pole[j+1] = pole[j+1], pole[j]
+    #Insertion sort
+    elif al == 2:
+        for i in range(1, len(pole)):
+            hodnota = pole[i]
+            pozice = i
+            while pozice > 0 and pole[pozice - 1] > hodnota:
+                pole[pozice] = pole[pozice -1]
+                pozice = pozice - 1
+            pole[pozice] = hodnota
+    #Seletion sort
+    else:
+        for i in range(len(pole)):
+
+            min_i = i
+            for j in range( i + 1, len(pole)):
+                if pole[min_i] > pole[j]:
+                    min_i = j
+            pole[i], pole[min_i] = pole[min_i], pole[i]
+    print("Seřazené pole:", pole)
 else:
     print("parametry nebo soubor neodpovídají požadavku")
 input("Zmáčkni Enter pro ukončení...")
