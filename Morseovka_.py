dc04de012d9e1e45f30e5fad5dc7f474745e1dd9
Niicina <73735248+Niicina@users.noreply.github.com>
2020-12-14 00:45:04 +0100

    Update Morseovka_.py

--- Morseovka_.py
+++ Morseovka_.py
@@ -1,6 +1,4 @@
-Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> message=input('Vloz text na preklad:')			
+	
 #uzivatel nejprve vlozi text, ktery chce prelozit do Morseovy abecedy
 
 message = message.upper()
