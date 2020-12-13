ca8f3cb4356f06b474d51791474cde8a9f92eb6f
David Bajer <baj3rdav@gmail.com>
2020-12-13 23:23:40 +0100

    presun vypoctu obsahu do funkce

--- trojuhelnik.py
+++ trojuhelnik.py
@@ -28,6 +28,28 @@ def getObvod(a, b, c):
     o = a + b + c
     return o
 
+def getObsah(a, b, c):
+    """Calculate the area of a triangle.
+
+    Return the area of the triangle rounded to 2 decimal places
+    (if possible to calculate)
+    >>> getObsah(6, 4, 5) == 9.92
+    True
+    >>> getObsah(1, 1, 23) >= 0
+    False
+    >>> getObsah(5, 5, 5)
+    10.83
+    >>> getObsah(2, 4, 8)
+    -1
+    """
+    s = (a + b + c) / 2
+    m = s * (s - a) * (s - b) * (s - c)
+    if m >= 0:
+        obs = round(math.sqrt(m), 2)
+    else:
+        obs = -1
+    return obs
+
 print("Zadejte souřadnice pro bod A na ose X: ")
 ax = float(input())
 print("Zadejte souřadnice pro bod A na ose Y: ")
@@ -52,13 +74,11 @@ else:
     print("Trojúhelník není sestrojitelný")
 obvod = getObvod(stra, strb, strc)
 print("Obvod trojúhelníku je " + str(obvod) + " cm")
-s = (stra + strb + strc) / 2
-m = s * (s - stra) * (s - strb) * (s - strc)
-if m >= 0:
-    obsah = math.sqrt(m)
+obsah = getObsah(stra, strb, strc)
+if obsah >= 0:
     print("Obsah trojúhelníku je " + str(obsah) + " cm")
 else:
-    print("Obsah nelze vypočítat (záporné číslo pod odmocninou")
+    print("Obsah nelze vypočítat (záporné číslo pod odmocninou)")
 if math.pow(2, stra) == math.pow(2, strb) + math.pow(2, strc) or math.pow(2, strb) == math.pow(2, strc) + math.pow(2, stra) or math.pow(2, strc) == math.pow(2, stra) + math.pow(2, strb):
     print("Trojúhelník je pravoúhlý")
 else:
