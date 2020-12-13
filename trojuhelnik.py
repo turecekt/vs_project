2b0b372f4dea7aeb6453a050ad92c44e4287a85d
David Bajer <baj3rdav@gmail.com>
2020-12-13 23:16:08 +0100

    uprava formatovani

--- trojuhelnik.py
+++ trojuhelnik.py
@@ -16,28 +16,28 @@ stra2 = by-cy
 stra1 = math.pow(2, stra1)
 stra2 = math.pow(2, stra2)
 stra = math.sqrt(stra1+stra2)
-print("Délka strany a: " + str(stra) + "cm")
+print("Délka strany a: " + str(stra) + " cm")
 strb1 = cx-ax
 strb2 = cy-ay
 strb1 = math.pow(2, strb1)
 strb2 = math.pow(2, strb2)
 strb = math.sqrt(strb1+strb2)
-print("Délka strany b: " + str(strb) + "cm")
+print("Délka strany b: " + str(strb) + " cm")
 strc1 = ax-bx
 strc2 = ay-by
 strc1 = math.pow(2, strc1)
 strc2 = math.pow(2, strc2)
 strc = math.sqrt(strc1+strc2)
-print("Délka strany c: " + str(strc) + "cm")
+print("Délka strany c: " + str(strc) + " cm")
 if stra + strb > strc and strb + strc > stra and strc + stra > strb:
     print("Trojúhelník je sestrojitelný")
 else:
     print("Trojúhelník není sestrojitelný")
 obvod = stra + strb + strc
-print("Obvod trojúhelníku je " + str(obvod) + "cm")
+print("Obvod trojúhelníku je " + str(obvod) + " cm")
 s = (stra + strb + strc) / 2
 obsah = math.sqrt(s * (s - stra) * (s - strb) * (s - strc))
-print("Obsah trojúhelníku je" + str(obsah) + "cm")
+print("Obsah trojúhelníku je " + str(obsah) + " cm")
 if math.pow(2, stra) == math.pow(2, strb) + math.pow(2, strc) or math.pow(2, strb) == math.pow(2, strc) + math.pow(2, stra) or math.pow(2, strc) == math.pow(2, stra) + math.pow(2, strb):
     print("Trojúhelník je pravoúhlý")
 else:
