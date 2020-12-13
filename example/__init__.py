62397e75a39961699efc073e1892452f7fbbdc3c
Katerina Chytilova <kate.chytilova@gmail.com>
2020-12-13 15:01:51 +0100

    Fix

---
+++ example/__init__.py
@@ -0,0 +1,19 @@
+"""This is the "example" module.
+
+The example module supplies one function: compute(). For example,
+
+>>> compute(3)
+3
+"""
+
+
+def compute(x):
+    """Functon compute returns evaluation of expression using argument x.
+
+    Args:
+        - x - Input of the function
+
+    Returns:
+        - output - Output of the function
+    """
+    return x * x - 2 * x
