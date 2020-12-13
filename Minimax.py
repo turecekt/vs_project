c0fd5f8861e3c7dd826046a3a6ee21bd0581451f
MartinDanielcik <75914086+MartinDanielcik@users.noreply.github.com>
2020-12-13 14:06:28 +0100

    Aktualizace Minimax.py
    Jedná se o verzi, která se umí ptát na velikost pole a jednotlivé prvky. Pokud není input nebo je menší jak 1 tak program náhodně vygeneruje pole s 20 prvky.

--- Minimax.py
+++ Minimax.py
@@ -1,7 +1,27 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sun Dec 13 13:31:54 2020
 
 @author: Martin Danielčík
 """
+
+# importuji funkci randint z modulu random
+from random import randint
+
+pole = [] 
+  
+# number of elemetns as input 
+n = int(input("Zadej počet hodnot v poli : ")) 
+
+if n > 0:
+# iterating till the range 
+    for i in range(0, n): 
+        s = f"Zadej {i+1}. hodnotu:"
+        tmp = int(input(s)) 
+        pole.append(tmp)
+    print(pole) 
+else:
+    for i in range(0, 20): 
+        pole.append(randint(1, 30))
+    print ("Pole s 20 pseudonáhodně vygenerovanými prvky mezi čísly 1 a 30:")
+    print(pole) 
 
