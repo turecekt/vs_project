43a239f9a47cd868debaebb08f5592f3a1fc442b
tesaro <o_tesar@utb.cz>
2020-12-13 16:21:11 +0100

    Pridana fukce pro kodovani Morseovky do latinky

--- morseovka.py
+++ morseovka.py
@@ -50,9 +50,32 @@ def encrypt(message):
         else:
             cipher += ''
             
-    return
+    return cipher
 
-
+def decrypt(message):
+    message +=''
+    
+    decipher = ''
+    citext = ''
+    for letter in message:
+        if (letter != ''):
+            
+            i = 0
+            
+            citext += letter
+            
+        else:
+            
+            i += 1
+            
+            if i == 2 :
+                
+                decipher +=
+            else:
+                
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICTvalues()).index(citext)]
+                citext = ''
+    return decipher
         
             
             
