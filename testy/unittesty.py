9fafacc77a2b96d67fd25a27c80b1d01cd2029f0
soucekma <m1_soucek@utb.cz>
2020-12-13 15:56:32 +0100

    Oprava docstringů...

--- testy/unittesty.py
+++ testy/unittesty.py
@@ -1,4 +1,4 @@
-"""Testování source"""
+"""Testování source."""
 import unittest
 import source_test
 
@@ -8,43 +8,43 @@ class TestMorseovky(unittest.TestCase):
 
     # PŘEVODY Z ABECEDY DO MORSEOVKY
     def test_1(self):
-        # jen písmena všechna mala
+    """jen písmena všechna mala."""
         result = source_test.prevod("sos")
         self.assertEqual(result, "... --- ... ")
 
-    # jen písmena všechna velká
     def test_2(self):
+        """jen písmena všechna velká."""
         result = source_test.prevod("AHOJ")
         self.assertEqual(result, ".- .... --- .--- ")
 
-    # kombinace velkých a malých písmen
     def test_3(self):
+        """kombinace velkých a malých písmen."""
         result = source_test.prevod("AhOj")
         self.assertEqual(result, ".- .... --- .--- ")
 
-    # jen čísla
     def test_4(self):
+        """jen čísla."""
         result = source_test.prevod("365")
         self.assertEqual(result, "...-- -.... ..... ")
 
-    # čísla a písmena
     def test_5(self):
+        """čísla a písmena."""
         result = source_test.prevod("Mam 20 let")
         self.assertEqual(result, "-- .- --   ..--- -----   .-.. . - ")
 
     # PŘEVODY Z MORSEOVKY DO ABECEDY
-    # jen písmena
     def test_6(self):
+        """jen písmena."""
         result = source_test.prevod(".- .... --- .---")
         self.assertEqual(result, "AHOJ")
 
-    # jen čísla
     def test_7(self):
+        """jen čísla."""
         result = source_test.prevod("...-- -.... .....")
         self.assertEqual(result, "365")
 
-    # čísla a písmena
     def test_8(self):
+        """čísla a písmena."""
         result = source_test.prevod("-- .- -- ..--- ----- .-.. . -")
         self.assertEqual(result, "MAM20LET")
 
