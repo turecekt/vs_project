50bf20a717d7544955cd2b9c328b001dff4e4127
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2020-12-13 23:35:50 +0100

    Update morseovka_Miroslav_Olivik.py

--- morseovka_Miroslav_Olivik.py
+++ morseovka_Miroslav_Olivik.py
@@ -1,72 +1,63 @@
 """Prekladac morseovky."""
 
-SLOVNIK_MORSEOVKA = {' ': '/', 'A': '.-',
-                     'B': '-...', 'C': '-.-.',
-                     'D': '-..', 'E': '.',
-                     'F': '..-.', 'G': '--.', 'H': '....',
-                     'I': '..', 'J': '.---',
-                     'K': '-.-', 'L': '.-..',
-                     'M': '--', 'N': '-.',
-                     'O': '---', 'P': '.--.',
-                     'Q': '--.-', 'R': '.-.',
-                     'S': '...', 'T': '-',
-                     'U': '..-', 'V': '...-',
-                     'W': '.--', 'X': '-..-',
-                     'Y': '-.--', 'Z': '--..',
-                     '1': '.----', '2': '..---',
-                     '3': '...--', '4': '....-',
-                     '5': '.....', '6': '-....',
-                     '7': '--...', '8': '---..',
-                     '9': '----.', '0': '-----',
-                     ',': '--..--', '.': '.-.-.-',
-                     '?': '..--..', '/': '-..-.', '-': '-....-',
-                     '(': '-.--.', ')': '-.--.-'}
+SLOVNIK_MORSEOVKA = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-'}
 
-"""Dále definujeme první funkci."""
-"""Jako první věc definujeme text, kterému máme doplnit text pro kodovani."""
-"""Dále vytvoříme převodový text na kod."""
-"""Vytvoříme string k danemu code."""
-"""A string necháme přečíst."""
+"""Dale definujeme prvni funkci.
+Jako prvni vec definujeme text, kterenu mame doplnit text pro kodovani.
+Dale vytvorime prevodovy text na kod.
+Vytvorime string k danemu codu.
+A string nechame precist."""
 
 
-def Text_To_Code(): """Funkce - překlad slov do morseovky."""
+def Text_To_Code(): """Fce pro preklad do morseovky"""
+    text2 = input("Napiste slova, ktera chcete kodovat: " <-
+    code2 = [SLOVNIK_MORSEOVKA
+             [i.upper()] + " "
+             for i in text2 if i.upper() SLOVNIK_MORSEOVKA.keys()]
+    morseovka = "".join(code2)
+    print(morseovka)
 
 
-text2 = input("Napiste slova, ktera chcete kodovat: " <-
-code2 = [SLOVNIK_MORSEOVKA
-         [i.upper()] + " "
-         for i in text2 if i.upper() in SLOVNIK_MORSEOVKA.keys()]
-morseovka = "".join(code2)
-print(morseovka)
+"""Stejny zpusob pouzijeme i zde i dekodovani nami zadaneho textu."""
+"""Vytvorime zde to, ze nami zadany kod program rozlozi na jednotky."""
+"""Opet nechame udelat string z code a nechame ho vypsat."""
 
 
-"""Stejný způsob použijeme i zde u dekodovani námi zadaného textu."""
-"""Vytvoříme zde to, že námi zadaný kod program rozloží na jednotky."""
-"""Opet nechame udělat string z code a necháme ho vypsat."""
+def Code_To_Text(): """Fce pro preklad morseovky."""
+    text1 = input("Napiste kod, ktery chcete dekodovat: " <-
+    code1 = [k for i in text1.split() for k,
+             v in SLOVNIK_MORSEOVKA.items() if i == v]
+    novytext = "".join(code1)
+    print(novytext)
 
 
-def Code_To_Text(): """Vytvořené funkce - přeložení morseovk."""
+"""Vypiseme menu pro vyber, co chceme aby program delal."""
+"""Na zaver vypisime cyklus, ktery nam k menu ve stringu priradi i funkce."""
+"""Zvolili jsme fci else, ktera nam pokryje jinou moznost nez-li 1-3."""
+       
+     
+print("""\n1 - TEXT KE KODOVANI\n2 - KOD K ROZKODOVANI\n3 - KONEC\,""")
 
-
-text1 = input("Napiste kod, ktery chcete dekodovat: " <-
-code1 = [k for i in text1.split() for k,
-         v in SLOVNIK_MORSEOVKA.items() if i == v]
-novytext = "".join(code1)
-print(novytext)
-
-
-"""Vypiseme menu pro vyber, co hceme aby program delal."""
-"""Na zaver vypisime cyklus, ktery nám k menu ve stringu priradi i funkce."""
-"""Zvolili jsme fci else, která nám pokryje jinou možnost než-li 1-3."""
-
-
-print("""\n1 - TEXT KE KODOVÁNÍ \n2 - KÓD K ROZKÓDOVÁNÍ\n3 - KONEC\n""")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     while True:
 
-        vyber = int(input("TVŮJ VÝBĚR: "))
+        vyber = int(input"TVUJ VYBER: ") <-
+
         if vyber == 1:
             print(Text_To_Code())
             break
@@ -76,8 +67,8 @@ if __name__ == '__main__':
             break
 
         elif vyber == 3:
-            print("UKONČUJI")
+            print("UKONCUJI")
             exit()
 
         else:
-            print("NESPRÁVNÁ VOLBA, ZVOLTE ZNOVU")
+            print("NESPRAVNA VOLBA, ZVOLTE ZNOVU")
