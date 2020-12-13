b008a846db6376ddf43cc90978100cd1a7d79602
plovecky <p_lovecky@utb.cz>
2020-12-13 18:05:01 +0100

    Upravení unittestu

--- testy/source_test.py
+++ testy/source_test.py
@@ -1,4 +1,5 @@
 ﻿"""Program pro převod z Morseovky do abecedy a naopak."""
+import unittest
 
 
 def prevod(vstup):
@@ -96,5 +97,77 @@ def main():
     print(prevod(vstup))
 
 
+"""Testování source."""
+# Testovácí class
+
+
+class TestMorseovky(unittest.TestCase):
+    """Tato class funguje pro testování našeho source."""
+
+    # PŘEVODY Z ABECEDY DO MORSEOVKY
+    def test_1(self):
+        """Jen písmena všechna malá."""
+        result = prevod("sos")
+        self.assertEqual(result, "... --- ... ")
+
+    def test_2(self):
+        """Jen písmena všechna velká."""
+        result = prevod("AHOJ")
+        self.assertEqual(result, ".- .... --- .--- ")
+
+    def test_3(self):
+        """Kombinace velkých a malých písmen."""
+        result = prevod("AhOj")
+        self.assertEqual(result, ".- .... --- .--- ")
+
+    def test_4(self):
+        """Jen čísla."""
+        result = prevod("365")
+        self.assertEqual(result, "...-- -.... ..... ")
+
+    def test_5(self):
+        """Čísla a písmena."""
+        result = prevod("Mam 20 let")
+        self.assertEqual(result, "-- .- --   ..--- -----   .-.. . - ")
+
+    # PŘEVODY Z MORSEOVKY DO ABECEDY
+    def test_6(self):
+        """Jen písmena."""
+        result = prevod(".- .... --- .---")
+        self.assertEqual(result, "AHOJ")
+
+    def test_7(self):
+        """Jen čísla."""
+        result = prevod("...-- -.... .....")
+        self.assertEqual(result, "365")
+
+    def test_8(self):
+        """Čísla a písmena."""
+        result = prevod("-- .- -- ..--- ----- .-.. . -")
+        self.assertEqual(result, "MAM20LET")
+
+    def test_9(self):
+        """Čísla, písmena."""
+        result = prevod("-.. -. . ... -.- .- .--- . .--. ."
+                                    " -.- -. -.-- -.. . -. .- .--- . ..--- "
+                                    "---.. ... - ..- .--. -. ..-")
+        self.assertEqual(result, "DNESKAJEPEKNYDENAJE28STUPNU")
+
+    def test_10(self):
+        """Nepovolené znaky na začátku."""
+        result = prevod("?ahoj")
+        self.assertEqual(result, 'Zadaná nesprávná kombince znaků')
+
+    def test_11(self):
+        """Nepovolené znaky uprostřed."""
+        result = prevod("ah?oj")
+        self.assertEqual(result, 'Zadaná nesprávná kombince znaků')
+
+    def test_12(self):
+        """Nepovolené znaky na konci."""
+        result = prevod("ahoj?")
+        self.assertEqual(result, 'Zadaná nesprávná kombince znaků')
+
+
 if __name__ == '__main__':
-    main()
+    unittest.main()
