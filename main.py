7a3f65882f6f7d629990ac0bf4f6bcecd7be715d
Vintriscoming <75955675+Vintriscoming@users.noreply.github.com>
2020-12-14 00:28:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,173 +1,113 @@
-"""Program pro převod z Morseovky do abecedy a naopak."""
 import unittest
 
+# morseovka
+abc = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'ch': '----',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    ' ': ' '
+}
 
-def prevod(vstup):
-    """Funkce pro tuto akci."""
-    # Vytvoření dictionary pro morseovku
-    morseovka = {
-        '1': '.----',
-        '2': '..---',
-        '3': '...--',
-        '4': '....-',
-        '5': '.....',
-        '6': '-....',
-        '7': '--...',
-        '8': '---..',
-        '9': '----.',
-        '0': '-----',
-        'A': '.-',
-        'B': '-...',
-        'C': '-.-.',
-        'D': '-..',
-        'E': '.',
-        'F': '..-.',
-        'G': '--.',
-        'H': '....',
-        'I': '..',
-        'J': '.---',
-        'K': '-.-',
-        'L': '.-..',
-        'M': '--',
-        'N': '-.',
-        'O': '---',
-        'P': '.--.',
-        'Q': '--.-',
-        'R': '.-.',
-        'S': '...',
-        'T': '-',
-        'U': '..-',
-        'V': '...-',
-        'W': '.--',
-        'X': '-..-',
-        'Y': '-.--',
-        'Z': '--..',
-        ' ': ' '}
 
-    g = ""
+# kodovaní z abecedy do morseovky
+def domorse(preloz):
+    zprava = ''  # proměna do ktere budeme dosazovat hodnoty
+    # za každe písmeno v preloz se uděla příkaz pod tím.
+    # Do proměne zpravy se zapisuje pismeno
+    for pismeno in preloz:
+        zprava += abc[pismeno] + ' '
 
-    # Převedení z morseovky na text
-    if vstup.startswith('.') or vstup.startswith('-'):
+    return zprava
 
-        # Rozdělení morseovky na jednotlivé kombinace
-        rozdVstup = vstup.split(' ')
 
-        # Vytvoření cyklu pro převod z morseovky na text
-        for prvek in rozdVstup:
-            if prvek not in morseovka.values():
-                # Ošetření vstupu
-                print('Zadaná nesprávná kombince znaků')
-                exit()
-            for i in morseovka:
-                if prvek == morseovka[i]:
-                    g = g + i
+# překlad z morseovky do abecedy
+def zmorse(preloz):
+    # promene
 
-        return g
+    global i
+    preloz += ' '
+    slovo = ''
+    citext = ''
+    for letter in preloz:  # zas loop
 
-    # Převedení z textu do morseovky
-    else:
+        # na rozlišovani nových slov bereme
+        if letter != ' ':
 
-        # Převedení textu na velká písmena
-        vstupV = vstup.upper()
+            # proměna pro přepinaní
+            i = 0
 
-        # Převedení textu na pole znaků
-        rozdVstup = list(vstupV)
+            #
+            citext += letter
 
-        # Vytvoření cyklu for pro převod zadaných znaků do morseovky
+        # jestli tam nastane mezera
+        else:
+            # k i se připočte 1 indikuje novou hodnotu
+            i += 1
 
-        for prvek in rozdVstup:
-            if prvek not in morseovka:
-                # Ošetření vstupu
-                g = 'Zadaná nesprávná kombince znaků'
-                return g
-            for i in morseovka:
-                if prvek == i:
-                    g = g + morseovka[i] + ' '
+            # jestli i=2 nové slovo
+            if i == 2:
 
-        return g
+                #
+                slovo += ' '
+            else:
+
+                # k vracene proměne se přípočíta klíč pomocí value
+                # kde index hledá tečky čárky
+                slovo += list(abc.keys())[list(abc.values()).index(citext)]
+                citext = ''
+
+    return slovo
 
 
 def main():
-    """Hlavní funkce pro spuštění programu."""
-    # Vytvoření vstupu
-    print('--KODÉR A DEKODÉR MORSEOVKY--')
-    vstup = input('Zadejte text pro převod: ')
+    moznost = input("Zakodovat(a) přeložit(b)")
+    if moznost == "a":
+        preloz = input("")
+        result = domorse(preloz.lower())
+        print(result)
+        input("")
 
-    # Výstup
-    print(prevod(vstup))
-
-
-"""Testování source."""
-# Testovácí class
-
-
-class TestMorseovky(unittest.TestCase):
-    """Tato class funguje pro testování našeho source."""
-
-    # PŘEVODY Z ABECEDY DO MORSEOVKY
-    def test_1(self):
-        """Jen písmena všechna malá."""
-        result = prevod("sos")
-        self.assertEqual(result, "... --- ... ")
-
-    def test_2(self):
-        """Jen písmena všechna velká."""
-        result = prevod("AHOJ")
-        self.assertEqual(result, ".- .... --- .--- ")
-
-    def test_3(self):
-        """Kombinace velkých a malých písmen."""
-        result = prevod("AhOj")
-        self.assertEqual(result, ".- .... --- .--- ")
-
-    def test_4(self):
-        """Jen čísla."""
-        result = prevod("365")
-        self.assertEqual(result, "...-- -.... ..... ")
-
-    def test_5(self):
-        """Čísla a písmena."""
-        result = prevod("Mam 20 let")
-        self.assertEqual(result, "-- .- --   ..--- -----   .-.. . - ")
-
-    # PŘEVODY Z MORSEOVKY DO ABECEDY
-    def test_6(self):
-        """Jen písmena."""
-        result = prevod(".- .... --- .---")
-        self.assertEqual(result, "AHOJ")
-
-    def test_7(self):
-        """Jen čísla."""
-        result = prevod("...-- -.... .....")
-        self.assertEqual(result, "365")
-
-    def test_8(self):
-        """Čísla a písmena."""
-        result = prevod("-- .- -- ..--- ----- .-.. . -")
-        self.assertEqual(result, "MAM20LET")
-
-    def test_9(self):
-        """Čísla, písmena."""
-        result = prevod("-.. -. . ... -.- .- .--- . .--. ."
-                        " -.- -. -.-- -.. . -. .- .--- . ..--- "
-                        "---.. ... - ..- .--. -. ..-")
-        self.assertEqual(result, "DNESKAJEPEKNYDENAJE28STUPNU")
-
-    def test_10(self):
-        """Nepovolené znaky na začátku."""
-        result = prevod("?ahoj")
-        self.assertEqual(result, 'Zadaná nesprávná kombince znaků')
-
-    def test_11(self):
-        """Nepovolené znaky uprostřed."""
-        result = prevod("ah?oj")
-        self.assertEqual(result, 'Zadaná nesprávná kombince znaků')
-
-    def test_12(self):
-        """Nepovolené znaky na konci."""
-        result = prevod("ahoj?")
-        self.assertEqual(result, 'Zadaná nesprávná kombince znaků')
+    if moznost == "b":
+        print("přiklad:.-. .-.. -.-- ")
+        preloz = input(" ")
+        result = zmorse(preloz)
+        print(result.capitalize())
+        input("")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
