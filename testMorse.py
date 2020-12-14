6be30d6be044fd2ccf168d90a3a679434d6a09ee
Lenka Felberova <l_felberova@utb.cz>
2020-12-14 20:31:45 +0000

    oprava testu...snad

--- testMorse.py
+++ testMorse.py
@@ -1,33 +1,48 @@
 import Morseovka
 
 def testMorse():
+
+    
 """
 Test 1
 Input: "Ahoj"
 Output: ".-....---.---"
 """
+
 assert MORSE_CODE_DICT("Ahoj") == ".-....---.---"
 
+
 def testMorse():
+
+    
 """
 Test 2
 Input: "SOS"
 Output: "... --- ..."
 """
+
 assert MORSE_CODE_DICT("SOS") == "... --- ..."
 
+
 def testMorse():
+
+    
 """
 Test 3
 Input: "jak se mas?"
 Output: ".--- .- -.-  ... .  -- .- ... ..--.. "
 """
-assert MORSE_CODE_DICT("jak se mas?") == ".--- .- -.-  ... .  -- .- ... ..--.. "
+
+assert MORSE_CODE_DICT("mas se?") == "-- .- ...  ... . ..--.."
+
 
 def testMorse():
+
+    
 """
 Test 4
 Input: "-.("
 Output: "-....- .-.-.- -.--."
 """
+
 assert MORSE_CODE_DICT("-.(") == "-....- .-.-.- -.--."
