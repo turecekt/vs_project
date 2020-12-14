88e6bcb19ec45281f626328b945421958525e3b4
KamilChytil <kamca114@seznam.cz>
2020-12-14 20:54:40 +0100

    uprava

--- prace.py
+++ prace.py
@@ -33,35 +33,35 @@ morseABECEDA = {
 }
 
 """Abeceda potřebná na překlad na text"""
-ABECEDAmorse = {
+morseovka_preklad = {
     '/': ' ',
-    '.–': 'A',
-    '–...': 'B',
-    '–.–.': 'C',
-    '–..': 'D',
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
     '.': 'E',
-    '..–.': 'F',
-    '––.': 'G',
+    '..-.': 'F',
+    '--.': 'G',
     '....': 'H',
-    '––––': 'CH',
+    '----': 'CH',
     '..': 'I',
-    '.–––': 'J',
-    '–.–': 'K',
-    '.–..': 'L',
-    '––': 'M',
-    '–.': 'N',
-    '–––': 'O',
-    '.––.': 'P',
-    '––.–': 'Q',
-    '.–.': 'R',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
     '...': 'S',
-    '−': 'T',
-    '..–': 'U',
-    '...–': 'V',
-    '.––': 'W',
-    '–..–': 'X',
-    '–.––': 'Y',
-    '––..': 'Z'
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
 }
 
 
@@ -77,37 +77,44 @@ def Txt_do_Morse(slovo):
 def Morse_do_Txt(morse):
     """Přeložení morse na text."""
     novy = morse.split('/')
-    code = [ABECEDAmorse[i] + '' for i in novy if i in ABECEDAmorse.keys()]
-    txt = ''.join(code)
-    print(txt)
-    return txt
+    text = ""
+    print(novy)
+    for i in novy:
+        if i in morseovka_preklad.keys():
+            text = text + morseovka_preklad[i]
+        if i == "":
+            text = text + " "
+    return text
 
 
 print('''\n1- Prelozit text na Morseovku \n2- Prelozit Morseovku na text ''')
 
 """Vybrání možnosti překladu text do morse nebo morse na text."""
-while True:
-    try:
-        selection = int(input('Vyberte si co na co chcete prekladat: '))
-        if selection == 1:
-            slovo = input('Napiš slovo ')
-            Txt_do_Morse(slovo)
-            break
-        elif selection == 2:
-            slovo = input('Napiš morseovku ')
-            Morse_do_Txt(slovo)
-            break
-        else:
+if __name__ == "__main__":
+    while True:
+        try:
+            selection = int(input('Vyberte si co na co chcete prekladat: '))
+            if selection == 1:
+                slovo = input('Napiš slovo ')
+                print(Txt_do_Morse(slovo))
+                break
+            elif selection == 2:
+                slovo = input('Napiš morseovku ')
+                print(Morse_do_Txt(slovo))
+                break
+            else:
+                print('Spatna volba')
+        except Exception:
             print('Spatna volba')
-    except Exception:
-        print('Spatna volba')
 
 
 def test_Txt_do_Morse():
     """Předklad text na morse."""
-    assert Txt_do_Morse("AHOJ") == ".–/..../–––/.–––/"
+    text = "AHOJ"
+    assert Txt_do_Morse(text) == ".–/..../–––/.–––/"
 
 
 def test_Morse_do_Txt():
     """Překlad morse na text."""
-    assert Morse_do_Txt("−/–––/––/.//.–––/.–/–.–//.–––/./") == "TOMEJAKJE"
+    code = ".---/.-/-.-//.---/./"
+    assert Morse_do_Txt(code) == "JAK JE "
