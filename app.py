084148af57be35081a509a9fa97106745c47d798
Michael Semerád <73735319+michaelsemerad@users.noreply.github.com>
2020-12-14 22:05:20 +0100

    Define method evaluate.

--- app.py
+++ app.py
@@ -46,21 +46,33 @@ if __name__ == "__main__":
             n1 = float(input("Insert your first number -> "))
             n2 = float(input("Insert your second number -> "))
 
-            # Use the add function and print the result.
-            if choice == 1:
-                print(n1, "+", n2, "=", calculator.add(n1, n2))
 
-            # Use the subtract function and print the result.
-            elif choice == 2:
-                print(n1, "-", n2, "=", calculator.subtract(n1, n2))
+            def evaluate(choice):
+                """Evaluate the choice and make the calculation.
 
-            # Use the multiply function and print the result.
-            elif choice == 3:
-                print(n1, "*", n2, "=", calculator.multiply(n1, n2))
+                Input of the function are choice, n1, n2.
 
-            # Use the divide function and print the result.
-            elif choice == 4:
-                print(n1, "/", n2, "=", calculator.divide(n1, n2))
-            break
+                Output of the function is a calculation of two numbers.
+                """
+                # Use the add function and print the result.
+                if choice == 1:
+                    print(n1, "+", n2, "=", calculator.add(n1, n2))
+
+                # Use the subtract function and print the result.
+                elif choice == 2:
+                    print(n1, "-", n2, "=", calculator.subtract(n1, n2))
+
+                # Use the multiply function and print the result.
+                elif choice == 3:
+                    print(n1, "*", n2, "=", calculator.multiply(n1, n2))
+
+                # Use the divide function and print the result.
+                elif choice == 4:
+                    print(n1, "/", n2, "=", calculator.divide(n1, n2))
+
+
+            evaluate(choice)
+
         else:
             print("Invalid Input!")
+        break
