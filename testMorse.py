860a6b1ee1b74515774bb6484bcb176ed2167889
Lenka Felberova <l_felberova@utb.cz>
2020-12-14 20:56:58 +0000

    oprava testu.prosiiiim

--- testMorse.py
+++ testMorse.py
@@ -1,44 +1,38 @@
 import Morseovka
 
+
 def testMorse01():
+    """
+    Test 1
+    Input:"Ahoj"
+    Output:".-....---.---"
+    """
 
-"""
-Test 1
-Input:"Ahoj"
-Output:".-....---.---"
-"""
-
-assert MORSE_CODE_DICT("Ahoj") == ".-....---.---"
+    assert MORSE_CODE_DICT("Ahoj") == ".-....---.---"
 
 
 def testMorse02():
-
-"""
-Test 2
-Input:"SOS"
-Output:"... --- ..."
-"""
-
-assert MORSE_CODE_DICT("SOS") == "... --- ..."
+    """
+    Test 2
+    Input:"SOS"
+    Output:"... --- ..."
+    """
+    assert MORSE_CODE_DICT("SOS") == "... --- ..."
 
 
 def testMorse03():
-
-"""
-Test 3
-Input:"jak se mas?"
-Output:".--- .- -.-  ... .  -- .- ... ..--.. "
-"""
-
-assert MORSE_CODE_DICT("mas se?") == "-- .- ...  ... . ..--.."
+    """
+    Test 3
+    Input:"jak se mas?"
+    Output:".--- .- -.-  ... .  -- .- ... ..--.. "
+    """
+    assert MORSE_CODE_DICT("mas se?") == "-- .- ...  ... . ..--.."
 
 
 def testMorse04():
-
-"""
-Test 4
-Input:"-.("
-Output:"-....- .-.-.- -.--."
-"""
-
-assert MORSE_CODE_DICT("-.(") == "-....- .-.-.- -.--."
+    """
+    Test 4
+    Input:"-.("
+    Output:"-....- .-.-.- -.--."
+    """
+    assert MORSE_CODE_DICT("-.(") == "-....- .-.-.- -.--."
