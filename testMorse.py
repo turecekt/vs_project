37c2c665443effaedc8193b9cafa62e2fa9c2275
Lenka Felberova <l_felberova@utb.cz>
2020-12-14 20:35:24 +0000

    oprava testu...snad

--- testMorse.py
+++ testMorse.py
@@ -2,7 +2,7 @@ import Morseovka
 
 def testMorse():
 
-    
+
 """
 Test 1
 Input: "Ahoj"
@@ -14,7 +14,7 @@ assert MORSE_CODE_DICT("Ahoj") == ".-....---.---"
 
 def testMorse():
 
-    
+
 """
 Test 2
 Input: "SOS"
@@ -26,7 +26,6 @@ assert MORSE_CODE_DICT("SOS") == "... --- ..."
 
 def testMorse():
 
-    
 """
 Test 3
 Input: "jak se mas?"
@@ -38,7 +37,7 @@ assert MORSE_CODE_DICT("mas se?") == "-- .- ...  ... . ..--.."
 
 def testMorse():
 
-    
+
 """
 Test 4
 Input: "-.("
