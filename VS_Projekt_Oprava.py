2c8fd3400b2d5a3faffc325da84de4fa224a9115
Marek <michal.vojdan@gmail.com>
2020-12-15 21:13:29 +0100

    pep8 modified

--- VS_Projekt_Oprava.py
+++ VS_Projekt_Oprava.py
@@ -1,54 +1,62 @@
 def vypocti_delitelov(cislo):
-    pocet_delitelov=0
-    if cislo>10:
-        pocet_delitelov=1
-    for i in range(1,10):
-        if cislo%i==0:
-              pocet_delitelov=pocet_delitelov+1
+    pocet_delitelov = 0
+    if cislo > 10:
+        pocet_delitelov = 1
+    for i in range(1, 10):
+        if cislo % i == 0:
+            pocet_delitelov = pocet_delitelov + 1
     return pocet_delitelov
 
+
 def je_prvocislo(cislo):
-    pocet_delitelov =  vypocti_delitelov(cislo)
-    if cislo<0:
-       return None
-    elif pocet_delitelov==2:
-       return True
-    elif pocet_delitelov!=2:
-       return False
-            
-       
+    pocet_delitelov = vypocti_delitelov(cislo)
+    if cislo < 0:
+        return None
+    elif pocet_delitelov == 2:
+        return True
+    elif pocet_delitelov != 2:
+        return False
+
 
 def popis(cislo):
     text = ''
     text2 = ''
     prvocislo = je_prvocislo(cislo)
-    if prvocislo == None:
-        text='Zadané číslo nie je prirodzené číslo.'
-    elif prvocislo == False:
-        text=('Číslo ' + str(cislo) + ' nie je prvočíslo')
-    elif prvocislo == True:
+    if prvocislo is None:
+        text = 'Zadané číslo nie je prirodzené číslo.'
+    elif prvocislo is False:
+        text = ('Číslo ' + str(cislo) + ' nie je prvočíslo')
+    elif prvocislo is True:
         text = ('Číslo ' + str(cislo) + ' je prvočíslo')
 
     if cislo < 0:
         text2 = ''
     elif cislo >= 10:
-        text2='Štatistická metóda'
+        text2 = 'Štatistická metóda'
     elif cislo < 10:
-        text2 = 'Metóda priameho delenia v cykle.'
+        text2 = 'Priame delenie v cykle.'
     return [text, text2]
+
+
 def test_vypocti_delitelov():
     assert vypocti_delitelov(19) == 2
     assert vypocti_delitelov(20) != 2
     # ...
+
+
 def test_je_prvocislo():
     assert je_prvocislo(-1) is None
-    assert je_prvocislo(3) == True
-    assert je_prvocislo(4) == False
+    assert je_prvocislo(3) is True
+    assert je_prvocislo(4) is False
     # ...
+
+
 def test_popis():
-    assert popis(-1) == ['Zadané číslo nie je prirodzené číslo.','']
-    assert popis(3) == ['Číslo 3 je prvočíslo','Metóda priameho delenia v cykle.']
-    assert popis(12) == ['Číslo 12 nie je prvočíslo','Štatistická metóda']
+    assert popis(-1) == ['Zadané číslo nie je prirodzené číslo.', '']
+    assert popis(3) == ['Číslo 3 je prvočíslo', 'Priame delenie v cykle.']
+    assert popis(12) == ['Číslo 12 nie je prvočíslo', 'Štatistická metóda']
     # ...
+
+
 if __name__ == '__main__':
     print(popis(int(input('Zadajte ľubovoľné prirodzené číslo: '))))
