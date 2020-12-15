cbf1ae1537cc7d9c55d5261d6ab320a08564c683
Dpapaj <74046937+Dpapaj@users.noreply.github.com>
2020-12-15 15:01:05 +0100

    Update for translator - input

--- Morse_code_translator/translator.py
+++ Morse_code_translator/translator.py
@@ -1,17 +1,15 @@
 """Input text or morse code .Output translated text or morse."""
 from morse import MorseCodeTranslator
 
-translator = MorseCodeTranslator()
-# Insert text for translation to morse
-text = "&@)(=!-+?"
-# Insert morse for translation to text
-morse = "-.. --- -... .-. -.--   -.. . -."
-
-# Translate text to morse code
-translated_morse = translator.translate_text(text)
-
-# Translate morse code to text
-translated_text = translator.translate_morse(morse)
-
-print(translated_morse)
-print(translated_text)
+if __name__ == "__main__":
+    translator = MorseCodeTranslator()
+    # Insert text for translation to morse
+    text = input("Zadejte text: ")
+    # Insert morse for translation to text
+    morse = input("Zadejte morse code: ")
+    # Translate text to morse code
+    translated_morse = translator.translate_text(text)
+    # Translate morse code to text
+    translated_text = translator.translate_morse(morse)
+    print(translated_morse)
+    print(translated_text)
