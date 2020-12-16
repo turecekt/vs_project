674193f85df4d014998782ddc06c5c5befae53a8
tesaro <o_tesar@utb.cz>
2020-12-16 23:16:11 +0100

    Uprava unit testu

--- Test_Projekt_Morseovka.py
+++ Test_Projekt_Morseovka.py
@@ -5,15 +5,31 @@ import unittest
 from Projekt_Morseovka import encrypt
 from Projekt_Morseovka import decrypt
 from Projekt_Morseovka import choice
-from Projekt_Morseovka import main
 
 class Morseovka_Test(unittest.TestCase):
-
-    "Unit Tests"
         
-    #Unit test - Latin to Morse function
-    def test_encrypt_1(self):
-        #encrypting word Morseovka
-        source = source.encrypt("Morseovka")
-        self.assertEqual(vstup, "- . ... - ")
 
+    "Unit test - Latin to Morse function"
+def test_encrypt_1(self):       
+    "encrypting a word Morseovka"
+    assert encrypt("Ondra") == ".../-./-../.-./.- "
+       
+    "Unit test - Latin to Morse function"
+def test_encrypt_2(self):       
+    "encrypting number Morseovka"
+    assert encrypt("1234") == ".----/..---/...--/....-"
+    
+    "Unit test - Morse to Latin function"
+def test_decrypt_1(self):    
+    "encrypting Morse to Latin Characters"
+    assert decrypt(".../-./-../.-./.-") == "Ondra"
+    
+    "Unit test - Morse to Latin Numbers function"
+def test_decrypt_2(self):    
+    "encrypting Morse to Latin Numbers"
+    assert decrypt(".---- ..--- ...-- ....-") == "1234"
+    
+    "Unit test - Choose translation function"
+def test_choice_1(self):    
+    assert choice("1") == "Your text:"
+    
