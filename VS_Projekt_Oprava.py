f64246a4972cc68cabf8fecfd0d7d42334f9606a
Marek <m_mitrik@utb.com>
2020-12-16 15:10:33 +0100

    Modified with flake8

--- VS_Projekt_Oprava.py
+++ VS_Projekt_Oprava.py
@@ -1,4 +1,29 @@
-def vypocti_delitelov(cislo): 
+""".
+
+SEMESTRALNY PROJEKT DO AP1VS
+
+VS_Projekt.py
+
+version:    1.1
+
+Author: Marek Mitrik      (https://github.com/Grontax)
+        Jozef Varhanik    (https://github.com/jvarhanik)
+        Michal Vojdan     (https://github.com/Migelvojto)
+
+---------------------------------------------------
+
+This algorithm is used to define if a
+value entered by the user is a prime number
+
+---------------------------------------------------
+
+This project was made for educational purpose only.
+
+"""
+
+
+def vypocti_delitelov(cislo):
+    """Funkcia na hladanie poctu delitelov daneho cisla."""
     pocet_delitelov = 0
     if cislo > 10:
         pocet_delitelov = 1
@@ -9,6 +34,7 @@ def vypocti_delitelov(cislo):
 
 
 def je_prvocislo(cislo):
+    """Funkcia na vyhodnotenie ci je cislo prvocislo alebo nie."""
     pocet_delitelov = vypocti_delitelov(cislo)
     if cislo < 0:
         return None  # Vratenie hodnoty none
@@ -19,8 +45,9 @@ def je_prvocislo(cislo):
 
 
 def popis(cislo):
-    text = ''
-    text2 = '' 
+    """Funkcia na urcenie popisu podla podmienok."""
+    text = ' '
+    text2 = ' '
     prvocislo = je_prvocislo(cislo)
     if prvocislo is None:
         text = 'Zadané číslo nie je prirodzené číslo.'
@@ -28,35 +55,38 @@ def popis(cislo):
         text = ('Číslo ' + str(cislo) + ' nie je prvočíslo')
     elif prvocislo is True:
         text = ('Číslo ' + str(cislo) + ' je prvočíslo')
-	# Podmienky ktore nam zmenia hodnotu textu
+# Podmienky ktore nam zmenia hodnotu textu
     if cislo < 0:
         text2 = ''
     elif cislo >= 10:
         text2 = 'Štatistická metóda'
     elif cislo < 10:
         text2 = 'Priame delenie v cykle.'
-	# podmienky ktore menia hodnotu textu 2
+# podmienky ktore menia hodnotu textu 2
     return [text, text2]
 
 
 def test_vypocti_delitelov():
+    """Unit test na kontrolu poctu delitelov."""
     assert vypocti_delitelov(19) == 2
     assert vypocti_delitelov(20) != 2
-    # Unit test otestuje vstup 19 a 20
+# Unit test otestuje vstup 19 a 20
 
 
 def test_je_prvocislo():
+    """Unit test na skontrolovanie spravnosti urcenia BOOL hodnoty."""
     assert je_prvocislo(-1) is None
     assert je_prvocislo(3) is True
     assert je_prvocislo(4) is False
-    # Unit test otestuje ci zadane cislo je prvocislo
+# Unit test otestuje ci zadane cislo je prvocislo
 
 
 def test_popis():
+    """Unit test na kontrolu zadanych popisov."""
     assert popis(-1) == ['Zadané číslo nie je prirodzené číslo.', '']
     assert popis(3) == ['Číslo 3 je prvočíslo', 'Priame delenie v cykle.']
     assert popis(12) == ['Číslo 12 nie je prvočíslo', 'Štatistická metóda']
-    # Vkladanie textu 
+# Vkladanie textu
 
 
 if __name__ == '__main__':
