8994e7c1d342836da9868c4cca405825c8b615c5
Ondřej Marýzek <ondrej.maryzek@gmail.com>
2020-12-16 11:28:56 +0100

    Cosmetic changes. Merged most of the "with" statements in test_main.test_init() into one for better readability. Added more information to the InputGenerator class docstring in the test_main module. Added the Visual Studio TestResults directory to .gitignore.

--- test_main.py
+++ test_main.py
@@ -14,7 +14,12 @@ def test_print_triangle_info():
 
 
 class InputGenerator:
-    """A class used to simulate user input in the CLI."""
+    """A class used to simulate user input in the CLI.
+
+    The main.request_point() function has an infinite loop that keeps
+    repeating until the user enters a valid value. This class is used to
+    simulate the user entering 2 invalid values and then a valid value.
+    """
 
     def __init__(self):
         """Initialize the iterator attribute."""
@@ -39,32 +44,30 @@ def test_request_point():
     """Unit test for the request_point function."""
     generator = InputGenerator()
     with mock.patch("builtins.input", wraps=generator.get_input):
-        with mock.patch.object(main.sys, 'argv', ["main"]):
-            point = main.request_point("A")
-            assert point.x == 0
+        point = main.request_point("A")
+        assert point.x == 0
 
 
 def test_init():
     """Unit test for the init function."""
-    with mock.patch.object(main, "__name__", "__main__"):
-        with mock.patch.object(main.sys, 'exit') as mock_exit:
-            with mock.patch.object(main, 'print_triangle_info'):
-                with mock.patch("main.request_point",
-                                return_value=Point(50, 50)):
-                    with mock.patch.object(main.sys, 'argv',
-                                           ["main", 0, 0, 100, 0, 0, 100]):
-                        main.init()
-                        assert mock_exit.call_args[0][0] == 0
+    with mock.patch.object(main, "__name__", "__main__"), \
+            mock.patch.object(main.sys, 'exit') as mock_exit, \
+            mock.patch.object(main, 'print_triangle_info'), \
+            mock.patch("main.request_point", return_value=Point(50, 50)):
+        with mock.patch.object(main.sys, 'argv',
+                               ["main", 0, 0, 100, 0, 0, 100]):
+            main.init()
+            assert mock_exit.call_args[0][0] == 0  # check the exit code
 
-                    with mock.patch.object(main.sys, 'argv',
-                                           ["main", 0, 0, "abc", 0, 0, 100]):
-                        main.init()
-                        assert mock_exit.call_args[0][0] != 0
+        with mock.patch.object(main.sys, 'argv',
+                               ["main", 0, 0, "abc", 0, 0, 100]):
+            main.init()
+            assert mock_exit.call_args[0][0] != 0  # check the exit code
 
-                    with mock.patch.object(main.sys, 'argv', ["main", 0, 0]):
-                        main.init()
-                        assert mock_exit.call_args[0][0] != 0
+        with mock.patch.object(main.sys, 'argv', ["main", 0, 0]):
+            main.init()
+            assert mock_exit.call_args[0][0] != 0  # check the exit code
 
-                    with mock.patch.object(main.sys, 'argv', ["main"]):
-                        main.init()
-                        assert mock_exit.call_args[0][0] == 0
+        with mock.patch.object(main.sys, 'argv', ["main"]):
+            main.init()
+            assert mock_exit.call_args[0][0] == 0  # check the exit code
