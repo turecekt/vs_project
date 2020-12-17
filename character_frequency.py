1d4f12e9351b37079502b548df479ea2020f89e1
Adam Huspenina <adam.huspenina@advantech.cz>
2020-12-18 00:23:46 +0100

    008: line length fix and space around operators.

--- character_frequency.py
+++ character_frequency.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# ###########################################################################
+##########################################################################
 # Project: AK1VS - Final project
 # Description: This project should show how to work with versioning tool such
 #              as git.
@@ -12,7 +12,7 @@
 #                   least frequent character
 #                   average occurence of character
 #                   frequency of each alphabetic character
-# ###########################################################################
+#############################################################################
 # UTB Zlin - FAI 2020
 # Author: Adam Huspenina
 
@@ -21,26 +21,26 @@ import argparse
 import statistics
 
 
-# ###########################################################################
+##########################################################################
 # Class contains whole functionality needed for completing deisred tasks.
 class CharacterFrequency:
     def __init__(self, intput):
-        self.input     = intput
+        self.input = intput
         self.char_dict = self._init_dictionary()
 
-    # ########################################################################
-    # Method returns touple of most frequent character list 
+    ##########################################################################
+    # Method returns touple of most frequent character list
     # (list because of equalities).
     def get_most_frequent(self):
         max_value = max(self.char_dict.values())
-        max_keys  = []
+        max_keys = []
         for k, v in self.char_dict.items():
             if v == max_value:
                 max_keys.append(k)
         return (max_keys, max_value)
 
-    # ########################################################################
-    # Method returns touple of least frequent character list 
+    ##########################################################################
+    # Method returns touple of least frequent character list
     # (list because of equalities).
     def get_least_frequent(self):
         min_value = min(self.char_dict.values())
@@ -50,19 +50,19 @@ class CharacterFrequency:
                 min_keys.append(k)
         return (min_keys, min_value)
 
-    # ########################################################################
-    # Method returns touple of average occurence of character list 
+    ##########################################################################
+    # Method returns touple of average occurence of character list
     # (list because of equalities).
     def get_average(self):
-        avg_value     = statistics.mean(self.char_dict.values())
+        avg_value = statistics.mean(self.char_dict.values())
         rounded_value = round(avg_value)
-        avg_keys      = []
+        avg_keys = []
         for k, v in self.char_dict.items():
             if v == rounded_value:
                 avg_keys.append(k)
         return (avg_value, avg_keys)
 
-    # ########################################################################
+    ##########################################################################
     # Method returns list of alphabetic character with their frequency
     # of occurence.
     def get_alpha_frequency(self):
@@ -72,15 +72,15 @@ class CharacterFrequency:
                 alpha_dict[k] = v
         return alpha_dict
 
-    # ########################################################################
+    ##########################################################################
     # Method for printing lists with result.
     def output(self, output_list):
         print(str(output_list))
 
-    # ########################################################################
+    ##########################################################################
     # Private method read input data and store them into dictionary.
     # For stdin dictionary contains only characters until '#' is read.
-    # 'DEBUG' option is for testing purposes, when tester can easily handle 
+    # 'DEBUG' option is for testing purposes, when tester can easily handle
     # input. It can be only reached through code.
     # Tester has to define self.char_dict by him self.
     def _init_dictionary(self):
@@ -111,22 +111,19 @@ class CharacterFrequency:
         return character_dict
 
 
-# ###########################################################################
+##########################################################################
 # Outter method for parsing input arguments from terminal.
 def parse_args():
     parser = argparse.ArgumentParser(
-        description = "Get information about character frequency form input.")
-    parser.add_argument("-i",
-                        nargs   = '?',
-                        help   = "File containing characters to be analyzed.",
-                        type    = argparse.FileType('r'),
-                        default = sys.stdin
-                        )
+        description="Get information about character frequency form input.")
+    parser.add_argument("-i",nargs='?',
+        help="File containing characters to be analyzed.",
+        type=argparse.FileType('r'), default=sys.stdin)
     args = parser.parse_args()
     return args
- 
 
-# ###########################################################################
+
+##########################################################################
 # Driving method for whole program.
 def main(args):
     characters = CharacterFrequency(args.i.name)
--- test_character_frequency.py
+++ test_character_frequency.py
@@ -13,57 +13,57 @@ from character_frequency import CharacterFrequency
 import unittest
 
 class TestCharacterFrequency(unittest.TestCase):
-    #############################################################################
+    ##########################################################################
     # This is executed only once.
     @classmethod
     def setUpClass(cls):
         cls.chars = CharacterFrequency("DEBUG")
-        cls.test_dict = { 'a' : 42, 'b' : 18, 'c' : 6, '&' : 1 }
+        cls.test_dict = {'a':42, 'b':18, 'c':6, '&':1}
 
-    #############################################################################
+    ##########################################################################
     # This is executed at the end.
     @classmethod
     def tearDownClass(cls):
         pass
 
-    #############################################################################
+    ##########################################################################
     # This is executed before every test-case.
     def setUp(self):
         self.chars.char_dict = self.test_dict
         pass
 
-    #############################################################################
+    ##########################################################################
     # This is executed after every test-case.
     def tearDown(self):
         pass
 
-    #############################################################################
+    ##########################################################################
     # Tests if most frequent character could be found.
-    #@unittest.skip('Intentionally skipped for development purposes.')
+    # @unittest.skip('Intentionally skipped for development purposes.')
     def test_most_frequency(self):
         self.chars.char_dict['X'] = 100
         result = self.chars.get_most_frequent()
         self.assertEqual(result[1], 100)
 
-    #############################################################################
+    ##########################################################################
     # Tests if least frequent character could be found.
-    #@unittest.skip('Intentionally skipped for development purposes.')
+    # @unittest.skip('Intentionally skipped for development purposes.')
     def test_least_frequency(self):
         self.chars.char_dict['X'] = 1
         result = self.chars.get_least_frequent()
         self.assertEqual(result[1], 1)
 
-    #############################################################################
+    ##########################################################################
     # Tests if character with average rate could be found.
-    #@unittest.skip('Intentionally skipped for development purposes.')
+    # @unittest.skip('Intentionally skipped for development purposes.')
     def test_average_frequency(self):
         self.chars.char_dict['X'] = 13
         result = self.chars.get_average()
         self.assertEqual(result[0], 15.5)
 
-    #############################################################################
+    ##########################################################################
     # Tests if alphabetic characters with rate could be filtered.
-    #@unittest.skip('Intentionally skipped for development purposes.')
+    # @unittest.skip('Intentionally skipped for development purposes.')
     def test_alphabetic_frequency(self):
         self.chars.char_dict['!'] = 13
         result = self.chars.get_alpha_frequency()
