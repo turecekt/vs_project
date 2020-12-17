52c4113e6392c5985e4b32eed37b24898c926038
Adam Huspenina <adam.huspenina@advantech.cz>
2020-12-18 00:10:00 +0100

    008: fixed comment format

--- character_frequency.py
+++ character_frequency.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#############################################################################
+# ###########################################################################
 # Project: AK1VS - Final project
 # Description: This project should show how to work with versioning tool such
 #              as git.
@@ -12,7 +12,7 @@
 #                   least frequent character
 #                   average occurence of character
 #                   frequency of each alphabetic character
-#############################################################################
+# ###########################################################################
 # UTB Zlin - FAI 2020
 # Author: Adam Huspenina
 
@@ -20,14 +20,15 @@ import sys
 import argparse
 import statistics
 
-#############################################################################
+
+# ###########################################################################
 # Class contains whole functionality needed for completing deisred tasks.
 class CharacterFrequency:
     def __init__(self, intput):
         self.input     = intput
         self.char_dict = self._init_dictionary()
 
-    ###########################################################################
+    # ########################################################################
     # Method returns touple of most frequent character list 
     # (list because of equalities).
     def get_most_frequent(self):
@@ -38,18 +39,18 @@ class CharacterFrequency:
                 max_keys.append(k)
         return (max_keys, max_value)
 
-    ###########################################################################
+    # ########################################################################
     # Method returns touple of least frequent character list 
     # (list because of equalities).
     def get_least_frequent(self):
         min_value = min(self.char_dict.values())
         min_keys  = []
         for k, v in self.char_dict.items():
-          if v == min_value:
-            min_keys.append(k)
+            if v == min_value:
+                min_keys.append(k)
         return (min_keys, min_value)
 
-    ###########################################################################
+    # ########################################################################
     # Method returns touple of average occurence of character list 
     # (list because of equalities).
     def get_average(self):
@@ -57,26 +58,26 @@ class CharacterFrequency:
         rounded_value = round(avg_value)
         avg_keys      = []
         for k, v in self.char_dict.items():
-          if v == rounded_value:
-            avg_keys.append(k)
+            if v == rounded_value:
+                avg_keys.append(k)
         return (avg_value, avg_keys)
 
-    ###########################################################################
+    # ########################################################################
     # Method returns list of alphabetic character with their frequency
     # of occurence.
-    def get_alpha_frequency(self):    
+    def get_alpha_frequency(self):
         alpha_dict = {}
         for k, v in self.char_dict.items():
-          if k.isalpha():
-            alpha_dict[k] = v
+            if k.isalpha():
+                alpha_dict[k] = v
         return alpha_dict
 
-    ###########################################################################
+    # ########################################################################
     # Method for printing lists with result.
     def output(self, output_list):
         print(str(output_list))
 
-    ###########################################################################
+    # ########################################################################
     # Private method read input data and store them into dictionary.
     # For stdin dictionary contains only characters until '#' is read.
     # 'DEBUG' option is for testing purposes, when tester can easily handle 
@@ -109,21 +110,23 @@ class CharacterFrequency:
             file_input.close()
         return character_dict
 
-#############################################################################
+
+# ###########################################################################
 # Outter method for parsing input arguments from terminal.
 def parse_args():
     parser = argparse.ArgumentParser(
         description = "Get information about character frequency form input.")
     parser.add_argument("-i",
                         nargs   = '?',
-                        help    = "File containing characters to be analyzed.",
+                        help   = "File containing characters to be analyzed.",
                         type    = argparse.FileType('r'),
                         default = sys.stdin
                         )
     args = parser.parse_args()
     return args
-  
-#############################################################################
+ 
+
+# ###########################################################################
 # Driving method for whole program.
 def main(args):
     characters = CharacterFrequency(args.i.name)
