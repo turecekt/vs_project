31fa3aa99267896661fb2f272259d81534e57c62
Adam Huspenina <adam.huspenina@advantech.cz>
2020-12-17 19:52:56 +0100

    002: added parsing user input logic.

--- character_frequency.py
+++ character_frequency.py
@@ -16,11 +16,15 @@
 # UTB Zlin - FAI 2020
 # Author: Adam Huspenina
 
+import sys
+import argparse
+
 #############################################################################
 # Class contains whole functionality needed for completing deisred output tasks.
 class CharacterFrequency:
   def __init__(self, intput):
-    self.input = intput
+    self.input     = intput
+    self.char_dict = self._init_dictionary()
 
   #############################################################################
   # Method returns list of most frequent character (list because of equalities).
@@ -44,13 +48,68 @@ class CharacterFrequency:
 
   #############################################################################
   # Method for printing lists with result.
-  def output(self):
-    return
+  def output(self, output_list):
+    print(str(output_list))
+
+  #############################################################################
+  # Private method read input data and store them into dictionary.
+  def _init_dictionary(self):
+    character_dict = {}
+    if (self.input == '<stdin>'):
+      while True:
+        char = sys.stdin.read(1)
+
+        if (char == '#'):
+          break
+        
+        dict_keys = character_dict.keys()
+
+        if (char in dict_keys):
+          character_dict[char] += 1
+        else:
+          character_dict[char] = 1
+
+    else:
+      file_input = open(self.input, 'r')
+
+      while True:
+        char = file_input.read(1)
+
+        if (char == ''):
+          break
+
+        dict_keys = character_dict.keys()
+
+        if (char in dict_keys):
+          character_dict[char] += 1
+        else:
+          character_dict[char] = 1
+
+      file_input.close()
+
+    return character_dict
 
 #############################################################################
-# Driving function preparing arguments.
-def main():
-  return 0
+# Outter method for parsing input arguments from terminal.
+def parse_args():
+  parser = argparse.ArgumentParser(description = "Get information about character frequency form input.")
+
+  parser.add_argument("-i",
+                      nargs   = '?',
+                      help    = "File containing characters to be analyzed.\nDefault = StdIn closed with #",
+                      type    = argparse.FileType('r'),
+                      default = sys.stdin
+                      )
+
+  args = parser.parse_args()
+
+  return args
+  
+#############################################################################
+# Driving method for whole program.
+def main(args):
+  characters = CharacterFrequency(args.i.name)
 
 if __name__ == "__main__":
-  main()
+  args = parse_args()
+  main(args)
