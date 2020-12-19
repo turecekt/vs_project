b017cd7cb8a7fb3565f0850a628631af75de2d6c
Vojtech Kordac <74076141+KordacVojtech@users.noreply.github.com>
2020-12-19 18:30:13 +0100

    lint

--- projectRom.py
+++ projectRom.py
@@ -2,37 +2,37 @@
 
 
 def intToRom(userInp):
-        """Vezme input a prevede na rimske."""
-        # Nadefinujeme si cisla na ktere pak budem odkazovat
-        val = [
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4,
-            1
-        ]
-        # Nadefinujeme si symboly na ktere pak budem odkazovat
-        syb = [
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV",
-            "I"
-        ]
-        # Vytvorim transNum ktery zatim bude mit value prazdneho stringu
+    """Vezme input a prevede na rimske."""
+    # Nadefinujeme si cisla na ktere pak budem odkazovat
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    # Nadefinujeme si symboly na ktere pak budem odkazovat
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    # Vytvorim transNum ktery zatim bude mit value prazdneho stringu
 
-        transNum = ''
+    transNum = ''
 
-        # While loopem loopnu syb a val, priradim hodnotu k transNum
-        # ktera odpovida danemu user inputu (userInp)
+    # While loopem loopnu syb a val, priradim hodnotu k transNum
+    # ktera odpovida danemu user inputu (userInp)
 
-        i = 0
+    i = 0
 
-        while userInp > 0:
-            for _ in range(userInp // val[i]):
-                transNum += syb[i]
-                userInp -= val[i]
-            i += 1
-        # Vratim transNum
-        return transNum
+    while userInp > 0:
+        for _ in range(userInp // val[i]):
+            transNum += syb[i]
+            userInp -= val[i]
+        i += 1
+    # Vratim transNum
+    return transNum
 
 
 if __name__ == '__main__':
