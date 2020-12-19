325f2a4c6c9d1ddf983a82cd43b08951438d01ad
Vojtech Kordac <74076141+KordacVojtech@users.noreply.github.com>
2020-12-19 18:32:16 +0100

    Lint testu

--- test_import.py
+++ test_import.py
@@ -11,12 +11,10 @@ class MyTestCase(unittest.TestCase):
         result = projectRom.intToRom(10)
         self.assertEqual(result, 'X')
 
-
     def test_jedenact(self):
         """Vytvoreni testu ktery testne 11."""
         result = projectRom.intToRom(11)
         self.assertEqual(result, 'XI')
-
 
     def test_stojedenact(self):
         """Vytvoreni testu ktery testne 111."""
@@ -29,12 +27,10 @@ class MyTestCase(unittest.TestCase):
         result = projectRom.intToRom(9)
         self.assertEqual(result, 'IX')
 
-
     def test_nula(self):
         """Vytvoreni testu ktery tesne 0."""
         result = projectRom.intToRom(0)
         self.assertEqual(result, '')
-
 
 if __name__ == '__main__':
     unittest.main()
