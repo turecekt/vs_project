3c184abfc9abc4cc279a2d232769d6cb51b33cd7
trupmichal <76405131+trupmichal@users.noreply.github.com>
2020-12-20 20:33:54 +0100

    Update morse.py

--- kod/morse.py
+++ kod/morse.py
@@ -1,9 +1,8 @@
-"""Zadefinovanie funkcie prekladacu"""
+"""Zadefinovanie funkcie prekladacu."""
 
 
 def prekladac(text):
-
-    """Syntax pre prelozenie klasickej abecedy do Morseovej"""
+    """Syntax pre prelozenie klasickej abecedy do Morseovej."""
     code = {' ': '| ', 'A': '.- ', 'B': '-... ',
             'C': '-.-. ', 'D': '-.. ', 'E': '. ',
             'F': '..-. ', 'G': '--. ', 'H': '.... ',
@@ -24,16 +23,16 @@ def prekladac(text):
     """Prekladanie individualnych charakterov klasickej
     abecedy do Morseoveho
     kodu pomocou vyssie zadefinovanej syntaxe
-    a konvertacia malych pismen na velke"""
+    a konvertacia malych pismen na velke."""
     for x in text:
         morse_code += code[x.upper()]
     return morse_code
 
 
 if __name__ == '__main__':
-    """Vyzadovanie vstupu od uzivatela prekladacu"""
+    """Vyzadovanie vstupu od uzivatela prekladacu."""
     text = input("Zadajte text ktory chcete prelozit do Morseovky: ")
 
     text2 = prekladac(text)
-    """Volanie funkcie prekladacu"""
+    """Volanie funkcie prekladacu."""
     print("Text prelozeny do Morseovky vyzera takto: ", text2)
