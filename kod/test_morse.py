4ac14c9a75ad75d92038e2756aad7311589b2df9
trupmichal <76405131+trupmichal@users.noreply.github.com>
2020-12-20 20:28:27 +0100

    Update test_morse.py

--- kod/test_morse.py
+++ kod/test_morse.py
@@ -2,21 +2,21 @@ import morse
 
 
 def test_skuska1():
-    """ Overenie toho, ze prekladac dokaze prelozit individualne znaky. """
+    """Overenie toho, ze prekladac dokaze prelozit individualne znaky."""
     assert morse.prekladac('A') == '.- '
 
-    """ Overenie toho, ze prekladac dokaze prelozit cele slova. """
+    """Overenie toho, ze prekladac dokaze prelozit cele slova."""
     assert morse.prekladac('SKUSKA2') == '... -.- ..- ... -.- .- ..--- '
 
-    """ Overenie toho, ze prekladac dokaze prelozit medzery v texte. """
+    """Overenie toho, ze prekladac dokaze prelozit medzery v texte."""
     assert morse.prekladac('M E D Z E R E') \
            == '-- | . | -.. | --.. | . | .-. | . '
 
-    """ Overenie toho, ze prekladac dokaze prelozit symboly v texte. """
+    """Overenie toho, ze prekladac dokaze prelozit symboly v texte."""
     assert morse.prekladac(',?YM80LY/') \
            == '--..-- ..--.. -.-- -- ---.. ----- .-.. -.-- -..-. '
 
-    """ Overenie toho, ze prekladac dokaze prelozit cele vety pisane
-    pomocou malych pismen) """
+    """Overenie toho, ze prekladac dokaze prelozit cele vety pisane
+    pomocou malych pismen)"""
     assert morse.prekladac('male na velke') \
            == '-- .- .-.. . | -. .- | ...- . .-.. -.- . '
