71dfc18fb34b24a024abf1917a3fab98d0670076
Tesař Ondřej <73735244+tesaro@users.noreply.github.com>
2020-12-21 17:49:53 +0100

    Update Test_Projekt_Morseovka.py

--- Test_Projekt_Morseovka.py
+++ Test_Projekt_Morseovka.py
@@ -26,30 +26,30 @@ def test_encrypt_2(self):
     # encrypting number Morseovka
     assert encrypt("1234") == ".----/..---/...--/....-"
 
-
     """
     Unit test - Morse to Latin function
     """
-    
-    
+  
+
 def test_decrypt_1(self):
     # encrypting Morse to Latin Characters
     assert decrypt(".../-./-../.-./.-") == "Ondra"
 
-
     """
     Unit test - Morse to Latin Numbers function
     """
-    
-    
+
+
 def test_decrypt_2(self):
     # encrypting Morse to Latin Numbers
     assert decrypt(".---- ..--- ...-- ....-") == "1234"
 
-
     """
     Unit test - Choose translation function
     """
+    
+    
 def test_choice_1(self):
+    
     assert choice("1") == "1"
  
