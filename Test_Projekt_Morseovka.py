35fd23c0e1e02df133e4a160c19da71ff0afabb0
Tesař Ondřej <73735244+tesaro@users.noreply.github.com>
2020-12-21 17:38:19 +0100

    Update Test_Projekt_Morseovka.py

--- Test_Projekt_Morseovka.py
+++ Test_Projekt_Morseovka.py
@@ -5,44 +5,43 @@ from Projekt_Morseovka import encrypt
 from Projekt_Morseovka import decrypt
 from Projekt_Morseovka import choice
 
+
 class Morseovka_Test(unittest.TestCase):
- 
-
     """
     Unit test - Latin to Morse function
     """
-def test_encrypt_1(self):       
+def test_encrypt_1(self):
     # encrypting a word Morseovka
-    assert encrypt("Ondra") == ".../-./-../.-./.- "
+  assert encrypt("Ondra") == ".../-./-../.-./.- "
 
 
     """
     Unit test - Latin to Morse function
     """
-def test_encrypt_2(self):       
+def test_encrypt_2(self):
     # encrypting number Morseovka
-    assert encrypt("1234") == ".----/..---/...--/....-"
+  assert encrypt("1234") == ".----/..---/...--/....-"
 
 
     """
     Unit test - Morse to Latin function
     """
-def test_decrypt_1(self):    
+def test_decrypt_1(self):
     # encrypting Morse to Latin Characters
-    assert decrypt(".../-./-../.-./.-") == "Ondra"
+  assert decrypt(".../-./-../.-./.-") == "Ondra"
 
 
     """
     Unit test - Morse to Latin Numbers function
     """
-def test_decrypt_2(self):    
+def test_decrypt_2(self):
     # encrypting Morse to Latin Numbers
-    assert decrypt(".---- ..--- ...-- ....-") == "1234"
+  assert decrypt(".---- ..--- ...-- ....-") == "1234"
 
 
     """
     Unit test - Choose translation function
     """
-def test_choice_1(self):    
-    assert choice("1") == "1"
-  
+def test_choice_1(self):
+ assert choice("1") == "1"
+ 
