588fc6fd2364f867dd855629cfc23253d7490f28
Tomexe <t_dano@utb.cz>
2020-12-21 21:26:44 +0100

    provedeni flake8

--- test_morseovka.py
+++ test_morseovka.py
@@ -1,37 +1,41 @@
-import morseovka
-import pytest
+"""Testy na funkci Morseovka"""
 
-{
-def test_TnM():
-    """Sifrovani do morseova kodu.
-    Funkce TnM() porovnava
-    vraceny string s morseovym kodem.
-    """
-    assert TnM('A A') == '.- / .-'
-    assert TnM('test') == '- . ... -'
-    assert TnM('33 -  10 = 23') == '...-- ...-- / -....- / / .---- ----- / / ..--- ...--'
-    assert TnM('/+-: .    .') == '-..-. .-.-. -....- ---...  .-.-.-     .-.-.-'
-    assert TnM('1234567890') == '.---- ..--- ...-- ....- ..... -.... --... ---.. ----. -----'
-    assert TnM('Pokud je zadán text, je přeložen do morseovky a naopak. ')
-               == '.--. --- -.- ..- -.. / .--- . / --.. .- -.. -. /
-                     ' - . -..- - / .--- . / .--. . .-.. --- . -. / -.. --- /
-                     ' -- --- .-. ... . --- ...- -.- -.-- /
-                     ' .- / -. .- --- .--. .- -.- .-.-.-'
-           
+import unittest
+import morseovka.py
 
 
-def test_MnT():
-    """Desifrovani morseova kodu.
-    Funkce test_MnT() porovnava
-    morseuv kod se stringem podle Morseovy abecedy.
-    """
-  
-   
-    assert MnT('-..-. -....- / .-.-.- / .-.-.-') == '/+-: . .'
-    assert MnT('.---- ..--- ...-- ....- ..... -.... --... ---.. ----. -----') \
-           == '1234567890'
-    assert MnT('.--. -.-- - .... --- -. / .--- . / -. . .--- .-.. . .--. ... .. 
-               '/ .--- .- --.. -.-- -.- / -. .- / ... ...- . - . .-.-.-') \
-           == 'PYTHON JE NEJLEPSI JAZYK NA SVETE.'
-}
-        
+class TestSifrovani(unittest.TestCase):
+        """"Testy na funkci Morseovka_na_Text."""""
+
+    def test_sifrovani_1(self):
+        """Test testujici string o jednom slove."""
+
+        expected1 = "- . ... - ..--- "
+        self.assertEqual(morseovka.Text_na_Morseovku("test1"), expected1)
+
+    def test_sifrovani_2(self):
+        """Test testujici string o vice slovech."""
+
+        expected2 = "-.. .-. .-- .... -.-- / - . ... - "
+        self.assertEqual(morseovka.Text_na_Morseovku("druhy test"), expected2)
+
+
+class TestDesifrovani(unittest.TestCase):
+        """Testy na funkci Morseovka_na_Text."""
+
+    def test_desifrovani_1(self):
+        """Test testujici desifrovani stringu o dvou slovech."""
+
+        expected3 = "TEST PROSEL"
+        self.assertEqual(morseovka.Morseovka_na_Text
+                         ("- . ... - / .--. .-. --- ... . .-.. "),
+                         expected3)
+
+    def test_desifrovani_2(self):
+        """Test testujici desifrovani stringu s vice slovy a cisly."""
+
+        expected4 = "TEST DOKONCEN 415 3 5 7"
+        self.assertEqual(morseovka.Morseovka_na_Text
+                         ("- . ... - / -.. --- -.- --- -. -.-. . -. / "
+                          "..... ..--- -.... / ....- / -.... / ---.. "),
+                         expected4)
