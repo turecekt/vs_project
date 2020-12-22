5ed49b1ef4da5fcd0ed641e0a98bfedf055f9b2e
PospisilJ <j1_pospisil@utb.cz>
2020-12-22 23:11:34 +0100

    ladeni22

--- projekt1/test_main.py
+++ projekt1/test_main.py
@@ -48,4 +48,4 @@ class Test(TestCase):
         self.assertEqual(rozsifrovani
                          ("-.. --- -... .-. -.--  -.. . -. --..--"
                           "  .--- .- -.-  ... .  -- .- - . ..--.. "),
-                            "DOBRY DEN, JAK SE MATE? ", "OK")
+                        "DOBRY DEN, JAK SE MATE? ", "OK")
