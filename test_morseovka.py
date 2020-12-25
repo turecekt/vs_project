f515513880ea1d6e1d30cc18c4a04fd004d9952b
Tomexe <t_dano@utb.cz>
2020-12-25 13:30:54 +0100

    spustil jsem pytest skrz terminal

--- test_morseovka.py
+++ test_morseovka.py
@@ -3,28 +3,28 @@
 import morseovka
 
 
-def test_sifrovani1(self):
+def test_sifrovani1():
     """Test testujici string o jednom slove."""
     expected1 = "- . ... -"
     assert morseovka.sifrovani
     ("test") == expected1
 
 
-def test_sifrovani(self):
+def test_sifrovani():
     """Test testujici string o vice slovech."""
     expected2 = "-.. .-. .-- .... -.-- / - . ... -"
     assert morseovka.sifrovani
     ("druhy test") == expected2
 
 
-def test_desifrovani1(self):
+def test_desifrovani1():
     """Test testujici desifrovani stringu o dvou slovech."""
     expected3 = "TEST PROSEL"
     assert morseovka.desifrovani
     ("- . ... - / .--. .-. --- ... . .-.. ") == expected3
 
 
-def test_desifrovani(self):
+def test_desifrovani():
     """Test testujici desifrovani stringu s cisly."""
     expected4 = "TEST 415 3 5 7"
     assert morseovka.desifrovani
