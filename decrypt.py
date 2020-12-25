b0dd0f0d2dc67460615921336effbdec1c7198f2
808hana <76486192+808hana@users.noreply.github.com>
2020-12-25 17:06:13 +0100

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,17 @@
+import morse1
+import unittest
+
+class Check2(unittest.TestCase):
+    known_values = (('.-', 'A'), ('-...', 'B'), ('-.-.', 'C'), ('-..', 'D'), ('.', 'E'), ('..-.', 'F'),
+                    ('--.', 'G'),
+               ('....', 'H'), ('..', 'I'), ('.---', 'J'), ('-.-', 'K'), ('.-..', 'L'), ('--', 'M'), ('-.', 'N'),
+               ('---', 'O'), ('.--.', 'P'), ('--.-', 'Q'), ('.-.', 'R'), ('...', 'S'), ('-', 'T'), ('..-', 'U'),
+               ('...-', 'V'), ('.--', 'W'), ('-..-', 'X'), ('-.--', 'Y'), ('--..', 'Z'))
+    
+    def test_decrypt_known_values(self):
+        for string, char in self.known_values:
+            result = morse1.decrypt(string)
+            self.assertEqual(char, result)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ encrypt.py
@@ -0,0 +1,17 @@
+import morse1
+import unittest
+
+class Check(unittest.TestCase):
+    known_values = (('A','.-'), ('B','-...'), ('C','-.-.'), ('D','-..'), ('E','.'), ('F','..-.'),
+                    ('G','--.'),
+               ('H','....'), ('I','..'), ('J','.---'), ('K','-.-'), ('L','.-..'), ('M','--'), ('N','-.'),
+               ('O','---'), ('P','.--.'), ('Q','--.-'), ('R','.-.'), ('S','...'), ('T','-'), ('U','..-'),
+               ('V','...-'), ('W','.--'), ('X','-..-'), ('Y','-.--'), ('Z','--..'), (' ', ''))
+    
+    def test_encrypt_known_values(self):
+        for char, string in self.known_values:
+            result = morse1.encrypt(char)
+            self.assertEqual(string, result)
+
+if __name__ == '__main__':
+    unittest.main()
