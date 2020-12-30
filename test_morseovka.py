1dd1932e8221567c1b88f26387598b76887f74fe
tomashnanicek <tomashnanicek@gmail.com>
2020-12-30 13:12:54 +0100

    přidání souboru s pytestem do adresáře

---
+++ test_morseovka.py
@@ -0,0 +1,31 @@
+#import zdrojového souboru#
+
+import morseovka
+
+def test_prelozit_do():
+    #ověření překladače, jestli zvládne přeložit jednotlivé znaky#
+    assert morseovka.prelozit_do('A') == '.- '
+    #ověření překladače, jestli zvládne přeložit slova s mezerou#
+    assert morseovka.prelozit_do('tomas hnanicek') == '- --- -- .- ... | .... -. .- -. .. -.-. . -.- '
+    #ověření překladače, jestli zvládne přeložit jednotlivé znaky oddělené mezerami#
+    assert morseovka.prelozit_do('A H O J') == '.- | .... | --- | .--- '
+    #ověření překladače, jestli zvládne přeložit symboly#
+    assert morseovka.prelozit_do('/,?)(') == '-..-. --..-- ..--.. -.--.- -.--. '
+    #ověření překladače, jestli zvládne přeložit různé symboly, čísla a znaky ve větě#
+    assert morseovka.prelozit_do('Lo(rem 4 ipsum dolor sit amet, 6 consecte/tuer adipiscing elit? Cras el)ementum. Nulla accumsan, 5') == '.-.. --- -.--. .-. . -- | ....- | .. .--. ... ..- -- | -.. --- .-.. --- .-. | ... .. - | .- -- . - --..-- | -.... | -.-. --- -. ... . -.-. - . -..-. - ..- . .-. | .- -.. .. .--. .. ... -.-. .. -. --. | . .-.. .. - ..--.. | -.-. .-. .- ... | . .-.. -.--.- . -- . -. - ..- -- .-.-.- | -. ..- .-.. .-.. .- | .- -.-. -.-. ..- -- ... .- -. --..-- | ..... '
+    #ověření překladače, jestli zvládne přeložit čísla oddělené mezerami#
+    assert morseovka.prelozit_do('1 2 3 4') == '.---- | ..--- | ...-- | ....- '
+  
+def test_prelozit_z():
+    #ověření překladače, jestli zvládne přeložit jednotlivé znaky z Morseovy abecedy#
+    assert morseovka.prelozit_z('.-') == 'A'
+    #ověření překladače, jestli zvládne přeložit celé slovo#
+    assert morseovka.prelozit_z('- --- -- .- ...') == 'TOMAS'
+    #ověření překladače, jestli zvládne přeložit čísla oddělené mezerami#
+    assert morseovka.prelozit_z('.---- | ..--- | ...-- | ....-') == '1 2 3 4'
+    #ověření překladače, jestli zvládne přeložit symboly#
+    assert morseovka.prelozit_z('..--.. -..-. -....- .-.-.-') == '?/-.'
+    #ověření překladače, jestli zvládne přeložit slova oddělená mezerou#
+    assert morseovka.prelozit_z('- --- -- .- ... | .... -. .- -. .. -.-. . -.-') == 'TOMAS HNANICEK'
+    #ověření překladače, jestli zvládne přeložit větu#
+    assert morseovka.prelozit_z('.-.. --- .-. . -- | .. .--. ... ..- -- | -.. --- .-.. --- .-. | ... .. - | .- -- . -') == 'LOREM IPSUM DOLOR SIT AMET'
