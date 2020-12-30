d0c92b5090edd8e9abca32d5059677d42b9f50c7
radimzaluda <73735267+radimzaluda@users.noreply.github.com>
2020-12-30 23:07:40 +0100

    optimalizace pomocí flake8

--- example/morseovka.py
+++ example/morseovka.py
@@ -50,30 +50,25 @@ MORSEOVKA = {
 }
 
 
-"""Deklarace funkce prevodDoMorseovky s parametrem vstup.
-for cyklus pro prochazeni jednotlivych znaku zadaneho vstupniho rezetezce
-nacitani znaku do promenne vystup s pripojenim lomitka
-(pro lepsi citelnost morseovy abecedy na konzoli)
-"""
-
-
 def prevodDoMorseovky(vstup):
+    """Deklarace funkce prevodDoMorseovky s parametrem vstup.
+      for cyklus pro prochazeni jednotlivych znaku zadaneho vstupniho rezetezce
+      nacitani znaku do promenne vystup s pripojenim lomitka
+      (pro lepsi citelnost morseovy abecedy na konzoli)
+    """    
     vystup = ""
     for znak in vstup:
         vystup = vystup + MORSEOVKA[znak] + "/"
     print(vystup)
 
-
-"""Deklarace funkce prevodZMorseovky s parametrem vstup
-pridani mezery na konec vstupniho retezce Morseovy abecedy.
-for cyklus pro prochazeni jednotlivych znaku vstupního retezce
-pokud nalezne mezeru ulozi do promenne
-pokud nalezne 2 po sobe jdouci mezery, prida treti nakonec,
- jakmile nelezne posledni mezeru, jedna se o posledni znak
-"""
-
-
 def prevodZMorseovky(vstup):
+    """Deklarace funkce prevodZMorseovky s parametrem vstup
+        pridani mezery na konec vstupniho retezce Morseovy abecedy.
+        for cyklus pro prochazeni jednotlivych znaku vstupního retezce
+        pokud nalezne mezeru ulozi do promenne
+        pokud nalezne 2 po sobe jdouci mezery, prida treti nakonec,
+        jakmile nelezne posledni mezeru, jedna se o posledni znak
+    """
     vstup += ' '
     vystup = ''
     pomocna = ''
@@ -92,26 +87,17 @@ def prevodZMorseovky(vstup):
         print(vystup)
 
 
-"""Unit test pro zakodovani do morseovky
-"""
-
 
 def test_prevodDoMorseovky():
+    #Unit test pro zakodovani do morseovky
+
     assert test_prevodDoMorseovky(
         "RADIM13578") == ".-. .- -.. .. -- .---- ...-- ..... --... ---.."
 
-
-"""Unit test pro dekodovani z morseovky
-"""
-
-
 def test_prevodZMorseovky():
+    #Unit test pro dekodovani z morseovky
     assert test_prevodZMorseovky(
         ".-. .- -.. .. -- .---- ...-- ..... --... ---..") == "RADIM13578"
-
-
-"""podminkou if - elif (stisknutim klavesy 1 nebo 2) zakodovat nebo dekodovat
-"""
 
 
 if __name__ == '__main__':
