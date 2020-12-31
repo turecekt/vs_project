829df3374c282d211cbcf20638dda13102cfc738
radimzaluda <radim.zaluda@gmail.com>
2020-12-31 19:08:23 +0100

    -UNIT testy

--- example/morseovka_testy.py
+++ example/morseovka_testy.py
@@ -6,13 +6,14 @@ from morseovka import prevodZMorseovky
 
 def test_prevodDoMorseovky():
     """Unit test pro zakodovani do morseovky."""
-    assert prevodDoMorseovky("radim").upper == ".-. .- -.. .. -- "
-    assert prevodDoMorseovky("13578") == ".---- ...-- ..... --... ---.. "
-    assert prevodDoMorseovky("ahoj").upper == ".- .... --- .--- "
+    assert prevodDoMorseovky("RADIM") == ".-. .- -.. .. -"
+    assert prevodDoMorseovky("13578") == ".---- ...-- ..... --... ---.."
+    assert prevodDoMorseovky("ahoj").upper == ".- .... --- .---"
+    assert prevodDoMorseovky("radim").upper == ".-. .- -.. .. -"
 
 
 def test_prevodZMorseovky():
     """Unit test pro dekodovani z morseovky."""
     assert prevodZMorseovky(".-. .- -.. .. -- ") == "radim"
-    assert prevodDoMorseovky(".---- ...-- ..... --... ---.. ") == "13578"
-    assert prevodDoMorseovky(".- .... --- .--- ") == "ahoj"
+    assert prevodDoMorseovky(".---- ...-- ..... --... ---..") == "13578"
+    assert prevodDoMorseovky(".- .... --- .---") == "ahoj"
