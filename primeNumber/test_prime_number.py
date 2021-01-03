117b61bbe4b427ec656a1ab13292256c1d6e4ff6
RentlyBB <patrikchleboun8@gmail.com>
2021-01-03 20:36:23 +0100

    testovaci kometar 4

--- primeNumber/test_prime_number.py
+++ primeNumber/test_prime_number.py
@@ -1,7 +1,10 @@
 from prime_number import is_prime_number
 
+""" ahoj """
+
 
 def test_is_prime_number_input_int():
+    """ ahoj """
     desired_result = "3 je prvocislo"
     result = is_prime_number(3)
     assert result == desired_result
