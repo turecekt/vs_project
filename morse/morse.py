7e304b8c33a5c3a2cc406f2987c495d2da3288f8
Sammy-Jk <samueljirsak.b@gmail.com>
2021-01-04 10:00:05 +0100

    Another modifying at the suggestion

--- morse/morse.py
+++ morse/morse.py
@@ -1,13 +1,11 @@
 """Morse translator run file."""
 from translator import encode, decode
 
-text_for_encoding = "Hello World!"
-
 if __name__ == '__main__':
     text_for_encoding = input("Insert text for encoding: ")
 
-morse_code = encode(text_for_encoding)
-print(morse_code)
+    morse_code = encode(text_for_encoding)
+    print(morse_code)
 
-back_to_text = decode(morse_code)
-print(back_to_text)
+    back_to_text = decode(morse_code)
+    print(back_to_text)
