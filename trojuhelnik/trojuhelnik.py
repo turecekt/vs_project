d6b52653e2f3384aaac3dc9f8799cb804395335c
Filip Kovář <filip.kovar@technodat.cz>
2021-01-09 10:29:21 +0100

    Test - first commit.

---
+++ trojuhelnik/trojuhelnik.py
@@ -0,0 +1,25 @@
+"""Triangle"""
+
+
+class Triangle():
+    """Constructability triangle"""
+
+
+a = 0
+b = 0
+c = 0
+
+
+def isConstructable():
+    """Check triangle constructability
+    Return True if thi triangle is constructable
+    >>> isConstructable()
+    true
+    """
+
+    triangleIsConstructable = True
+
+    if a + b < c or b + c < a or c + a < b:
+        triangleIsConstructable = False
+
+    return triangleIsConstructable
