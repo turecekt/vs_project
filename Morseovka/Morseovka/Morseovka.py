a15488ac1af818966e0b3600b11eea24510239b7
MaxMixcoffe <maxmixko68@gmail.com>
2021-01-12 22:37:21 +0100

    Update updated

--- Morseovka/Morseovka/Morseovka.py
+++ Morseovka/Morseovka/Morseovka.py
@@ -1,124 +1,72 @@
 """Prekladac z/do textu do/z kodu morse."""
 
-
-def texttomorse(text):
-    """Prelozeni textu do kodu morse."""
-    sp = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.',
-          'f': '..-.',
-          'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-',
-          'l': '.-..',
-          'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-',
-          'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-',
-          'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..',
-          '.': '.', '-': '-', '1': '.----', '2': '..---', '3': '...--',
-          '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
-          '9': '----.', '0': '-----', ' ': ' '}
-    # vsechny mozne symboly, ktere se mohou zapsat do promenne
-    mozne = set('abcdefghijklmnopqrstyuvwxz.1234567890 ')
-    stroka = ''
-    for i in text:  # kontrola zadaneho textu
-        # kontroluje, jestli symbol ze zadaneho textu se lisi
-        # od moznych symbolu
-        if set(i).difference(mozne):
-            print('nespravne znaky')
-            stroka = 'nespravne znaky'
-            break
-        for j in i:  # vypisuje prelozeny text
-            print(sp[j], end=" ")
-            # zapisuje prelozeny text do promenne
-            stroka = stroka + ' ' + sp[j]
-        print()
-    if stroka == '':  # kontroluje, jestli do stroki je zapsana jenom mezera
-        print('prazdne pole')
-        return 'prazdne pole'
-    else:
-        return stroka
+# Pridani slovniku
+Slovnik = {
+    'a': '.-', 'b': '-...', 'c': '-.-.',
+    'd': '-..', 'e': '.', 'f': '..-.',
+    'g': '--.', 'h': '....', 'ch': '----',
+    'i': '..', 'j': '.---', 'k': '-.-',
+    'l': '.-..', 'm': '--', 'n': '-.',
+    'o': '---', 'p': '.--.', 'q': '--.-',
+    'r': '.-.', 's': '...', 't': '-',
+    'u': '..-', 'v': '...-', 'w': '.--',
+    'x': '-..-', 'y': '-.--', 'z': '--..',
+    '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', ' ': '|', '': ''
+}
 
 
-def morsetotext(text):
-    """Prelozeni z kodu morse do textu."""
-    sp = {'.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e',
-          '..-.': 'f',
-          '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k',
-          '.-..': 'l',
-          '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q',
-          '.-.': 'r', '...': 's', '-': 't', '..-': 'u', '...-': 'v',
-          '.--': 'w', '-..-': 'x', '-.--': 'y', '--..': 'z',
-          '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
-          '-....': '6', '--...': '7', '---..': '8', '----.': '9',
-          '-----': '0', '/': ' '}
-    # vsechny mozne symboly, ktere se mohou zapsat do promenne
-    mozne = set('.- ')
-    stroka = ''
-    for j in text:  # kontrola zadaneho textu
-        # kontroluje, jestli symbol ze zadaneho textu se lisi
-        # od moznych symbolu
-        if set(j).difference(mozne):
-            print('nespravne znaky')
-            stroka = 'nespravne znaky'
-            continue
+def texttomorse(prelozeni):
+    """Prelozeni textu do kodu Morse."""
+    retezec = ''
+    # pro kazdy zapsany symbol nachazi svuj kod Morse
+    for symbol in prelozeni:
+        retezec += Slovnik[symbol] + ' '
+    return retezec
+
+
+def morsetotext(prelozeni):
+    """Prelozeni kodu Morse do textu."""
+    n = 0
+    slovo = ''
+    promena = ''
+    prelozeni += ' '
+
+    for symbol in prelozeni:
+
+        # Kontroluje jestli uzivatel zapsal specialni symbol mezery
+        if symbol != ' ':
+            n = 0
+            promena += symbol
+
+        # Jestli se tam objevi symbol mezery, udela tam mezeru
         else:
-            # kontroluje, jestli zadany text je v sp
-            if set(text).difference(sp):
-                print('chyba pri zadavani znaku')
-                stroka = 'chyba pri zadavani znaku'
-                continue
+            n += 1
+            if n == 2:
+                slovo += ' '
             else:
-
-                print(sp[j], end=" ")
-                stroka = stroka + sp[j]  # zapisuje prelozeny text do promenne
-    print()
-    if stroka == '':  # kontroluje, jestli do stroki je zapsana jenom mezera
-        print('prazdne pole')
-        return 'prazdne pole'
-    else:
-        return stroka
+                # Preklada kod Morse na odpovidajici symbol
+                slovo += list(Slovnik.keys())[list(
+                    Slovnik.values()).index(promena)]
+                promena = ''
+    # Pak vraci prelozeny text z kodu Morse
+    return slovo
 
 
 def main():
-    """Zadavani textu do jinych funkce."""
-    vyber = input('1 - TEXT -> MORSE \n2 - MORSE -> TEXT \n')
-    if vyber == str(1):  # kontroluje vyber zadavatele
-        text = input("Zapiste vetu:").lower().split()  # zadavani textu
-        texttomorse(text)
-    elif vyber == str(2):  # kontroluje vyber zadavatele
-        text = input("Zapiste Moresouv kod:").lower().split()  # zadavani textu
-        morsetotext(text)
-    else:  # jestli bude neco jineho, nez 1 a 2
-        print("Zvolte prosim 1 nebo 2")
+    """Tato funkce umoznuje vyber ze dvou cest."""
+    vyber = input("1 - TEXT -> MORSE" "\n2 - MORSE -> TEXT")
+    if vyber == "1":
+        retezec = input("Zapiste vetu: ")
+        vysledek = texttomorse(retezec.lower())
+        print(vysledek)
+    elif vyber == "2":
+        retezec = input("Zapiste kod Morse: ")
+        vysledek = morsetotext(retezec)
+        print(vysledek)
 
 
 if __name__ == '__main__':
     main()
-
-"""Unittesty."""
-
-
-# text na Moresuv kod1
-
-def test_prelozeni1():
-    """Test prevodu textu do kodu morse."""
-    assert texttomorse("asd") == " .- ... -.."
-
-
-def test_prelozeni2():
-    """Test prevodu textu do kodu morse."""
-    assert texttomorse("sos") == " ... --- ..."
-
-
-def test_prelozeni3():
-    """Test prevodu textu do kodu morse."""
-    assert texttomorse("dfg") == " -.. ..-. --."
-
-
-# Moresuv kod na text
-
-
-def test_prelozeni4():
-    """Test na prelozeni kodu morse do textu."""
-    assert morsetotext(['...', '---', '...']) == 'sos'
-
-
-def test_prelozeni5():
-    """Test na prelozeni kodu morse do textu."""
-    assert morsetotext(['.-', '...', '-..']) == 'asd'
