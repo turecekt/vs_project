04433e536aefbd13e2e691065fe7212ca79fbae1
808hana <76486192+808hana@users.noreply.github.com>
2021-01-15 11:25:58 +0100

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -15,8 +15,8 @@ Dvě funkce. Jedna pro zakódování(encrypt), jedna pro překlad(decrypt).
 
 
 class Check(unittest.TestCase):
-    
-    """Snad už to bude fungovat.
+    """
+    Snad už to bude fungovat.
 
     Dvě funkce. Jedna pro zakódování(encrypt), jedna pro překlad(decrypt).
     """
