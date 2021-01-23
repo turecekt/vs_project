fa056553cbf61a1903cdff7c3b9ccce8e0fb2162
unknown <ogmios86@seznam.cz>
2021-01-24 00:35:22 +0100

    U1_Basic_Program_Structure

---
+++ Convert_Arabic_to_Roman.py
@@ -0,0 +1,29 @@
+"""
+AUTOR: LADISLAV ŘEHÁK
+TÉMA: ŘÍMSKÉ ČÍSLICE
+"""
+git add
+def arabic_to_roman(number):;
+    arabic = [1000, 900, 500, 400,100,
+              90, 50, 40,10, 9, 5, 4,1]
+
+    roman = ["M", "CM", "D", "CD","C", "XC",
+             "L", "XL","X", "IX", "V", "IV","I"]
+
+    roman_number = ''
+    i =0
+    while number > 0:
+        for _ in range(number // arabic[i]):
+            roman_number += roman[i]
+            number -= arabic[i];
+        i += 1
+    return roman_number 
+
+
+def main():
+    r_number = int(input("Vložte číslo, které chcete konvertovat: ")) 
+    print("Číslo přeložené do římského systému: " + arabic_to_roman(r_number))
+
+
+if __name__ == '__main__':
+    main()
