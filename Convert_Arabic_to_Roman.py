2b1edecfc75c4f4417a76602040e1c74b09103c1
ogmios86 <77903786+ogmios86@users.noreply.github.com>
2021-01-24 01:44:06 +0100

    U5_Unit_Test_Tweaks

--- Convert_Arabic_to_Roman.py
+++ Convert_Arabic_to_Roman.py
@@ -7,38 +7,39 @@ import unittest
 
 
 class TestMethods(unittest.TestCase):
+    """Třída pro test konverze arabských a římských čísel."""
 
     def test1(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.1."""
         assert arabic_to_roman(9) == 'IX'
 
     def test2(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.2."""
         assert arabic_to_roman(99) == 'XCIX'
 
     def test3(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.3."""
         assert arabic_to_roman(499) == 'CDXCIX'
 
     def test4(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.4."""
         assert arabic_to_roman(999) == 'CMXCIX'
 
     def test5(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.5."""
         assert arabic_to_roman(1111) == 'V'
 
     def test6(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.6."""
         assert arabic_to_roman(1888) == 'IX'
 
     def test7(self):
-        """test správnosti přiřazeného čísla"""
+        """Test správnosti č.7."""
         assert arabic_to_roman(2205) == 'X'
-
+        
         
 def arabic_to_roman(number):
-    """def"""
+    """Def."""
     arabic = [1000, 900, 500, 400, 100,
               90, 50, 40, 10, 9, 5, 4, 1]
 
@@ -56,7 +57,7 @@ def arabic_to_roman(number):
 
 
 def main():
-    """def"""
+    """Def."""
     r_number = int(input("Vložte číslo, které chcete konvertovat: "))
     print("Číslo přeložené do římského systému: " + arabic_to_roman(r_number))
 
