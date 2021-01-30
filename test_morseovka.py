e820d9fe2ed809f1052de4436f82bc365e691dd5
miroslavolivk1 <74539789+miroslavolivk1@users.noreply.github.com>
2021-01-30 12:47:40 +0100

    Update and rename string_test.py to test_morseovka.py

---
+++ test_morseovka.py
@@ -0,0 +1,33 @@
+"""Unittest pro prekladac morseovky."""
+
+import unittest
+import morseovka
+
+
+class TestMorseovka(unittest.TestCase):
+    """Tato trida funguje jako testovaci trida."""
+    
+    def text_4(self)
+        """Testovani prekladu kodu do textu."""
+        self.asserEqual(morseovka.Code_To_Text("... --- ... ", "sos")
+
+    def test_3(self):
+        """Testovani prekladu do morseovky."""
+        self.assertEqual(morseovka.Text_To_Code("sos"), "... --- ... ")
+      
+    def test_1(self):
+        """Testovani zvetsovani pismen."""
+        self.asserEqual("abc".upper(), "ABC")
+        self.assertTrue("ABC".isupper())
+        self.assertFalse("Abc".isupper())
+
+    def test_2(self):
+        """Prevod na rozlozeni textu."""
+        s = "miroslav olivik"
+        self.assertEqual(s.split(), ["miroslav", "olivik"])
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == "main":
+    unittest.main()
