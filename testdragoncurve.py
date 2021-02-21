b14de6ba103c2b00aa08bf97c423a7e7d51f2803
JanLusnak <73744820+JanLusnak@users.noreply.github.com>
2021-02-22 00:36:54 +0100

    Update testdragoncurve.py
    Co-authored-by: Tomáš Tureček <42441627+turecekt@users.noreply.github.com>

--- testdragoncurve.py
+++ testdragoncurve.py
@@ -1,15 +1,19 @@
+"""File documentation."""
+
 from dragonCurve1 import functioncycle, functionprinttantsrlrl
 
-
 def test_cycle():
+    """Function documentation."""
     assert functioncycle(2) == "rrl"
     assert functioncycle(3) == "rrlrrll"
     assert functioncycle(4) == "rrlrrllrrrllrll"
     assert functioncycle(5) == "rrlrrllrrrllrllrrrlrrlllrrllrll"
 
 def test_print():
+    """Function documentation."""
     assert functionprinttantsrlrl("y", "10") == "10"
     assert functionprinttantsrlrl("n", "10") == ""
 
 if __name__ == '__main__':
     test_cycle()
+    test_print()
