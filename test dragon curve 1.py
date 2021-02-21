1dde8f2a3ba4473e44d96e2cf605b88fe02b430e
JanLusnak <73744820+JanLusnak@users.noreply.github.com>
2021-02-22 00:35:39 +0100

    Add files via upload

---
+++ test dragon curve 1.py
@@ -0,0 +1,17 @@
+from dragonCurve1 import functioncycle, functionprinttantsrlrl
+
+
+def test_cycle():
+    """test of cycle"""
+    assert functioncycle(2) == "rrl"
+    assert functioncycle(3) == "rrlrrll"
+    assert functioncycle(4) == "rrlrrllrrrllrll"
+    assert functioncycle(5) == "rrlrrllrrrllrllrrrlrrlllrrllrll"
+
+def test_print():
+    """test of rl form"""
+    assert functionprinttantsrlrl("y", "10") == "10"
+    assert functionprinttantsrlrl("n", "10") == ""
+
+if __name__ == '__main__':
+    test_cycle()
