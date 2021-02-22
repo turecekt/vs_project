c6cd636dd7c229c8d65ad427752e5c483caa6635
JanLusnak <73744820+JanLusnak@users.noreply.github.com>
2021-02-22 01:29:13 +0100

    Update test dragon curve 1.py

--- test dragon curve 1.py
+++ test dragon curve 1.py
@@ -3,14 +3,14 @@
 from dragonCurve1 import functioncycle, functionprinttantsrlrl
 
 def test_cycle():
-    """test of cycle"""
+    """Test of cycle"""
     assert functioncycle(2) == "rrl"
     assert functioncycle(3) == "rrlrrll"
     assert functioncycle(4) == "rrlrrllrrrllrll"
     assert functioncycle(5) == "rrlrrllrrrllrllrrrlrrlllrrllrll"
 
 def test_print():
-    """test of rl form"""
+    """Test of Lsystem form"""
     assert functionprinttantsrlrl("y", "10") == "10"
     assert functionprinttantsrlrl("n", "10") == ""
 
