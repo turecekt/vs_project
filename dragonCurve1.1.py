9256ff83d395428bd6c43867e3f5bd130af1eca4
JanLusnak <73744820+JanLusnak@users.noreply.github.com>
2021-02-22 21:38:23 +0100

    Update dragonCurve1.1.py

--- dragonCurve1.1.py
+++ dragonCurve1.1.py
@@ -7,7 +7,7 @@ import turtle
 
 
 def functioncycle(iteration):
-      """Functions that is used for printing iterations"""
+    """Functions that is used for printing iterations"""
 
     r = 'r'  # make variables for the right and left containg 'r' and 'l'
     l = 'l'
