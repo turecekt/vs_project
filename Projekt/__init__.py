5b864338a3ca5f050e06b6c68514a9a8e6d894bd
Jerosh0 <jeronym.holer@gmail.comm>
2021-11-12 10:07:25 +0100

    ošetření vstupu

--- Projekt/__init__.py
+++ Projekt/__init__.py
@@ -19,17 +19,21 @@ morseovka = { 'A':'.-', 'B':'-...',
  
 # Funkce zakoduje do morseovky
 def zakodovat(zprava):
-    kod = ''
-    for pismeno in zprava:
-        if pismeno != ' ':
+    try:
+        zprava = zprava.upper()
+        kod = ''
+        for pismeno in zprava:
+            if pismeno != ' ':
  
-            # Najde ve slovniku odpovidajici kod pro jednotlive znaky
-            kod = kod + morseovka[pismeno] + ' '
-        else:
-            # 1 mezera jsou ruzne znaky, 2 mezery ruzna slova
-            kod += ' '
+                # Najde ve slovniku odpovidajici kod pro jednotlive znaky
+                kod = kod + morseovka[pismeno] + '/'
+            else:
+                # 1 / jsou ruzne znaky, 2 / ruzna slova
+                kod += '/'
  
-    return kod
- 
-# Funkce dekoduje z morseovky
-def dekodovat(zprava):
+        return kod
+    # Pøi nedefinovaném vstupu se vypíše chybová hláška
+    except KeyError: 
+        print("Znak", pismeno, "není definovaný v morseovì abecedì")
+        print("Definované znaky jsou: A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 , . ? / - ( )")
+       
