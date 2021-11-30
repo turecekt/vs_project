cf115448329c6ec9424021fe13b417ee633af035
Lukáš Ondřej <l1_ondrej@utb.cz>
2021-11-30 13:07:23 +0100

    Okomentování základu

--- morse.py
+++ morse.py
@@ -1,29 +1,42 @@
-DIR = {'A': '.-', 'B': '-...',
-       'C': '-.-.', 'D': '-..', 'E': '.',
-       'F': '..-.', 'G': '--.', 'H': '....',
-       'I': '..', 'J': '.---', 'K': '-.-',
-       'L': '.-..', 'M': '--', 'N': '-.',
-       'O': '---', 'P': '.--.', 'Q': '--.-',
-       'R': '.-.', 'S': '...', 'T': '-',
-       'U': '..-', 'V': '...-', 'W': '.--',
-       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+# Slovník pro překlad
+DIR = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
+       'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+       'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+       'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+       'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+       'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+       'Y': '-.--', 'Z': '--..',
        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
        '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
 
 
-def morseovka(cislo):
-
+# Překlad do morseovky
+def morseovka(retezec):
     morse = []
-    for i in cislo:
+    # přeložené znaky se ukládají do pole morse
+
+    for i in retezec:
         if i in DIR:
             morse.append(DIR[i])
+
+            # vložený znak je nalezen ve slovníku a vložen na konec pole morse
+
     return " ".join(morse)
+    # přidání mezery na konec přeloženého znaku
 
 
+# Hlavní funkce - input
 def main():
     vstup = input("Zadejte text: ")
     preklad = morseovka(vstup.upper())
+
+    """ 
+    proměnné preklad je přidělen výsledek funkce morseovka
+    vstup.upper() = vložené písmeno je převedené na velký znak
+    """
+
     print(preklad)
+    # Výstup přeloženého vstupu
 
 
 if __name__ == "__main__":
