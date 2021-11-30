a43e6e4a1991c0ed12b46d33109583e0a7dcc2fe
Your Name <you@example.com>
2021-11-30 19:25:22 +0100

    Přidání slovníku

--- example/Morseovka.py
+++ example/Morseovka.py
@@ -1,19 +1,17 @@
-"""This is the "example" module.
 
-The example module supplies one function: compute(). For example,
+dict = {'A': '.-',       'B': '-...',
+        'C': '-.-.',     'D': '-..',      'E': '.',
+        'F': '..-.',     'G': '--.',      'H': '....',
+        'I': '..',       'J': '.---',     'K': '-.-',
+        'L': '.-..',     'M': '--',       'N': '-.',
+        'O': '---',      'P': '.--.',     'Q': '--.-',
+        'R': '.-.',      'S': '...',      'T': '-',
+        'U': '..-',      'V': '...-',     'W': '.--',
+        'X': '-..-',     'Y': '-.--',     'Z': '--..',
+        '1': '.----',    '2': '..---',    '3': '...--',
+        '4': '....-',    '5': '.....',    '6': '-....',
+        '7': '--...',    '8': '---..',    '9': '----.',
+        '0': '-----',    ',': '--..--',   '.': '.-.-.-',
+        '?': '..--..',   '/': '-..-.',    '-': '-....-',
+        '(': '-.--.',    ')': '-.--.-'}
 
->>> compute(3)
-3
-"""
-
-
-def compute(x):
-    """Functon compute returns evaluation of expression using argument x.
-
-    Args:
-        - x - Input of the function
-
-    Returns:
-        - output - Output of the function
-    """
-    return x * x - 2 * x
