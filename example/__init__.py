24561821c3ecefe3a8ac781faaddbfb294cdf87f
petervacho <p_vacho@utb.cz>
2021-11-30 15:54:28 +0100

    Pridanie quick-sort algoritmu

--- example/__init__.py
+++ example/__init__.py
@@ -41,7 +41,68 @@ def bubble_sort(cisla):
     return cisla
 
 
-cisla = [2,7,1,23,6]
-zoradene = bubble_sort(cisla)
+def _partition(arr, start, end):
+    """
+    Podporna funkcia pre quick_sort.
+
+    V tejto funkcii zoradujeme dany list na 2 skupiny:
+    1. Cisla vatsie ako pivot
+    2. Cisla mensie ako pivot
+    - Pivot je cislo vybrane zo sekvencie, v tomto pripade
+    vyberame posledny prvok.
+
+    Funkcia prejde cez elementy listu v danom intervale a
+    presunie ich pred alebo za pivot index.
+
+    Vraciame index tohoto pivotu, a list zoradujeme na mieste (in-place).
+    """
+    # Nastavenie pivotu na posledny element
+    # Pivot je hodnota na zaklade ktorej budeme porovnavat
+    # ostatne elementy
+    pivot_hodnota = arr[end]
+    pivot_index = start
+
+    # Prejdeme cez start-end elementy a vymenime dany element
+    # s elementom na pivot_indexe vzdy ked je hodnota mensia
+    # ako hodnota pivotu po vymene zvysime pivot_index o 1
+    for i in range(start, end):
+        if arr[i] < pivot_hodnota:
+            arr[i], arr[pivot_index] = arr[pivot_index], arr[i]
+            pivot_index += 1
+    # Posledna vymena aby sme dostali pivot do stredu
+    arr[end], arr[pivot_index] = arr[pivot_index], arr[end]
+    return pivot_index
+
+
+def quick_sort(arr, start, end):
+    """
+    Tato funkcia vyuziva quick sort algoritmus na zoradenie daneho listu cisiel.
+
+    Na vstupe je zadany list ktory chceme zoradit. Radenie sa deje in-place (
+    nevytvarame kopiu!). Quick sort ma komplexitu O(n*log n).
+    """
+    # Ak sa dostaneme na koniec, sekcia je zoradena
+    if start >= end:
+        return
+
+    # Zoradenie prvkov k pivotu (strednej hodnote), index je
+    # pozicia tohoto pivotu
+    index = _partition(arr, start, end)
+
+    # Zoradenie hodnot v 2 zostavajucich sekciach (pred a za
+    # strednym indexom pivotu)
+    quick_sort(arr, start, index - 1)
+    quick_sort(arr, index + 1, end)
+
+
+cisla = [2, 7, 1, 23, 6]
+
+# bubble sort vyrvara novy zoradeny list
+bubble_sort_zoradene = bubble_sort(cisla)
+# quick sort funguje in-place.
+quick_sort_zoradenie = cisla.copy()
+quick_sort(quick_sort_zoradenie, 0, len(quick_sort_zoradenie))
+
 print(minimax(cisla))
-print(zoradene)
+print(bubble_sort_zoradene)
+print(quick_sort_zoradenie)
