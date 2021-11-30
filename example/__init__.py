ca2fd2db60709f3a90b6e00ad67ee7057ccfe6f4
petervacho <p_vacho@utb.cz>
2021-11-30 10:24:38 +0100

    Pridanie docstringu a uprava stylu (pep8/flake8)

--- example/__init__.py
+++ example/__init__.py
@@ -1,10 +1,26 @@
 def minimax(cislo):
-  minimum = cislo[0]
-  maximum = cislo[0]
-  for num in cislo:
-    if num < minimum:
-        minimum = num 
-    if num > maximum:
-        maximum = num
-  return minimum,maximum
-print(minimax([1,2,3,-1]))
+    """
+    Funkcia pre najdenie maxima a minima zadanej sekvencie cisiel.
+
+    Funkcia vracia 2 hodnoty: minimum, maximum (v poradi)
+    >>> minimax([1, 2, 3, -1])
+    (-1, 3)
+
+    Implementacia hladania minima a maxima nevyuziva built-in funkcie
+    min(x)/max(x), pretoze to pravdepodobne nebolo cielom tohoto zadania.
+    Bezne by sme ale chceli vyuzit tieto funkcie, namiesto implementovania
+    niecoho co jazyk samotny uz podporuje.
+    """
+    minimum = cislo[0]
+    maximum = cislo[0]
+
+    for num in cislo:
+        if num < minimum:
+            minimum = num
+        if num > maximum:
+            maximum = num
+
+    return minimum, maximum
+
+
+print(minimax([1, 2, 3, -1]))
