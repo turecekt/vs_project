910c5dc2e44d4802217a942886f645bc03c6ede0
Lukáš Ondřej <l1_ondrej@utb.cz>
2021-12-05 15:36:58 +0100

    Přidání funkce k překladu z morseovky

--- morse.py
+++ morse.py
@@ -1,23 +1,45 @@
-# Slovník pro překlad
-DIR = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
-       'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-       'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
-       'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
-       'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-       'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-       'Y': '-.--', 'Z': '--..',
-       '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
-       '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
+"""Program pro překlad do morseovy abecedy a zpět."""
+
+# Slovníky pro překlad
+DIR_DoMorseovky = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
+                   'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+                   'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                   'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                   'Y': '-.--', 'Z': '--..',
+                   '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+                   '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+                   '.': '.-.-.-', ', ': '--..--', '-': '-....-'}
+
+DIR_ZMorseovky = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
+                  '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
+                  '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+                  '--': 'M', '-.': 'N', '---': 'N', '.--.': 'P',
+                  '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+                  '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+                  '-.--': 'Y', '--..': 'Z',
+                  '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+                  '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9',
+                  '.-.-.-': '.', '--..--': ', ', '-....-': '-'}
 
 
 # Překlad do morseovky
-def morseovka(retezec):
+def to_morse(retezec):
+
+    """Funkce to_morse vrací vstupní řetězec převedený do morseovky.
+        Argumenty:
+            - retezec - Vstup funkce
+        Vrací:
+            - string - Výstup funkce
+        """
+
     morse = []
     # přeložené znaky se ukládají do pole morse
 
     for i in retezec:
-        if i in DIR:
-            morse.append(DIR[i])
+        if i in DIR_DoMorseovky:
+            morse.append(DIR_DoMorseovky[i])
 
             # vložený znak je nalezen ve slovníku a vložen na konec pole morse
 
@@ -25,20 +47,52 @@ def morseovka(retezec):
     # přidání mezery na konec přeloženého znaku
 
 
-# Hlavní funkce - input
+def from_morse(retezec):
+
+    """Funkce from_morse vrací vstupní řetězec převedený z morseovky.
+            Argumenty:
+                - retezec - Vstup funkce
+            Vrací:
+                - string - Výstup funkce
+            """
+
+    morse = retezec.split()
+    # vstup se rozdělí na jednotlivé znaky které následně hledá ve slovníku
+    preklad = []
+    # přeložené znaky se ukládají do pole preklad
+
+    for i in morse:
+        if i in DIR_ZMorseovky:
+            preklad.append(DIR_ZMorseovky[i])
+
+            # vložený znak je nalezen ve slovníku a vložen na konec pole morse
+
+    return "".join(preklad)
+
+
+# Hlavní funkce
 def main():
-    vstup = input("Zadejte text: ")
-    preklad = morseovka(vstup.upper())
+    operace = int(input("Vyberte si překlad:\n"
+                        "1 - Do morseovky\n"
+                        "2 - Z morseovky\n"))
 
-    """ 
-    proměnné preklad je přidělen výsledek funkce morseovka
-    vstup.upper() = vložené písmeno je převedené na velký znak
-    """
+    # Dle vybrané operace se následně provede překlad
 
-    print(preklad)
-    # Výstup přeloženého vstupu
+    if operace == 1:
+        vstup = input("Zadejte text k překladu do morseovky: ")
+        preklad = to_morse(vstup.upper())
+        print(preklad)
+
+    elif operace == 2:
+        vstup = input("Zadejte text k překladu z morseovky: ")
+        preklad = from_morse(vstup)
+        print(preklad)
+
+    elif operace != 1 & operace != 2:
+        print("Špatně zadaná operace")
+
+    # V případě špatného výběru operace program končí
 
 
 if __name__ == "__main__":
     main()
-
