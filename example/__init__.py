60470ebfd58b4830161c64bac2173d57f3d011da
hymat <matyashybl@gmail.com>
2021-12-06 23:19:41 +0100

    Kostra programu a user input

--- example/__init__.py
+++ example/__init__.py
@@ -1,19 +1,59 @@
-"""This is the "example" module.
+"""Výpočet vlastností trojuhelníka."""
 
-The example module supplies one function: compute(). For example,
+"""Zaznamenat vstup z konzole"""
+print("Zadejte souřadnice trojuhelníku\n")
+A = (input("bod A - souřadnice x: "), input("bod A - souřadnice y: "))
+B = (input("bod B - souřadnice x: "), input("bod B - souřadnice y: "))
+C = (input("bod C - souřadnice x: "), input("bod C - souřadnice y: "))
+Triangle = [A, B, C]
+print("\nZadali jste Trojuhelník se souřadnicemi:")
+print("A: ", A)
+print("B: ", B)
+print("C: ", C)
 
->>> compute(3)
-3
-"""
 
+def is_valid(Triangle):
+    """Funkce is_valid vyhodnotí je-li daný trojuhelník sestrojitelný.
 
-def compute(x):
-    """Functon compute returns evaluation of expression using argument x.
+    Jinak řečeno, zdali všechny body neleží na stejné přímce.
 
-    Args:
-        - x - Input of the function
-
-    Returns:
-        - output - Output of the function
+    >>> Triangle = [(0,0), (3,0), (0,4)]
+    >>> is_valid(Triangle)
+    True
+    >>> Triangle = [(1,0), (1,7), (1,4)]
+    >>> is_valid(Triangle)
+    False
     """
-    return x * x - 2 * x
+    return True
+
+
+def side_lenghts(Triangle):
+    """Funkce side_lengths vrací list délek stran trojuhelníku.
+
+    >>> Triangle = [(0,0), (3,0), (0,4)]
+    >>> side_lenghts(Triangle)
+    [5,4,3]
+    """
+    sides = [1, 1, 1]
+    return sides
+
+
+def circumference(Triangle):
+    """Funkce circumference vrací obvod trojuhelníku.
+
+    >>> Triangle = [(0,0), (3,0), (0,4)]
+    >>> circumference(Triangle)
+    12
+    """
+    obvod = 5
+    return obvod
+
+
+def is_right(Triangle):
+    """Funkce is_right vyhodnotí je-li daný trojuhelník pravoúhlý.
+
+    >>> Triangle = [(0,0), (3,0), (0,4)]
+    >>> is_right(Triangle)
+    True
+    """
+    return False
