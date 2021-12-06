5ff84137fac47d1b326028ef4c19e019977fcb78
Petr Nosek <petrkes@gmail.com>
2021-12-06 21:13:33 +0100

    Oprava flake8 test_morseovka.py

--- test_morseovka.py
+++ test_morseovka.py
@@ -1,16 +1,27 @@
-from main import * 
+"""Test morse."""
+
+from main import encodovani, decodovani
+
 
 def test_encodovani_slovo():
+    """Test encodovani."""
     assert encodovani("AHOJ") == ".- .... --- .---"
     assert encodovani("LEON") == ".-.. . --- -."
 
+
 def test_encodovani_veta():
-    assert encodovani("MAM RAD PALACINKY") == "-- .- --  .-. .- -..  .--. .- .-.. .- -.-. .. -. -.- -.--"
+    """Test encodovani."""
+    assert encodovani("MAM RAD PALACINKY") == "-- .- --  .-. .- -..  .--." \
+                                              " .- .-.. .- -.-. .. -. -.- -.--"
+
 
 def test_decodovani_znaky():
+    """Test decodovani."""
     assert decodovani(".-") == "A"
     assert decodovani("-...") == "B"
     assert decodovani("-.-.") == "C"
 
+
 def test_decodovani_slovo():
+    """Test decodovani."""
     assert decodovani(".- .... --- .---") == "AHOJ"
