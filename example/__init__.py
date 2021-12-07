0a718346ce9e613727d6eb140dc4c0531a8aaed8
hymat <matyashybl@gmail.com>
2021-12-07 11:57:03 +0100

    odstranění doctestů, ošetření vstupu uživatele

--- example/__init__.py
+++ example/__init__.py
@@ -1,59 +1,54 @@
 """Výpočet vlastností trojuhelníka."""
+import math
 
-"""Zaznamenat vstup z konzole"""
-print("Zadejte souřadnice trojuhelníku\n")
-A = (0, 0)
-B = (0, 0)
-C = (0, 0)
-A = (input("bod A - souřadnice x: "), input("bod A - souřadnice y: "))
-B = (input("bod B - souřadnice x: "), input("bod B - souřadnice y: "))
-C = (input("bod C - souřadnice x: "), input("bod C - souřadnice y: "))
-Triangle = [A, B, C]
-print("\nZadali jste Trojuhelník se souřadnicemi:")
-print("A: ", A)
-print("B: ", B)
-print("C: ", C)
+
+def ask(prompt, default):
+    """Funkce pro zachycení souřadnic zadaných uživatele."""
+    vstup = input(prompt)
+    try:
+        val = int(vstup)
+    except ValueError:
+        print("Špatně zadaná hodnota.")
+        print("Použije se defaultní hodnota - ", default)
+        val = default
+    return val
 
 
 def is_valid(Triangle):
-    """Funkce is_valid vyhodnotí je-li daný trojuhelník sestrojitelný.
-
-    Jinak řečeno, zdali všechny body neleží na stejné přímce.
-
-    >>> Triangle = [(1,0), (1,7), (1,4)]
-    >>> is_valid(Triangle)
-    False
-    """
+    """Funkce is_valid vyhodnotí je-li daný trojuhelník sestrojitelný."""
     return True
 
 
 def side_lengths(Triangle):
-    """Funkce side_lengths vrací list délek stran trojuhelníku.
-
-    >>> Triangle = [(0,0), (3,0), (0,4)]
-    >>> side_lengths(Triangle)
-    [5,4,3]
-    """
-    sides = [1, 1, 1]
+    """Funkce side_lengths vrací list délek stran trojuhelníku."""
+    A = Triangle[0]
+    B = Triangle[1]
+    C = Triangle[2]
+    a = abs(math.sqrt((B[0] - C[0])**2+(B[1] - C[1])**2))
+    b = abs(math.sqrt((C[0] - A[0])**2+(C[1] - A[1])**2))
+    c = abs(math.sqrt((A[0] - B[0])**2+(A[1] - B[1])**2))
+    sides = [a, b, c]
     return sides
 
 
 def circumference(Triangle):
-    """Funkce circumference vrací obvod trojuhelníku.
-
-    >>> Triangle = [(0,0), (3,0), (0,4)]
-    >>> circumference(Triangle)
-    12
-    """
+    """Funkce circumference vrací obvod trojuhelníku."""
     obvod = 5
     return obvod
 
 
 def is_right(Triangle):
-    """Funkce is_right vyhodnotí je-li daný trojuhelník pravoúhlý.
-
-    >>> Triangle = [(0,0), (3,0), (0,4)]
-    >>> is_right(Triangle)
-    True
-    """
+    """Funkce is_right vyhodnotí je-li daný trojuhelník pravoúhlý."""
     return False
+
+
+"""Zaznamenat vstup z konzole"""
+print("Zadejte souřadnice trojuhelníku\n")
+A = (ask("bod A - souřadnice x: ", 0), ask("bod A - souřadnice y: ", 0))
+B = (ask("bod B - souřadnice x: ", 3), ask("bod B - souřadnice y: ", 0))
+C = (ask("bod C - souřadnice x: ", 0), ask("bod C - souřadnice y: ", 4))
+Triangle = [A, B, C]
+print("\nZadali jste Trojuhelník se souřadnicemi:")
+print("A: ", A)
+print("B: ", B)
+print("C: ", C)
