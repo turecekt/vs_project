e156f4877fdd97821684e4cf3386b14727716368
Teleriann <Teleriann9@gmail.com>
2021-12-08 12:17:28 +0100

    Pridanie unit testu na funkciu bubble_sort

--- test_minimax/test_utils.py
+++ test_minimax/test_utils.py
@@ -3,7 +3,7 @@ from minimax import utils
 
 
 class TestUtils(unittest.TestCase):
-   def test_minimax(self):
+    def test_minimax(self):
         """Otestovanie správnosti funkcie minimax."""
         vysledky = (
             ([1, 8, 0], (0, 2, 8, 1)),
@@ -16,4 +16,14 @@ class TestUtils(unittest.TestCase):
 
         for list_cisel, ocakavany_vysledok in vysledky:
                 obdrzany_vysledok = utils.minimax(list_cisel)
+                self.assertEqual(ocakavany_vysledok, obdrzany_vysledok)
+
+    def test_bubble_sort(self):
+        """Otestovanie správnosti funkcie bubblesort."""
+        vysledky = (
+            ([1, 8, 4], [1, 4, 8]),
+        )
+
+        for list_cisel, ocakavany_vysledok in vysledky:
+                obdrzany_vysledok = utils.bubble_sort(list_cisel)
                 self.assertEqual(ocakavany_vysledok, obdrzany_vysledok)
