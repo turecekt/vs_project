d731122ba1b2273c4e864bfa4780d73623d297a0
Extreme69 <matolehmden@gmail.com>
2021-12-08 16:28:10 +0100

    Flake8 formátovanie pre test___main__.py

--- test_minimax/test___main__.py
+++ test_minimax/test___main__.py
@@ -6,27 +6,29 @@ from minimax import utils
 
 class TestMain(unittest.TestCase):
     def test_parse_args(self):
-        with self.subTest(msg="Overenie fungovania parse_args pre nahodne cisla (bez argumentu)"):
+        with self.subTest(msg="Overenie fungovania parse_args pre nahodne cisla (bez arg)"):
             with unittest.mock.patch("minimax.__main__.sys.argv", ["subor"]):
                 with unittest.mock.patch("minimax.__main__.random.randint", return_value=1):
                     vysledne_cisla = main.parse_args()
-                    # dostaneme 20 nahodnych cisiel, kedze ale randint vzdy vrati 1, dostaneme 20 jednotiek
+                    # dostaneme 20 nahodnych cisiel,
+                    # kedze ale randint vzdy vrati 1, dostaneme 20 jednotiek
                     self.assertEqual(vysledne_cisla, [1] * 20)
 
-        with self.subTest(msg="Overenie fungovania parse_args pre zadane cisla (cisla v argumentoch)"):
-            ocakavane_cisla = [1, 2, 3]
+        with self.subTest(msg="Overenie fungovania parse_args pre zadane cisla (cisla v arg)"):
             with unittest.mock.patch("minimax.__main__.sys.argv", ["subor", "1", "2", "3"]):
                 vysledne_cisla = main.parse_args()
                 # Dostaneme 1, 2, 3 ako bolo zadane v argumentoch
                 self.assertEqual(vysledne_cisla, [1, 2, 3])
 
-        with self.subTest(msg="Overenie fungovania parse_args pre subor (argument je cesta k suboru)."):
-            with unittest.mock.patch("minimax.__main__.open", unittest.mock.mock_open(read_data="1 2 3")):
+        with self.subTest(msg="Overenie fungovania parse_args pre subor (arg je cesta k suboru)."):
+            with unittest.mock.patch(
+                "minimax.__main__.open", unittest.mock.mock_open(read_data="1 2 3")
+            ):
                 with unittest.mock.patch("minimax.__main__.sys.argv", ["subor", "subor.txt"]):
                     vysledne_cisla = main.parse_args()
                     # Dostaneme 1, 2, 3 z obsahu suboru
                     self.assertEqual(vysledne_cisla, [1, 2, 3])
-    
+
     def test_vyber_algoritmus(self):
         """Otestovanie správnosti funkcie vyber_algoritmus."""
         vysledky = (
