4a8d3d81f3d1b0b7555a6609452b9393aeaa0e73
Robin-Dostal <dostal.robi@gmail.com>
2021-12-09 22:10:47 +0100

    Upradte draciKrivka - funkcni + zakomentovane

--- draciKrivka/draciKrivka/draciKrivka.py
+++ draciKrivka/draciKrivka/draciKrivka.py
@@ -1,47 +1,62 @@
 
-# import the turtle module to use turtle graphics
+# Naimportovani turtle modulu pro pouziti grafiky
 import turtle
-  
-iterace = int(input('Zadejte pocet iteraci: '))
 
+# vyzve uzivatele pro zadani poctu iteraci  
+iterace = input('Zadejte pocet iteraci: ')
+
+# vyzve uzivatele pro zadani barvy pera
 barvaPera = input('Zadejte barvu pera: ')
 
-barvaPozadi = input('Zadejte barvu pozdi: ')
+# vyzve uzivatele pro zadani barvy pozadi
+barvaPozadi = input('Zadejte barvu pozadi: ')
 
-def Krivka(iterace, delka, uhel):
-     if iterace == 0:
-        turtle.forward(delka)
+# funkce pro vykresleni draci krivky
+def Krivka(val, delka, uhel):               # nazev funkce a vstupni parametry
+     if val == 0:
+        turtle.forward(delka)               # vykresli caru
      else:
-         Krivka(iterace - 1, delka, 90)
+         Krivka(val - 1, delka, 90)         # otoceni o 90 stupnu mezi carami
          turtle.left(uhel)
-         Krivka(iterace - 1, delka, -90)
+         Krivka(val - 1, delka, -90)        # otoceni o 90 stupnu mezi carami
      
        
-
+# nastaveni prostredi
 turtle.Screen().clear()
 turtle.speed(0 )
-turtle.pensize(2)
+turtle.pensize(1)
 
+# pocatecni bod vykreslovani
 turtle.penup()
 turtle.goto(100, 100)
 turtle.pendown()
 
+
+#zachyceni vstupu pro iterace
+try:
+    val = int(iterace)              # ulozeni hodnoty, kterou zada uzivatel
+    if val < 0:                     # pokud je zadane cislo mensi jak 0 nastavi se vychozi hodnota iteraci na 9
+        val = 9
+except ValueError:                  # pokud hodnota neni cislo nastavi vychozi hodnotu iteraci na 9
+    val = 9
+
 if barvaPera:
-    turtle.color(barvaPera)
+    turtle.color(barvaPera)         # pokud uzivatel necha pole prazdne, nastavi se vychozi barva cervena
 else:
     turtle.color('red')
-
+# podminka pro barvu pozadi
 if barvaPozadi:
     turtle.bgcolor(barvaPozadi)
 else:
-    turtle.bgcolor('black')
+    turtle.bgcolor('black')         # pokud uzivatel necha pole prazdne, nastavi se vychozi barva cerna
     
 
+# preddefinovane parametry
+delka = 2                           # delka cary, ktera se vykresli
+uhel = 90                           # uhel mezi rekurzemi
 
-delka = 5
-uhel = 90
 
-
-Krivka(iterace, delka, uhel)
+# zavolani funkce Krivka
+Krivka(val, delka, uhel)
 
 turtle.done()
