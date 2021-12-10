801f5258e896be58055b3259899072b8f4780d12
jkadlecikova <91805211+jkadlecikova@users.noreply.github.com>
2021-12-10 18:56:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def min_max(numbers):
     max_n = f"Max number:{max(numbers)}, Index:{numbers.index(max(numbers))}"
 
     return f"{min_n}\n{max_n}"
+
+def bubblesort(list):
+    """Funkce na serazeni listu pomoci bubblesortu."""
+    for i in range(len(list)):
+        for n in range(len(list) - 1):
+            if list[n] > list[n + 1]:
+                list[n], list[n + 1] = list[n + 1], list[n]
+    return list
+
