afb9b0d470bffc7788fd5415eb713cfff1a3dacd
janecek.vojta <janecek.vojta@gmail.com>
2021-12-10 21:22:14 +0100

    created selectionsort algorithm

--- main.py
+++ main.py
@@ -9,11 +9,24 @@ def min_max(numbers):
 
     return f"{min_n}\n{max_n}"
 
+
 def bubblesort(list):
     """Funkce na serazeni listu pomoci bubblesortu."""
     for i in range(len(list)):
         for n in range(len(list) - 1):
             if list[n] > list[n + 1]:
                 list[n], list[n + 1] = list[n + 1], list[n]
+    return list
+
+
+def selectionsort(list):
+    """Funkce na serazeni listu pomoci selectionsortu."""
+    for i in range(len(list) - 1):
+        minimum = i
+        for j in range(i + 1, len(list)):
+            if(list[j] < list[minimum]):
+                minimum = j
+        if(minimum != i):
+            list[i], list[minimum] = list[minimum], list[i]
     return list
 
