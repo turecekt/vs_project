9678ccde71ebb829d61086ff919e896311d8e98e
jkadlecikova <91805211+jkadlecikova@users.noreply.github.com>
2021-12-13 18:15:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,18 @@ def selectionsort(list):
             list[i], list[minimum] = list[minimum], list[i]
     return list
 
+
+def shellsort(list):
+    """Funkce na serazeni listu pomoci shellsortu."""
+    n = len(list)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = list[i]
+            j = i
+            while j >= gap and list[j-gap] > temp:
+                list[j] = list[j-gap]
+                j = j - gap
+            list[j] = temp
+        gap = gap // 2
+    return list
