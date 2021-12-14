857e71843e02946c22b263cdc6912ea15c61bcab
Josef Bouchal <josef-bouchal@seznam.cz>
2021-12-14 20:40:42 +0100

    rozsireni unit testu

--- unittest_morseovka.py
+++ unittest_morseovka.py
@@ -7,6 +7,12 @@ class unittest_morseovka(unittest.TestCase):
 		self.assertEqual(morseovka.encrypt("T"), "- ")
 	def test_encrypt_veta(self):
 		self.assertEqual(morseovka.encrypt("TESTOVACI VETA"), "- . ... - --- ...- .- -.-. ..  ...- . - .- ")
-
+	def test_encrypt_cislo(self):
+		self.assertEqual(morseovka.encrypt("1234567890"), ".---- ..--- ...-- ....- ..... -.... --... ---.. ----. ----- ")
+	def test_decrypt_pismeno(self):
+		self.assertEqual(morseovka.decrypt("-..- "), "X")
+	def test_decrypt_pismeno(self):
+		self.assertEqual(morseovka.decrypt(".--. .-. . ...- --- -..  -. .-  ... .-.. --- ...- ---  --..  -.- --- -.. ..-"), "PREVOD NA SLOVO Z KODU")
+	
 if __name__ == "main":
 	unittest.main()
