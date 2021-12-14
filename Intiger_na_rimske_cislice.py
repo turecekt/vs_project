50649cc4bdf26274a6d569f6a24965306747fa0f
adamsims1 <35937506+adamsims1@users.noreply.github.com>
2021-12-14 09:51:04 +0100

    Update Intiger_na_rimske_cislice.py

--- Intiger_na_rimske_cislice.py
+++ Intiger_na_rimske_cislice.py
@@ -44,6 +44,75 @@ class TestMethods(unittest.TestCase):
 
     def test12(self):
         assert integerToRoman(12) == 'XII'
+        
+    def test13(self):
+        assert integerToRoman(13) == 'XIII'
+
+    def test14(self):
+        assert integerToRoman(14) == 'XIV'
+
+    def test15(self):
+        assert integerToRoman(15) == 'XV'
+        
+    def test16(self):
+        assert integerToRoman(16) == 'XVI'
+
+    def test17(self):
+        assert integerToRoman(17) == 'XVII'
+
+    def test18(self):
+        assert integerToRoman(18) == 'XVIII'
+        
+    def test19(self):
+        assert integerToRoman(19) == 'XIX'
+
+    def test20(self):
+        assert integerToRoman(20) == 'XX'
+
+    def test21(self):
+        assert integerToRoman(21) == 'XXI'
+        
+    def test22(self):
+        assert integerToRoman(22) == 'XXII'
+
+    def test23(self):
+        assert integerToRoman(23) == 'XXIII'
+
+    def test24(self):
+        assert integerToRoman(24) == 'XXIV'
+    
+    def test25(self):
+        assert integerToRoman(25) == 'XXV'
+
+    def test26(self):
+        assert integerToRoman(26) == 'XXVI'
+        
+    def test27(self):
+        assert integerToRoman(27) == 'XXVII'
+
+    def test28(self):
+        assert integerToRoman(28) == 'XXVIII'
+
+    def test29(self):
+        assert integerToRoman(29) == 'XXIX'
+        
+    def test30(self):
+        assert integerToRoman(30) == 'XXX'
+
+    def test31(self):
+        assert integerToRoman(31) == 'XXXI'
+
+    def test32(self):
+        assert integerToRoman(32) == 'XXXII'
+        
+    def test33(self):
+        assert integerToRoman(33) == 'XXXIII'
+
+    def test34(self):
+        assert integerToRoman(34) == 'XXXIV'
+
+    def test35(self):
+        assert integerToRoman(35) == 'XXXV'
 
 def integerToRoman(A):
     def test_integer_to_roman(self):
