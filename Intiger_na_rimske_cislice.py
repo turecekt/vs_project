8ab1e03aa86b5eeb2a6bdf167edc9821635ff620
adamsims1 <35937506+adamsims1@users.noreply.github.com>
2021-12-14 19:27:27 +0100

    Update Intiger_na_rimske_cislice.py

--- Intiger_na_rimske_cislice.py
+++ Intiger_na_rimske_cislice.py
@@ -25,16 +25,28 @@ class Testy(unittest.TestCase):
         assert num2roman(10) == 'X'
 
 
-num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+num_map = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I'),
+    ]
 
 
 def num2roman(num):
-
     roman = ''
 
     while num > 0:
-        for i, r in num_map:
+        for (i, r) in num_map:
             while num >= i:
                 roman += r
                 num -= i
