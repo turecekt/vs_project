6aced468fe6e4b31341d5e8935c785768d002093
adamsims1 <35937506+adamsims1@users.noreply.github.com>
2021-12-14 19:33:53 +0100

    Update Intiger_na_rimske_cislice.py

--- Intiger_na_rimske_cislice.py
+++ Intiger_na_rimske_cislice.py
@@ -1,3 +1,12 @@
+# coding=utf-8
+
+"""
+This is an example script.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
+
 import unittest
 
 
