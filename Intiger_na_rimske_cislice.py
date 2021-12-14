068a2685b2debf3e6418dc02a1c853eb9eaf75a2
adamsims1 <35937506+adamsims1@users.noreply.github.com>
2021-12-14 19:49:45 +0100

    Update Intiger_na_rimske_cislice.py

--- Intiger_na_rimske_cislice.py
+++ Intiger_na_rimske_cislice.py
@@ -1,14 +1,37 @@
-# coding=utf-8
-
-"""
-This is an example script.
-
-It seems that it has to have THIS docstring with a summary line, a blank line
-and sume more text like here. Wow.
-"""
-
-
 import unittest
+
+num_map = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I'),
+    ]
+
+
+def num2roman(num):
+    """cislo na rim"""
+    roman = ''
+
+    while num > 0:
+        for (i, r) in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+                print(roman)
+
+    return roman
+
+
+num2roman(5)
 
 
 class Testy(unittest.TestCase):
@@ -33,36 +56,3 @@ class Testy(unittest.TestCase):
 
     def test7(self):
         assert num2roman(10) == 'X'
-
-
-num_map = [
-    (1000, 'M'),
-    (900, 'CM'),
-    (500, 'D'),
-    (400, 'CD'),
-    (100, 'C'),
-    (90, 'XC'),
-    (50, 'L'),
-    (40, 'XL'),
-    (10, 'X'),
-    (9, 'IX'),
-    (5, 'V'),
-    (4, 'IV'),
-    (1, 'I'),
-    ]
-
-
-def num2roman(num):
-    roman = ''
-
-    while num > 0:
-        for (i, r) in num_map:
-            while num >= i:
-                roman += r
-                num -= i
-                print(roman)
-
-    return roman
-
-
-num2roman(5)
