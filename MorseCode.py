ad54d2dd2ff3489b1a6894ed0541511c11ec8fcf
Libor Kavka <kavka@utb.cz>
2021-12-15 20:58:12 +0100

    Pridanie decryptu

--- MorseCode.py
+++ MorseCode.py
@@ -1,10 +1,17 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 
 # Dictionary representing the morse code chart
 import pytest
 
-MORSE_CODE_DICT = { '1':'.----', '2':'..---', '3':'...--',
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
                     '4':'....-', '5':'.....', '6':'-....',
                     '7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-',
@@ -32,6 +39,44 @@ def encrypt(message):
  
 # Function to decrypt the string
 # from morse to english
+def decrypt(message):
+ 
+    # extra space added at the end to access the
+    # last morse code
+    message += ' '
+ 
+    decipher = ''
+    citext = ''
+    for letter in message:
+ 
+        # checks for space
+        if (letter != ' '):
+ 
+            # counter to keep track of space
+            i = 0
+ 
+            # storing morse code of a single character
+            citext += letter
+ 
+        # in case of space
+        else:
+            # if i = 1 that indicates a new character
+            i += 1
+ 
+            # if i = 2 that indicates a new word
+            if i == 2 :
+ 
+                 # adding space to separate words
+                decipher += ' '
+            else:
+ 
+                # accessing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                .values()).index(citext)]
+                citext = ''
+ 
+    return decipher
+ 
 # Hard-coded driver function to run the program
 def main():
     message = input("Zadaj nieco: ")
@@ -57,10 +102,3 @@ def test_answer3():
    
 
     
-[DEBUG ON]
-[DEBUG OFF]
-encrytp("123")
-Traceback (most recent call last):
-  File "<pyshell#0>", line 1, in <module>
-    encrytp("123")
-NameError: name 'encrytp' is not defined
