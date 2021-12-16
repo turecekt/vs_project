eefd7d554c433de31bafdd3d289ceb26cd06c748
j-srubar <96244405+j-srubar@users.noreply.github.com>
2021-12-16 16:05:02 +0100

    fix more issues raised by github flake8

--- test_morse.py
+++ test_morse.py
@@ -1,44 +1,45 @@
 # 16.12.2021
-""" Unit testy """
+"""Unit testy."""
 
 import unittest
 from morse import frommorse, tomorse
 
 
 class RunMorseTest(unittest.TestCase):
+    """Unittest Class."""
 
     def test_tomorse01(self):
-        """ Test Codovani jednoducheho textu. """
+        """Test Codovani jednoducheho textu."""
         src = "hello world"
         dst = "...././.-../.-../---/,/.--/---/.-./.-../-.."
         self.assertEqual(tomorse(src), dst)
 
     def test_tomorse02(self):
-        """ Test Codovani chybnych znaku. """
+        """Test Codovani chybnych znaku."""
         src = "I@I"
         dst = "../{@}/.."
         self.assertEqual(tomorse(src), dst)
 
     def test_tomorse03(self):
-        """ Test Codovani prazdneho vstupu. """
+        """Test Codovani prazdneho vstupu."""
         src = ""
         dst = ""
         self.assertEqual(tomorse(src), dst)
 
     def test_frommorse01(self):
-        """ Test Decodovani jednoducheho textu. """
+        """Test Decodovani jednoducheho textu."""
         src = "...././.-../.-../---/,/.--/---/.-./.-../-.."
         dst = "hello world".upper()
         self.assertEqual(frommorse(src), dst)
 
     def test_frommorse02(self):
-        """ Test Decodovani chybnych znaku. """
+        """Test Decodovani chybnych znaku."""
         src = "../---./.."
         dst = "I{---.}I"
         self.assertEqual(frommorse(src), dst)
 
     def test_frommorse03(self):
-        """ Test Decodovani chybnych lomitek. """
+        """Test Decodovani chybnych lomitek."""
         dst = "SOS"
         # vice lomitek mezi znaky
         src = "...//---///..."
@@ -48,19 +49,19 @@ class RunMorseTest(unittest.TestCase):
         self.assertEqual(frommorse(src), dst)
 
     def test_frommorse04(self):
-        """ Test Decodovani prazdneho vstupu. """
+        """Test Decodovani prazdneho vstupu."""
         src = ""
         dst = ""
         self.assertEqual(frommorse(src), dst)
 
     def test_frommorse05(self):
-        """ Test Decodovani vstupu se znaky ' ' a '\n'. """
+        r"""Test Decodovani vstupu se znaky ' ' a '\n'."""
         src = " ... ./. /\n.-../.-../---/, /.--/---/.-./.\n-../-..\n"
         dst = "hello world".upper()
         self.assertEqual(frommorse(src), dst)
 
     def test_final(self):
-        """ Test Codovani a Decodovani vstupu. """
+        """Test Codovani a Decodovani vstupu."""
         src = "There is no elevator to success, you have to take the stairs."
         dst = src.upper()
         self.assertEqual(frommorse(tomorse(src)), dst)
