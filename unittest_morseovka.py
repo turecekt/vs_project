82732bb59d717e661f8a94a93b8f3fb108b5a576
Josef Bouchal <josef-bouchal@seznam.cz>
2021-12-20 20:45:18 +0100

    úprava kódu pomocí flake (potřetí)

--- unittest_morseovka.py
+++ unittest_morseovka.py
@@ -1,37 +1,37 @@
 """Unit test pro morseovku."""
 import unittest
-import morseovka
+from morseovka import encrypt, decrypt
 
 
-class unittest_morseovka(unittest.TestCase):
-	"""Trida pro spusteni unit testu."""
+class Unittest_morseovka(unittest.TestCase):
+    """Trida pro spusteni unit testu."""
 
-	def test_encrypt_pismeno(self):
-		"""Test prevodu pismene na kod."""
-		self.assertEqual(morseovka.encrypt("T"), "- ")
+    def test_encrypt_pismeno(self):
+        """Test prevodu pismene na kod."""
+        self.assertEqual(encrypt("T"), "- ")
 
-	def test_encrypt_veta(self):
-		"""Test prevodu vety na kod."""
-		self.assertEqual(morseovka.encrypt(
-			"TESTOVACI VETA"), "- . ... - --- ...- .- -.-. ..  ...- . - .- ")
+    def test_encrypt_veta(self):
+        """Test prevodu vety na kod."""
+        self.assertEqual(encrypt(
+            "TESTOVACI VETA"), "- . ... - --- ...- .- -.-. ..  ...- . - .- ")
 
-	def test_encrypt_cislo(self):
-		"""Test prevodu cisla na kod."""
-		self.assertEqual(morseovka.encrypt(
-			"1234567890"), ".---- ..--- ...-- ....- ....."
-			" -.... --... ---.. ----. ----- ")
+    def test_encrypt_cislo(self):
+        """Test prevodu cisla na kod."""
+        self.assertEqual(encrypt(
+            "1234567890"), ".---- ..--- ...-- ....- ....."
+            " -.... --... ---.. ----. ----- ")
 
-	def test_decrypt_pismeno(self):
-		"""Test prevodu kodu na pismeno."""
-		self.assertEqual(morseovka.decrypt("-..-"), "X")
+    def test_decrypt_pismeno(self):
+        """Test prevodu kodu na pismeno."""
+        self.assertEqual(decrypt("-..-"), "X")
 
-	def test_decrypt_text(self):
-		"""Test prevodu kodu na text."""
-		self.assertEqual(morseovka.decrypt(
-			".--. .-. . ...- --- -..  -. .-  ... .-.."
-			" --- ...- ---  --..  -.- --- -.. ..-"),
-			"PREVOD NA SLOVO Z KODU")
+    def test_decrypt_text(self):
+        """Test prevodu kodu na text."""
+        self.assertEqual(decrypt(
+            ".--. .-. . ...- --- -..  -. .-  ... .-.."
+            " --- ...- ---  --..  -.- --- -.. ..-"),
+            "PREVOD NA SLOVO Z KODU")
 
 
-if __name__ == "main":
-	unittest.main()
+if __name__ == '__main__':
+    unittest.main()
