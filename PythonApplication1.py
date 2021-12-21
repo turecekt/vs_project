b573e44b2f3aebad18704a484a6905097c42c910
VHoracek <96260480+VHoracek@users.noreply.github.com>
2021-12-21 19:16:36 +0100

    Novy kod

---
+++ PythonApplication1.py
@@ -0,0 +1,42 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    message = message.upper()
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            cipher += ' '
+ 
+    return cipher.strip()
+ 
+
+
+def main():
+    message = "Test"
+    result = encrypt(message.upper())
+    print (result)
+    print (message)
+ 
+
+if __name__ == '__main__':
+    main()
+    
+
+def test_answers ():
+    assert encrypt (" ")==" "
