41daca456061e8b2d0f7aeda5565025dca425132
Radek Kratochvil <r1_kratochvil@utb.cz>
2021-12-27 18:23:52 +0100

    Doplneni freq.Min/Max

--- Project/CetnostZnaku.py
+++ Project/CetnostZnaku.py
@@ -1,39 +1,75 @@
+"""################POTOM SMAZAT
+PyUnit
+Kód musí obsahovat unit testy (pokrytí kódu testy by se mělo blížit 100%)
+Zdrojový kód musí projít kontrolním testem na githubu v sekci Actions (je nutné povolit). 
+Tzn. musí projít všechny unit testy a flake8
+Kód musí být okomentovaný (ideálně všechny entity)
+"""
+
+
 """ cetnost znaku project
 by Radek Kratochvíl and Petr Slavík
 """
 
 #####
+#   Testovaci text vse hodit na UPPERCASE
+testStr = "Testovaci text ktery je zakonceny #"
 #
 #   • Textový soubor (obsahující text bez diakritiky) jako parametr programu
-#
+#       vstup = ".\TopicPicker.txt"
+#   nacist vstup 
+#    pokud neni vstup tak vyzadat text od uzivatele zakonceny #
+#def defVstup():
+#    [1] vlozit cestu
+#    [2] vlozit text
+#   
 ######
-vstup = ".\TopicPicker.txt"
+
 
 #def FileInput(vstup):
 #    return open(vstup)
 
-
-
-vstupFile = open("TopicPicker.txt","r")
  #####################Informace o celkovém počtu znaků#####################
-data = vstupFile.read().replace(" ","")
-num_of_char = len(data)
-print('Number of characters in text file without spaces:',num_of_char)
+def PocetZnak(vstup):
+    vstupFile = open(vstup,"r")
+    data = vstupFile.read().replace(" ","")
+    pocet_char = len(data)
+    print('Pocet znaku v celem txt souboru bez mezer :',pocet_char)
 
+PocetZnak('.\TopicPicker.txt')
 
 #####################Informace o nejčastějším znaku#####################
 
+def MaxZnak():
+    print ("The original string is : " + testStr)
+    all_freqMax = {}
+    for i in testStr:
+        if i in all_freqMax:
+         all_freqMax[i] += 1
+        else:
+            all_freqMax[i] = 1
+    res = max(all_freqMax, key = all_freqMax.get)
+    print ("The maximum of all characters in text is : " + str(res)) 
+
+MaxZnak()
 
 
 
  #####################Informace o nejméně častém znaku#####################
+def MinZnak():
+    print ("The original string is : " + testStr)
+    all_freqMin = {}
+    for i in testStr:
+        if i in all_freqMin:
+         all_freqMin[i] += 1
+        else:
+            all_freqMin[i] = 1
+    res = min(all_freqMin, key = all_freqMin.get)
+    print ("The minimum of all characters in text is : " + str(res)) 
+   
+MinZnak()
 
-
- #####################Informace o průměrné četnosti#####################
- 
- 
- 
- 
+#####################Informace o průměrné četnosti#####################
  
  
  
@@ -41,19 +77,16 @@ print('Number of characters in text file without spaces:',num_of_char)
  #####################Informace o četnosti jednotlivých znaků abecedy (bez diakritiky)#####################
 
 
-#print(vstupFile.read())
-#characters=0
-#for line in vstupFile:
-#    line = line.strip("\#")
-#
-#    words= line.split()
-#    count_words += len(words)
-#    count_char += len(line)
-#
-#    vstupFile.close()
-#
-#    print("nm_char:",count_char)
-#
+
+
+
+#####Unit testy
+
+#Neni zakonceno na #
+#Neni soubor
+
+
+
 
 
 #####
