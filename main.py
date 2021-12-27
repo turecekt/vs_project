0e2c92aea5661146531836eaad1971fb3e1af218
janecek.vojta <janecek.vojta@gmail.com>
2021-12-27 10:56:45 +0100

    typo fix

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def min_max(numbers):
     """Funkce na zjisteni nejmensiho a nejvetsiho cisla a jeho indexu."""
     min_n = f"Min number:{min(numbers)}, Index:{numbers.index(min(numbers))}"
     max_n = f"Max number:{max(numbers)}, Index:{numbers.index(max(numbers))}"
-
     return f"{min_n}\n{max_n}"
 
 
@@ -70,6 +69,7 @@ def main():
             for i in range(1, len(sys.argv)):
                 numbers.append(sys.argv[i])
     return numbers
+
 
 def run():
     """Script ktery spousti program"""
