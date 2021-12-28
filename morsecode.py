3f00e16966ea27596f842d410d0a418e071878af
mlmlynar <mlmlynar@gmail.com>
2021-12-28 13:03:21 +0100

    Pridanie funkcie prevodu z morseovho kodu na text

--- morsecode.py
+++ morsecode.py
@@ -24,17 +24,49 @@ def to_morse(vstup):
         else:
             morse_code += pismeno.lower() + " "
 
-        return morse_code  
+        return morse_code
+    
+def from_morse(vstup):
+
+    vstup += ' '
+ 
+    text = ''
+    bvstup = ''
+    for pismeno in vstup:
+ 
+        if (pismeno != ' '):
+ 
+            i = 0
+ 
+            bvstup += pismeno
+ 
+        else:
+            i += 1
+ 
+            if i == 2 :
+ 
+                text += ' '
+            else:
+ 
+                text += list(abeceda.keys())[list(abeceda
+                .values()).index(bvstup)]
+                bvstup = ''
+ 
+    return text
 
 def main():
 
     #vstupní formulár pro zadáni textu, kt. má být zakódován do morseovho kódu
     text = input("Zadej text který chceš přenést do morseovky : ")
 
-    out = to_morse(text)
+    out_morse = to_morse(text)
         
     # Výstup překladače    
-    print(out)
+    print(out_morse)
+
+    out_text = from_morse(out_morse)
+
+    print(out_text)
 
 import unittest   # Importování Unit testu
 
