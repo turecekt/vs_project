d9ce990f16f38c175a55a7e9e91dddc3168a7d16
r1kolarik <94187745+r1kolarik@users.noreply.github.com>
2021-12-29 18:01:30 +0100

    Fix test 3

--- morsecode.py
+++ morsecode.py
@@ -1,21 +1,26 @@
-#Překladač textu do morseova kodu a dekóder morseova kodu do textu
+"""
 
-#Funkce pro definovani abecedy
+Překladač textu do morseova kodu a dekóder morseova kodu do textu.
+Kod obsahuje i výběrové menu pro překlad na text nebo morseovku.
+
+"""
+
+# Funkce pro definovani abecedy
 def abeceda():
 
     # Abeceda pro překlad textu do morseovho kodu
-    abeceda = {"a":".-","b":"-...","c":"-.-.","d":"-..","e":".","f":"..-.","g":"--.","h":"....","i":"..","j":".---",
-               "k":"-.-","l":".---","m":"--","n":"-.","o":"---","p":".--.","q":"--.-","r":".-.","s":"...","t":"-",
-               "u":"..-","v":"...-","w":".--","x":"-..-","y":"-.--","z":"--..","1":".----","2":"..---", "3":"...--",
-               "4":"....-","5":".....","6":"-....","7":"--...","8":"---..", "9":"----.","0":"-----",",":"--..--",
-               ".":".-.-.-","?":"..--..","/":"-..-.","-":"-....-","(":"-.--.",")":"-.--.-",":":"---...",";":"-.-.-.",
-               "+":".-.-.","=":"-...-"}
+    abeceda = {"a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---", 
+               "k": "-.-", "l": ".---", "m": "--", "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", 
+               "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--..", "1": ".----", "2": "..---", "3": "...--", 
+               "4": "....-", "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.", "0": "-----", ",": "--..--", 
+               ".": ".-.-.-", "?": "..--..", "/": "-..-.", "-": "-....-", "(": "-.--.", ")": "-.--.-", ":": "---...", ";": "-.-.-.", 
+               "+": ".-.-.", "=": "-...-"}
                
     return abeceda
 
 abeceda = abeceda()
 
-#Funkce pro překlad textu do morseovho kodu
+# Funkce pro překlad textu do morseovho kodu
 def to_morse(vstup):
     morse_code = ""
     for pismeno in vstup:
@@ -25,7 +30,8 @@ def to_morse(vstup):
             morse_code += pismeno.lower() + " "
 
     return morse_code
-    
+
+# Fuknce pro překlad morseovky na text    
 def from_morse(vstup):
 
     vstup += ' '
@@ -48,12 +54,12 @@ def from_morse(vstup):
                 text += ' '
             else:
  
-                text += list(abeceda.keys())[list(abeceda
-                .values()).index(bvstup)]
+                text += list(abeceda.keys())[list(abeceda.values()).index(bvstup)]
                 bvstup = ''
  
     return text
 
+# Menu pro překlad textu na morseovku nebo morseovky na text
 def main():
 
     menu = input("Vyber si :\n\n 1. Chcem zakódovať text do morzeovky (Zadaj číslo 1).\n 2. Chcem dekódovať text z morzeovky (Zadaj číslo 2).\n 3. Ukončiť program (Zadaj ľubovolný znak alebo čísla od 3 do 9)\n\n  - Zadajte svoju voľbu : ")
@@ -66,9 +72,11 @@ def main():
     else:
         vypis = "\n --- Ukončili ste program --- "
     print(vypis)
-
     
-import unittest   # Importování Unit testu
+    
+
+# Importování Unit testu    
+import unittest   
 
 if __name__ == '__main__':
     main()
@@ -77,11 +85,11 @@ if __name__ == '__main__':
    
 
 def test_to():
-    #Test překladu textu do morseovky
+    # Test překladu textu do morseovky
     assert(to_morse("ahoj")) == ".- .... --- .--- "
 
 
 def test_from():
-    #Test překladu morseovky na text
+    # Test překladu morseovky na text
     assert(from_morse("--.. -.. .- .-. . -.-.")) == "zdarec"
     
