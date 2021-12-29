c083107fc8bea52c6199ef39c7e8bea22fe8da75
ftomsu <95984516+ftomsu@users.noreply.github.com>
2021-12-29 22:59:31 +0100

    Update main.py
    Co-authored-by: Tomáš Tureček <42441627+turecekt@users.noreply.github.com>

--- main.py
+++ main.py
@@ -26,25 +26,21 @@ def sest(x):
     return hex(x)[2:].upper()
 
 
-print('Prekladac z desitkove soustavy'
-      '\nVyber si do jake soustavy chces prekladat: ')
-print('1) Dvojkova\n2) Osmickova\n3) Sestnactkova')
-# vyber = int(input('Vyber: '))  # zakomentovano z duvodu selhani testu
-# x = int(input('Zadej cislo: '))  # zakomentovano z duvodu selhani testu
-vyber = 1  # Zaznamena vyber z nabidky
-x = 150  # Zazamena int input do x
+if __name__ == '__main__':
+    print('Prekladac z desitkove soustavy'
+          '\nVyber si do jake soustavy chces prekladat: ')
+    print('1) Dvojkova\n2) Osmickova\n3) Sestnactkova')
+    vyber = int(input('Vyber: '))
+    x = int(input('Zadej cislo: '))
 
-if vyber == 1 and x >= 0:  # Rozhoduje podle vstupu vyber
-    dva(x)  # Provede fuknci dva(x)
-    print('Prevod do 2 soustavy je: ', dva(x))  # Vytiskne vysledek
-elif vyber == 2 and x >= 0:
-    osm(x)  # Provede funkci osm(x)
-    print('Prevod do 8 soustavy je: ', osm(x))
-elif vyber == 3 and x >= 0:
-    sest(x)  # Provede se funkce sest(x)
-    print('Prevod do 16 soustavy je: ', sest(x))
-else:  # Pokud neni cislo = 1, 2 nebo 3
-    print('Tato moznost neexistuje')
+    if vyber == 1 and x >= 0:  # Rozhoduje podle vstupu vyber
+        print('Prevod do 2 soustavy je: ', dva(x))
+    elif vyber == 2 and x >= 0:
+        print('Prevod do 8 soustavy je: ', osm(x))
+    elif vyber == 3 and x >= 0:
+        print('Prevod do 16 soustavy je: ', sest(x))
+    else:  # Pokud neni cislo = 1, 2 nebo 3
+        print('Tato moznost neexistuje')
 
 
 def test_dva():
