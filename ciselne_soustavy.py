c01f16141e01b38c8e903df52bae39727ff536a3
HonzinKost <95584638+HonzinKost@users.noreply.github.com>
2021-12-29 10:42:09 +0100

    Přidán DIGITS a fce Preved

--- ciselne_soustavy.py
+++ ciselne_soustavy.py
@@ -1,5 +1,8 @@
 import sys
 
+# vycet cislic pro soustavy se zakladem 2 az po cislice se zakladem 32
+DIGITS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def main():
 
 def zadejCislo():
@@ -32,3 +35,24 @@ def zadejSoustava():
 			print('Spatny vstup. Zadejte cilovou ciselnou soustavu jako cele kladne cislo.')
 		else:
 			print('Spatny vstup. Zadejte cilovou ciselnou soustavu v rozsahu intervalu (2..32)')
+			
+def preved(cislo: int, cilova_ciselna_soustava: int) -> str:
+	'''
+	Metoda preved prevede <cislo> z desitkove soustavy do ciselne soustavy se zakladem <cilova_ciselna_soustava>
+	'''
+
+	# cislo se bude nejprve ukladat do pole znaku
+	prevedene = []
+
+	prevedene.append(DIGITS[cislo % cilova_ciselna_soustava])
+	cislo //= cilova_ciselna_soustava
+
+	while cislo:
+		prevedene.append(DIGITS[cislo % cilova_ciselna_soustava])
+		cislo //= cilova_ciselna_soustava
+
+	# pole znaku se prevede na string ve spravnem (opacnem nez bylo vkladane do pole) poradi
+	vysledek = ''
+	for s in prevedene[::-1]:
+		vysledek += s
+	return vysledek
