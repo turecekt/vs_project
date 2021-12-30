f4984a440a0ae553a587e4ff19e851e283776b9e
Jaroslav Lev <j_lev@utb.cz>
2021-12-30 14:51:52 +0100

    dalsi upravy k testum

--- hlavniFunkce.py
+++ hlavniFunkce.py
@@ -6,48 +6,27 @@ a vraci vysledek na konzoli.
 """
 
 
-# import time
-
-# PRINT_TIMINGS = False
-
-
 def isPrime(n):
     """Hlavni overovaci funkce."""
-    """start = time.time()
-
-    def printTime(start):
-        if PRINT_TIMINGS:
-            print("\rFinished in %f seconds" % (time.time() - start))
-
-    def printProgress(i):
-        if PRINT_TIMINGS:
-            print("\r%f%%" % (i*i/n*100), end="")
-"""
     # vylouceni zapornych cisel, nuly a jednicky
     if n <= 1:
-#        printTime(start)
         return False
 
     # potvrzeni cisel 2, 3
     if n <= 3:
-#        printTime(start)
         return True
 
     # vylouceni sudych cisel a cisel delitelnych 3
     if n % 2 == 0 or n % 3 == 0:
-#        printTime(start)
         return False
 
     # Vsechna prvocisla jsou ve tvaru 6n +/-1
     # Use P = 6k +/- 1 to allow iterating by 6
     i = 5
     while i * i <= n:
-#        printProgress(i)
 
         if n % i == 0 or n % (i + 2) == 0:
-#            printTime(start)
             return False
         i += 6
 
-#    printTime(start)
     return True
--- test_isPrime.py
+++ test_isPrime.py
@@ -1,35 +1,22 @@
 """Unit testy pro program IsPrime."""
-from .isPrime import isPrime
+
+
+from hlavniFunkce import isPrime
 import unittest
 
 
-class PrimesTestCase(unittest.TestCase):
-    """Funkce overujici prvocisla."""
+def test_should_be_prime():
+    """Test s vysledkem JE prvocislo."""
+    assert isPrime(3) == True
+    assert isPrime(7) == True
 
-    def test_should_be_prime(self):
-        """Test s vysledkem JE prvocislo."""
-        self.assertEqual(True, isPrime(2))
-        self.assertEqual(True, isPrime(3))
-        self.assertEqual(True, isPrime(17))
-        self.assertEqual(True, isPrime(33391))
-        self.assertEqual(True, isPrime(199933))
 
-    def test_should_not_be_prime(self):
-        """Test s vysledkem NENI prvocislo."""
-        self.assertEqual(False, isPrime(-13))
-        self.assertEqual(False, isPrime(-2))
-        self.assertEqual(False, isPrime(-1))
-        self.assertEqual(False, isPrime(0))
-        self.assertEqual(False, isPrime(1))
-        self.assertEqual(False, isPrime(4))
-        self.assertEqual(False, isPrime(16))
-        self.assertEqual(False, isPrime(30))
-        self.assertEqual(False, isPrime(35))
-        self.assertEqual(False, isPrime(81))
-        self.assertEqual(False, isPrime(88))
-        self.assertEqual(False, isPrime(121))
-        self.assertEqual(False, isPrime
-                         (1999331999331999331999331999331999331999331999331999335))
+def test_should_not_be_prime():
+    """Test s vysledkem NENI prvocislo."""
+    assert isPrime(-11) == False
+    assert isPrime(0) == False
+    assert isPrime(12) == False
+
 
 if __name__ == '__main__':
     unittest.main()
