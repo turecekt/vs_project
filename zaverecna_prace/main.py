8e3312faf36b576e62a027cfe688f54cb7428fbf
Filip Chromý <f_chromy@utb.cz>
2021-12-31 13:37:39 +0100

    Committing default program body

---
+++ zaverecna_prace/main.py
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------
+# REAKCNI RYCHLOSTI
+# Program se postupně dotazuje uživatele na výsledek pěti pseudonáhodně generovaných
+# jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž měří reakční dobu a
+# vyhodnocuje správnost.
+# VSTUP
+# • Reakce uživatele na příklady v podobě zadaných výsledků
+# VÝSTUP
+# • Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
+# • Informace o průměrné reakční době.
+# Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
+# ------------------------------------------------------------
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
