6406b955019f349f55fa404be1d367b0f8a8aa2e
Filip Chromý <f_chromy@utb.cz>
2021-12-31 16:19:02 +0100

    Flake8 Comments fix

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-    # ------------------------------------------------------------
-    # REAKCNI RYCHLOSTI
-    # Program se postupně dotazuje uživatele na výsledek pěti pseudonáhodně generovaných
-    # jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž měří reakční dobu a
-    # vyhodnocuje správnost.
-    # VSTUP
-    # • Reakce uživatele na příklady v podobě zadaných výsledků
-    # VÝSTUP
-    # • Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
-    # • Informace o průměrné reakční době.
-    # Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
-    # ------------------------------------------------------------
+  # ------------------------------------------------------------
+  # REAKCNI RYCHLOSTI
+  # Program se postupně dotazuje uživatele na výsledek pěti pseudonáhodně generovaných
+  # jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž měří reakční dobu a
+  # vyhodnocuje správnost.
+  # VSTUP
+  # • Reakce uživatele na příklady v podobě zadaných výsledků
+  # VÝSTUP
+  # • Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
+  # • Informace o průměrné reakční době.
+  # Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
+  # ------------------------------------------------------------
 
 import random
 import time
