e9807ef7d12822dd0668d2f458e9fd5f91496e08
Filip Chromý <f_chromy@utb.cz>
2021-12-31 16:11:43 +0100

    Flake8 oprava

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-#  ------------------------------------------------------------
-#  REAKCNI RYCHLOSTI
-#  Program se postupně dotazuje uživatele na výsledek pěti pseudonáhodně generovaných
-#  jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž měří reakční dobu a
-#  vyhodnocuje správnost.
-#  VSTUP
-#  • Reakce uživatele na příklady v podobě zadaných výsledků
-#  VÝSTUP
-#  • Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
-#  • Informace o průměrné reakční době.
-#  Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
-#  ------------------------------------------------------------
+  # ------------------------------------------------------------
+  # REAKCNI RYCHLOSTI
+  # Program se postupně dotazuje uživatele na výsledek pěti pseudonáhodně generovaných
+  # jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž měří reakční dobu a
+  # vyhodnocuje správnost.
+  # VSTUP
+  # • Reakce uživatele na příklady v podobě zadaných výsledků
+  # VÝSTUP
+  # • Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
+  # • Informace o průměrné reakční době.
+  # Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
+  # ------------------------------------------------------------
 
 import random
 import time
 
-RYCHLOSTI_REAKCI = [] #  Globální list pro uložení reakčních rychlostí
+RYCHLOSTI_REAKCI = []  # Globální list pro uložení reakčních rychlostí
 POCET_SPRAVNYCH_ODPOVEDI = 0
 NAHODNY_VYRAZ = ""
 
 
 def vygeneruj_nahodny_vyraz():
     """Generuje náhodny matematicky vyraz."""
-    operatory = ["+", "-", "/", "*"] #  list operátorů
-    a = random.randint(-10, 10) #  náhodné b číslo v intervalu <-10,10>
-    b = random.randint(-10, 10) #  náhodné a číslo v intervalu <-10,10>
-    operator = operatory[random.randint(0,3)] #  výběr náhodného operátoru z listu operátorů
+    operatory = ["+", "-", "/", "*"]  # list operátorů
+    a = random.randint(-10, 10)  # náhodné b číslo v intervalu <-10,10>
+    b = random.randint(-10, 10)  # náhodné a číslo v intervalu <-10,10>
+    operator = operatory[random.randint(0,3)]  # výběr náhodného operátoru z listu operátorů
     return f"{a} {operator} {b}"
 
 
@@ -82,9 +82,9 @@ def secti(a, b):
 def vyhodnot_vstup_uzivatel():
     """Vyhodnotí vysledek nahodneho vyrazu a porovná jej se vstupem uživatele."""
     global NAHODNY_VYRAZ
-    a = NAHODNY_VYRAZ.split(" ")[0] #  přístup k elementu a ve výrazu
-    oper = NAHODNY_VYRAZ.split(" ")[1] #  přístup k operátoru ve výrazu
-    b = NAHODNY_VYRAZ.split(" ")[2] #  přístup k elementu b ve výrazu
+    a = NAHODNY_VYRAZ.split(" ")[0]  # přístup k elementu a ve výrazu
+    oper = NAHODNY_VYRAZ.split(" ")[1]  # přístup k operátoru ve výrazu
+    b = NAHODNY_VYRAZ.split(" ")[2]  # přístup k elementu b ve výrazu
     if oper == "+":
         vysledek = secti(int(a), int(b))
     elif oper == "-":
@@ -94,9 +94,9 @@ def vyhodnot_vstup_uzivatel():
     elif oper == "*":
         vysledek = vynasob(int(a), int(b))
     vysledek_uzivatele = vstup_uzivatele()
-    if vysledek_uzivatele == float("%.2f"%vysledek): #  kontrola správnosti výsledků zadaného uživatelem
+    if vysledek_uzivatele == float("%.2f"%vysledek):  # kontrola správnosti výsledků zadaného uživatelem
         global POCET_SPRAVNYCH_ODPOVEDI
-        POCET_SPRAVNYCH_ODPOVEDI = POCET_SPRAVNYCH_ODPOVEDI + 1 #  inkrementace počtu správných odpovědí
+        POCET_SPRAVNYCH_ODPOVEDI = POCET_SPRAVNYCH_ODPOVEDI + 1  # inkrementace počtu správných odpovědí
 
 
 def prumerna_reakcni_rychlost(reakcni_rychlosti):
@@ -117,8 +117,8 @@ def vytvor_test():
         end = time.time()
         RYCHLOSTI_REAKCI.append(end - start)
 
-    print(f"\nPocet spravnych odpovedi: {POCET_SPRAVNYCH_ODPOVEDI} z 5") #  Vytisteni poctu spravnych odpovedi
-    print(f"Prumerna reakcni rychlost: %.2fs" %prumerna_reakcni_rychlost(RYCHLOSTI_REAKCI)) #  Vytisteni prumerneho reakcni rychlosti
+    print(f"\nPocet spravnych odpovedi: {POCET_SPRAVNYCH_ODPOVEDI} z 5")  # Vytisteni poctu spravnych odpovedi
+    print(f"Prumerna reakcni rychlost: %.2fs" %prumerna_reakcni_rychlost(RYCHLOSTI_REAKCI))  # Vytisteni prumerneho reakcni rychlosti
     input("\nPro ukončení stiskněte ENTER")
 
 
@@ -129,7 +129,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-#  Testovací funkce
+  # Testovací funkce
 
 def test_secti():
     """testuje funkcni sčítání."""
