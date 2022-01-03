e9fec7c78c375c9e4e6aa018772b0fdea22771db
Amoniacik <kulasova.monika@gmail.com>
2022-01-03 17:33:50 +0100

    upravy v kode, unit testy a pridavanie oprav flake8

--- MinMax zadanie Susoliakova Milosicova/sorts.py
+++ MinMax zadanie Susoliakova Milosicova/sorts.py
@@ -23,48 +23,6 @@ def merge_sort(array, left_index, right_index):
     merge_sort(array, middle + 1, right_index)
     merge(array, left_index, right_index, middle)
 
-def merge(array, left_index, right_index, middle):
-    # Make copies of both arrays we're trying to merge
-
-    # The second parameter is non-inclusive, so we have to increase by 1
-    left_copy = array[left_index:middle + 1]
-    right_copy = array[middle+1:right_index+1]
-
-    # Initial values for variables that we use to keep
-    # track of where we are in each array
-    left_copy_index = 0
-    right_copy_index = 0
-    sorted_index = left_index
-
-    # Go through both copies until we run out of elements in one
-    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
-
-        # If our left_copy has the smaller element, put it in the sorted
-        # part and then move forward in left_copy (by increasing the pointer)
-        if left_copy[left_copy_index] <= right_copy[right_copy_index]:
-            array[sorted_index] = left_copy[left_copy_index]
-            left_copy_index = left_copy_index + 1
-        # Opposite from above
-        else:
-            array[sorted_index] = right_copy[right_copy_index]
-            right_copy_index = right_copy_index + 1
-
-        # Regardless of where we got our element from
-        # move forward in the sorted part
-        sorted_index = sorted_index + 1
-
-    # We ran out of elements either in left_copy or right_copy
-    # so we will go through the remaining elements and add them
-    while left_copy_index < len(left_copy):
-        array[sorted_index] = left_copy[left_copy_index]
-        left_copy_index = left_copy_index + 1
-        sorted_index = sorted_index + 1
-
-    while right_copy_index < len(right_copy):
-        array[sorted_index] = right_copy[right_copy_index]
-        right_copy_index = right_copy_index + 1
-        sorted_index = sorted_index + 1
-
 def insertion_sort(numbers):
      # We start from 1 since the first element is trivially sorted
     for index in range(1, len(numbers)):
@@ -123,3 +81,45 @@ def partition(array, start, end):
     array[start], array[high] = array[high], array[start]
 
     return high
+
+def merge(array, left_index, right_index, middle):
+    # Make copies of both arrays we're trying to merge
+
+    # The second parameter is non-inclusive, so we have to increase by 1
+    left_copy = array[left_index:middle + 1]
+    right_copy = array[middle+1:right_index+1]
+
+    # Initial values for variables that we use to keep
+    # track of where we are in each array
+    left_copy_index = 0
+    right_copy_index = 0
+    sorted_index = left_index
+
+    # Go through both copies until we run out of elements in one
+    while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
+
+        # If our left_copy has the smaller element, put it in the sorted
+        # part and then move forward in left_copy (by increasing the pointer)
+        if left_copy[left_copy_index] <= right_copy[right_copy_index]:
+            array[sorted_index] = left_copy[left_copy_index]
+            left_copy_index = left_copy_index + 1
+        # Opposite from above
+        else:
+            array[sorted_index] = right_copy[right_copy_index]
+            right_copy_index = right_copy_index + 1
+
+        # Regardless of where we got our element from
+        # move forward in the sorted part
+        sorted_index = sorted_index + 1
+
+    # We ran out of elements either in left_copy or right_copy
+    # so we will go through the remaining elements and add them
+    while left_copy_index < len(left_copy):
+        array[sorted_index] = left_copy[left_copy_index]
+        left_copy_index = left_copy_index + 1
+        sorted_index = sorted_index + 1
+
+    while right_copy_index < len(right_copy):
+        array[sorted_index] = right_copy[right_copy_index]
+        right_copy_index = right_copy_index + 1
+        sorted_index = sorted_index + 1
