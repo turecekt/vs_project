0a8de97b1d190577d649efa1741e441b9d38f0ae
FilipChromy <74801581+FilipChromy@users.noreply.github.com>
2022-01-04 12:49:05 +0100

    Update main.py
    Co-authored-by: Tomáš Tureček <42441627+turecekt@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-"""Změř průměrnou reakční rychlost odpovědí na nádhodné výrazy."""
-# ------------------------------------------------------------
-# REAKCNI RYCHLOSTI
-# Program se postupně dotazuje uživatele
-# na výsledek pěti pseudonáhodně generovaných
-# jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž
-# měří reakční dobu a
-# vyhodnocuje správnost.
-# VSTUP
-# • Reakce uživatele na příklady v podobě zadaných výsledků
-# VÝSTUP
-# • Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
-# • Informace o průměrné reakční době.
-# Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
-# ------------------------------------------------------------
+"""Změř průměrnou reakční rychlost odpovědí na nádhodné výrazy.
+
+REAKCNI RYCHLOSTI
+Program se postupně dotazuje uživatele
+na výsledek pěti pseudonáhodně generovaných
+jednoduchých výrazů a [+-*/] b ( a , b∈⟨−10,10⟩ ), přičemž
+měří reakční dobu a
+vyhodnocuje správnost.
+VSTUP
+• Reakce uživatele na příklady v podobě zadaných výsledků
+VÝSTUP
+• Informace o úspěšnosti (kolikrát bylo zodpovězeno správně),
+• Informace o průměrné reakční době.
+Autoři: Filip Chromý, Drahomíra Šťastná, Jan Fojtík, Tomáš Blabla
+"""
 
 import random
 import time
