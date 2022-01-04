b0df7a1c2ff5b3b746c0ef6259cfb5ff7fef560f
Lucie Šikudová <xsikudov@fi.muni.cz>
2022-01-04 11:45:57 +0100

    unit tests - sorting

--- minmax.py
+++ minmax.py
@@ -1,7 +1,74 @@
-from os import path
 import sys
-import numpy as np
+import unittest
+from os import path
 from typing import Tuple, List
+
+import numpy as np
+
+
+# -----------------------------------
+# This project contains three sorting algorithms,
+# pseudo-random generator and functions to find minimum and maximum in array.
+#
+# Author: Lucie Šikudová
+# -----------------------------------
+
+class TestInsertSort(unittest.TestCase):
+    def test_sorting_small_sorted(self):
+        self.assertEqual(insert_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_sorting_small_unsorted(self):
+        self.assertEqual(insert_sort([11, 15, 3, 29, 14, 39, 81, 45, 31]), [3, 11, 14, 15, 29, 31, 39, 45, 81])
+
+    def test_sorting_large_sorted(self):
+        self.assertEqual(insert_sort([1, 3, 8, 12, 21, 35, 39, 81, 113, 198, 265]),
+                         [1, 3, 8, 12, 21, 35, 39, 81, 113, 198, 265])
+
+    def test_sorting_large_unsorted(self):
+        self.assertEqual(insert_sort([212, 28, 58, 25, 183, 197, 199, 127, 119, 97, 143, 136, 175, 185]),
+                         [25, 28, 58, 97, 119, 127, 136, 143, 175, 183, 185, 197, 199, 212, 277])
+
+    def test_sorting_empty_array(self):
+        self.assertEqual(insert_sort([]), [])
+
+
+class TestSelectionSort(unittest.TestCase):
+    def test_sorting_small_sorted(self):
+        self.assertEqual(selection_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_sorting_small_unsorted(self):
+        self.assertEqual(selection_sort([11, 15, 3, 29, 14, 39, 81, 45, 31]), [3, 11, 14, 15, 29, 31, 39, 45, 81])
+
+    def test_sorting_large_sorted(self):
+        self.assertEqual(selection_sort([1, 3, 8, 12, 21, 35, 39, 81, 113, 198, 265]),
+                         [1, 3, 8, 12, 21, 35, 39, 81, 113, 198, 265])
+
+    def test_sorting_large_unsorted(self):
+        self.assertEqual(selection_sort([212, 28, 58, 25, 183, 197, 199, 127, 119, 97, 143, 136, 175, 185]),
+                         [25, 28, 58, 97, 119, 127, 136, 143, 175, 183, 185, 197, 199, 212, 277])
+
+    def test_sorting_empty_array(self):
+        self.assertEqual(selection_sort([]), [])
+
+
+class TestBubbleSort(unittest.TestCase):
+    def test_sorting_small_sorted(self):
+        self.assertEqual(bubble_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_sorting_small_unsorted(self):
+        self.assertEqual(bubble_sort([11, 15, 3, 29, 14, 39, 81, 45, 31]), [3, 11, 14, 15, 29, 31, 39, 45, 81])
+
+    def test_sorting_large_sorted(self):
+        self.assertEqual(bubble_sort([1, 3, 8, 12, 21, 35, 39, 81, 113, 198, 265]),
+                         [1, 3, 8, 12, 21, 35, 39, 81, 113, 198, 265])
+
+    def test_sorting_large_unsorted(self):
+        self.assertEqual(bubble_sort([212, 28, 58, 25, 183, 197, 199, 127, 119, 97, 143, 136, 175, 185]),
+                         [25, 28, 58, 97, 119, 127, 136, 143, 175, 183, 185, 197, 199, 212, 277])
+
+    def test_sorting_empty_array(self):
+        self.assertEqual(bubble_sort([]), [])
+
 
 """
 Implementation of insertion sort.
@@ -111,12 +178,13 @@ def input_process(file_or_array: bool, number_of_arg: int):
     # input is a txt file with integers
     else:
         print("the input is a txt file")
-        f = open(sys.argv[1], "r")
+        f = open(sys.argv[1], "r")  # open a file with "r" mode to read it
         array = []
-        data = f.readline()
-        data = data.split()
+        data = f.readline()  # read all integers
+        data = data.split()  # split integers into n integers
         for each in data:
-            array.append(each)
+            array.append(each)  # make an array from the txt file
+
     print(array)
     print(find_minimum(array))
     print(find_maximum(array))
