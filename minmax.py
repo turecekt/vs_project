3d310f4f248762548cce8559e4312ca0c32ba534
Lucie Šikudová <xsikudov@fi.muni.cz>
2022-01-04 10:48:59 +0100

    function comments

--- minmax.py
+++ minmax.py
@@ -3,6 +3,12 @@ import sys
 import numpy as np
 from typing import Tuple, List
 
+"""
+Implementation of insertion sort.
+A simple sorting algorithm, efficient for small data sets.
+Time complexity: O(n^2)
+"""
+
 
 def insert_sort(array: List[int]) -> None:
     for i in range(1, len(array)):
@@ -12,6 +18,13 @@ def insert_sort(array: List[int]) -> None:
             array[j + 1] = array[j]
             j -= 1
         array[j + 1] = current
+
+
+"""
+Implementation of sorting algorithm.
+A simple comparison sorting algorithm
+Time complexity: O(n^2)
+"""
 
 
 def selection_sort(array: List[int]) -> None:
@@ -24,11 +37,24 @@ def selection_sort(array: List[int]) -> None:
         array[i], array[min_index] = array[min_index], array[i]
 
 
+"""
+Implementation of bubble sort.
+A simple comparing sorting algorithm.
+This algorithm performs poorly in real world, it is used as an educational tool.
+Time complexity: O(n^2)
+"""
+
+
 def bubble_sort(array: List[int]) -> None:
     for i in range(len(array) - 1):
         for j in range(0, len(array) - i - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
+
+
+"""
+Implementation of function that finds the minimum in the array.
+"""
 
 
 def find_minimum(array: List[int]) -> Tuple[int, int]:
@@ -41,6 +67,11 @@ def find_minimum(array: List[int]) -> Tuple[int, int]:
     return min_elem, min_index
 
 
+"""
+Implementation of function that finds the maximum in the array.
+"""
+
+
 def find_maximum(array: List[int]) -> Tuple[int, int]:
     max_index = 0
     max_elem = array[0]
@@ -51,20 +82,33 @@ def find_maximum(array: List[int]) -> Tuple[int, int]:
     return max_elem, max_index
 
 
+"""
+Function that generates array of (pseudo)random integers.
+"""
+
+
 def pseudo_generator():
     rng = np.random.default_rng()
     return rng.integers(100000, size=20)
 
 
+"""
+Function that process the user input.
+"""
+
+
 def input_process(file_or_array: bool, number_of_arg: int):
+    # no input given, pseudo-random generator is used
     if number_of_arg - 1 == 0:
         print("the input is empty, just use pseudo generator")
         array = pseudo_generator()
+    # input is an array of integers
     elif file_or_array:
         print("the input is an array of numbers")
         array = []
         for each in sys.argv[1:]:
             array.append(int(each))
+    # input is a txt file with integers
     else:
         print("the input is a txt file")
         f = open(sys.argv[1], "r")
@@ -80,6 +124,11 @@ def input_process(file_or_array: bool, number_of_arg: int):
     # selection_sort(array)
     insert_sort(array)
     print(array)
+
+
+"""
+Function to report wrong input.
+"""
 
 
 def wrong_input(input):
