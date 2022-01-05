d1240fc581e99a693f948d45da8be43b97ce6ac1
Radec <app.kratochvil@gmail.com>
2022-01-06 00:30:20 +0100

    Uprava def

--- Project/CetnostZnaku.py
+++ Project/CetnostZnaku.py
@@ -9,44 +9,51 @@ Kód musí obsahovat unit testy (pokrytí kódu testy by se mělo blížit 100%)
 #python -m venv c:\path\to\myenv
 #""" cetnost znaku project
 #by Radek Kratochvíl and Petr Slavík
-#"""
-#
-######
-##   Testovaci text vse hodit na UPPERCASE
-#testStr = input() 
-##
-##   • Textový soubor (obsahující text bez diakritiky) jako parametr programu
-##       vstup = ".\TopicPicker.txt"
-##   nacist vstup 
-##    pokud neni vstup tak vyzadat text od uzivatele zakonceny #
-##def defVstup():
-##    [1] vlozit cestu
-##    [2] vlozit text
-def CheckVstup(vloz_text):
-    vloz_text = str(input("File nebo text zakonceny #: "))
-    endis = "#"
-    if vloz_text.endswith(endis):
-        print("Text bude zpracovan")
-    else:
-        print("Vlozeny text neni ukonceny #")
- 
-#######
-#
-#
-##def FileInput(vstup):
-##    return open(vstup)
-#
-# #####################Informace o celkovém počtu znaků#####################
-#def PocetZnak(vstup):
-#    vstupFile = open(vstup,"r")
-#    data = vstupFile.read().replace(" ","")
-#    pocet_char = len(data)
-#    print('Pocet znaku v celem txt souboru bez mezer :',pocet_char)
-#
-#PocetZnak('.\TopicPicker.txt')
-##
+
 #######################Informace o nejčastějším znaku#####################
-#
+
+def MaxZnak(vstup): 
+    endis = "#"
+    if vstup.endswith(endis):
+        charStore = {} 
+        data = (vstup.replace(" ","")).upper()   
+        alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        for iChar in alpha:
+            for sChar in vstup:
+                if iChar == sChar:
+                    charStore[sChar] += 1
+                else:
+                    charStore[sChar] = 1    
+        res = max(charStore, key = charStore.get)
+        print ("Znak s nejmensim poctem je: " + str(res))
+
+    elif vstup.endswith('.txt'): #za predpokladu ze je vstup file input .txt
+        with open(vstup) as text_file:
+                data = text_file.read().upper().replace(" ","")
+                pocet_char = len(data) #musime vynechat # a veskere nestandartni znaky
+                valid = data.endswith(endis)
+                if valid:
+                    charStore = {}   
+                    print(charStore)                  
+                    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+                    for iChar in alpha:
+                        for sChar in vstup:
+                            if iChar == sChar:
+                                charStore[sChar] += 1
+                            else:
+                                charStore[sChar] = 1
+                    res = max(charStore, key = charStore.get) 
+                    print ("Znak s nejvetsim poctem je: " + str(res))            
+                else:
+                    print("Vlozeny file nema ukonceni s #")
+                             
+    else:       
+        print("Vlozeny text neni ukonceny #")            
+    
+    
+#MaxZnak(input("File nebo text zakonceny #: "))
+
+
 #def MaxZnak():
 #    print ("The original string is : " + testStr)
 #    all_freqMax = {}
@@ -59,61 +66,139 @@ def CheckVstup(vloz_text):
 #    res = res.upper
 #    print ("Znak s nejvetsim poctem je: " + str(res)) 
 #
-#
-##
-##
-#
  #####################Informace o nejméně častém znaku#####################
-#def MinZnak(testStr):
-#    all_freqMin = {}
-#    print(testStr)
-#    for i in testStr:
-#        if i in all_freqMin:
-#         all_freqMin[i] += 1
-#        else:
-#            all_freqMin[i] = 1
-#    res = min(all_freqMin, key = all_freqMin.get)
-#    print ("Znak s nejmensim poctem je: " + str(res)) 
-#   
-#MinZnak("Je tu jen jedno A")
+def MinZnak(vstup):
+    endis = "#"
+    if vstup.endswith(endis):
+        charStore = {} 
+        data = (vstup.replace(" ","")).upper()  
+        alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        for iChar in alpha:
+            for sChar in data:
+                if iChar == sChar:
+                    charStore[iChar] += 1
+                else:
+                    charStore[iChar] = 1    
+        res = min(charStore, key = charStore.get)
+        print ("Znak s nejmensim poctem je: " + str(res))
+
+    elif vstup.endswith('.txt'): #za predpokladu ze je vstup file input .txt
+        with open(vstup) as text_file:
+                data = text_file.read().upper().replace(" ","")
+                pocet_char = len(data) #musime vynechat # a veskere nestandartni znaky
+                valid = data.endswith(endis)
+                if valid:
+                    charStore = {}                     
+                    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+                    for iChar in alpha:
+                        for sChar in data:
+                            if iChar == sChar:
+                                charStore[iChar] += 1
+                                print(charStore)
+                            else:
+                                charStore[iChar] = 1
+                    res = min(charStore, key = charStore.get) 
+                    print ("Znak s nejmensim poctem je: " + str(res))            
+                else:
+                    print("Vlozeny file nema ukonceni s #")
+                             
+    else:       
+        print("Vlozeny text neni ukonceny #")            
+    
+    
+MinZnak(input("File nebo text zakonceny #: "))
+    
+    
+    
+    
+    
+    
+    
+    #for characters in testStr.upper():
+    #    print(charStore)
+    #    if characters in charStore:
+    #        charStore[characters] += 1
+    #    else:
+    #        charStore[characters] = 1
+#
+    #res = min(charStore, key = charStore.get)
+    #print ("Znak s nejmensim poctem je: " + str(res)) 
+   
+#MinZnak("Je tu jen jedno A#")
+
+#pridat vystup pole znaku kdyz nejmensi maji stejne
+
+
+
+
+
 #
 ######################Informace o průměrné četnosti#####################
-#def PrumerKazdyZnak(vstup):
-#    vstupFile = open(vstup,"r")
-#    data = (vstupFile.read().replace(" ","")).upper()
-#    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-#    pocet_char = len(data)
-#    for iChar in alpha:
-#        pocet = data.count(iChar)/pocet_char*100
-#        if data.count(iChar) > 0:
-#            print(iChar, "vyskyt je",f"{pocet:.2f}", "%")
-#        
-#PrumerKazdyZnak('.\TopicPicker.txt')
-# 
-# 
+def PrumerKazdyZnak(vstup):
+    endis = "#"
+    if vstup.endswith(endis):
+        data = (vstup.replace(" ","")).upper()
+        pocet_char = len(data)   
+        alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        for iChar in alpha:
+            pocet = data.count(iChar)/pocet_char*100
+            if data.count(iChar) > 0:
+                print(iChar, "vyskyt je",f"{pocet:.2f}", "%")
+    elif vstup.endswith('.txt'): #za predpokladu ze je vstup file input .txt
+        with open(vstup) as text_file:
+                data = text_file.read().upper().replace(" ","")
+                pocet_char = len(data) #musime vynechat # a veskere nestandartni znaky
+                valid = data.endswith(endis)
+                if valid:
+                    print("if valid",data)
+                    print(data)
+                    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+                    for iChar in alpha:
+                        pocet = data.count(iChar)/pocet_char*100
+                        if data.count(iChar) > 0:
+                            print(iChar, "vyskyt je",f"{pocet:.2f}", "%")
+                else:
+                    print("Vlozeny file nema ukonceni s #")
+                             
+    else:       
+        print("Vlozeny text neni ukonceny #")
+
+#PrumerKazdyZnak(input("File nebo text zakonceny #: "))
+
+
 # #####################Informace o četnosti jednotlivých znaků abecedy (bez diakritiky)#####################
+
+def PocetKazdyZnak(vstup):
+    endis = "#"
+    if vstup.endswith(endis):
+        data = (vstup.replace(" ","")).upper()   
+        alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        for iChar in alpha:
+            pocet = data.count(iChar)
+            if data.count(iChar) > 0:
+                print(iChar, "je",pocet)
+    elif vstup.endswith('.txt'): #za predpokladu ze je vstup file input .txt
+        with open(vstup) as text_file:
+                data = text_file.read().upper().replace(" ","")
+                valid = data.endswith(endis)
+                if valid:
+                    print("if valid",data)
+                    print(data)
+                    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+                    for iChar in alpha:
+                        pocet = data.count(iChar)
+                        if data.count(iChar) > 0:
+                            print(iChar, "je",pocet)
+                else:
+                    print("Vlozeny file nema ukonceni s #")
+                             
+    else:       
+        print("Vlozeny text neni ukonceny #")
+
+#PocetKazdyZnak(input("File nebo text zakonceny #: "))
+       
+# 2 testy a to je jsou to neobyc znaky a nebo cislice a nebo odradkovani tak to bude test fail
 #
-#
-#def PocetKazdyZnak(vstup):
-#    vstupFile = open(vstup,"r")
-#    data = (vstupFile.read().replace(" ","")).upper()
-#    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-#    for iChar in alpha:
-#        pocet = data.count(iChar)
-#        if data.count(iChar) > 0:
-#            print(iChar, "je",pocet)
-#        
-#    
-#
-#PocetKazdyZnak('.\TopicPicker.txt')
-#
+#jen pro ukazku jestli funguje import CetnostZnaku.py
 def secti(a,b):
     return a+b
-
-
-
-#
-##vstup = input('Write your paragraph,ends with #: ')
-#
-#
-#
--- Project/test_Cetnosti.py
+++ Project/test_Cetnosti.py
@@ -1,12 +1,12 @@
 #### testovani funkcionalit #####
 import unittest
 #prozatim import touto cestou nez bude venv
-"""import importlib.util
+import importlib.util
 spec = importlib.util.spec_from_file_location("CetnostZnaku", "C:/01_Batovka/AK1VS/vs_project/Project/CetnostZnaku.py")
 foo = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(foo)
-"""
-import CetnostZnaku
+
+#import CetnostZnaku
 def test_secti():
     assert secti(1,2) == 3
 """
