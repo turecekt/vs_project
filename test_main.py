1d8aa42b98fcdac41e8322b9b1fbf255f59bd188
WinterKRALLe <82891169+WinterKRALLe@users.noreply.github.com>
2022-01-05 10:06:50 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,9 +7,15 @@ from main import rimskeCislice
 
 class TestRimskeCislice(unittest.TestCase):
     """Třída, ve které se testují funkce."""
+    
     def testRimskeCislice23(self):
-        """Funkce, ve které se volá funkce, která přebýrá jako první parametr funkci rimskeCislice
-        a jako druhý parametr, očekávaný výstup."""
+        """.
+        
+        Funkce, ve které se volá funkce, 
+        která přebýrá jako první parametr funkci rimskeCislice
+        a jako druhý parametr, očekávaný výstup.
+        
+        """
         self.assertEqual(rimskeCislice(23), "XXIII")
 
     def testRimskeCislice1234(self):
