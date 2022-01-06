9a46b6b522cca554c9ac2a1eb0573b89e74cc7a2
ZifBarb <95978665+ZifBarb@users.noreply.github.com>
2022-01-06 08:56:13 +0100

    Calling funcions from main script

--- test_calcOfEquations.py
+++ test_calcOfEquations.py
@@ -6,97 +6,89 @@ Created on Fri Dec 31 14:07:17 2021
 
 import unittest
 import random
+from reakcniRychlost import *
 
-def calcOfEquations_forTest(a,b,sgn):
-    #Calculation of given equation
-    if   sgn == "+": correct = a+b
-    elif sgn == "-": correct = a-b
-    elif sgn == "*": correct = a*b
-    elif sgn == "/" and b != 0: correct = round(a/b,3)
-    elif sgn == "/" and b == 0: raise ZeroDivisionError('Cannot divide by zero!')
-    else: raise ValueError("Unsupported math operation")
-    return correct
 
 class calcOfEquationsPlusSign(unittest.TestCase):
     def test_addPositiveNumbers(self):
-        actual = calcOfEquations_forTest(5,5,'+')
+        actual = calcOfEquations(5,5,'+')
         self.assertEqual(actual,10)
         
     def test_addNegativeNumbers(self):
-        actual = calcOfEquations_forTest(-5,-5,'+')
+        actual = calcOfEquations(-5,-5,'+')
         self.assertEqual(actual,-10)
         
     def test_addNegPosNumbers(self):
-        actual = calcOfEquations_forTest(-5,8,'+')
+        actual = calcOfEquations(-5,8,'+')
         self.assertEqual(actual,3)       
 
     def test_negativeZero(self):
-        actual = calcOfEquations_forTest(5,-0,'+')
+        actual = calcOfEquations(5,-0,'+')
         self.assertEqual(actual,5) 
         
 class calcOfEquationsMinusSign(unittest.TestCase):
     def test_subPositiveNumbers(self):
-        actual = calcOfEquations_forTest(5,5,'-')
+        actual = calcOfEquations(5,5,'-')
         self.assertEqual(actual,0)
 
     def test_subPositiveNumbers2(self):
-        actual = calcOfEquations_forTest(5,8,'-')
+        actual = calcOfEquations(5,8,'-')
         self.assertEqual(actual,-3)
         
     def test_subNegNumbers(self):
-        actual = calcOfEquations_forTest(-8,-3,'-')
+        actual = calcOfEquations(-8,-3,'-')
         self.assertEqual(actual,-5)        
         
     def test_subNegNumbers2(self):
-        actual = calcOfEquations_forTest(-3,-8,'-')
+        actual = calcOfEquations(-3,-8,'-')
         self.assertEqual(actual,5) 
         
     def test_negativeZero(self):
-        actual = calcOfEquations_forTest(-3,-0,'-')
+        actual = calcOfEquations(-3,-0,'-')
         self.assertEqual(actual,-3) 
         
 class calcOfEquationsMultiplicationSign(unittest.TestCase):
     def test_multiplyPositiveNumbers(self):
-        actual = calcOfEquations_forTest(5,5,'*')
+        actual = calcOfEquations(5,5,'*')
         self.assertEqual(actual,25)    
         
     def test_multiplyPosNegNumbers(self):
-        actual = calcOfEquations_forTest(-5,5,'*')
+        actual = calcOfEquations(-5,5,'*')
         self.assertEqual(actual,-25)      
 
     def test_multiplyNegNumbers(self):
-        actual = calcOfEquations_forTest(-5,-5,'*')
+        actual = calcOfEquations(-5,-5,'*')
         self.assertEqual(actual,25) 
         
     def test_multiplyZero(self):
-        actual = calcOfEquations_forTest(0,-5,'*')
+        actual = calcOfEquations(0,-5,'*')
         self.assertEqual(actual,0) 
         
 class calcOfEquationsDivisionSign(unittest.TestCase):
     def test_dividePositiveNumbers(self):
-        actual = calcOfEquations_forTest(5,5,'/')
+        actual = calcOfEquations(5,5,'/')
         self.assertEqual(actual,1)    
         
     def test_dividePosNegNumbers(self):
-        actual = calcOfEquations_forTest(-5,5,'/')
+        actual = calcOfEquations(-5,5,'/')
         self.assertEqual(actual,-1)      
 
     def test_divideNegNumbers(self):
-        actual = calcOfEquations_forTest(-5,-5,'/')
+        actual = calcOfEquations(-5,-5,'/')
         self.assertEqual(actual,1) 
         
     def test_divideZero(self):
-        actual = calcOfEquations_forTest(0,-5,'/')
+        actual = calcOfEquations(0,-5,'/')
         self.assertEqual(actual,0) 
         
     def test_divideZeroByZero(self):
         with self.assertRaises(ZeroDivisionError):
-            actual = calcOfEquations_forTest(0,0,'/')   
+            actual = calcOfEquations(0,0,'/')   
         
 class calcOfEquationsError(unittest.TestCase):
     def test_error(self):
         with self.assertRaises(ZeroDivisionError):
-            actual = calcOfEquations_forTest(5,0,'/')         
+            actual = calcOfEquations(5,0,'/')         
         
 
 
