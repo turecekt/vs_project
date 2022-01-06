42fbdfa3ec27f32832df223735e5d14ea27d382d
ZifBarb <95978665+ZifBarb@users.noreply.github.com>
2022-01-06 08:58:15 +0100

    Calling function from main script

--- test_randomInputs.py
+++ test_randomInputs.py
@@ -7,26 +7,19 @@ Created on Thu Dec 30 09:35:11 2021
 
 import unittest
 import random
-
-
-def random_inputs_forTest():
-    a = random.randint(-10, 10) #random integer from interval -10;10
-    b = random.randint(-10, 10) #random integer from interval -10;10
-    sgn = random.choice("+-*/")  #random sign +-*/
-    print('\nWhat is ',a,sgn,b, '?') #Math equation
-    return a, b, sgn
+from reakcniRychlost import *
     
 class randomInputs(unittest.TestCase):
     def test_random_inputs_abIsInInterval(self):
-        a,b,sgn = random_inputs_forTest()
+        a,b,sgn = random_inputs()
         self.assertTrue(-10 <= a,b <= 10)
       
     def test_random_inputs_abIsInteger(self):
-        a,b,sgn = random_inputs_forTest()
+        a,b,sgn = random_inputs()
         self.assertTrue(isinstance(a,int)) and self.assertTrue(isinstance(b,int))
  
     def test_sgn(self):
-        a,b,sgn = random_inputs_forTest()
+        a,b,sgn = random_inputs()
         self.assertTrue(sgn in ['+','-','*','/'])
    
 if __name__ == '__main__':
