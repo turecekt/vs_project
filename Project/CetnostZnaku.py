3db46b427fa3a82c9e157c27af67b8335b79a9cc
Radek Kratochvil <r1_kratochvil@utb.cz>
2022-01-08 18:48:04 +0100

    Oprava MinMaxZnaku

--- Project/CetnostZnaku.py
+++ Project/CetnostZnaku.py
@@ -17,33 +17,30 @@ def MaxZnak(vstup):
     if vstup.endswith(endis):
         charStore = {} 
         data = (vstup.replace(" ","")).upper()   
-        alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-        for iChar in alpha:
-            for sChar in vstup:
-                if iChar == sChar:
-                    charStore[sChar] += 1
-                else:
-                    charStore[sChar] = 1    
+        for sChar in data:
+            if sChar in charStore:
+                charStore[sChar] += 1
+            else:
+                charStore[sChar] = 1    
         res = max(charStore, key = charStore.get)
-        print ("Znak s nejmensim poctem je: " + str(res))
+        print ("Znak s nejvetsim poctem je: " + str(res) )
 
     elif vstup.endswith('.txt'): #za predpokladu ze je vstup file input .txt
         with open(vstup) as text_file:
                 data = text_file.read().upper().replace(" ","")
                 pocet_char = len(data) #musime vynechat # a veskere nestandartni znaky
                 valid = data.endswith(endis)
+                print(data)
                 if valid:
-                    charStore = {}   
-                    print(charStore)                  
-                    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-                    for iChar in alpha:
-                        for sChar in vstup:
-                            if iChar == sChar:
-                                charStore[sChar] += 1
-                            else:
-                                charStore[sChar] = 1
-                    res = max(charStore, key = charStore.get) 
-                    print ("Znak s nejvetsim poctem je: " + str(res))            
+                    charStore = {}  
+                    for sChar in data:
+                        if sChar in charStore:
+                            charStore[sChar] += 1
+                        else:
+                            charStore[sChar] = 1    
+                    res = max(charStore, key = charStore.get)
+                    print ("Znak s nejvetsim poctem je: " + str(res) )                 
+                         
                 else:
                     print("Vlozeny file nema ukonceni s #")
                              
@@ -51,34 +48,20 @@ def MaxZnak(vstup):
         print("Vlozeny text neni ukonceny #")            
     
     
-#MaxZnak(input("File nebo text zakonceny #: "))
+#MaxZnak(input("MaxZnak File nebo text zakonceny #: "))
 
 
-#def MaxZnak():
-#    print ("The original string is : " + testStr)
-#    all_freqMax = {}
-#    for i in testStr:
-#        if i in all_freqMax:
-#         all_freqMax[i] += 1
-#        else:
-#            all_freqMax[i] = 1
-#    res = max(all_freqMax, key = all_freqMax.get)
-#    res = res.upper
-#    print ("Znak s nejvetsim poctem je: " + str(res)) 
-#
- #####################Informace o nejméně častém znaku#####################
+#####################Informace o nejméně častém znaku#####################
 def MinZnak(vstup):
     endis = "#"
     if vstup.endswith(endis):
         charStore = {} 
-        data = (vstup.replace(" ","")).upper()  
-        alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-        for iChar in alpha:
-            for sChar in data:
-                if iChar == sChar:
-                    charStore[iChar] += 1
-                else:
-                    charStore[iChar] = 1    
+        data = (vstup.replace(" ","")).upper()   
+        for sChar in data:
+            if sChar in charStore:
+                charStore[sChar] += 1
+            else:
+                charStore[sChar] = 1    
         res = min(charStore, key = charStore.get)
         print ("Znak s nejmensim poctem je: " + str(res))
 
@@ -88,16 +71,16 @@ def MinZnak(vstup):
                 pocet_char = len(data) #musime vynechat # a veskere nestandartni znaky
                 valid = data.endswith(endis)
                 if valid:
-                    charStore = {}                     
-                    alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-                    for iChar in alpha:
-                        for sChar in data:
-                            if iChar == sChar:
-                                charStore[iChar] += 1
-                                print(charStore)
-                            else:
-                                charStore[iChar] = 1
-                    res = min(charStore, key = charStore.get) 
+                    charStore = {}  
+                    for sChar in data:
+                        if sChar in charStore:
+                            charStore[sChar] += 1
+                        else:
+                            charStore[sChar] = 1    
+                    #res = min(charStore, key = charStore.get)
+                    res = min(charStore)
+                    print (charStore.get) 
+                    print(charStore)                               
                     print ("Znak s nejmensim poctem je: " + str(res))            
                 else:
                     print("Vlozeny file nema ukonceni s #")
@@ -106,25 +89,14 @@ def MinZnak(vstup):
         print("Vlozeny text neni ukonceny #")            
     
     
-MinZnak(input("File nebo text zakonceny #: "))
+MinZnak(input("MinZnak File nebo text zakonceny #: "))
+    
+ 
+ 
     
     
     
-    
-    
-    
-    
-    #for characters in testStr.upper():
-    #    print(charStore)
-    #    if characters in charStore:
-    #        charStore[characters] += 1
-    #    else:
-    #        charStore[characters] = 1
-#
-    #res = min(charStore, key = charStore.get)
-    #print ("Znak s nejmensim poctem je: " + str(res)) 
-   
-#MinZnak("Je tu jen jedno A#")
+
 
 #pridat vystup pole znaku kdyz nejmensi maji stejne
 
--- Project/test_Cetnosti.py
+++ Project/test_Cetnosti.py
@@ -1,17 +1,20 @@
 #### testovani funkcionalit #####
 import unittest
+from CetnostZnaku import *
 #prozatim import touto cestou nez bude venv
-import importlib.util
-spec = importlib.util.spec_from_file_location("CetnostZnaku", "C:/01_Batovka/AK1VS/vs_project/Project/CetnostZnaku.py")
-foo = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(foo)
+#import importlib.util
+#spec = importlib.util.spec_from_file_location("CetnostZnaku", "C:/01_Batovka/AK1VS/vs_project/Project/CetnostZnaku.py")
+#foo = importlib.util.module_from_spec(spec)
+#spec.loader.exec_module(foo)
 
 #import CetnostZnaku
 def test_secti():
     assert secti(1,2) == 3
-"""
-def test_CheckVstup():
-    assert foo.CheckVstup('Vstup #'.endswith('#'))
-"""
-    
+
+
+
+def test_MaxZnak():
+    assert MaxZnak('Vstup je tu AAA#') == 3
+
+ 
     
