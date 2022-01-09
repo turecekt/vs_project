74d59f6e47f1bc689ef69ee3b1bff7366812e776
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-09 23:18:28 +0100

    Formating

--- reakcniRychlost.py
+++ reakcniRychlost.py
@@ -20,9 +20,13 @@ def introduction():
     print('\nReady!')
     time.sleep(1)  # Little break of 1s
     print('\nSteady!')
-    time.sleep(random.randint(1, 4))  # Break 2 - 5 sec
-                                      # (random, so the player must be ready!)
-        
+
+    # Break 2 - 5 sec
+    # (random, so the player must be ready!)
+    time.sleep(random.randint(1, 4))
+
+
+
 # Generation of random inputs
 def random_inputs():
     a = random.randint(-10, 10)  # random integer from interval -10;10
@@ -35,7 +39,7 @@ def random_inputs():
 # Calculation of generated equations - correct results
 def calcOfEquations(a, b, sgn):
     # Calculation of given equation
-    if sgn == "+": 
+    if sgn == "+":
         correct = a+b
     elif sgn == "-":
         correct = a-b
@@ -45,16 +49,18 @@ def calcOfEquations(a, b, sgn):
         correct = round(a/b, 3)
     elif sgn == "/" and b == 0:
         raise ZeroDivisionError('Cannot divide by zero!')
-    #else: raise ValueError("Unsupported math operation")
     return correct
 
 # Final evaluation
 def evaluation(results,reactTime):
-    success = round(results.count(1)/5*100, 2)  # Calculation of percentage success
+    # Calculation of percentage success
+    success = round(results.count(1)/5*100, 2)
     avRate = round(mean(reactTime), 2)  # Calculation of mean react time
     reT = [round(elem, 2) for elem in reactTime]  # Formatted react times
-    print('\n \nYour results are:',results,'\nOverall success is:',success,' %')
-    print('Details of your react times are:', reT, ' s \nMean react time is: ', avRate, ' s')
+    print('\n \nYour results are:',results,
+          '\nOverall success is:', success, ' %')
+    print('Details of your react times are:', reT,
+          ' s \nMean react time is: ', avRate, ' s')
     return success, avRate, reT
 
 #Termination
@@ -68,19 +74,28 @@ def evaluation(results,reactTime):
 def myFunction():
     introduction()
     i = 0
-    results = [0, 0, 0, 0, 0]  # Creation of list with results (1 for correct, 0 for wrong)
-    reactTime = [0, 0, 0, 0, 0]  # Creation of list with react times, initialized with zeros
+    # Creation of list with results (1 for correct, 0 for wrong)
+    results = [0, 0, 0, 0, 0]
+
+    # Creation of list with react times, initialized with zeros
+    reactTime = [0, 0, 0, 0, 0]
 
     while i < 5:  # 5 iterations - from 0 to 4 (5 math equations)
-        a, b, sgn = random_inputs()  # Return values from random_inputs() function stored in tuple a,b,sgn
-        t1 = perf_counter()  #Time - start
+        # Return values from random_inputs() function stored in tuple a,b,sgn
+        a, b, sgn = random_inputs()
+
         # Handling of errors - what happens if the user inputs wrong character
+        t1 = perf_counter()  # Time - start
         try:  # if the input is integer, the program executes the 'try' part
             userResult = float(input('Vysledek:')) # User inputs his answer
             t2 = perf_counter()  # Time - end
             reactTime[i] = t2 - t1;  # Duration from start to end
-            correct = calcOfEquations(a, b, sgn)  # Result of calcOfEquations function stored in var correct
-            # Evaluation of user's answers. Ones/zeros are stored in the list results
+
+            # Result of calcOfEquations function stored in var correct
+            # Evaluation of user's answers.
+            # Ones/zeros are stored in the list results
+            correct = calcOfEquations(a, b, sgn)
+
             if correct == userResult:
                 print('Correct')
                 results[i] = 1
@@ -93,17 +108,17 @@ def myFunction():
         except ValueError:
             print('Wrong input, sorry, good luck with other equations')
             t2 = perf_counter()  # Time - end
-            reactTime[i] = t2 - t1;  # Duration from start to end
+            reactTime[i] = t2 - t1  # Duration from start to end
             results[i] = 0  # Result is automatically 0
 
-        i = i + 1 # next iteration
+        i = i + 1  # next iteration
 
     evaluation(results, reactTime)  # Final evaluation
 
-   # termination()  # Termination of the program
+    # termination()  # Termination of the program
 
 
 if '__main__' == __name__:
 
     myFunction()  # Calling of the main function that has additional functions
-                  # This prevents from calling whole script when executing the unit tests
+    # This prevents from calling whole script when executing the unit tests
