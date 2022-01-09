9a64a571abe53e8b6727da01d3fb3328610cd4ff
Petr Slavík <p_slavik@utb.cz>
2022-01-09 16:03:25 +0100

    doplneni komentaru

--- test_Cetnosti.py
+++ test_Cetnosti.py
@@ -21,43 +21,38 @@ class TestCases(unittest.TestCase):
 
 
 
-    def test_MinZnak(self):
+    def test_MinZnak(self):                       ###   test informace o minimálním počtu znaků ve vstupním textu ###
     
-        vstup = "Python ma jen jednou X Y Z a mozna jeste nejaka dalsi pismena #"         
-
+        vstup = "Python ma jen jednou X Y Z a mozna jeste nejaka dalsi pismena #"        
         self.assertEqual(MinZnak(vstup), ['H', 'U', 'X', 'K', 'L', '#'])
 
 
 
-    def test_MinZnakNoHash(self):
+    def test_MinZnakNoHash(self):               ###     test informace o minimálním počtu znaků ve vstupním textu bez povinného atributu #(hash)    ###
 
         vstup = ""
-
         self.assertEqual(MinZnak(vstup), 0)
 
 
 
-    def test_MaxZnakText(self):
+    def test_MaxZnakText(self):                 ###     test informace o maximálním počtu znaků ve vstupním textu   ###
 
         vstup = "Python ma spustu AAAAAAA#"
-
         self.assertEqual(MaxZnak(vstup), ['A'])
 
     
     
     
-    def test_MaxZnakFile(self):
+    def test_MaxZnakFile(self):                 ###     test informace o maximálním počtu znaků ve vstupním souboru   ###
     
         vstup = "test_fileMax.txt"
-
         self.assertEqual(MaxZnak(vstup), ['A'])
 
     
 
-    def test_MinZnakNoHashinFile(self):
+    def test_MinZnakNoHashinFile(self):         ###     test informace o minimálním počtu znaků ve vstupním souboru bez povinného atributu #(hash)    ###
     
         vstup = "test_file.txt"
-
         self.assertEqual(MinZnak(vstup),0)
 
 
