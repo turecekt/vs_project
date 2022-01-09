a9db2bcf75478f086e7f5b33e19d08345810f01f
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-09 21:13:56 +0100

    Code formating

--- test_randomInputs.py
+++ test_randomInputs.py
@@ -1,26 +1,34 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-.
 """
 Created on Thu Dec 30 09:35:11 2021
-
 @author: zifca
 """
 
 import unittest
-import random
-from reakcniRychlost import *
-    
+from reakcniRychlost import random_inputs
+
+
 class randomInputs(unittest.TestCase):
     def test_random_inputs_abIsInInterval(self):
-        a,b,sgn = random_inputs()
-        self.assertTrue(-10 <= a,b <= 10)
-      
+        '''Tests if given values are in correct
+        interval.'''
+
+        a, b, sgn = random_inputs()
+        self.assertTrue(-10 <= a, b <= 10)
+
     def test_random_inputs_abIsInteger(self):
-        a,b,sgn = random_inputs()
-        self.assertTrue(isinstance(a,int)) and self.assertTrue(isinstance(b,int))
- 
+        '''Tests if given values are integers.'''
+
+        a, b, sgn = random_inputs()
+        self.assertTrue(isinstance(a, int)) \
+            and self.assertTrue(isinstance(b, int))
+
     def test_sgn(self):
-        a,b,sgn = random_inputs()
-        self.assertTrue(sgn in ['+','-','*','/'])
-   
+        '''Tests if given sign is allowed'''
+
+        a, b, sgn = random_inputs()
+        self.assertTrue(sgn in ['+', '-', '*', '/'])
+
+
 if __name__ == '__main__':
     unittest.main()
