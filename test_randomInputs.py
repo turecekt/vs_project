6e7a8af464c48ca6511bb634f4d5e4b12784366c
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-09 21:24:50 +0100

    Code formating

--- test_randomInputs.py
+++ test_randomInputs.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-.
+# -*- coding: utf-8 -*-
+
 """
 Created on Thu Dec 30 09:35:11 2021
 @author: zifca
@@ -9,23 +10,22 @@ from reakcniRychlost import random_inputs
 
 
 class randomInputs(unittest.TestCase):
+    """Set of tests for randomInputs function."""
     def test_random_inputs_abIsInInterval(self):
-        '''Tests if given values are in correct
-        interval.'''
-
+        """Tests if given values are in correct
+        interval.
+        """
         a, b, sgn = random_inputs()
         self.assertTrue(-10 <= a, b <= 10)
 
     def test_random_inputs_abIsInteger(self):
-        '''Tests if given values are integers.'''
-
+        """Tests if given values are integers."""
         a, b, sgn = random_inputs()
         self.assertTrue(isinstance(a, int)) \
             and self.assertTrue(isinstance(b, int))
 
     def test_sgn(self):
-        '''Tests if given sign is allowed'''
-
+        """Tests if given sign is allowed."""
         a, b, sgn = random_inputs()
         self.assertTrue(sgn in ['+', '-', '*', '/'])
 
