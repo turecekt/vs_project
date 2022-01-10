99443a0ab789ea741961d0249dfcc818963a7b6d
TadeasKosik <95754947+TadeasKosik@users.noreply.github.com>
2022-01-10 20:34:23 +0100

    Update reakcniRychlost.py
    Ed.

--- reakcniRychlost.py
+++ reakcniRychlost.py
@@ -5,19 +5,19 @@ Created on Wed Dec 29 14:35:27 2021.
 @author: zifca
 """
 # Import of necessary packages and modules
-from time import perf_counter
-import time
-import random
-from statistics import mean
+from time import perf_counter # Return the value (in fractional seconds)
+# of a performance counter
+import time # Time acces
+import random # Implement pseudo-random number generators for various distributions.
+from statistics import mean #Return the sample arithmetic mean
+# of data which can be a sequence or iterable.
 # import sys
 
 
 # Short description of the program
 def introduction():
     """Script initiation.
-
     Prints information about this script.
-    
     Prints countdown.
     """
     print('The program generates 5 easy math tasks.'
@@ -27,31 +27,25 @@ def introduction():
     time.sleep(1)  # Little break of 1s
     print('\nSteady!')
     time.sleep(random.randint(1, 4))
-    """ Break for 2 - 5 sec.
-    
-    Random, so the player must be ready! 
-    """
+    # Break for 2 - 5 sec.
+    # Random, so the player must be ready!
 
     
 # Generation of random inputs
 def random_inputs():
-    """Implement pseudo-random numbers and signs from various distribution.
-
-    Generates integers used in equations.
-    """
+    ''' Generates integers used in equations.
+    '''
     a = random.randint(-10, 10)  # Random integer from interval -10;10
     b = random.randint(-10, 10)  # Random integer from interval -10;10
     sgn = random.choice("+-*/")  # Random sign +, -, * or /
-    print('\nWhat is ', a, sgn, b, '?')  
+    print('\nWhat is ', a, sgn, b, '?')
     return a, b, sgn # Return a mathematical equation
 
 
 # Calculation of generated equations - correct results
 def calcOfEquations(a, b, sgn):
     """Calculate given equation.
-
     Calculates generated equations.
-    
     Tests for ilegal operations.
 
     Returns:
@@ -66,21 +60,17 @@ def calcOfEquations(a, b, sgn):
     elif sgn == "/" and b != 0:
         correct = round(a/b, 3) # Equation for division, the divisor cannot be zero
     elif sgn == "/" and b == 0:
-        raise ZeroDivisionError('Cannot divide by zero!') # If the divisor is zero, it returns an error
+        raise ZeroDivisionError('Cannot divide by zero!') # If the divisor is zero, return an error
     return correct
 
 
 # Final evaluation
 def evaluation(results, reactTime):
-    """Calculate success percentage.
-    """
-    success = round(results.count(1)/5*100, 2) 
-    """ Calculates percentage of successful answers. 
-    
-    The correct answers aredivided by the total number and multiplied by 100.
-    
-    The round function returns a number rounded to the nearest 2 decimal place.
-    """
+    ''' Calculate success percentage.
+    '''
+    success = round(results.count(1)/5*100, 2) # Calculate percentage of successful answers.
+    # The correct answers aredivided by the total number and multiplied by 100.
+    # The round function returns a number rounded to the nearest 2 decimal place.
     avRate = round(mean(reactTime), 2)  # Return the sample arithmetic mean of react time
     reT = [round(elem, 2) for elem in reactTime]  # Formatted react times
     print('\n \nYour results are:', results,
@@ -109,15 +99,11 @@ def myFunction():
     i = 0
     # Creation of list with results (1 for correct, 0 for wrong) initialized with zeros
     results = [0, 0, 0, 0, 0]
-
     # Creation of list with react times, initialized with zeros
     reactTime = [0, 0, 0, 0, 0]
 
-    while i < 5:  
-        """ 5 iterations - from 0 to 4 (5 math equations)
-        
-        Return values from random_inputs() function stored in tuple a,b,sgn
-        """
+    while i < 5: # 5 iterations - from 0 to 4 (5 math equations)
+        # Return values from random_inputs() function stored in tuple a,b,sgn
         a, b, sgn = random_inputs()
 
         # Determination of time and results
@@ -126,23 +112,21 @@ def myFunction():
             userResult = float(input('Vysledek:'))  # User inputs his answer
             t2 = perf_counter()  # Time - end
             reactTime[i] = t2 - t1  # Duration from start to end
-
-            correct = calcOfEquations(a, b, sgn) 
-            """Result of calcOfEquations function stored in var correct
-            
-            Ones/zeros are stored in the list results
-            """                  
+              
+            correct = calcOfEquations(a, b, sgn)
+            # Result of calcOfEquations function stored in var correct
+            # Ones/zeros are stored in the list results
             if correct == userResult: # Evaluation of user's answers
                 print('Correct')
                 results[i] = 1
             else:
                 print('Wrong')
-                results[i] = 0 
+                results[i] = 0
         
         # Chech the correctness of the answers
-        except ValueError:
-            """ If the input is not integer, the 'try' part is not executed, sentence below is printed and the result is automatically set to 0
-            """
+        except ValueError: 
+            # If the input is not integer, the 'try' part is not executed, 
+            # sentence below is printed and the result is automatically set to 0
             print('Wrong input, sorry, good luck with other equations')
             t2 = perf_counter()  # Time - end
             reactTime[i] = t2 - t1  # Duration from start to end
@@ -153,7 +137,6 @@ def myFunction():
     evaluation(results, reactTime)  # Final evaluation
 
     # termination()  # Termination of the program
-
 
 if '__main__' == __name__:
 
