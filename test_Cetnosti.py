77e1b945f00f00a5537a8026f6f097458904dce8
Radec <app.kratochvil@gmail.com>
2022-01-10 16:55:05 +0100

    Uprava komentare

--- test_Cetnosti.py
+++ test_Cetnosti.py
@@ -41,17 +41,19 @@ class TestCases(unittest.TestCase):
         """Test informace o minimálním počtu znaků ve vstupním souboru 
         bez povinného atributu #(hash)."""
         vstup = "test_file.txt"
-        self.assertEqual(MinZnak(vstup),0)
+        self.assertEqual(MinZnak(vstup), 0)
 
     def test_PocetKazdyZnakInFile(self):
         """Test funkce ktera vypise pocet kazdeho znaku z vlozeneho file."""          
         vstup="test_filePocet.txt"
-        self.assertEqual(PocetKazdyZnak(vstup),{'T': 5, 'E': 4, 'X': 1, 'P': 2, 'R': 1, 'O': 2, 'S': 1, 'F': 1, 'N': 2, 'C': 2, 'K': 2, 'A': 2, 'Z': 2, 'D': 1, 'Y': 1, '#': 1})
+        self.assertEqual(PocetKazdyZnak(vstup), {'T': 5, 'E': 4, 'X': 1, 'P': 2, 'R': 1,
+        'O': 2, 'S': 1, 'F': 1, 'N': 2, 'C': 2, 'K': 2, 'A': 2, 'Z': 2, 'D': 1, 'Y': 1, '#': 1})
 
     def test_PocetKazdyZnakInText(self):
         """Test funkce ktera vypise pocet kazdeho znaku z vlozeneho textu."""
         vstup="Python max znaku#"
-        self.assertEqual(PocetKazdyZnak(vstup),{'P': 1, 'Y': 1, 'T': 1, 'H': 1, 'O': 1, 'N': 2, 'M': 1, 'A': 2, 'X': 1, 'Z': 1, 'K': 1, 'U': 1, '#': 1})
+        self.assertEqual(PocetKazdyZnak(vstup), {'P': 1, 'Y': 1, 'T': 1, 'H': 1,
+        'O': 1, 'N': 2, 'M': 1, 'A': 2, 'X': 1, 'Z': 1, 'K': 1, 'U': 1, '#': 1})
 
 if __name__ == '__main__':
 
