374e1ac0e6dd946d80877d26ebbe372c8e6df956
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-10 19:50:36 +0100

    Test fix

--- test_myFunction.py
+++ test_myFunction.py
@@ -2,10 +2,13 @@ import unittest
 from reakcniRychlost import myFunction
 
 class tests_myFunction(unittest.TestCase):
+    
+    def setUp():
+        self.monkeypatch = MonkeyPatch()
 
-    def test_myFunction(monkeypatch):
+    def test_myFunction(self):
 
-        monkeypatch.setattr('builtins.input', lambda _: 5)
+        self.monkeypatch.setattr('builtins.input', lambda _: 5)
         myFunction()
     #assert i == "XXX"
     
