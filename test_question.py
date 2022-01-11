02f720bd499b4f03c05720a76c849e68ab38df30
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-11 15:47:28 +0100

    Documentation

--- test_question.py
+++ test_question.py
@@ -1,19 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 10:00:00 2022.
+@author: BitterMug
+"""
+
 import unittest
 import reakcniRychlost
 
+
 def autoInput():
+    """Monkeypatch function.
+
+    Simulates User input.
+    Returns:
+    int: User input.
+    """
     return 5
 
 class tests_question(unittest.TestCase):
+    """Set of tests for question function."""
+    
     def test_question_correct(self):
+        """Test correct answer handling."""
         reakcniRychlost.user_input = autoInput
         reakcniRychlost.question(2, 3, "+")
 
     def test_question_wrong(self):
+        """Test incorrect answer handling."""
         reakcniRychlost.user_input = autoInput
         reakcniRychlost.question(2, 0, "+")
 
     #def test_question_err(self):
+    #    """Test error answer handling."""
     #    reakcniRychlost.user_input = autoInput
     #    with self.assertRaises(ValueError):
     #        reakcniRychlost.question(2, "a", "+")
