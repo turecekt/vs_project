260b3bfe448632a7f88961d429f2bdcbb7ffe456
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-11 15:00:30 +0100

    Documentation

--- test_myFunction.py
+++ test_myFunction.py
@@ -1,11 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 10:00:00 2022.
+"""
 import unittest
 import reakcniRychlost
 
 def myFunction_False(a, b, c):
+    """ Monkeypatch function
+    
+    Returns:
+    boolean: Wrong answer indicator.
+    float: Reaction time indicator.
+    """
     return False, 1.2
 
 class tests(unittest.TestCase):
+    """Set of tests for main myFunction function.
+    """
+    
     def test_myFunction_False(self):
+        """Test myFunction with valid values.
+        """
         reakcniRychlost.question = myFunction_False
         reakcniRychlost.myFunction()
     
