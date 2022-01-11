b8c79106ea94f9fc95013b05f2d9bf972959bd36
BitterMug <77463856+BitterMug@users.noreply.github.com>
2022-01-11 19:24:24 +0100

    Formating

--- test_question.py
+++ test_question.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 11 10:00:00 2022.
+
 @author: BitterMug
 """
 
@@ -17,9 +18,10 @@ def autoInput():
     """
     return 5
 
+
 class tests_question(unittest.TestCase):
     """Set of tests for question function."""
-    
+
     def test_question_correct(self):
         """Test correct answer handling."""
         reakcniRychlost.user_input = autoInput
@@ -30,12 +32,12 @@ class tests_question(unittest.TestCase):
         reakcniRychlost.user_input = autoInput
         reakcniRychlost.question(2, 0, "+")
 
-    #def test_question_err(self):
-    #    """Test error answer handling."""
-    #    reakcniRychlost.user_input = autoInput
-    #    with self.assertRaises(ValueError):
-    #        reakcniRychlost.question(2, "a", "+")
-    
+#    def test_question_err(self):
+#        """Test error answer handling."""
+#        reakcniRychlost.user_input = autoInput
+#        with self.assertRaises(ValueError):
+#            reakcniRychlost.question(2, "a", "+")
+
 
 if __name__ == '__main__':
     unittest.main()
