32d5cab38e1e04368b661badc75313cf595071e3
MarekBereznaj <55427090+MarekBereznaj@users.noreply.github.com>
2022-01-18 17:04:00 +0100

    Update Morse.py

--- Morse.py
+++ Morse.py
@@ -99,3 +99,32 @@ if __name__ == '__main__':
         message = input("Zadej morseový kod, ktery chceš přeložit do textu: ")
         result = dekodovat(message.upper())
         print(result)
+
+        class Unittest_morseovka(unittest.TestCase):
+    """Trida pro spusteni unit testu."""
+
+    def test_kodovat_pismeno(self):
+        """Test pro převod písmena na kod."""
+        self.assertEqual(kodovat("A"), ".- ")
+
+    def test_kodovat_veta(self):
+        """Test pro převodu vety do morseovky"""
+        self.assertEqual(kodovat(
+            "MORSEOVKA"), "-- --- .-. ... . --- ...- -.- .- ")
+
+    def test_kodovat_cislo(self):
+        """Test pro převod číslic do morseovky."""
+        self.assertEqual(kodovat(
+            "1234567890"), ".---- ..--- ...-- ....- ....."
+            " -.... --... ---.. ----. ----- ")
+
+    def test_dekodovat_pismeno(self):
+        """Test pro morseovky na pismeno."""
+        self.assertEqual(dekodovat("-.--"), "Y")
+
+    def test_dekodovat_text(self):
+        """Test pro převod morseovky do textové věty."""
+        self.assertEqual(dekodovat(
+            ".--. .-. . ...- --- -..  -. .-  ... .-.."
+            " --- ...- ---  --..  -.- --- -.. ..-"),
+            "PREVOD NA SLOVO Z KODU")
