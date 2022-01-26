a46d1f9d29ebcde4927c6206c3c068f401b42e31
pilcik_patrik <86748415+Pilous15@users.noreply.github.com>
2022-01-26 13:33:28 +0100

    Update testmorse.py (add self parameter)

--- testmorse.py
+++ testmorse.py
@@ -2,16 +2,16 @@ import unittest
 
 class Test_testmorse(unittest.TestCase):
     #test zakodovani textu
-    def test_zakodovani():
+    def test_zakodovani(self):
         assert Morseovka.zakodovani('.-', '....', '---', '.---') == AHOJ
     #test dekodovani textu
-    def test_dekodovani():
+    def test_dekodovani(self):
         assert Morseovka.dekodovani(AHOJ) == '.-' '....' '---' '.---'
     #test zakodovani prazdneho vstupu
-    def test_zakodovani01():
+    def test_zakodovani01(self):
         assert Morseovka.zakodovani("") == ""
     #test dekodovani prazdneho vstupu
-    def test_dekodovani01():
+    def test_dekodovani01(self):
         assert Morseovka.dekodovani("") == ""
 
 if __name__ == '__main__':
