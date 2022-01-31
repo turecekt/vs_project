acc80e95d83475f11d18030b785e938e97d172f7
kovarovam <96140062+kovarovam@users.noreply.github.com>
2022-01-31 21:13:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python
+"""Run program"""
 
 import primeCheck
 
-
-primeCheck.getInput();
+primeCheck.getInput(7)
+primeCheck.getInput(8)
+primeCheck.getInput(113)
+primeCheck.getInput(120)
