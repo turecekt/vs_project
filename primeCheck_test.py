c7ba4ffadf44749c3b5c473e58997aeff3f1d638
kovarovam <96140062+kovarovam@users.noreply.github.com>
2022-01-31 21:36:39 +0100

    Update primeCheck_test.py

--- primeCheck_test.py
+++ primeCheck_test.py
@@ -1,6 +1,4 @@
-"""
-Unit tests.
-"""
+"""Unit tests."""
 
 import primeCheck
 import unittest
@@ -9,65 +7,49 @@ from unittest.mock import patch
 
 class Test(unittest.TestCase):
 
-    """
-    Check method isPrime.
-    """
     @patch('builtins.print')
     def test_isPrime(self, mock_print):
+        """Check method isPrime."""
         number = 7;
         approach = 'Deterministic'
         primeCheck.isPrimeAndRepeat(number, approach)
         mock_print.assert_called_with(number, 
         	"is a prime number.", approach, "approach.")
 
-    """
-    Check isNotPrime.
-    """
     @patch('builtins.print')
     def test_isNotPrime(self, mock_print):
+        """Check isNotPrime."""
         number = 8
         approach = 'Deterministic'
         primeCheck.isNotPrimeAndRepeat(number, approach)
         mock_print.assert_called_with(number, 
         	"is not a prime number.", approach, "approach.")
 
-    """
-    Check method isInteger.
-    """
     def test_isInteger(self):
+        """Check method isInteger."""
         self.assertEqual(primeCheck.isInteger(8), True)
 
-    """
-    Check method isNotInteger.
-    """
     def test_isNotInteger(self):
+        """Check method isNotInteger."""
         self.assertEqual(primeCheck.isInteger('AA'), False)
 
-    """
-    Check program with input 7.
-    """
     def test_prime(self):
+        """Check program with input 7."""
         self.assertEqual(primeCheck.start(7), 'PRIME')
 
-    """
-    Check program with input 8.
-    """
     def test_notPrime(self):
+        """Check program with input 8."""
         self.assertEqual(primeCheck.start(8), 'NOT PRIME')
 
-    """
-    Check method isNotValidInput.
-    """
     @patch('builtins.print')
     def test_isNotValidInput(self, mock_print):
+        """Check method isNotValidInput."""
         primeCheck.start('')
         mock_print.assert_called_with('Not valid input')
 
-    """
-    Check if input is zero.
-    """
     @patch('builtins.print')
     def test_isZero(self, mock_print):
+        """Check if input is zero."""
         primeCheck.start(0)
         mock_print.assert_called_with('0 is not a valid input')
 
