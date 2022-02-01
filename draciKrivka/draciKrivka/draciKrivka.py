cbe9e70d567248c3412a2f7e3ed6a4403b397699
Robin-Dostal <dostal.robi@gmail.com>
2022-02-01 21:59:18 +0100

    vraceni do puvodniho stavu

--- draciKrivka/draciKrivka/draciKrivka.py
+++ draciKrivka/draciKrivka/draciKrivka.py
@@ -1,7 +1,8 @@
 
 """Heighway Dragon."""
 
-# Naimportovani turtle modulu pro pouziti grafiky a _tkinter pro vymezeni vyjimek u funkci
+# Naimportovani turtle modulu pro pouziti grafiky a _tkinter
+# pro vymezeni vyjimek u funkci
 import turtle
 import _tkinter
 
@@ -10,13 +11,20 @@ print('Seznam dostupnych barev: blue, green, red, cyan, megenta, yellow, white, 
 # vyzve uzivatele pro zadani parametru
 """Nastaveni parametru."""
 
-iterace = input('Zadejte pocet iteraci: ')
-barvaPera = input('Zadejte barvu pera: ')
-barvaPozadi = input('Zadejte barvu pozadi: ')
+#iterace = input('Zadejte pocet iteraci: ')
+#barvaPera = input('Zadejte barvu pera: ')
+#barvaPozadi = input('Zadejte barvu pozadi: ')
+
 
 def parametry():
+    global iterace
+    global barvaPera
+    global barvaPozadi
     global delka
     global uhel
+    iterace = input('Zadejte pocet iteraci: ')
+    barvaPera = input('Zadejte barvu pera: ')
+    barvaPozadi = input('Zadejte barvu pozadi: ')
     delka = 2
     uhel = 90
 
@@ -30,11 +38,14 @@ def vrat_cislo(iterace):
     '12'
     """
     try:
-        val = int(iterace)              # ulozeni hodnoty, kterou zada uzivatel
-        if val < 0:                     # pokud je zadane cislo zaporne nastavi se kladna hodnota zadaneho cisla
+        val = int(iterace)              
+        # ulozeni hodnoty, kterou zada uzivatel
+        if val < 0:                     
+        # pokud je zadane cislo zaporne nastavi se kladna hodnota zadaneho cisla
             val = val * -1
   
-    except ValueError:                  # pokud hodnota neni cislo nastavi vychozi hodnotu iteraci na 9
+    except ValueError:                  
+        # pokud hodnota neni cislo nastavi vychozi hodnotu iteraci na 9
         val = 9
     
     return val
@@ -47,9 +58,12 @@ def nbp():
     """
     try:
      turtle.color(barvaPera)
-     if barvaPera == str(''):                                  # pokud je hodnota retezce prazdna, nastavi se barva pera na cervenou
+     if barvaPera == str(''):                                  
+         # pokud je hodnota retezce prazdna, nastavi se barva pera na cervenou
          turtle.color('red') 
-    except (turtle.TurtleGraphicsError, _tkinter.TclError):    # pokud je hodnota retezce neplatna (barva neexistuje a nastatne error), zachyti tuto chybu a nastavi barvu pera na cervenou
+    except (turtle.TurtleGraphicsError, _tkinter.TclError):    
+        # pokud je hodnota retezce neplatna (barva neexistuje a nastatne
+        # error), zachyti tuto chybu a nastavi barvu pera na cervenou
         turtle.color('red') 
 
     
@@ -61,10 +75,13 @@ def nbpo():
     """
     try:
       turtle.bgcolor(barvaPozadi)
-      if barvaPozadi == str(''):                            # pokud je hodnota retezce prazdna, nastavi se barva pozadi na cernou
+      if barvaPozadi == str(''):                            
+          # pokud je hodnota retezce prazdna, nastavi se barva pozadi na cernou
           turtle.bgcolor('black') 
     except (turtle.TurtleGraphicsError, _tkinter.TclError):
-        turtle.bgcolor('black')                             # pokud je hodnota retezce neplatna (barva neexistuje a nastatne error), zachyti tuto chybu a nastavi barvu pozadi na cernou
+        turtle.bgcolor('black')                             
+        # pokud je hodnota retezce neplatna (barva neexistuje a nastatne
+        # error), zachyti tuto chybu a nastavi barvu pozadi na cernou
      
 
 # funkce pro vykresleni draci krivky
