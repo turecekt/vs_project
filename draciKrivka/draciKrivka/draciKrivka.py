1fd392d127de95289e8c83a93a398ede65fbf09f
Robin-Dostal <dostal.robi@gmail.com>
2022-02-04 21:52:30 +0100

    zmeny v return a nove testy_3

--- draciKrivka/draciKrivka/draciKrivka.py
+++ draciKrivka/draciKrivka/draciKrivka.py
@@ -86,6 +86,37 @@ def nastav_prostredi():
     turtle.pendown()
 
 
+def test_vrat_cislo_spravne():
+    """Test vraceni spravneho cisla."""
+    assert vrat_cislo("10") == 10
+    assert vrat_cislo("0") == 0
+
+def test_vrat_cislo_zaporne():
+    """Test vraceni zaporneho cisla."""
+    assert vrat_cislo("-10") == 10
+    assert vrat_cislo("-0") == 0
+
+
+def test_vrat_cislo_chybny_vstup():
+    """Test vraceni chybneho vstupu cisla."""
+    assert vrat_cislo("sdfe") == 9
+    assert vrat_cislo("slovo") == 9
+    assert vrat_cislo("") == 9
+    assert vrat_cislo("7.6") == 9
+
+def test_vrat_nbp():
+    """Test vraceni barvy pera."""
+    assert nbp('blue') == 'blue'
+    assert nbp('') == 'red'
+    assert nbp('sfk7') == 'red'
+
+def test_vrat_nbpo():
+    """Test vraceni barvy pozadi."""
+    assert nbpo('blue') == 'blue'
+    assert nbpo('') == 'black'
+    assert nbpo('ak33k') == 'black'
+
+
 if __name__ == '__main__':
     iterace = input('Zadejte pocet iteraci: ')
     barvaPera = input('Zadejte barvu pera: ')
--- draciKrivka/draciKrivka/draciKrivka_test.py
+++ draciKrivka/draciKrivka/draciKrivka_test.py
@@ -1,41 +1,41 @@
-"""draciKrivka_test"""
+#"""draciKrivka_test"""
 
 
-from draciKrivka import vrat_cislo, nbp, nbpo
-import unittest
+#from draciKrivka import vrat_cislo, nbp, nbpo
+#import unittest
 
 
 
 
-def test_vrat_cislo_spravne():
-    """Test vraceni spravneho cisla."""
-    assert vrat_cislo("10") == 10
-    assert vrat_cislo("0") == 0
+#def test_vrat_cislo_spravne():
+#    """Test vraceni spravneho cisla."""
+#    assert vrat_cislo("10") == 10
+#    assert vrat_cislo("0") == 0
 
-def test_vrat_cislo_zaporne():
-    """Test vraceni zaporneho cisla."""
-    assert vrat_cislo("-10") == 10
-    assert vrat_cislo("-0") == 0
+#def test_vrat_cislo_zaporne():
+#    """Test vraceni zaporneho cisla."""
+#    assert vrat_cislo("-10") == 10
+#    assert vrat_cislo("-0") == 0
 
 
-def test_vrat_cislo_chybny_vstup():
-    """Test vraceni chybneho vstupu cisla."""
-    assert vrat_cislo("sdfe") == 9
-    assert vrat_cislo("slovo") == 9
-    assert vrat_cislo("") == 9
-    assert vrat_cislo("7.6") == 9
+#def test_vrat_cislo_chybny_vstup():
+#    """Test vraceni chybneho vstupu cisla."""
+#    assert vrat_cislo("sdfe") == 9
+#    assert vrat_cislo("slovo") == 9
+#    assert vrat_cislo("") == 9
+#    assert vrat_cislo("7.6") == 9
 
-def test_vrat_nbp():
-    """Test vraceni barvy pera."""
-    assert nbp("blue") == "blue"
-    assert nbp("") == "red"
-    assert nbp("896UAB") == "red"
+#def test_vrat_nbp():
+#    """Test vraceni barvy pera."""
+#    assert nbp('blue') == 'blue'
+#    assert nbp('') == 'red'
+#    assert nbp('sfk7') == 'red'
 
-def test_vrat_nbpo():
-    """Test vraceni barvy pozadi."""
-    assert nbpo("blue") == "blue"
-    assert nbpo("") == "black"
-    assert nbpo("ak33k") == "black"
+#def test_vrat_nbpo():
+#    """Test vraceni barvy pozadi."""
+#    assert nbpo('blue') == 'blue'
+#    assert nbpo('') == 'black'
+#    assert nbpo('ak33k') == 'black'
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
