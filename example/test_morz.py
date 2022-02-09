f5dcf081b49d73a3a1679d179809736b18f3d05b
Demaxxxer <74795858+Demaxxxer@users.noreply.github.com>
2022-02-09 23:00:41 +0100

    Add files via upload
    úprava pytest

--- example/test_morz.py
+++ example/test_morz.py
@@ -3,17 +3,11 @@ import morz
 
 def test_kod():
     assert morz.kod('A') == '.- '
-    assert morz.kod('AHOJ SVETE.') == '.- .... --- .---  ... ...- . - . .-.-.-'
+    assert morz.kod('AHOJ') == '.- .... --- .--- '
     assert morz.kod('GRANKO') == '--. .-. .- -. -.- --- '
-    assert morz.kod('KRECEK MIRECEK DOSEL A NEPOSEL') == """-.- .-. . -.-. . -.-  -- ..
-    .-. . -.-. . -.-  -.. --- ... . .-..  .-  -. . .--. --- ... . .-.. """
 
 
 def test_dekod():
     assert morz.dekod('.-') == 'A'
-    assert morz.dekod('.- .... --- .---  ... ...- . - . .-.-.- ') == """AHOJ
-    SVETE."""
-    assert morz.dekod('--. .-. .- -. -.- --- ') == 'GRANKO '
-    assert morz.dekod("""-.- .-. . -.-. . -.-  -- .. .-. . -.-. . -.-  -.. ---
-                      ... . .-..  .-  -. . .--. --- ... . .-.. """) == """KRECEK
-    MIRECEK DOSEL A NEPOSEL"""
+    assert morz.dekod('.- .... --- .---') == 'AHOJ'
+    assert morz.dekod('--. .-. .- -. -.- ---') == 'GRANKO'
