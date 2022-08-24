791ec9f9620023a1255a2b3dfafb104976cbb4f1
pilcik_patrik <86748415+pilcikpatrik@users.noreply.github.com>
2022-08-24 12:32:13 +0200

    Oprava chyb

--- Morseovka.py
+++ Morseovka.py
@@ -1,3 +1,5 @@
+import unittest
+
 #Definování morseovky
 MORSE_CODE_DICT = {
     'A':'.-',
@@ -42,9 +44,9 @@ def zakodovani(text): #funkce pro zakodovani
     zakodovany_text = "" #zakodovany text = string
     for pismena in text: 
         if pismena != " ": #kontrola mista
-            zakodovany_text = zakodovany_text + MORSE_CODE_DICT.get(pismena)#vyhleda slovnik a prida odpovidajici znak + mezeru
+            zakodovany_text = zakodovany_text + MORSE_CODE_DICT.get(pismena) + " " #vyhleda slovnik a prida odpovidajici znak + mezeru
         else:
-            zakododvany_text += " " #pridani mezery
+            zakodovany_text += " " #pridani mezery
     print(zakodovany_text) #vypis zakodovany text
 
 
@@ -65,18 +67,17 @@ def dekodovani(text): #funkce pro dekodovani
             else:
                 normal = normal + key_list[val_list.index(kod)] #pristup ke klicum pomoci jejich hodnot
                 kod = ""
-    print(normal) #vypis dekodovanz text
+    print(normal) #vypis dekodovany text
 
-if __name__ == '__main__':
-    print("\n\n\n\t\tMorse Code Generator")
-    ch = input("Stiskni E k zakodovani nebo D k Dekodovani : ")
-    if ch == 'E' or ch == 'e': #stiskni "e" a muzes zakodovat svoji vetu,slovo nebo pismeno
-        text_k_zakodovani = input("Vlož text k zakodovani : ").upper()
-        zakodovani(text_k_zakodovani)
-    else:
-        text_k_dekodovani = input("Vlož text k dekodovani : ") #stiskni jakoukoli jinou klavesu a muzes dekodovat svoji vetu,slovo nebo pismeno
-        dekodovani(text_k_dekodovani)
-        
+print("\n\n\n\t\tMorse Code Generator")
+ch = input("Stiskni E k zakodovani nebo D k Dekodovani : ")
+if ch == 'E' or ch == 'e': #stiskni "e" a muzes zakodovat svoji vetu,slovo nebo pismeno
+    text_k_zakodovani = input("Vlož text k zakodovani : ").upper()
+    zakodovani(text_k_zakodovani)
+else:
+    text_k_dekodovani = input("Vlož text k dekodovani : ") #stiskni jakoukoli jinou klavesu a muzes dekodovat svoji vetu,slovo nebo pismeno
+    dekodovani(text_k_dekodovani)
+
 #test zakodovani textu
 def test_zakodovani():
     assert zakodovani('.-') == 'A'
