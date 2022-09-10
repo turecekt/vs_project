1d3b2f9950b13436f5c3bb9b9a2d7fe11d127c35
Jurica, Dušan <dusan.jurica@thermofisher.com>
2022-09-10 15:49:30 +0200

    linting

--- minimax/modules.py
+++ minimax/modules.py
@@ -1,2 +1,10 @@
+"""Basic function module.
+
+"""
+
+
 def a() -> int:
+    """This is a function A().
+
+    """
     return 2
--- minimax/test_modules.py
+++ minimax/test_modules.py
@@ -1,5 +1,12 @@
+"""Test suite for basic module.
+
+"""
+
+
 from modules import a
 
 
 def test_a():
+    """Test for function A().
+    """
     assert a() == 2
