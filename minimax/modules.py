6e92eb9294397374bcdca87188c736d2f8c38b21
Jurica, Dušan <dusan.jurica@thermofisher.com>
2022-09-10 15:53:51 +0200

    fixing

--- minimax/modules.py
+++ minimax/modules.py
@@ -1,10 +1,12 @@
 """Basic function module.
 
+This will do stuff.
 """
 
 
 def a() -> int:
     """This is a function A().
 
+    It also will do stuff.
     """
     return 2
--- minimax/test_modules.py
+++ minimax/test_modules.py
@@ -8,5 +8,7 @@ from modules import a
 
 def test_a():
     """Test for function A().
+
+    This must not be empty.
     """
     assert a() == 2
