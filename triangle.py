1d26fe96ab0368ef02ae1b570eb17eda046ea5dc
tomtrenz <tomtrenz@github.com>
2022-09-13 16:54:14 +0200

    Dodelani unit testu - mezikrok

--- triangle.py
+++ triangle.py
@@ -1,43 +1,54 @@
-"""Triangle program core definitions"""
+"""Triangle program core definitions."""
 import math
 
+
 def start(args):
-    """Demonstrate an example to start with."""
+    """Print results of default calculations."""
     if len(args) != 7:
-        print('Please provide 6 numbers \
-                in format point1x point1y point2x point2y point3x point3y')
+        print('Please provide 6 numbers '
+              'in format point1x point1y point2x point2y point3x point3y')
     else:
         point1 = (args[1], args[2])
         point2 = (args[3], args[4])
         point3 = (args[5], args[6])
 
-        #Points
+        # Points
         print(f"Points are {point1} {point2} {point3}")
 
-        #Side length
+        # Side length
         length1 = sideLength(point1, point2)
         length2 = sideLength(point2, point3)
         length3 = sideLength(point3, point1)
 
-        print(f"Lengths of the sides of the triangle are: {length1}, {length2}, {length3}")
+        print("Lengths of the sides of the triangle are:\n"
+              f"{length1}, {length2}, {length3}")
 
-        #Perimeter
+        # Perimeter
         perimeter = trianglePerimeter(length1, length2, length3)
 
         print(f"Perimeter of the triangle is: {perimeter}")
 
-        #Area
+        # Area
         area = triangleArea(length1, length2, length3)
 
         print(f"Area of the triangle is: {area}")
 
+
 def sideLength(point1, point2):
-    return math.sqrt(math.pow((int(point2[0]) - int(point1[0])), 2) + math.pow((int(point2[1]) - int(point1[1])), 2))
+    """Return lenghts of sides."""
+    return math.sqrt(
+                     math.pow((int(point2[0]) - int(point1[0])), 2)
+                     + math.pow((int(point2[1]) - int(point1[1])), 2)
+                    )
+
 
 def trianglePerimeter(a, b, c):
+    """Return perimeter."""
     return a + b + c
 
+
 def triangleArea(a, b, c):
+    """Return triangle area."""
     s = (a + b + c) / 2
     area = math.sqrt(s * (s - a) * (s - b) * (s - c))
     return area
--- triangle_test.py
+++ triangle_test.py
@@ -2,6 +2,8 @@
 """Run triangle tests."""
 
 import unittest
+from io import StringIO
+from unittest.mock import patch
 import triangle
 
 
@@ -13,9 +15,16 @@ class SimpleTest(unittest.TestCase):
     def test(self):
         """First test of valid STDIN input."""
         test_args = ['.\\main.py', '1', '2', '3', '4', '5', '6']
-        test_output = "Points are ('1', '2') ('3', '4') ('5', '6')"
+        test_output = """Points are ('1', '2') ('3', '4') ('5', '6')
+Lengths of the sides of the triangle are:
+2.8284271247461903, 2.8284271247461903, 5.656854249492381
+Perimeter of the triangle is: 11.313708498984761
+Area of the triangle is: 0.0
+"""
         #  Test evaluation
-        self.assertEqual(triangle.start(test_args), test_output)
+        with patch('sys.stdout', new_callable=StringIO) as f:
+            triangle.start(test_args)
+            self.assertEqual(f.getvalue(), test_output)
 
 
 if __name__ == '__main__':
