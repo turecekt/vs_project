2d7c2c404bbac9475e26d576d2dbb2fa6501fcfb
MartinStritesky <113254850+MartinStritesky@users.noreply.github.com>
2022-11-04 13:16:52 +0100

    Deterministic method added

--- projekt.py
+++ projekt.py
@@ -1,13 +1,13 @@
 import unittest
 
 """This file consists of two ways of printing out prime numbers
-    primes_sieve() function is a heuristic method of finding a prime number"""
+    primes_sieve() function is a heuristic method of finding a prime number
+    is_prime function is a deterministic method."""
+
 
 def primes_sieve(limit):
-    
     limitn = limit+1
-    primes = [i for i in range(2,limitn)]
-    is_prime = False
+    primes = [i for i in range(2, limitn)]
 
     for i in primes:
         factors = range(i, limitn, i)
@@ -15,23 +15,36 @@ def primes_sieve(limit):
             if f in primes:
                 primes.remove(f)
     if limit in primes:
-        is_prime = True
+        return True
     else:
-        is_prime = False
-        
-    return is_prime
-
-vstup = input("Please write a number to be checked: ")
-vstup_int = int(vstup)
-checkbig = primes_sieve(vstup_int)
+        return False
 
 
-if vstup_int >= 100:
-    if checkbig:
-        print("your number is prime")
+def is_prime(vstup):
+    if vstup == 2 or vstup == 3:
+        return True
+
+    for i in range(2, int(vstup ** 0.5) + 1):
+        if vstup % i == 0:
+            return False
+    return True
+
+
+try:
+    vstup = int(input("Please write a number to be checked: "))
+
+    if vstup >= 100:
+        if primes_sieve(vstup):
+            print("Your number is prime.")
+        else:
+            print("Your number isn't prime.")
+    elif 100 > vstup > 1:
+        if is_prime(vstup):
+            print("Your number is prime.")
+        else:
+            print("Your number isn't prime.")
     else:
-        print("your number isn't prime")
-elif vstup_int < 100:
-    """insert funkce"""
-else:
-    print("not a number!")
+        print("Your number isn't prime.")
+
+except ValueError:
+    print("Please enter a whole number!")
