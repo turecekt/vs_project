19af6d1f4222a6c63b3927fdd5881e4b7e1b60c5
Jan Koňařík <101633132+konj27@users.noreply.github.com>
2022-11-04 10:05:14 +0100

    Heuristic method

---
+++ projekt.py
@@ -0,0 +1,23 @@
+"""This file consists of two ways of printing out prime numbers
+    primes_sieve() function is a heuristic method of finding a prime number"""
+
+def primes_sieve(limit):
+    limitn = limit+1
+    primes = range(2, limitn)
+
+    for i in primes:
+        factors = range(i, limitn, i)
+        for f in factors[1:]:
+            if f in primes:
+                primes.remove(f)
+    return primes
+
+vstup=input()
+print("write a number" + vstup)
+if vstup>100:
+    primes_sieve(vstup)
+elif vstup<100:
+    """placeholder"""
+else:
+    print("not a number!")
+
