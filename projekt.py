d3f967a8201b0369914ee0cc0a4d301c8fd9ef11
Jan Koňařík <101633132+konj27@users.noreply.github.com>
2022-11-04 11:53:49 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -2,6 +2,13 @@
     primes_sieve() function is a heuristic method of finding a prime number"""
 
 def primes_sieve(limit):
+    """Functon compute returns evaluation of expression using argument x.
+    :param x: First input parameter.
+    :return: Expression value for x.
+    >>> compute(200)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+    
+    """
     limitn = limit+1
     primes = [i for i in range(2,limitn)]
 
