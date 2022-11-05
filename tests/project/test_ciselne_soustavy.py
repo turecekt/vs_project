f01c3aa9a8cf0ab3fa7a06d39383cb887c89322d
Tadeáš Kot <tadeas.kot@firma.seznam.cz>
2022-11-05 21:22:28 +0100

    restrukturizace a pridani testu

---
+++ tests/project/test_ciselne_soustavy.py
@@ -0,0 +1,38 @@
+from project.ciselne_soustavy import is_number_int
+from project.ciselne_soustavy import number_conversion
+import pytest
+
+
+class TestIsNumberInt:
+    @pytest.mark.parametrize(
+        "number", ["0", "5", "55", "444", "065", "66.0", "0.0", "066.0"]
+    )
+    def test_true(self, number):
+        result = is_number_int(number)
+
+        assert result is True
+
+    @pytest.mark.parametrize("number", ["a", "5a", "55.5", "-5", "55.a", "a5"])
+    def test_false(self, number):
+        result = is_number_int(number)
+
+        assert result is False
+
+
+class TestNumberConversion:
+    @pytest.mark.parametrize(
+        "number, number_system, expected",
+        [
+            (2, 4, "2"),
+            (4, 2, "100"),
+            (1, 30, "1"),
+            (35, 36, "Z"),
+            (46655, 36, "ZZZ"),
+            (0, 6, "0"),
+            (55, 10, "55")
+        ],
+    )
+    def test_ok(self, number, number_system, expected):
+        result = number_conversion(number, number_system)
+
+        assert result == expected
