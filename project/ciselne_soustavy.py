0ced87066c8daf472f5dc8c01cba537462eafbc7
Marodia <m1_odstrcilova@utb.cz>
2022-11-06 00:03:04 +0100

    Added new functions, tests passed.

--- project/ciselne_soustavy.py
+++ project/ciselne_soustavy.py
@@ -1,8 +1,22 @@
+import re
+
+
 def is_number_int(number):
-    return number.isdigit()
+    return re.match(r"[+]?\d+(\.0*)?$", number) is not None
 
 
+def number_conversion(number, base_system):         # max base value = 36
+    result = ""
+    if int(number) == 0:
+        result += str(number)
+    else:
+        while number > 0:
+            inter = int(number) % base_system
+            if inter <= 9:
+                result += str(inter)
+            else:
+                result += chr(ord('A') + inter - 10)
+            number //= base_system
 
-def number_conversion(number, number_system):
-    result = None
+    result = result[::-1]
     return result
