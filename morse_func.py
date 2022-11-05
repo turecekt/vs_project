aff85b6d4b34330e3a5e08d9a4008d0e0f90ad75
ejan87 <emil.jandal@gmail.com>
2022-11-05 10:49:30 +0100

    committing morse_func.py

---
+++ morse_func.py
@@ -0,0 +1,64 @@
+# Function that converts text into morse code
+def encode(user_input):
+    """
+    Contains a dictionary of alphabet to morse code.
+    User input is looped through and compared to dictionary keys.
+    Individual values are concatenated into a variable which is
+    printed to the terminal.
+    :parameter: user_input
+    :return: Morse code.
+    """
+    # Empty variable which will be used to store converted text into morse code
+    encode_output = ""
+    # A dictionary containing the following value pairs:
+    # key = letter of the alphabet, value = morse sign
+    alpha_to_morse = {
+        "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".",
+        "F": "..-.", "G": "--.", "H": "....", "I": "..", "J": ".---",
+        "K": "-.-", "L": ".-..", "M": "--", "N": "-.", "O": "---",
+        "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-",
+        "U": "..-", "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--",
+        "Z": "--..", "1": ".----", "2": "..---", "3": "...--",
+        "4": "....-", "5": ".....", "6": "-....", "7": "--...",
+        "8": "---..", "9": "----.", "0": "-----", " ": "|"
+    }
+    # Looping through the list of letters of the alphabet and
+    # getting the corresponding values (morse characters) from the dictionary
+    for letter in user_input.upper():
+        encode_output += alpha_to_morse.get(letter) + " "
+    return encode_output
+
+
+# Function that converts morse code into text
+def decode(user_input):
+    """
+    Contains a dictionary of morse code to alphabet.
+    User input in morse code is made into a list which is
+    then looped through and compared to dictionary keys.
+    Individual values are concatenated into a variable which is
+    printed to the terminal.
+    :parameter: user_input
+    :return: Morse code translation.
+    """
+    # Empty variable which will be used to store converted morse code into text
+    decode_output = ""
+    # A dictionary containing the following value pairs:
+    # key = morse sign, value = alphabet letter
+    morse_to_alpha = {
+        ".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E",
+        "..-.": "F", "--.": "G", "....": "H", "..": "I", ".---": "J",
+        "-.-": "K", ".-..": "L", "--": "M", "-.": "N", "---": "O",
+        ".--.": "P", "--.-": "Q", ".-.": "R", "...": "S", "-": "T",
+        "..-": "U", "...-": "V", ".--": "W", "-..-": "X", "-.--": "Y",
+        "--..": "Z", ".----": "1", "..---": "2", "...--": "3",
+        "....-": "4", ".....": "5", "-....": "6", "--...": "7",
+        "---..": "8", "----.": "9", "-----": "0", "|": " "
+    }
+    # Individual morse code characters that are typed in by the user
+    # are split upon a space and stored in the code variable
+    code = user_input.split(" ")
+    # Looping through the list of morse code characters and
+    # getting the corresponding values (alphabet letters) from the dictionary
+    for sign in code:
+        decode_output += morse_to_alpha.get(sign)
+    return decode_output
