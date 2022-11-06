36de68911cade6e9daeeef83b120b83576ea2ab4
ejan87 <emil.jandal@gmail.com>
2022-11-06 14:47:11 +0100

    committing files

---
+++ morse_code/test_morse_func.py
@@ -0,0 +1,19 @@
+"""Module containing functions test_encode and test_decode."""
+# importing functions
+import morse_func
+
+
+# function responsible for testing the encode function
+def test_encode():
+    """Tests encode function."""
+    assert morse_func.encode("ahoj") == ".- .... --- .--- "
+    assert morse_func.encode("SOS") == "... --- ... "
+    assert morse_func.encode("1231") == ".---- ..--- ...-- .---- "
+
+
+# function responsible for testing the decode function
+def test_decode():
+    """Tests decode function."""
+    assert morse_func.decode(".- .... --- .---") == "AHOJ"
+    assert morse_func.decode("... --- ...") == "SOS"
+    assert morse_func.decode(".---- ..--- ...-- .----") == "1231"
