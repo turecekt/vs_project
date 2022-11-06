497ffadbe1e56378669e8c8b6fc3b23b4411e7da
Michal Markus <lachim@markus-it.local>
2022-11-06 21:34:39 +0100

    Úprava výpočtu délky strany - v inputProcessor.py zjednodušení výpočtu ve funkci sideLength - Odstranění nově nepotřebné knihovny math z inputProcessor.py

--- inputProcessor.py
+++ inputProcessor.py
@@ -2,26 +2,23 @@
 """Processes input for triangle program."""
 
 import sys
-import math
 
 
 def sideLength(point1, point2):
     """Return lenghts of sides."""
-    return math.sqrt(
-                     math.pow((int(point2[0]) - int(point1[0])), 2)
-                     + math.pow((int(point2[1]) - int(point1[1])), 2)
-                    )
+    vector = (point2[0] - point1[0], point2[1] - point1[1])
+    return (vector[0] ** 2 + vector[1] ** 2) ** 0.5
 
 
 def getSides():
     args = sys.argv
     if len(args) != 7:
-            print('Please provide 6 numbers '
-                  'in format point1x point1y point2x point2y point3x point3y')
+        print('Please provide 6 numbers '
+              'in format point1x point1y point2x point2y point3x point3y')
     else:
-        point1 = (args[1], args[2])
-        point2 = (args[3], args[4])
-        point3 = (args[5], args[6])
+        point1 = (float(args[1]), float(args[2]))
+        point2 = (float(args[3]), float(args[4]))
+        point3 = (float(args[5]), float(args[6]))
 
         # Points
         print(f"Points are {point1} {point2} {point3}")
@@ -32,5 +29,5 @@ def getSides():
         length3 = sideLength(point3, point1)
 
         print("Lengths of the sides of the triangle are:\n"
-                f"{length1}, {length2}, {length3}")
+              f"{length1}, {length2}, {length3}")
     return [length1, length2, length3]
