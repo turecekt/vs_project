cf52a2dde7ce5331b2384ef4b1a534b7e040ddbe
ejan87 <emil.jandal@gmail.com>
2022-11-07 14:11:02 +0100

    fixing imports

--- test_morse_func.py
+++ test_morse_func.py
@@ -6,14 +6,14 @@ from .morse_func import encode, decode
 # function responsible for testing the encode function
 def test_encode():
     """Tests encode function."""
-    assert morse_func.encode("ahoj") == ".- .... --- .--- "
-    assert morse_func.encode("SOS") == "... --- ... "
-    assert morse_func.encode("1231") == ".---- ..--- ...-- .---- "
+    assert encode("ahoj") == ".- .... --- .--- "
+    assert encode("SOS") == "... --- ... "
+    assert encode("1231") == ".---- ..--- ...-- .---- "
 
 
 # function responsible for testing the decode function
 def test_decode():
     """Tests decode function."""
-    assert morse_func.decode(".- .... --- .---") == "AHOJ"
-    assert morse_func.decode("... --- ...") == "SOS"
-    assert morse_func.decode(".---- ..--- ...-- .----") == "1231"
+    assert decode(".- .... --- .---") == "AHOJ"
+    assert decode("... --- ...") == "SOS"
+    assert decode(".---- ..--- ...-- .----") == "1231"
