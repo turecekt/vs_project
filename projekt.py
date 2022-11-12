a2e86fd228b6f0d377787b790e7a7c124989fe6b
ErikJukin <67873549+ErikJukin@users.noreply.github.com>
2022-11-12 13:41:39 +0100

    Update projekt.py
    pracovní verze projektu, ještě bude updatenuto.

--- projekt.py
+++ projekt.py
@@ -1,12 +1,26 @@
 import unittest
+import math
+import time
 
-"""This file consists of two ways of printing out prime numbers
-    primes_sieve() function is a heuristic method of finding a prime number
-    is_prime function is a deterministic method."""
-
-
+"""
+This file consists of two ways of printing out prime numbers
+primes_sieve() function is a heuristic method of finding a prime number
+is_prime function is a deterministic method.
+simple timetracker is included for measuring performance of different algorithms    
+"""
 def primes_sieve(limit):
-    limitn = limit+1
+    """
+    This function is implementation of sieve of Eratosthenes algorithm for finding all prime numbers up to any given limit.
+    List of prime numbers is generated up to the upper limit given by the parameter ``limit``.
+    Function then tests if ``limit`` is in the generated list.
+
+    :param limit: int
+        number that is tested for primality
+    :return: bool
+        function returns true if parameter limit is prime number
+    """
+
+    limitn = limit + 1
     primes = [i for i in range(2, limitn)]
 
     for i in primes:
@@ -20,31 +34,56 @@ def primes_sieve(limit):
         return False
 
 
-def is_prime(vstup):
+"""def is_prime(vstup):
     if vstup == 2 or vstup == 3:
         return True
 
     for i in range(2, int(vstup ** 0.5) + 1):
         if vstup % i == 0:
             return False
+    return True"""
+
+def is_prime(vstup):
+    """
+    This function test if ``vstup`` is prime number by modulation of this number.
+
+    :param vstup: int
+        number that is tested for primality
+    :return: bool
+        function returns true if vstup is primal number
+    """
+
+    if vstup <= 1:#Function is_prime first tests that input variable vstup is equal or bigger than 1
+        return False
+    if vstup % 2 == 0:
+        return vstup == 2 #it will return True if the number is equal to 2 else false
+
+    max_div = math.floor(math.sqrt(vstup))
+    for i in range(3, 1 + max_div, 2):#If the given number is divisible by any of the numbers from 3 to the square root of the number skipping all the even numon will return False" \
+        if vstup % i == 0:
+            return False
     return True
 
-
+t0 = time.time()
 try:
+
     vstup = int(input("Please write a number to be checked: "))
 
     if vstup >= 100:
         if primes_sieve(vstup):
-            print("Your number is prime.")
+            print("Your number is prime. This was determined by heuristic method.")
         else:
-            print("Your number isn't prime.")
+            print("Your number isn't prime. This was determined by heuristic  method.")
     elif 100 > vstup > 1:
         if is_prime(vstup):
-            print("Your number is prime.")
+            print("Your number is prime. Deterministic method was used.")
         else:
-            print("Your number isn't prime.")
+            print("Your number isn't prime. Deterministic method was used.")
     else:
         print("Your number isn't prime.")
 
 except ValueError:
     print("Please enter a whole number!")
+
+t1 = time.time()
+print("Time required :", t1 - t0)
