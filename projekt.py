77ca850a726a3e078d97ea66aada4b5df366fe8e
MartinStritesky <113254850+MartinStritesky@users.noreply.github.com>
2022-11-12 14:12:28 +0100

    Probabilistic method added - Fermat´s method Partially commented - please finish the function comments including their variables

--- projekt.py
+++ projekt.py
@@ -1,50 +1,98 @@
 import unittest
-
+import random
 """This file consists of two ways of printing out prime numbers
-    primes_sieve() function is a heuristic method of finding a prime number
-    is_prime function is a deterministic method."""
+    primes_sieve() function is a deterministic method of finding a prime number
+    is_prime_prob function is a probabilistic method."""
 
 
-def primes_sieve(limit):
-    limitn = limit+1
-    primes = [i for i in range(2, limitn)]
+def primes_sieve(n):
+    """
+    Insert function description
+    :param n:
+    :return:
+    """
+    tmp = n+1
+    primes = [i for i in range(2, tmp)]
 
     for i in primes:
-        factors = range(i, limitn, i)
+        factors = range(i, tmp, i)
         for f in factors[1:]:
             if f in primes:
                 primes.remove(f)
-    if limit in primes:
+    if vstup in primes:
         return True
     else:
         return False
 
 
-def is_prime(vstup):
-    if vstup == 2 or vstup == 3:
-        return True
+def power(a, n, p):
+    """
+    Insert function description
+    :param a:
+    :param n:
+    :param p:
+    :return:
+    """
 
-    for i in range(2, int(vstup ** 0.5) + 1):
-        if vstup % i == 0:
+    # Initializing "res"
+    res = 1
+    # If "a" is greater than "p", it is set to the modulus of this division
+    a = a % p
+    # Calculating the "a" to the power of "n"
+    while n > 0:
+        # If "n" is odd number, subtract 1 from it to make it even
+        # and "res"
+        if n % 2:
+            res = (res * a) % p
+            n = n - 1
+        else:
+            # Calculating the actual mid-result of "a" with making "a" to the power of 2 and taking modulus by "p"
+            a = (a ** 2) % p
+            # "n" is updated to the nearest whole number after its division by 2, "n" is even
+            n = n // 2
+    # Function "power" returns the final modulus
+    return res % p
+
+
+def is_prime_prob(n, k):
+    """
+    Insert function description
+    :param n:
+    :param k:
+    :return:
+    """
+    # Number of tries the program performs in order to determine if it is prime number or not
+    for i in range(k):
+        # Assigns random whole number from 2 to n -2
+        a = random.randint(2, n - 2)
+        # If result from "power" function is not equal to 1, it is most probably not a prime number
+        if power(a, n - 1, n) != 1:
             return False
     return True
 
 
+# Try - except block to catch if the user input was incorrect
 try:
+    # Reading user input and converting it to integer
     vstup = int(input("Please write a number to be checked: "))
-
-    if vstup >= 100:
+    # If input is small number, use deterministic method
+    if 1 > vstup <= 10000:
+        # If function returns True boolean, the input is prime number
         if primes_sieve(vstup):
             print("Your number is prime.")
         else:
             print("Your number isn't prime.")
-    elif 100 > vstup > 1:
-        if is_prime(vstup):
+    # if input is larger number, use probabilistic method
+    elif vstup > 10000:
+        # If function returns True boolean, the input is prime number
+        # Second parameter is number of tries
+        if is_prime_prob(vstup, 10):
             print("Your number is prime.")
         else:
             print("Your number isn't prime.")
+    # Taking care of negative number inputs
     else:
         print("Your number isn't prime.")
 
 except ValueError:
-    print("Please enter a whole number!")
+    print("Please enter a whole number.")
