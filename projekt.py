3abb2bfbc0ba40de5bdfbe72f1f7781b8a6656e2
ErikJukin <67873549+ErikJukin@users.noreply.github.com>
2022-11-19 12:29:57 +0100

    Update projekt.py
    linted via Flake8

--- projekt.py
+++ projekt.py
@@ -3,16 +3,28 @@ import math
 import time
 
 """
-This file consists of two ways of printing out prime numbers
-''primes_sieve()'' function is a deterministic method of finding a prime number.
-''primes_sieve()'' is more is more suited for creating list of primes, but for educational purposes is used for numbers lesser than 100.
-Second function ''is_prime'' is also deterministic function, but is designed for faster operation when searching for primality of given number.
-Simple timetracker is included for measuring performance of different algorithms. Python time library is used by this tracker.
+This file consists of two methods for determination of primality
+
+Firrst is ''primes_sieve()''
+Deterministic method of finding a prime number.
+Method is more is more suited for creating list of primes.
+
+Second function ''is_prime''
+also deterministic function
+is designed for faster operation when searching for primality of given number.
+
+Simple timetracker is included for performance measuring.
+Python time library is used.
 """
+
+
 def primes_sieve(limit):
+
     """
-    This function is implementation of sieve of Eratosthenes algorithm for finding all prime numbers up to any given limit.
-    List of prime numbers is generated up to the upper limit given by the parameter ``limit``.
+    This function is implementation of sieve of Eratosthenes algorithm
+    for finding all prime numbers up to any given limit.
+    List of prime numbers is generated up to the upper limit
+    given by the parameter ``limit``.
     Function then tests if ``limit`` is in the generated list.
 
     :param limit: int
@@ -34,9 +46,11 @@ def primes_sieve(limit):
     else:
         return False
 
+
 def is_prime(vstup):
     """
-    This function test if ``vstup`` is prime number by modulation of this number.
+    This function test if input variable ``vstup`` is prime number.
+    Function reduces number of modulators to save time.
     Python math library needs to imported before deployment of this function.
 
     :param vstup: int
@@ -45,16 +59,17 @@ def is_prime(vstup):
         function returns true if vstup is primal number
     """
 
-    if vstup <= 1:#Function is_prime first tests that input variable vstup is equal or bigger than 1
+    if vstup <= 1:
         return False
     if vstup % 2 == 0:
-        return vstup == 2 #it will return True if the number is equal to 2 else false
+        return vstup == 2
 
     max_div = math.floor(math.sqrt(vstup))
-    for i in range(3, 1 + max_div, 2):#If the given number is divisible by any of the numbers from 3 to the square root of the number skipping all the even numbers, function will return False
+    for i in range(3, 1 + max_div, 2):
         if vstup % i == 0:
             return False
     return True
+
 
 t0 = time.time()
 try:
@@ -62,7 +77,7 @@ try:
     vstup = int(input("Please write a number to be checked: "))
 
     if vstup >= 100:
-        if is_prime(vstup) :
+        if is_prime(vstup):
             print("Your number is prime.")
         else:
             print("Your number isn't prime.")
