6177dd0b6e5988ae990fd4eeacd52f0982e67ef6
Tadeáš Kot <tadeas.kot@firma.seznam.cz>
2022-11-26 21:56:54 +0100

    test_ciselne_soustavy.py flake8 valid

--- tests/project/test_ciselne_soustavy.py
+++ tests/project/test_ciselne_soustavy.py
@@ -1,25 +1,35 @@
+"""Unit tests for `ciselne_soustavy.py`"""
+
 from project.ciselne_soustavy import is_number_int
 from project.ciselne_soustavy import number_conversion
 import pytest
 
 
 class TestIsNumberInt:
+    """Unit tests for function `is_number_int`."""
+
     @pytest.mark.parametrize(
         "number", ["0", "5", "55", "444", "065", "66.0", "0.0", "066.0", "-5"]
     )
     def test_true(self, number):
+        """Testing valid input."""
         result = is_number_int(number)
 
         assert result is True
 
-    @pytest.mark.parametrize("number", ["a", "5a", "55.5", "55.a", "a5", "-5.6"])
+    @pytest.mark.parametrize(
+        "number", ["a", "5a", "55.5", "55.a", "a5", "-5.6"]
+    )
     def test_false(self, number):
+        """Testing not valid input."""
         result = is_number_int(number)
 
         assert result is False
 
 
 class TestNumberConversion:
+    """Unit tests for function `number_conversion`."""
+
     @pytest.mark.parametrize(
         "number, number_system, expected",
         [
@@ -33,18 +43,7 @@ class TestNumberConversion:
         ],
     )
     def test_ok(self, number, number_system, expected):
+        """Test valid conversions."""
         result = number_conversion(number, number_system)
 
         assert result == expected
-
-
-# tadeaskuv test
-# def fnc():
-#     a = input()
-#     b = input()
-#     return (a, b)
-#
-# def test_input(mocker):
-#     mocker.patch("builtins.input", side_effect=["a", "b"])
-#
-#     assert ["a", "b"] == fnc()
