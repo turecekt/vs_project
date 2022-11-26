e1a39b0e3d06c13cb5fb488c864e0e9adc704e08
Marodia <m1_odstrcilova@utb.cz>
2022-11-26 22:17:16 +0100

    Flake8

--- project/ciselne_soustavy.py
+++ project/ciselne_soustavy.py
@@ -2,10 +2,30 @@ import re
 
 
 def is_number_int(number):
+    """
+    Checks if the value is an integer.
+
+    Checks if the specified string contains values representing integers.
+    Both integers and integers written in float form are recognized.
+
+    :param number: String
+    :return: Bool
+    """
     return re.match(r"[-+]?\d+(\.0*)?$", number) is not None
 
 
 def number_conversion(number, base_system):  # max base value = 36
+    """
+    Converts the selected number to the selected number system.
+
+    This function uses conditional statements to convert a number to the selected number system.
+    If the specified number is equal to 0, zero is automatically returned.
+    If the number is other than zero, the result is calculated by successive division.
+
+    :param number: String
+    :param base_system: String
+    :return: String
+    """
     result = ""
     number = int(number)
     base_system = int(base_system)
@@ -25,6 +45,15 @@ def number_conversion(number, base_system):  # max base value = 36
 
 
 def converter():
+    """
+    Input values.
+
+    The function requires two input values - a number and a number system.
+    It checks if the values are entered correctly (correct number of inputs and the "number" value is an integer).
+    If incorrect values are entered, the function asks for new values.
+
+    :return: None
+    """
     number = input("Enter number in decimal base: ")
     base_system = input("Enter required base system: ")
     if is_number_int(number) is True:
