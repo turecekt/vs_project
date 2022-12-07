6b4ab2a9350b9231c3f1fc52a3045b6c0718f5ba
Dominik Strnad <d_strnad@utb.cz>
2022-12-07 17:14:33 +0100

    Separate functions

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import sys
 
 from os.path import exists
-from collections import Counter
 
-
-line = "-" * 45 # separator
-occurrence = {}
-num = 0
-alpha = 0
-other_sym = 0
-text = ""
+line_separator = "-" * 45 # separator
+char_occurrence_dict = {}
+alphabet_chars_dict = {}
 
 # spouštění programu: "py main.py"
 # spouštění programu s textovým souborem: "py main.py soubor.txt"
@@ -23,6 +18,8 @@ def main():
 
     :return:
     """
+
+    text = ""
 
     if sys.argv[1:]:
         if sys.argv[1].endswith(".txt") or sys.argv[1].endswith(".log"):
@@ -48,57 +45,36 @@ def main():
         print("\n Nebyl zadán žádný text.")
         exit()
 
-
-    # TODO Vytvořit metody a v main() je jen volat
-    # TODO Přehlednější výpisy např.: "Celkový počet znaků: 123"
-    print("Celkový počet znaků")
-    print(len(text))
-
-    # TODO "abeceda" 2x "a" 2x "e" - ale musí vypsat oboje
-    print("\nNejčastější znak")
-    print(Counter(text).most_common(1))
-
-    print("\nNejméně častý znak")
-    print(Counter(text).most_common()[-1])
-
-
-    print("\nPrůměrná četnost")
-    rounded_average = round(len(text) / len(sorted(Counter(text))), 2)
-    print(rounded_average)
-
-    print(line)
-
     count_to_dictionary(text)
-    char_type_counting(alpha, num, other_sym)
-    occurrence_graph()
+    char_max_values()
+    char_min_values()
+    char_average()
+    occurrence_to_alphabetic_dict(char_occurrence_dict)
+    alphabet_occurrence_graph()
 
+def char_average():
+    rounded_average = round(sum(char_occurrence_dict.values()) / len(char_occurrence_dict), 2)
+    print(f"Průměrná četnost: {rounded_average}")
 
-# TODO Bonus - počet čísel, počet písmen, počet speciálních znaků
+def char_min_values():
+    min_values = [key for key, value in char_occurrence_dict.items() if value == min(char_occurrence_dict.values())]
+    if len(min_values) > 1:
+        print(f"Nejméně časté znaky: {min_values}")
+    else:
+        print(f"Nejméně častý znak: {min_values[0]}")
 
-def char_type_counting(alpha, num, other_sym):
-    """
+def char_max_values():
+    max_values = [key for key, value in char_occurrence_dict.items() if value == max(char_occurrence_dict.values())]
+    if len(max_values) > 1:
+        print(f"Nejčastější znaky: {max_values}")
+    else:
+        print(f"Nejčastější znak: {max_values[0]}")
 
-    :param alpha:
-    :param num:
-    :param other_sym:
-    :return:
-    """
-    for key, value in occurrence.items():
+def occurrence_to_alphabetic_dict(char_occurrence_dict):
+    for key, value in char_occurrence_dict.items():
         if key.isalpha():
-            alpha += 1
-        elif key.isdigit():
-            num += 1
-        else:
-            other_sym += 1
+            alphabet_chars_dict[key] = value
 
-    print("Počet písmen")
-    print(alpha)
-
-    print("Počet čísel")
-    print(num)
-
-    print("Počet specialních symbolů")
-    print(other_sym)
 
 def count_to_dictionary(text):
     """
@@ -108,40 +84,41 @@ def count_to_dictionary(text):
     :param text: Text from file or from user input.
     This function returns nothing.
     """
+    char_counter = 0
     for char in text:
-        if char != "\n":
-            if char in occurrence:
-                occurrence[char] = occurrence[char] + 1
-            else:
-                occurrence[char] = 1
+        #if char != "\n":
+        char_counter += 1
+        if char in char_occurrence_dict:
+            char_occurrence_dict[char] += 1
+        else:
+            char_occurrence_dict[char] = 1
+    print(f"Celkový počet znaků: {char_counter}")
 
 
 # Graf četnost znaků
-def occurrence_graph():
+def alphabet_occurrence_graph():
     """
     Function will show table of char occurences.
     This function returns nothing.
     """
-
     max_value = 0
 
-    for key, value in occurrence.items():
+    print(line_separator)
+    for key, value in alphabet_chars_dict.items():
         if value > max_value:
             max_value = value
 
-    if max_value <= len("OCCURRENCES"):
-        print(f"CHAR|OCCURRENCES|NR.")
+    if max_value <= len("ČETNOST"):
+        print(f"ZNAK|ČETNOST|NR.")
     else:
-        print(f"CHAR|OCCURRENCES{' ' * (max_value - len('OCCURRENCES'))}|NR.")
-    print(line)
+        print(f"ZNAK|ČETNOST{' ' * (max_value - len('ČETNOST'))}|NR.")
 
-    for key, value in sorted(occurrence.items()):
-        if max_value > len("OCCURRENCES"):
+    for key, value in sorted(alphabet_chars_dict.items()):
+        if max_value > len("ČETNOST"):
             print(f"   {key}|{'*' * value}{' ' * (max_value-value)}|{value}")
         else:
-            print(f"   {key}|{'*' * value}{' ' * (len('OCCURRENCES')-value)}|{value}")
+            print(f"   {key}|{'*' * value}{' ' * (len('ČETNOST')-value)}|{value}")
 
-    print(line)
 
 if __name__ == "__main__":
     main()
