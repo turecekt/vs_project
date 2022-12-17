500d79ed7ab4fe89e0f43393fc7804fba4ed28d2
KeziTheDev <74493919+KeziTheDev@users.noreply.github.com>
2022-12-17 04:28:01 +0100

    Úprava kódu Test_MorseCodePrekladac.py

--- Test_MorseCodePrekladac.py
+++ Test_MorseCodePrekladac.py
@@ -1,13 +1,13 @@
 # Pytest MorseCodePrekladac
 from MorseCodePrekladac import sifrovani, desifrovani
 
-def test_sifrovani1():
+def test_sifrovani():
     # Testování překladu do abecedy z morseovky 
     assert sifrovani("AHOJ!") == ".- .... --- .--- -.-.--"
     assert sifrovani("733290807") == "--... ...-- ...-- ..--- ----. ----- ---.. ----- --..."
     assert sifrovani("ME ZE RY") == "-- . | --.. . | .-. -.--"
 
-def test_desifrovani1():
+def test_desifrovani():
     # Testování překladu z morseovky do abecedy
     assert desifrovani("--.. -.. .-. .- ...- .. --") == "ZDRAVIM"
     assert desifrovani("-.... ----- ..... ---.. ----. ..--- ----- ----- ---..") == "605892008"
