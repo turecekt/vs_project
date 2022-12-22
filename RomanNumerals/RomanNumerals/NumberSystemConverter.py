c6ad6d9b5a533645c55902be933815e804b421b4
janvanek5@gmail.com <janvanek5@gmail.com>
2022-12-22 14:37:12 +0100

    added encapsulation, removed unused parts of code, unit tests was edited coverage 100%

--- RomanNumerals/RomanNumerals/NumberSystemConverter.py
+++ RomanNumerals/RomanNumerals/NumberSystemConverter.py
@@ -1,363 +1,298 @@
-"""
-NumberSystemConverter Package
-----------------------------------
-Contains functions for converting between numeric systems.
-"""
 
 
-def integerToRoman(integer):
+class NumberSystemConverter:
     """
-    Function for converting integers to Roman numbers.
-    Converts any positive integer in range from 1 to 3999.
-
-    :param name: integer - Number to be converted.
-    :param type: int
-    :return: str
+    NumberSystemConverter Class
+    ----------------------------------
+    Contains functions for converting between numeric systems.
     """
 
-    # Checks if the input is None.
-    if (integer is None):
-        return "Input can not be None."
+    def integerToRoman(integer):
+        """
+        Function for converting integers to Roman numbers.
+        Converts any positive integer in range from 1 to 3999.
 
-    # Checks if the input is really an integer in range 1-3999.
-    if (not str(integer).isnumeric() or (integer < 1 or integer > 3999)):
-        return "Input must be any positive integer from 1 to 3999."
+        :param name: integer - Number to be converted.
+        :param type: int
+        :return: str
+        """
 
-    # Declaration of the variable "roman", which will represent
-    # the value of the input integer in the system of Roman numerals.
-    roman = ""
+        # Checks if the input is None.
+        if (integer is None):
+            return "Input can not be None."
 
-    # Filling variable "roman" with Roman numerals.
-    while (integer > 0):
+        # Checks if the input is really an integer in range 1-3999.
+        if (not str(integer).isnumeric() or (integer < 1 or integer > 3999)):
+            return "Input must be any positive integer from 1 to 3999."
 
-        if (integer >= 1000):
-            integer -= 1000
-            roman += "M"
+        # Declaration of the variable "roman", which will represent
+        # the value of the input integer in the system of Roman numerals.
+        roman = ""
 
-        elif (integer >= 900):
-            integer -= 900
-            roman += "CM"
+        # Filling variable "roman" with Roman numerals.
+        while (integer > 0):
 
-        elif (integer >= 500):
-            integer -= 500
-            roman += "D"
+            if (integer >= 1000):
+                integer -= 1000
+                roman += "M"
 
-        elif (integer >= 400):
-            integer -= 400
-            roman += "CD"
+            elif (integer >= 900):
+                integer -= 900
+                roman += "CM"
 
-        elif (integer >= 100):
-            integer -= 100
-            roman += "C"
+            elif (integer >= 500):
+                integer -= 500
+                roman += "D"
 
-        elif (integer >= 90):
-            integer -= 90
-            roman += "XC"
+            elif (integer >= 400):
+                integer -= 400
+                roman += "CD"
 
-        elif (integer >= 50):
-            integer -= 50
-            roman += "L"
+            elif (integer >= 100):
+                integer -= 100
+                roman += "C"
 
-        elif (integer >= 40):
-            integer -= 40
-            roman += "XL"
+            elif (integer >= 90):
+                integer -= 90
+                roman += "XC"
 
-        elif (integer >= 10):
-            integer -= 10
-            roman += "X"
+            elif (integer >= 50):
+                integer -= 50
+                roman += "L"
 
-        elif (integer >= 9):
-            integer -= 9
-            roman += "IX"
+            elif (integer >= 40):
+                integer -= 40
+                roman += "XL"
 
-        elif (integer >= 5):
-            integer -= 5
-            roman += "V"
+            elif (integer >= 10):
+                integer -= 10
+                roman += "X"
 
-        elif (integer >= 4):
-            integer -= 4
-            roman += "IV"
+            elif (integer >= 9):
+                integer -= 9
+                roman += "IX"
 
-        elif (integer >= 1):
-            integer -= 1
-            roman += "I"
+            elif (integer >= 5):
+                integer -= 5
+                roman += "V"
 
-    # Returns value of the input in the system of Roman numerals.
-    return roman
+            elif (integer >= 4):
+                integer -= 4
+                roman += "IV"
+
+            elif (integer >= 1):
+                integer -= 1
+                roman += "I"
+
+        # Returns value of the input in the system of Roman numerals.
+        return roman
 
 
-def romanToInteger(roman):
-    """
-    Function for converting Roman numbers to integers.
-    Converts any Roman number of valid format.
+    def romanToInteger(roman):
+        """
+        Function for converting Roman numbers to integers.
+        Converts any Roman number of valid format.
 
-    :param name: roman - Number to be converted.
-    :param type: str
-    :return: int
-    """
+        :param name: roman - Number to be converted.
+        :param type: str
+        :return: int
+        """
 
-    # Checks if the input is None.
-    if (roman is None):
-        return "Input can not be None."
+        # Checks if the input is None.
+        if (roman is None):
+            return "Input can not be None."
 
-    # Make sure the input is a string.
-    roman = str(roman)
+        # Make sure the input is a string.
+        roman = str(roman)
 
-    # Defining allowed characters.
-    allowedChars = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        # Defining allowed characters.
+        allowedChars = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
-    # Checks if the input contains only allowed characters.
-    for char in roman:
-        if (char not in allowedChars):
-            return "Contains illegal characters."
+        # Checks if the input contains only allowed characters.
+        for char in roman:
+            if (char not in allowedChars):
+                return "Contains illegal characters."
 
-    # Declaration of the variable "parsed_roman", which will contain
-    # individual numerals.
-    parsed_roman = [roman[0]]
+        # Declaration of the variable "parsed_roman", which will contain
+        # individual numerals.
+        parsed_roman = [roman[0]]
 
-    # Counter initialization.
-    i = 1
+        # Counter initialization.
+        i = 1
 
-    # Filling variable "parsed_roman" with individual numerals.
-    while i < len(roman):
+        # Filling variable "parsed_roman" with individual numerals.
+        while i < len(roman):
 
-        # "last_roman" represents the last numeral that was inserted
-        # into "parsed_roman".
-        last_roman = parsed_roman[len(parsed_roman) - 1]
+            # "last_roman" represents the last numeral that was inserted
+            # into "parsed_roman".
+            last_roman = parsed_roman[len(parsed_roman) - 1]
 
-        # "current_roman" represents next character from "roman".
-        current_roman = roman[i]
+            # "current_roman" represents next character from "roman".
+            current_roman = roman[i]
 
-        # If combination of "last_roman" and "current_roman" results
-        # in a valid numeral, last numeral in "parsed_roman" is replaced
-        # by this combination.
-        if (valueOf(last_roman + current_roman) > 0):
-            parsed_roman[len(parsed_roman) - 1] = last_roman + current_roman
+            # If combination of "last_roman" and "current_roman" results
+            # in a valid numeral, last numeral in "parsed_roman" is replaced
+            # by this combination.
+            if (NumberSystemConverter.__valueOf(last_roman + current_roman) > 0):
+                parsed_roman[len(parsed_roman) - 1] = last_roman + current_roman
 
-        # If not, "current_roman" is added to "parsed_roman" like a new
-        # last numeral.
+            # If not, "current_roman" is added to "parsed_roman" like a new
+            # last numeral.
+            else:
+                parsed_roman.append(current_roman)
+
+            # Counter increment.
+            i += 1
+
+        # Declaration of the variable "integer", which will represent
+        # the value of the input in the decimal system.
+        integer = NumberSystemConverter.__valueOf(parsed_roman[0])
+
+        # Counter initialization.
+        i = 1
+
+        # Filling variable "integer" with its value.
+        while i < len(parsed_roman):
+
+            # "last_integer" represents the value of last numeral that was added.
+            last_integer = NumberSystemConverter.__valueOf(parsed_roman[i - 1])
+
+            # "current_integer" represents the value that will be added
+            # in the current step.
+            current_integer = NumberSystemConverter.__valueOf(parsed_roman[i])
+
+            # If "last_integer" is greater than "current_integer" and highest
+            # usable integer after "last_integer" is greater of equal to
+            # "current_integer", it means, that "roman" is in correct format
+            # and "current_integer" is added to "integer".
+            if (last_integer > current_integer and
+                    NumberSystemConverter.__getHighestUsableNumeralAfter(last_integer) >= current_integer):
+                integer += current_integer
+
+            # If not, error message is returned.
+            else:
+                return "Invalid format."
+
+            # Counter increment.
+            i += 1
+
+        # Returns value of the input in the decimal system.
+        return integer
+
+
+    def __valueOf(roman_numeral):
+        """
+        Function for converting Roman numerals to integers.
+        Converts any valid Roman numeral.
+        If numeral is not valid, returns 0.
+
+        :param name: roman_numeral - Numeral to be converted.
+        :param type: str
+        :return: int
+        """
+
+        if (roman_numeral == 'I'):
+            return 1
+
+        elif (roman_numeral == 'II'):
+            return 2
+
+        elif (roman_numeral == 'III'):
+            return 3
+
+        elif (roman_numeral == 'IV'):
+            return 4
+
+        elif (roman_numeral == 'V'):
+            return 5
+
+        elif (roman_numeral == 'IX'):
+            return 9
+
+        elif (roman_numeral == 'X'):
+            return 10
+
+        elif (roman_numeral == 'XX'):
+            return 20
+
+        elif (roman_numeral == 'XXX'):
+            return 30
+
+        elif (roman_numeral == 'XL'):
+            return 40
+
+        elif (roman_numeral == 'L'):
+            return 50
+
+        elif (roman_numeral == 'XC'):
+            return 90
+
+        elif (roman_numeral == 'C'):
+            return 100
+
+        elif (roman_numeral == 'CC'):
+            return 200
+
+        elif (roman_numeral == 'CCC'):
+            return 300
+
+        elif (roman_numeral == 'CD'):
+            return 400
+
+        elif (roman_numeral == 'D'):
+            return 500
+
+        elif (roman_numeral == 'CM'):
+            return 900
+
+        elif (roman_numeral == 'M'):
+            return 1000
+
+        elif (roman_numeral == 'MM'):
+            return 2000
+
+        elif (roman_numeral == 'MMM'):
+            return 3000
+
         else:
-            parsed_roman.append(current_roman)
-
-        # Counter increment.
-        i += 1
-
-    # Declaration of the variable "integer", which will represent
-    # the value of the input in the decimal system.
-    integer = valueOf(parsed_roman[0])
-
-    # Counter initialization.
-    i = 1
-
-    # Filling variable "integer" with its value.
-    while i < len(parsed_roman):
-
-        # "last_integer" represents the value of last numeral that was ad0ded.
-        last_integer = valueOf(parsed_roman[i - 1])
-
-        # "current_integer" represents the value that will be added
-        # in the current step.
-        current_integer = valueOf(parsed_roman[i])
-
-        # If "last_integer" is greater than "current_integer" and highest
-        # usable integer after "last_integer" is greater of equal to
-        # "current_integer", it means, that "roman" is in correct format
-        # and "current_integer" is added to "integer".
-        if (last_integer > current_integer and
-                getHighestUsableNumeralAfter(last_integer) >= current_integer):
-            integer += current_integer
-
-        # If not, error message is returned.
-        else:
-            return "Invalid format."
-
-        # Counter increment.
-        i += 1
-
-    # Returns value of the input in the decimal system.
-    return integer
+            return 0
 
 
-def valueOf(roman_numeral):
-    """
-    Function for converting Roman numerals to integers.
-    Converts any valid Roman numeral.
-    If numeral is not valid, returns 0.
+    def __getHighestUsableNumeralAfter(number):
+        """
+        Returns decimal value of highest usable Roman numeral after "number".
 
-    :param name: roman_numeral - Numeral to be converted.
-    :param type: str
-    :return: int
-    """
+        :param name: number - Value of roman numeral.
+        :param type: int
+        :return: int
+        """
 
-    if (roman_numeral == 'I'):
-        return 1
+        if (number >= 1000 and number <= 3000):
+            return 900
 
-    elif (roman_numeral == 'II'):
-        return 2
+        if (number == 900):
+            return 90
 
-    elif (roman_numeral == 'III'):
-        return 3
+        if (number == 500):
+            return 300
 
-    elif (roman_numeral == 'IV'):
-        return 4
+        if (number >= 100 and number <= 400):
+            return 90
 
-    elif (roman_numeral == 'V'):
-        return 5
+        if (number == 90):
+            return 9
 
-    elif (roman_numeral == 'IX'):
-        return 9
+        if (number == 50):
+            return 30
 
-    elif (roman_numeral == 'X'):
-        return 10
+        if (number >= 10 and number <= 40):
+            return 9
 
-    elif (roman_numeral == 'XX'):
-        return 20
+        if (number == 9):
+            return 0
 
-    elif (roman_numeral == 'XXX'):
-        return 30
+        if (number == 5):
+            return 3
 
-    elif (roman_numeral == 'XL'):
-        return 40
-
-    elif (roman_numeral == 'L'):
-        return 50
-
-    elif (roman_numeral == 'XC'):
-        return 90
-
-    elif (roman_numeral == 'C'):
-        return 100
-
-    elif (roman_numeral == 'CC'):
-        return 200
-
-    elif (roman_numeral == 'CCC'):
-        return 300
-
-    elif (roman_numeral == 'CD'):
-        return 400
-
-    elif (roman_numeral == 'D'):
-        return 500
-
-    elif (roman_numeral == 'CM'):
-        return 900
-
-    elif (roman_numeral == 'M'):
-        return 1000
-
-    elif (roman_numeral == 'MM'):
-        return 2000
-
-    elif (roman_numeral == 'MMM'):
-        return 3000
-
-    else:
-        return 0
-
-
-def getHighestUsableNumeralAfter(number):
-    """
-    Returns decimal value of highest usable Roman numeral after "number".
-
-    :param name: number - Value of roman numeral.
-    :param type: int
-    :return: int
-    """
-
-    if (number is None):
-        return 0
-
-    if (not str(number).isnumeric()):
-        return 0
-
-    if (number >= 1000 and number <= 3000):
-        return 900
-
-    if (number == 900):
-        return 90
-
-    if (number == 500):
-        return 300
-
-    if (number >= 100 and number <= 400):
-        return 90
-
-    if (number == 90):
-        return 9
-
-    if (number == 50):
-        return 30
-
-    if (number >= 10 and number <= 40):
-        return 9
-
-    if (number == 9):
-        return 0
-
-    if (number == 5):
-        return 3
-
-    if (number >= 1 and number <= 4):
-        return 0
-
-    return 0
-
-
-def coverage():
-
-    integerToRoman(None)
-    integerToRoman(0)
-    integerToRoman(4000)
-    integerToRoman(3.5)
-    integerToRoman(1666)
-    integerToRoman(999)
-    integerToRoman(444)
-
-    romanToInteger(None)
-    romanToInteger(1)
-    romanToInteger("p")
-    romanToInteger("CCM")
-    romanToInteger("XCL")
-    romanToInteger("MD")
-
-    valueOf(None)
-    valueOf("I")
-    valueOf("II")
-    valueOf("III")
-    valueOf("IV")
-    valueOf("V")
-    valueOf("IX")
-    valueOf("X")
-    valueOf("XX")
-    valueOf("XXX")
-    valueOf("XL")
-    valueOf("L")
-    valueOf("XC")
-    valueOf("C")
-    valueOf("CC")
-    valueOf("CCC")
-    valueOf("CD")
-    valueOf("D")
-    valueOf("CM")
-    valueOf("M")
-    valueOf("MM")
-    valueOf("MMM")
-
-    getHighestUsableNumeralAfter(1000)
-    getHighestUsableNumeralAfter(900)
-    getHighestUsableNumeralAfter(500)
-    getHighestUsableNumeralAfter(100)
-    getHighestUsableNumeralAfter(90)
-    getHighestUsableNumeralAfter(50)
-    getHighestUsableNumeralAfter(10)
-    getHighestUsableNumeralAfter(9)
-    getHighestUsableNumeralAfter(5)
-    getHighestUsableNumeralAfter(1)
-    getHighestUsableNumeralAfter(None)
-    getHighestUsableNumeralAfter("I")
-    getHighestUsableNumeralAfter(4000)
-
-
-if __name__ == '__main__':
-    coverage()
+        if (number >= 1 and number <= 4):
+            return 0
--- RomanNumerals/RomanNumerals/test_NumberSystemConverter.py
+++ RomanNumerals/RomanNumerals/test_NumberSystemConverter.py
@@ -1,8 +1,5 @@
 import unittest
-from NumberSystemConverter import integerToRoman
-from NumberSystemConverter import romanToInteger
-from NumberSystemConverter import valueOf
-from NumberSystemConverter import getHighestUsableNumeralAfter
+from NumberSystemConverter import NumberSystemConverter
 
 
 class Test_test_NumberSystemConverter(unittest.TestCase):
@@ -11,104 +8,65 @@ class Test_test_NumberSystemConverter(unittest.TestCase):
 
         expectedValue = "Input can not be None."
 
-        roman = integerToRoman(None)
+        roman = NumberSystemConverter.integerToRoman(None)
         self.assertEqual(roman, expectedValue)
 
         expectedValue = "Input must be any positive integer from 1 to 3999."
 
-        roman = integerToRoman(0)
-        self.assertEqual(roman, expectedValue)
-
-        roman = integerToRoman(4000)
-        self.assertEqual(roman, expectedValue)
-
-        roman = integerToRoman(3.5)
+        roman = NumberSystemConverter.integerToRoman(3.5)
         self.assertEqual(roman, expectedValue)
 
         expectedValue = "MDCLXVI"
 
-        roman = integerToRoman(1666)
+        roman = NumberSystemConverter.integerToRoman(1666)
         self.assertEqual(roman, expectedValue)
 
         expectedValue = "CMXCIX"
 
-        roman = integerToRoman(999)
+        roman = NumberSystemConverter.integerToRoman(999)
         self.assertEqual(roman, expectedValue)
 
         expectedValue = "CDXLIV"
 
-        roman = integerToRoman(444)
+        roman = NumberSystemConverter.integerToRoman(444)
         self.assertEqual(roman, expectedValue)
+
 
     def test_romanToInteger(self):
 
         expectedValue = "Input can not be None."
 
-        integer = romanToInteger(None)
+        integer = NumberSystemConverter.romanToInteger(None)
         self.assertEqual(integer, expectedValue)
 
         expectedValue = "Contains illegal characters."
 
-        integer = romanToInteger(1)
+        integer = NumberSystemConverter.romanToInteger(1)
         self.assertEqual(integer, expectedValue)
-
-        integer = romanToInteger("P")
-        self.assertEqual(integer, expectedValue)
-
+        
         expectedValue = "Invalid format."
 
-        integer = romanToInteger("CCM")
+        integer = NumberSystemConverter.romanToInteger("MCXIIII")
         self.assertEqual(integer, expectedValue)
 
-        integer = romanToInteger("XCL")
+        integer = NumberSystemConverter.romanToInteger("IXI")
         self.assertEqual(integer, expectedValue)
 
-        expectedValue = 1500
+        expectedValue = 3888
 
-        integer = romanToInteger("MD")
+        integer = NumberSystemConverter.romanToInteger("MMMDCCCLXXXVIII")
         self.assertEqual(integer, expectedValue)
 
-    def test_valueOf(self):
+        expectedValue = 999
 
-        self.assertEqual(valueOf(None), 0)
-        self.assertEqual(valueOf("I"), 1)
-        self.assertEqual(valueOf("II"), 2)
-        self.assertEqual(valueOf("III"), 3)
-        self.assertEqual(valueOf("IV"), 4)
-        self.assertEqual(valueOf("V"), 5)
-        self.assertEqual(valueOf("IX"), 9)
-        self.assertEqual(valueOf("X"), 10)
-        self.assertEqual(valueOf("XX"), 20)
-        self.assertEqual(valueOf("XXX"), 30)
-        self.assertEqual(valueOf("XL"), 40)
-        self.assertEqual(valueOf("L"), 50)
-        self.assertEqual(valueOf("XC"), 90)
-        self.assertEqual(valueOf("C"), 100)
-        self.assertEqual(valueOf("CC"), 200)
-        self.assertEqual(valueOf("CCC"), 300)
-        self.assertEqual(valueOf("CD"), 400)
-        self.assertEqual(valueOf("D"), 500)
-        self.assertEqual(valueOf("CM"), 900)
-        self.assertEqual(valueOf("M"), 1000)
-        self.assertEqual(valueOf("MM"), 2000)
-        self.assertEqual(valueOf("MMM"), 3000)
+        integer = NumberSystemConverter.romanToInteger("CMXCIX")
+        self.assertEqual(integer, expectedValue)
 
-    def test_getHighestUsableNumeralAfter(self):
+        expectedValue = 444
 
-        self.assertEqual(getHighestUsableNumeralAfter(1000), 900)
-        self.assertEqual(getHighestUsableNumeralAfter(900), 90)
-        self.assertEqual(getHighestUsableNumeralAfter(500), 300)
-        self.assertEqual(getHighestUsableNumeralAfter(100), 90)
-        self.assertEqual(getHighestUsableNumeralAfter(90), 9)
-        self.assertEqual(getHighestUsableNumeralAfter(50), 30)
-        self.assertEqual(getHighestUsableNumeralAfter(10), 9)
-        self.assertEqual(getHighestUsableNumeralAfter(9), 0)
-        self.assertEqual(getHighestUsableNumeralAfter(5), 3)
-        self.assertEqual(getHighestUsableNumeralAfter(1), 0)
-        self.assertEqual(getHighestUsableNumeralAfter(None), 0)
-        self.assertEqual(getHighestUsableNumeralAfter("I"), 0)
-        self.assertEqual(getHighestUsableNumeralAfter(4000), 0)
+        integer = NumberSystemConverter.romanToInteger("CDXLIV")
+        self.assertEqual(integer, expectedValue)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
