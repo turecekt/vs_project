2562dfd41acef730449d471966ca6e73086bc504
l-nagy-utb-fai <118170100+l-nagy-utb-fai@users.noreply.github.com>
2023-01-20 22:40:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,28 +4,37 @@ import functions
 
 class Test_Encode(TestCase):
     def test_encryptShortWord(self):
-        self.assertEqual('--.. -.- --- ..- ... -.- .-',
-                         functions.kodovani('ZKOUSKA'))
+        self.assertEqual('--.- .-- . .-. - --.. ..-',
+                         functions.kodovani('QWERTZU'))
 
     def test_encryptLongWord(self):
-        self.assertEqual('.- -. --- ..--..',
-                         functions.kodovani('ANO?'))
+        self.assertEqual('.. --- .--. .- ... -.. ..-.',
+                         functions.kodovani('IOPASDF'))
+
+    def test_encryptLongWord(self):
+        self.assertEqual('-.-. ...- -... -. --  .-.-.-',
+                         functions.kodovani('CVBNM .'))
 
     def test_encryptSentence(self):
-        inp = "NEBO NE."
-        expect_out = "-. . -... ---  -. . .-.-.-"
+        inp = "GHJKLYX"
+        expect_out = "--. .... .--- -.- .-.. -.-- -..-"
         self.assertEqual(expect_out, functions.kodovani(inp))
 
 
 class Test_Decipher(TestCase):
     def test_decryptShortWord(self):
-        self.assertEqual('ZKOUSKA',
-                         functions.dekodovani('--.. -.- --- ..- ... -.- .-'))
+        self.assertEqual('QWERTZU',
+                         functions.dekodovani('--.- .-- . .-. - --.. ..-'))
 
     def test_decryptLongWord(self):
-        self.assertEqual('ANO?', functions.dekodovani(".- -. --- ..--.."))
+        self.assertEqual('IOPASDF',
+                         functions.dekodovani(".. --- .--. .- ... -.. ..-."))
+
+    def test_decryptShortWord(self):
+        self.assertEqual('CVBNM .',
+                         functions.dekodovani('-.-. ...- -... -. --  .-.-.-'))
 
     def test_decryptSentence(self):
-        inp = "-. . -... ---  -. . .-.-.-"
-        expect_out = "NEBO NE."
+        inp = "--. .... .--- -.- .-.. -.-- -..-"
+        expect_out = "GHJKLYX"
         self.assertEqual(expect_out, functions.dekodovani(inp))
