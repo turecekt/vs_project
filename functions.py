ca75375898b20dd0fdf1b030f393672c8d236b7c
l-nagy-utb-fai <118170100+l-nagy-utb-fai@users.noreply.github.com>
2023-01-26 16:30:32 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-'''Funkce pro hlavní část kódu'''
+'''Hlavní modul funkcí pro hlavní část kódu'''
 PREVOD = {
         'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
         'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
@@ -12,19 +12,19 @@ PREVOD = {
 
 
 def kodovani(p1):
-'''Funkce kodovani'''
+    '''Funkce kodovani'''
     cipher = ""
     '''Příjem programu'''
     p1 = p1.upper()
     '''Zadefinování'''
     for letter in p1:
-    '''Cyklus'''
+        '''Cyklus'''
         if letter != ' ':
-        '''Pokud se nejedná o mezeru'''
+            '''Pokud se nejedná o mezeru'''
             cipher += PREVOD[letter] + ' '
             '''Převeď dle slovníku'''
         else:
-        '''Jinak'''
+            '''Jinak'''
             cipher += ' '
             '''Kóduj jako mezeru'''
     return cipher.rstrip()
@@ -32,7 +32,7 @@ def kodovani(p1):
 
 
 def dekodovani(p2):
-'''Funkce dekodovani'''
+    '''Funkce dekodovani'''
     p2 = p2.upper() + ' '
     '''Zadefinování'''
     decipher = ''
@@ -40,23 +40,23 @@ def dekodovani(p2):
     citext = ''
     '''Program vytvoří překlad dle zprávy uživatele'''
     for letter in p2:
-    '''Cyklus pro překlad písmena'''
+        '''Cyklus pro překlad písmena'''
         if (letter != ' '):
-        '''Pokud se nejedná o mezeru'''
+            '''Pokud se nejedná o mezeru'''
             i = 0
             '''Začátek cyklu'''
             citext += letter
             '''Přiřadíme písmeno'''
         else:
-        '''Pokud se jedná o mezeru'''
+            '''Pokud se jedná o mezeru'''
             i += 1
             '''Cyklus pokračuje'''
             if i == 2:
-            '''Pokud cyklus nic nenašel'''
+                '''Pokud cyklus nic nenašel'''
                 decipher += ' '
                 '''Program přidá mezeru'''
             else:
-            '''Pokud cyklus něco našel'''
+                '''Pokud cyklus něco našel'''
                 decipher += list(PREVOD.keys())[
                                     list(PREVOD.values()).index(citext)]
                 '''Převádíme písmeno do morseovy abecedy'''
