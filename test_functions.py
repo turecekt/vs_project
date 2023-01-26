56153a643fe8d7d6ed8902329fa42c8490c58e31
l-nagy-utb-fai <118170100+l-nagy-utb-fai@users.noreply.github.com>
2023-01-26 16:32:58 +0100

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,40 +1,64 @@
+'''Hlavní modul - Unit testy pro program'''
 from unittest import TestCase
+'''Vkládáme TestCase'''
 import functions
+'''Vkládáme funkce z jiného kódu'''
 
 
 class Test_Encode(TestCase):
-    def test_encryptShortWord(self):
+    '''Vytvoření třídy pro kódování'''
+    def test_encryptQWERTZU(self):
+        '''Kódování písmen QWERTZU'''
         self.assertEqual('--.- .-- . .-. - --.. ..-',
                          functions.kodovani('QWERTZU'))
+        '''Výsledek a příklad'''
 
-    def test_encryptLongWord(self):
+    def test_encryptIOPASDF(self):
+        '''Kódování písmen IOPASDF'''
         self.assertEqual('.. --- .--. .- ... -.. ..-.',
                          functions.kodovani('IOPASDF'))
+        '''Výsledek a příklad'''
 
-    def test_encryptLongWord(self):
+    def test_encryptCVBNM(self):
+        '''Kódování písemn CVBNM a mezery s tečkou'''
         self.assertEqual('-.-. ...- -... -. --  .-.-.-',
                          functions.kodovani('CVBNM .'))
 
-    def test_encryptSentence(self):
+    def test_encryptGHJKLYX(self):
+        '''Kódování písmen GHJKLYX'''
         inp = "GHJKLYX"
+        '''Příklad'''
         expect_out = "--. .... .--- -.- .-.. -.-- -..-"
+        '''Výsledek'''
         self.assertEqual(expect_out, functions.kodovani(inp))
+        '''Srovnání'''
 
 
 class Test_Decipher(TestCase):
-    def test_decryptShortWord(self):
+    '''Vytvoření třídy pro dekódování'''
+    def test_decryptQWERTZU(self):
+        '''Dekódování písmen QWERTZU'''
         self.assertEqual('QWERTZU',
                          functions.dekodovani('--.- .-- . .-. - --.. ..-'))
+        '''Výsledek a příklad'''
 
-    def test_decryptLongWord(self):
+    def test_decryptIOPASDF(self):
+        '''Dekódování písmen IOPASDF'''
         self.assertEqual('IOPASDF',
                          functions.dekodovani(".. --- .--. .- ... -.. ..-."))
+        '''Výsledek a příklad'''
 
-    def test_decryptShortWord(self):
+    def test_decryptCVBNM(self):
+        '''Dekódování písmen CVBNM a mezery s tečkou'''
         self.assertEqual('CVBNM .',
                          functions.dekodovani('-.-. ...- -... -. --  .-.-.-'))
+        '''Výsledek a příklad'''
 
-    def test_decryptSentence(self):
+    def test_decryptGHJKLYX(self):
+        '''Dekódování písemn GHJKLYX'''
         inp = "--. .... .--- -.- .-.. -.-- -..-"
+        '''Příklad'''
         expect_out = "GHJKLYX"
+        '''Výsledek'''
         self.assertEqual(expect_out, functions.dekodovani(inp))
+        '''Srovnání'''
