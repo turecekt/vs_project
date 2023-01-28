8870684ba3b4ad259c7635e18e438413a7bc8daf
Peter Kučera <10peterk10@gmail.com>
2023-01-28 19:20:33 +0100

    Zmena testov

--- test_prvocislo.py
+++ test_prvocislo.py
@@ -3,21 +3,21 @@
 from main import prvocislo
 
 
-def test_prvocislo(self):
-    self.assertFalse(prvocislo(1))
-    self.assertTrue(prvocislo(2))
-    self.assertTrue(prvocislo(3))
-    self.assertFalse(prvocislo(4))
-    self.assertTrue(prvocislo(5))
-    self.assertFalse(prvocislo(6))
-    self.assertTrue(prvocislo(149))
-    self.assertTrue(prvocislo(151))
-    self.assertTrue(prvocislo(317))
-    self.assertTrue(prvocislo(577))
-    self.assertTrue(prvocislo(977))
-    self.assertTrue(prvocislo(983))
-    self.assertTrue(prvocislo(991))
-    self.assertTrue(prvocislo(919))
-    self.assertTrue(prvocislo(929))
-    self.assertTrue(prvocislo(937))
-    self.assertTrue(prvocislo(983))
+def test_prvocislo():
+    assert prvocislo(1) is False
+    assert prvocislo(2) is True
+    assert prvocislo(3) is True
+    assert prvocislo(4) is False
+    assert prvocislo(5) is True
+    assert prvocislo(6) is False
+    assert prvocislo(149) is True
+    assert prvocislo(151) is True
+    assert prvocislo(317) is True
+    assert prvocislo(577) is True
+    assert prvocislo(977) is True
+    assert prvocislo(983) is True
+    assert prvocislo(991) is True
+    assert prvocislo(919) is True
+    assert prvocislo(929) is True
+    assert prvocislo(937) is True
+    assert prvocislo(983) is True
--- test_prvocislo_mr.py
+++ test_prvocislo_mr.py
@@ -4,16 +4,16 @@ from main import prvocislo_mr
 
 
 def test_prvocislo_mr(self):
-    self.assertFalse(prvocislo_mr(1001))
-    self.assertFalse(prvocislo_mr(1002))
-    self.assertFalse(prvocislo_mr(1003))
-    self.assertFalse(prvocislo_mr(1004))
-    self.assertFalse(prvocislo_mr(1005))
-    self.assertFalse(prvocislo_mr(1006))
-    self.assertFalse(prvocislo_mr(1007))
-    self.assertFalse(prvocislo_mr(1008))
-    self.assertTrue(prvocislo_mr(1009))
-    self.assertTrue(prvocislo_mr(6803))
-    self.assertTrue(prvocislo_mr(7057))
-    self.assertTrue(prvocislo_mr(7243))
-    self.assertTrue(prvocislo_mr(7457))
+    assert prvocislo_mr(1001) is False
+    assert prvocislo_mr(1002) is False
+    assert prvocislo_mr(1003) is False
+    assert prvocislo_mr(1004) is False
+    assert prvocislo_mr(1005) is False
+    assert prvocislo_mr(1006) is False
+    assert prvocislo_mr(1007) is False
+    assert prvocislo_mr(1008) is False
+    assert prvocislo_mr(1009) is True
+    assert prvocislo_mr(6803) is True
+    assert prvocislo_mr(7057) is True
+    assert prvocislo_mr(7243) is True
+    assert prvocislo_mr(7457) is True
